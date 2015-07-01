46f674c4713bee1c3680f038c4eed56d14ddb2f8
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-07-01 12:31:26 +0200

    Fix issue due to the fact geant3 libraries are in lib64.

--- geant3.sh
+++ geant3.sh
@@ -4,6 +4,9 @@ requires:
   - root
 source: http://root.cern.ch/git/geant3.git
 tag: v2-0
+prepend_path:
+  - "LD_LIBRARY_PATH": "$GEANT3_ROOT/lib64"
+  - "DYLD_LIBRARY_PATH": "$GEANT3_ROOT/lib64"
 ---
 #!/bin/sh
 
