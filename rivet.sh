6b1c4fcf8b8e107461334071c9f84362501917b5
Dario Berzano <dario.berzano@cern.ch>
2015-07-08 11:02:08 +0200

    Rivet v2.2.1

---
+++ rivet.sh
@@ -0,0 +1,27 @@
+package: Rivet
+version: "2.2.1"
+requires:
+  - GSL
+  - YODA
+---
+#!/bin/bash -e
+Url="http://www.hepforge.org/archive/rivet/Rivet-${PKGVERSION}.tar.bz2"
+# TODO: we will build them soon instead of getting them from CVMFS
+Boost="/cvmfs/alice.cern.ch/x86_64-2.6-gnu-4.1.2/Packages/boost/v1_53_0"
+HepMC="/cvmfs/alice.cern.ch/x86_64-2.6-gnu-4.1.2/Packages/HepMC/v2.06.09"
+FastJet="/cvmfs/alice.cern.ch/x86_64-2.6-gnu-4.1.2/Packages/fastjet/v3.0.6_1.012"
+Cgal="/cvmfs/alice.cern.ch/x86_64-2.6-gnu-4.1.2/Packages/cgal/v4.4"
+curl -Lo rivet.tar.bz2 "$Url"
+tar xjf rivet.tar.bz2
+cd Rivet-$PKGVERSION
+export LDFLAGS="-L$Cgal/lib ${LDFLAGS}"
+./configure \
+  --prefix="$INSTALLROOT" \
+  --disable-doxygen \
+  --with-yoda="$YODA_ROOT" \
+  --with-gsl="$GSL_ROOT" \
+  --with-hepmc="$HepMC" \
+  --with-fastjet="$FastJet" \
+  --with-boost="$Boost"
+make -j$JOBS
+make install -j$JOBS
