21e2735996e40d65021d47f45b05b03e1d4ba6a9
Dario Berzano <dario.berzano@cern.ch>
2015-07-09 17:29:17 +0200

    Modulefile for Rivet

--- rivet.sh
+++ rivet.sh
@@ -28,3 +28,33 @@ export LD_LIBRARY_PATH="${Cgal}/lib:${Boost}/lib:${LD_LIBRARY_PATH}"
   --with-boost="$Boost"
 make -j$JOBS
 make install -j$JOBS
+
+# Modulefile
+ModuleDir="${INSTALLROOT}/etc/Modules/modulefiles/${PKGNAME}"
+mkdir -p "$ModuleDir"
+cat > "${ModuleDir}/${PKGVERSION}-${PKGREVISION}" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "Module for loading $PKGNAME $PKGVERSION-$PKGREVISION for the ALICE environment"
+}
+set version $PKGVERSION-$PKGREVISION
+module-whatis "Module for loading $PKGNAME $PKGVERSION-$PKGREVISION for the ALICE environment"
+# Dependencies
+module load BASE/1.0 YODA/$YODA_VERSION-$YODA_REVISION FastJet/$FASTJET_VERSION-$FASTJET_REVISION GSL/$GSL_VERSION-$GSL_REVISION HepMC/v2.06.09
+# Our environment
+if { [info exists ::env(OVERRIDE_BASE)] && \$::env(OVERRIDE_BASE) == 1 } then {
+  puts stderr "Note: overriding base package $PKGNAME \$version"
+  set prefix \$ModulesCurrentModulefile
+  for {set i 0} {\$i < 5} {incr i} {
+    set prefix [file dirname \$prefix]
+  }
+  setenv YODA_BASEDIR \$prefix
+} else {
+  setenv YODA_BASEDIR \$::env(BASEDIR)/$PKGNAME/\$version
+}
+prepend-path LD_LIBRARY_PATH \$::env(YODA_BASEDIR)/lib
+prepend-path PATH \$::env(YODA_BASEDIR)/bin
+set pythonpath [exec rivet-config --pythonpath]
+prepend-path PYTHONPATH \$pythonpath
+EoF
