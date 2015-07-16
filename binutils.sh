5f8df98b2fe13de425a736aff6bc9e7b4cee49b3
Dario Berzano <dario.berzano@cern.ch>
2015-07-16 18:11:33 +0200

    Start Binutils from a clean slate

--- binutils.sh
+++ binutils.sh
@@ -5,6 +5,12 @@ tag: binutils-2_25
 ---
 #!/bin/bash -e
 
+cd "$SOURCEDIR"
+git reset --hard HEAD
+git clean -f -d
+git clean -fX
+
+cd "$BUILDDIR"
 "$SOURCEDIR"/configure --prefix="$INSTALLROOT"
 
 make ${JOBS+-j $JOBS}
