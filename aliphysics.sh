9da01b77d35e12bafe9f9f869fc80d8b3302f17a
Dario Berzano <dario.berzano@cern.ch>
2015-07-28 17:42:59 +0200

    AliPhysics: added recipe

---
+++ aliphysics.sh
@@ -0,0 +1,16 @@
+package: AliPhysics
+version: v5-06-35-01
+requires:
+  - AliRoot
+source: http://git.cern.ch/pub/AliPhysics
+tag: master
+---
+#!/bin/bash -e
+# TODO: build with -DFASTJET
+cmake "$SOURCEDIR" \
+      -DCMAKE_INSTALL_PREFIX="$INSTALLROOT" \
+      -DALIEN="$ALIEN_ROOT"/alien \
+      -DROOTSYS="$ROOT_ROOT" \
+      -DALIROOT="$ALIROOT_ROOT"
+make ${JOBS+-j $JOBS}
+make install
