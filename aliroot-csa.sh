4f16872776e245d4104c2be863c62b4829c7cf54
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-08-26 15:06:33 +0200

    Support for Clang Static Analyser, including Danilo's SAS.

---
+++ aliroot-csa.sh
@@ -0,0 +1,32 @@
+package: AliRoot-csa
+version: "%(short_hash)s"
+requires:
+  - ROOT
+  - SAS
+env:
+  ALICE_ROOT: "$ALIROOT_ROOT"
+source: http://git.cern.ch/pub/AliRoot
+write_repo: https://git.cern.ch/reps/AliRoot 
+tag: master
+---
+#!/bin/sh
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
+      -DCMAKE_C_COMPILER=`which ccc-analyzer` \
+      -DCMAKE_CXX_COMPILER=`which c++-analyzer`\
+      -DCMAKE_Fortran_COMPILER=`which gfortran` \
+      -DROOTSYS=$ROOT_ROOT \
+      -DALIEN=$ALIEN_ROOT/alien \
+      -DOCDB_INSTALL=PLACEHOLDER
+
+case $ARCHITECTURE in 
+  osx*) SONAME=dylib ;;
+esac
+
+scan-build -load-plugin $SAS_ROOT/lib/libSas.${SONAME:-so} \
+           -enable-checker sas.Performance \
+           -enable-checker sas.CodingConventions.General \
+           -enable-checker core \
+           -enable-checker cplusplus \
+           -enable-checker unix \
+           -o MyReportDir \
+           make -k ${JOBS+-j $JOBS} || true
--- clang.sh
+++ clang.sh
@@ -2,11 +2,18 @@ package: Clang
 version: "3.6"
 source: https://github.com/alisw/clang
 tag: master
-incremental_recipe: make ${JOBS+-j $JOBS} && make install
 ---
 #!/bin/sh
 cmake $SOURCEDIR \
+  -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX:PATH="$INSTALLROOT"
   
 make ${JOBS+-j $JOBS}
 make install
+case $ARCHITECTURE in
+  osx*)
+    ln -sf /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++ $INSTALLROOT/include
+  ;;
+esac
+find $SOURCEDIR/tools/clang/tools/scan-build -type f -perm +111 -exec cp {} $INSTALLROOT/bin \;
+find $SOURCEDIR/tools/clang/tools/scan-view -type f -perm +111 -exec cp {} $INSTALLROOT/bin \;
