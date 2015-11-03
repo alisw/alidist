0178d8384daa03e5abdc413e2536e904df133bb3
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-11-03 13:23:20 +0100

    Cleanup fairroot.sh

--- fairroot.sh
+++ fairroot.sh
@@ -1,6 +1,6 @@
 package: FairRoot
 version: master
-source: https://github.com/ktf/FairRoot
+source: https://github.com/FairRootGroup/FairRoot
 tag: master
 requires:
   - pythia6
@@ -28,5 +28,5 @@ cmake $SOURCEDIR \
       -DBOOST_LIBRARYDIR=$BOOST_ROOT/lib \
       -DBoost_NO_SYSTEM_PATHS=ON \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
-make ${JOBS+-j $JOBS}
+make ${JOBS:+-j $JOBS}
 make install
