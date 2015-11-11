65fc3e7dec5c88ddcec981e51814946db1ff4f23
Dario Berzano <dario.berzano@cern.ch>
2015-11-11 18:08:18 +0100

    Working GCC v4.9.3 and Binutils v2.25
    Built under the same root into the GCC-Toolchain package.
    
    Using system linker to link our GCC. Using our just-built linker to link our
    GCC causes problems on systems like Ubuntu where we build without
    --with-sysroot, but system's compiler was build with it.

---
+++ defaults-testgcc.sh
@@ -0,0 +1,12 @@
+package: defaults-testgcc
+version: v1
+env:
+  CXXFLAGS: "-fPIC -g -O2"
+  CFLAGS: "-fPIC -g -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+---
+# This file is included in any build recipe and it's only used to set
+# environment variables. Which file to actually include can be defined by the
+# "--defaults" option of alibuild.
+#
+# This is a copy of defaults-release to append "GCC" to software versions.
---
+++ gcc-toolchain.sh
@@ -0,0 +1,127 @@
+package: GCC-Toolchain
+version: "%(tag_basename)s"
+source: https://github.com/alisw/gcc-toolchain
+tag: alice/v4.9.3
+prepend_path:
+  "LD_LIBRARY_PATH": "$GCC_TOOLCHAIN_ROOT/lib64"
+  "DYLD_LIBRARY_PATH": "$GCC_TOOLCHAIN_ROOT/lib64"
+build_requires:
+ - autotools
+---
+#!/bin/bash -e
+
+USE_GOLD=
+case $ARCHITECTURE in
+  osx*)
+    EXTRA_LANGS=',objc,obj-c++'
+  ;;
+esac
+
+rsync -a --exclude='**/.git' --delete --delete-excluded "$SOURCEDIR/" ./
+
+# Binutils
+mkdir build-binutils
+pushd build-binutils
+  ../binutils/configure --prefix="$INSTALLROOT" \
+                        ${USE_GOLD:+--enable-gold=yes} \
+                        --enable-ld=default \
+                        --enable-lto \
+                        --enable-plugins \
+                        --enable-threads \
+                        --disable-nls
+  make ${JOBS:+-j$JOBS}
+  make install
+  hash -r
+popd
+
+# Test program
+cat > test.c <<EOF
+#include <string.h>
+#include <stdio.h>
+int main(void) { printf("The answer is 42.\n"); }
+EOF
+
+# GCC and deps
+pushd gcc
+  for EXT in mpfr gmp mpc isl cloog; do
+    pushd $EXT
+      autoreconf -ivf
+    popd
+  done
+popd
+
+mkdir build-gcc
+pushd build-gcc
+  ../gcc/configure --prefix="$INSTALLROOT" \
+               --enable-languages="c,c++,fortran${EXTRA_LANGS}" \
+               --disable-multilib \
+               ${USE_GOLD:+--enable-gold=yes} \
+               --enable-ld=default \
+               --enable-lto \
+               --disable-nls
+  make ${JOBS+-j $JOBS} bootstrap-lean
+  make install
+  hash -r
+
+  # GCC creates c++, but not cc
+  ln -nfs gcc "$INSTALLROOT/bin/cc"
+  rm -rf "$INSTALLROOT/lib/pkg-config"
+
+  # Provide a custom specs file if needed
+  #SPEC="$(dirname $(gcc -print-libgcc-file-name))/specs"
+  #gcc -dumpspecs > $SPEC
+  #perl -pe '++$x and next if /^\*link:/; $x-- and s/^(.*)$/\1 -rpath-link \/lib64:\/lib/ if $x' $SPEC > $SPEC.0
+  #mv $SPEC.0 $SPEC
+
+  rm -f $INSTALLROOT/lib/*.la \
+        $INSTALLROOT/lib64/*.la
+popd
+
+# From now on, use own linker and GCC
+export PATH="$INSTALLROOT/bin:$PATH"
+export LD_LIBRARY_PATH="$INSTALLROOT/lib64:$INSTALLROOT/lib:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$INSTALLROOT/lib64:$INSTALLROOT/lib:$DYLD_LIBRARY_PATH"
+hash -r
+
+# Test own linker and own GCC
+which ld
+which g++
+g++ test.c
+./a.out
+rm -f a.out
+
+# GDB
+mkdir build-gdb
+pushd build-gdb
+  ../gdb/configure --prefix="$INSTALLROOT" \
+                   --disable-multilib
+  make ${JOBS:+-j$JOBS}
+  make install
+  hash -r
+  rm -f $INSTALLROOT/lib/*.la
+popd
+
+# If fixincludes is not desired, see:
+# http://ewontfix.com/12/
+# https://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/eclass/toolchain.eclass?view=markup&sortby=log#l1524
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+setenv GCC_TOOLCHAIN_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path LD_LIBRARY_PATH \$::env(GCC_TOOLCHAIN_ROOT)/lib
+prepend-path LD_LIBRARY_PATH \$::env(GCC_TOOLCHAIN_ROOT)/lib64
+prepend-path PATH \$::env(GCC_TOOLCHAIN_ROOT)/bin
+EoF
