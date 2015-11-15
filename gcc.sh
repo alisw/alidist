58b2d4f01edc34ec3c5783a8d25f1f4011bcb716
Dario Berzano <dario.berzano@cern.ch>
2015-11-15 13:03:20 +0100

    Work around GCC's fixincludes issue
    Without this we cannot have a working GCC if we build on one platform and run it
    on another (e.g. SLC5 -> SLC6).

--- gcc.sh
+++ gcc.sh
@@ -25,12 +25,29 @@ done
 ./configure --prefix="$INSTALLROOT" \
             --enable-languages="c,c++,fortran${EXTRA_LANGS}" \
             --disable-multilib
+# bootstrap-lean saves some space when compiling
 make ${JOBS+-j $JOBS} bootstrap-lean
 make install
 
 # GCC creates c++, but not cc
 ln -nfs gcc "$INSTALLROOT/bin/cc"
 rm -rf "$INSTALLROOT/lib/pkg-config"
+
+# GCC needs to "fix" some system header files according to its taste. "Fixed"
+# headers are needed for GCC compilation and will override system's ones on the
+# destination platform. This will cause a GCC built with, e.g., SLC5 not to be
+# able to work when running from, e.g., SLC6. To avoid this we remove the
+# "fixed" headers assuming that the ones on the system are OK. This is done by
+# nearly all major Linux distributions. Here we are using Gentoo's technique.
+#
+# See:
+# http://ewontfix.com/12/
+# https://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/eclass/toolchain.eclass?view=markup&sortby=log#l1524
+INCLUDE_FIXED=$(find "$INSTALLROOT" -type d -name include-fixed | head -n1)
+[[ "$INCLUDE_FIXED" != '' ]]
+while read INC; do
+  grep -q 'It has been auto-edited by fixincludes from' $INC && rm -f $INC
+done < <(find "$INCLUDE_FIXED" -name '*.h')
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
