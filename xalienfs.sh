e99e5638d96d9709b412596b5722a5d3c0d2b6bb
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-11-17 17:48:52 +0100

    Make sure Alien-Runtime works on mac
    - Bring in our own SWIG and UUID, so that one does not require
      having brew

--- xalienfs.sh
+++ xalienfs.sh
@@ -5,20 +5,14 @@ source: https://github.com/alisw/xalienfs.git
 build_requires:
  - autotools
  - XRootD
+ - SWIG
+ - UUID
 ---
 #!/bin/bash -e
 rsync -a --delete --exclude='**/.git' --delete-excluded \
       $SOURCEDIR/ ./
 ./bootstrap.sh
 autoreconf -ivf
-if [[ -d /usr/share/swig ]]; then
-  SWIG_INC=$(ls -1rd /usr/share/swig/* 2> /dev/null)
-elif [[ -d /usr/local/share/swig ]]; then
-  SWIG_INC=$(ls -1rd /usr/local/share/swig/* 2> /dev/null)
-else
-  SWIG_INC=/usr/share/swig2.0
-fi
-[ -d "$SWIG_INC" ]
 export CFLAGS="-I$XROOTD_ROOT/include -I$XROOTD_ROOT/include/xrootd/private"
 case $ARCHITECTURE in
   osx*)
@@ -27,13 +21,13 @@ case $ARCHITECTURE in
 esac
 export CPPFLAGS="$CFLAGS"
 export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-L$XROOTD_ROOT/lib"
-./configure --prefix=$INSTALLROOT \
-            --with-xrootd-location=$XROOTD_ROOT \
-            --enable-perl-module \
-            --with-perl=perl \
-            --with-swig-inc="$SWIG_INC" \
-            --enable-build-server \
+export LDFLAGS="-L$XROOTD_ROOT/lib -L$UUID_ROOT/lib"
+./configure --prefix=$INSTALLROOT                    \
+            --with-xrootd-location=$XROOTD_ROOT      \
+            --enable-perl-module                     \
+            --with-perl=perl                         \
+            ${SWIG_INC:+--with-swig-inc="$SWIG_INC"} \
+            --enable-build-server                    \
             --disable-readline
 # May not work in multicore
 make
