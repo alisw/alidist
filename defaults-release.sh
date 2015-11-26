eddcecae390f6a838b697078c3d30f86a1936974
Dario Berzano <dario.berzano@cern.ch>
2015-11-26 14:45:06 +0100

    Add defaults from upstream

---
+++ defaults-release.sh
@@ -0,0 +1,10 @@
+package: defaults-release
+version: v1
+env:
+  CXXFLAGS: "-fPIC -g -O2"
+  CFLAGS: "-fPIC -g -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBUG"
+---
+# This file is included in any build recipe and it's only used to set
+# environment variables. Which file to actually include can be defined by the
+# "--defaults" option of alibuild.
