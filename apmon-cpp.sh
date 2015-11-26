bf8e031c2c49d23e7744e8bf4ac1966211ea2692
Dario Berzano <dario.berzano@cern.ch>
2015-11-26 14:56:11 +0100

    Align AliEn-Runtime to upstream

---
+++ apmon-cpp.sh
@@ -0,0 +1,13 @@
+package: ApMon-CPP
+version: "%(tag_basename)s"
+tag: v2.2.8
+source: https://github.com/alisw/apmon-cpp.git
+build_requires:
+ - autotools
+---
+#!/bin/bash -e
+rsync -a --exclude='**/.git' --delete --delete-excluded \
+      $SOURCEDIR/ ./
+./configure --prefix=$INSTALLROOT
+make ${JOBS:+-j$JOBS}
+make install
---
+++ gsoap.sh
@@ -0,0 +1,21 @@
+package: gSOAP
+version: "%(tag_basename)s"
+tag: v2.7.13
+source: https://github.com/alisw/gsoap.git
+build_requires:
+ - autotools
+ - "OpenSSL:(?!osx)"
+---
+#!/bin/bash -e
+rsync -a --exclude='**/.git' --delete --delete-excluded \
+      $SOURCEDIR/ ./
+export CFLAGS="-fPIC -I$OPENSSL_ROOT/include -I$ZLIB_ROOT/include -L$OPENSSL_ROOT/lib -L$ZLIB_ROOT/lib"
+export CXXFLAGS="$CFLAGS"
+export CPPFLAGS="$CFLAGS"
+autoreconf -ivf
+./configure --prefix=$INSTALLROOT \
+            --enable-ssl \
+            ${OPENSSL_ROOT:+--with-openssl=$OPENSSL_ROOT}
+# Does not build in multicore!
+make
+make install
