fe1b7d467d0f9d617ab62d9329f28c56c69353e0
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-12-07 18:50:34 +0100

    Workaround to avoid compiling GCC itself with coverage support

--- defaults-coverage.sh
+++ defaults-coverage.sh
@@ -1,8 +1,8 @@
 package: defaults-coverage
 version: v1
 env:
-  CXXFLAGS: "-fPIC -g -O2 -fprofile-arcs -ftest-coverage"
-  CFLAGS: "-fPIC -g -O2 -fprofile-arcs -ftest-coverage"
+  CXXFLAGS: "-fPIC -g -O2"
+  CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: Coverage
 ---
 # This file is included in any build recipe and it's only used to set
