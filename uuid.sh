a1652a8d3501f890d3fe46d9cd2da1f37ac0216a
Dario Berzano <dario.berzano@cern.ch>
2016-01-08 10:34:49 +0100

    Fix UUID recipe

--- uuid.sh
+++ uuid.sh
@@ -1,17 +1,29 @@
 package: UUID
-version: v1.6.2
-source: https://github.com/alisw/UUID
-tag: v1.6.2
+version: v2.27.1
+source: https://github.com/alisw/uuid
+tag: alice/v2.27.1
 build_requires:
  - "GCC-Toolchain:(?!osx)"
+ - autotools
 ---
 #!/bin/sh
 rsync -av --delete --exclude "**/.git" $SOURCEDIR/ .
-./configure --prefix=$INSTALLROOT        \
-            --includedir=$SOURCEDIR/ossp \
-            --without-perl               \
-            --without-php                \
-            --without-pgsql
-
-make ${JOBS:+-j$JOBS}
-make ${JOBS:+-j$JOBS} install
+autoreconf -ivf
+./configure $([[ ${ARCHITECTURE:0:3} == osx ]] && echo --disable-shared) \
+            --libdir=$INSTALLROOT/lib \
+            --prefix=$INSTALLROOT \
+            --disable-all-programs \
+            --disable-silent-rules \
+            --disable-tls \
+            --disable-rpath \
+            --without-ncurses \
+            --enable-libuuid
+make ${JOBS:+-j$JOBS} libuuid.la
+mkdir -p $INSTALLROOT/lib
+cp -p .libs/libuuid.a* $INSTALLROOT/lib
+if [[ ${ARCHITECTURE:0:3} != osx ]]; then
+  cp -p .libs/libuuid.so* $INSTALLROOT/lib
+fi
+mkdir -p $INSTALLROOT/include
+make install-uuidincHEADERS
+rm -rf $INSTALLROOT/man
