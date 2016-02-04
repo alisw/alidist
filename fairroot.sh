7803593a98150e42ab829eb41f24a29c6b9f3380
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-02-04 12:02:11 +0100

    Pick up zeromq from the system, where possible

--- fairroot.sh
+++ fairroot.sh
@@ -19,6 +19,7 @@ case $ARCHITECTURE in
   osx*)
     # If we preferred system tools, we need to make sure we can pick them up.
     [[ ! $BOOST_ROOT ]] && BOOST_ROOT=`brew --prefix boost`
+    [[ ! $ZMQ_ROOT ]] && ZMQ_ROOT=`brew --prefix zeromq`
   ;;
 esac
 
@@ -31,7 +32,7 @@ cmake $SOURCEDIR                                             \
       -DGeant3_DIR=$GEANT3_ROOT                              \
       ${GEANT4_ROOT:+-DGeant4_DIR=$GEANT4_ROOT}              \
       -DFAIRROOT_MODULAR_BUILD=ON                            \
-      -DZMQ_DIR=$ZEROMQ_ROOT                                 \
+      ${ZEROMQ_ROOT:+-DZMQ_DIR=$ZEROMQ_ROOT}                 \
       ${BOOST_ROOT:+-DBOOST_ROOT=$BOOST_ROOT}                \
       ${BOOST_ROOT:+-DBOOST_INCLUDEDIR=$BOOST_ROOT/include}  \
       ${BOOST_ROOT:+-DBOOST_LIBRARYDIR=$BOOST_ROOT/lib}      \
