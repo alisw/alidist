feb0bc21ebc3e0486bc108681c472cb1672dcdfe
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-02-17 10:28:39 +0100

    Move to latest OCDB for tests

--- ocdb-test.sh
+++ ocdb-test.sh
@@ -6,7 +6,7 @@ env:
   ALICE_ROOT: "$ALIROOT_ROOT"
 source: http://git.cern.ch/pub/AliRoot
 write_repo: https://git.cern.ch/reps/AliRoot 
-tag: v5-07-10
+tag: v5-08-00
 ---
 #!/bin/sh
 rsync -av $SOURCEDIR/OCDB/ $INSTALLROOT/
