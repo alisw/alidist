e0ee1462723f13c487c68180e1d33fd922124d01
Dario Berzano <dario.berzano@cern.ch>
2016-03-02 17:25:53 +0100

    Fix OpenSSL recipe
    * Compile only on SLC5 and 6 (prefer system on the rest)
    * Use own clone for sources (external endpoints are unreliable)
    * On OSX, check if Homebrew OpenSSL exists and it is usable (brew --prefix might
      return a nonexisting path for uninstalled recipes!)
    
    Note: backported from IB/v5-08/prod.

--- openssl.sh
+++ openssl.sh
@@ -1,8 +1,10 @@
 package: OpenSSL
 version: v0.9.8zf
-prefer_system: osx
+tag: "v0.9.8_1.2.4"
+source: https://github.com/alisw/alice-openssl.git
+prefer_system: (?!slc5|slc6)
 prefer_system_check: |
-  echo '#include <openssl/bio.h>' | c++ -x c++ - -I`brew --prefix openssl`/include -c -o /dev/null
+  if [ `uname` == Darwin ]; then test -d `brew --prefix openssl || echo /dev/nope` || exit 1; echo '#include <openssl/bio.h>' | c++ -x c++ - -I`brew --prefix openssl`/include -c -o /dev/null || exit 1; else exit 0; fi
 build_requires:
  - zlib
  - "GCC-Toolchain:(?!osx|slc5)"
@@ -24,16 +26,9 @@ exit 1
   ;;
 esac
 
-# OpenSSL FIPS
-FIPS_SHA1SUM='a79825c0c6743830ead3b6c3fd4c4e7fb09448a5'
-curl -Lo openssl-fips.tgz \
-         http://www.openssl.org/source/old/fips/openssl-fips-1.2.4.tar.gz
-[ "$(sha1sum openssl-fips.tgz | awk '{print $1}')" == "$FIPS_SHA1SUM" ]
-rm -rf openssl-fips
-mkdir openssl-fips
+rsync -av --delete --exclude="**/.git" $SOURCEDIR/ .
+
 pushd openssl-fips
-  tar --strip-components=1 -xzf ../openssl-fips.tgz
-  rm -f ../openssl-fips.tgz
   ./config --openssldir=$INSTALLROOT/fips \
            fipscanisterbuild \
            no-asm
@@ -42,16 +37,7 @@ pushd openssl-fips
   make install
 popd
 
-# OpenSSL
-OPENSSL_SHA1SUM='3f2f4ca864b13a237ae063cd34d01bbdbc8f108f'
-HTTPCODE=$(curl -Lo openssl.tgz \
-           http://www.openssl.org/source/old/0.9.x/openssl-0.9.8zf.tar.gz)
-[ "$(sha1sum openssl.tgz | awk '{print $1}')" == "$OPENSSL_SHA1SUM" ]
-rm -rf openssl
-mkdir openssl
 pushd openssl
-  tar --strip-components=1 -xzf ../openssl.tgz
-  rm -f ../openssl.tgz
   ./config --openssldir="$INSTALLROOT" \
            --with-fipslibdir="$INSTALLROOT/fips/lib" \
            fips \
