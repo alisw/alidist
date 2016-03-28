806bbd186e2ebbd5ef3bac640577faf3f8703ca3
Dario Berzano <dario.berzano@cern.ch>
2016-03-28 04:07:40 +0200

    Compile ROOT with CMake

--- root.sh
+++ root.sh
@@ -7,23 +7,19 @@ requires:
   - GSL
 env:
   ROOTSYS: "$ROOT_ROOT"
-incremental_recipe: |
-  export ROOTSYS=$BUILDDIR
-  make ${JOBS:+-j$JOBS}
-  export ROOTSYS=$INSTALLROOT
-  make install
+incremental_recipe: make ${JOBS:+-j$JOBS} install
 ---
 #!/bin/bash -e
 
 COMPILER_CC=cc
 COMPILER_CXX=c++
 COMPILER_LD=c++
+echo $CXXFLAGS | grep -q -- '-std=c++11' && CXX11=ON || true
 
 case $ARCHITECTURE in 
   osx*)
-    ENABLE_COCOA=true
-    DISABLE_FINK=true
-    WITH_CLANG=true
+    ENABLE_COCOA=1
+    WITH_CLANG=1
     COMPILER_CC=clang
     COMPILER_CXX=clang++
     COMPILER_LD=clang
@@ -31,53 +27,38 @@ case $ARCHITECTURE in
   ;;
 esac
 
-export ROOTSYS=$BUILDDIR
-case $ARCHITECTURE in
-  *ppc64)
-    cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
-  ;;
-  *)
-"$SOURCEDIR/configure"                         \
-  --with-pythia6-uscore=SINGLE                 \
-  --with-alien-incdir=$GSHELL_ROOT/include     \
-  --with-alien-libdir=$GSHELL_ROOT/lib         \
-  --with-monalisa-incdir=$GSHELL_ROOT/include  \
-  --with-monalisa-libdir=$GSHELL_ROOT/lib      \
-  --with-xrootd=$GSHELL_ROOT                   \
-  --enable-http                                \
-  --enable-minuit2                             \
-  --enable-roofit                              \
-  --enable-soversion                           \
-  --enable-builtin-freetype                    \
-  --enable-builtin-pcre                        \
-  --enable-mathmore                            \
-  ${ENABLE_COCOA+--enable-cocoa}               \
-  --disable-bonjour                            \
-  ${DISABLE_FINK+--disable-fink}               \
-  --with-f77=gfortran                          \
-  --with-cc=$COMPILER_CC                       \
-  --with-cxx=$COMPILER_CXX                     \
-  --with-ld=$COMPILER_LD                       \
-  ${CXXFLAGS:+--cxxflags="$CXXFLAGS"}          \
-  ${WITH_CLANG+--with-clang}                   \
-  --disable-shadowpw                           \
-  --disable-astiff                             \
-  ${LIBXML2_ROOT:+--with-xml-incdir=$ALIEN_RUNTIME_ROOT/include/libxml2 --with-xml-libdir=$ALIEN_RUNTIME_ROOT/lib}  \
-  --disable-globus                               \
-  --with-ssl-libdir=$ALIEN_RUNTIME_ROOT/lib      \
-  --with-ssl-incdir=$ALIEN_RUNTIME_ROOT/include  \
-  --with-ssl-shared=yes                          \
-  --enable-mysql
-  ;;
-esac
+cmake $SOURCEDIR                                          \
+      -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE                \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                 \
+      -DALIEN_DIR=$ALIEN_RUNTIME_ROOT                     \
+      -DMONALISA_DIR=$ALIEN_RUNTIME_ROOT                  \
+      -DXROOTD_ROOT_DIR=$ALIEN_RUNTIME_ROOT               \
+      -Dhttp=ON                                           \
+      ${CXX11:+-Dcxx11=ON}                                \
+      -Dbuiltin_freetype=ON                               \
+      -Dbuiltin_pcre=ON                                   \
+      ${ENABLE_COCOA:+-Dcocoa=ON}                         \
+      -DCMAKE_CXX_COMPILER=$COMPILER_CXX                  \
+      -DCMAKE_C_COMPILER=$COMPILER_CC                     \
+      -DCMAKE_LINKER=$COMPILER_LD                         \
+      ${OPENSSL_ROOT:+-DOPENSSL_ROOT=$ALIEN_RUNTIME_ROOT} \
+      ${LIBXML2_ROOT:+-DLIBXML2_ROOT=$ALIEN_RUNTIME_ROOT} \
+      ${GSL_ROOT:+-DGSL_DIR=$GSL_ROOT}                    \
+      -Dminuit2=ON                                        \
+      -Dpythia6_nolink=ON                                 \
+      -Droofit=ON                                         \
+      -Dsoversion=ON                                      \
+      -Dvdt=ON
 
-[[ "$ALIEN_RUNTIME_ROOT" == '' ]] || ./bin/root-config --has-alien | grep -q yes
-./bin/root-config --has-opengl | grep -q yes
-./bin/root-config --has-xml | grep -q yes
+# Check if essential features are enabled
+bin/root-config --features
+for FEATURE in alien monalisa xrootd http builtin_freetype          \
+               builtin_pcre mathmore xml ssl opengl ${CXX11:+cxx11} \
+               minuit2 pythia6 roofit soversion vdt; do
+  bin/root-config --has-$FEATURE | grep -q yes
+done
 
-make ${JOBS+-j$JOBS}
-export ROOTSYS=$INSTALLROOT
-make install
+make ${JOBS+-j$JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
