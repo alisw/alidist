493d4b49ee8d788f3a8d05b54554e0adade331ce
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-03-31 09:55:17 +0200

    Add template recipe for people who want to contribute

---
+++ template-recipe.sh
@@ -0,0 +1,35 @@
+package: MyGenerator
+version: "v1.0.0"
+source: https://github.com/alisw/MyGenerator
+requires:
+ - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - CMake
+---
+#!/bin/bash -e
+
+cmake  $SOURCEDIR                           \
+       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
+
+make ${JOBS+-j $JOBS}
+make install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+setenv MYGENERATOR_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(MYGENERATOR_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(MYGENERATOR_ROOT)/lib
+EoF
