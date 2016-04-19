f57d99a8a1860423e71b2d38bc14687bdcf5047e
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-19 14:29:11 +0200

    Add checks for system requirements (#298)
    In particular:
    
    - bz2
    - bison & flex
    - SIP being disabled

---
+++ yacc-like.sh
@@ -0,0 +1,9 @@
+package: yacc-like
+version: 1.0
+system_requirement_missing: "Please install bison and flex on your system"
+system_requirement: ".*"
+system_requirement_check: |
+  which bison && which flex
+---
+
+
