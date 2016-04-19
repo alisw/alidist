c63b1856959a91bdf48295c534ec183ffc0a141d
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-19 17:39:13 +0200

    Make texinfo a system_requirement for GCC

---
+++ texinfo.sh
@@ -0,0 +1,8 @@
+package: texinfo
+version: 1.0
+system_requirement_missing: "Please install texinfo / makeinfo package on your system"
+system_requirement: ".*"
+system_requirement_check: |
+  which makeinfo
+---
+
