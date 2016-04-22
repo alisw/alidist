14edf2af5ea6060792ef583adabd670d88761987
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-22 15:14:02 +0200

    Add modulefiles for AliceO2 packages
    This adds modulefiles for the following packages:
    
    - AliceO2
    - DDS
    - FairRoot
    - sodium
    - ZeroMQ
    
    so that alienv works with them.

--- zeromq.sh
+++ zeromq.sh
@@ -19,3 +19,22 @@ $SOURCEDIR/configure --prefix=$INSTALLROOT \
 
 make ${JOBS+-j $JOBS}
 make install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ${SODIUM_ROOT:+sodium/$SODIUM_VERSION-$SODIUM_REVISION} ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+# Our environment
+setenv ZEROMQ_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path LD_LIBRARY_PATH \$::env(ZEROMQ_ROOT)/lib
+EoF
