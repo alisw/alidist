976846e5e69a78c2643718bd26be829dd3696b49
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-29 10:53:13 +0200

    Improve system requirements checks (#349)
    - sip-check now works on older OSX without csrutil
    - swig needs bison
    - xalienfs needs libperl

--- sip-check.sh
+++ sip-check.sh
@@ -1,15 +1,15 @@
 package: sip-check
 version: 1.0
 system_requirement_missing: |
-  Please make sure you have SIP disabled. 
+  Please make sure you have SIP disabled.
 
-  See: 
+  See:
 
       http://alisw.github.io/alibuild/troubleshooting.html#fastjet-fails-to-compile-on-my-brand-new-mac
-  
+
   for more information.
 system_requirement: "(osx.*)"
 system_requirement_check: |
-  csrutil status | grep disabled
+  (which csrutil && (csrutil status | grep disabled) ) || ! which csrutil
 ---
 
