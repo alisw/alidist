alisw/alidist

commit 1afe344a9bf289fcff8adaf434d135a41a5dcdf2

Allow to compile AliROOT with system ROOT (#354)
Only works if you specify --disable ROOT

aliroot.sh
@@ -20,6 +20,7 @@ incremental_recipe: |
 #!/bin/bash -e
 cmake $SOURCEDIR                                                  \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                         \
+      ${ROOT_ROOT:--DROOTSYS=$(root-config --prefix)}             \
       ${ROOT_ROOT:+-DROOTSYS=$ROOT_ROOT}                          \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"} \
       ${ALIEN_RUNTIME_ROOT:+-DALIEN=$ALIEN_RUNTIME_ROOT}          \

commit bbe6afe580b4baca091f9514673b58e940022cf4

Fix issue introduced by #354

aliphysics.sh
@@ -12,9 +12,14 @@ incremental_recipe: |
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
 #!/bin/bash -e
+# Picking up ROOT from the system when our is disabled
+if [ "X$ROOT_ROOT" = X ]; then
+  ROOT_ROOT="$(root-config --prefix)"
+fi
+
 cmake "$SOURCEDIR"                                                 \
       -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"                        \
-      ${ROOT_ROOT:+-DROOTSYS="$ROOT_ROOT"}                         \
+      -DROOTSYS="$ROOT_ROOT"                                       \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"}  \
       ${ALIEN_RUNTIME_ROOT:+-DALIEN="$ALIEN_RUNTIME_ROOT"}         \
       ${FASTJET_ROOT:+-DFASTJET="$FASTJET_ROOT"}                   \

aliroot.sh
@@ -18,13 +18,18 @@ incremental_recipe: |
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
 #!/bin/bash -e
-cmake $SOURCEDIR                                                  \
-      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                         \
-      ${ROOT_ROOT:--DROOTSYS=$(root-config --prefix)}             \
-      ${ROOT_ROOT:+-DROOTSYS=$ROOT_ROOT}                          \
-      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"} \
-      ${ALIEN_RUNTIME_ROOT:+-DALIEN=$ALIEN_RUNTIME_ROOT}          \
-      ${FASTJET_ROOT:+-DFASTJET=$FASTJET_ROOT}                    \
+
+# Picking up ROOT from the system when our is disabled
+if [ "X$ROOT_ROOT" = X ]; then
+  ROOT_ROOT="$(root-config --prefix)"
+fi
+
+cmake $SOURCEDIR                                                   \
+      -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"                        \
+      -DROOTSYS="$ROOT_ROOT"                                       \
+      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"}  \
+      ${ALIEN_RUNTIME_ROOT:+-DALIEN=$ALIEN_RUNTIME_ROOT}           \
+      ${FASTJET_ROOT:+-DFASTJET=$FASTJET_ROOT}                     \
       -DOCDB_INSTALL=PLACEHOLDER
 
 if [[ $GIT_TAG == master ]]; then