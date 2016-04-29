f2b83fdff311fde06f2aba3d2ed0583ce21d5537
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-29 16:37:05 +0200

    Add driver script for GEANT3 / GEANT4 tests (#360)

---
+++ geant-vmc-test.sh
@@ -0,0 +1,27 @@
+package: GEANT-VMC-test
+version: "%(year)s%(month)s%(day)s%(defaults_upper)s"
+force_rebuild: 1
+requires:
+  - GEANT4_VMC
+  - geant3_vmc-examples
+---
+#!/bin/sh
+
+g3vmc_testE01
+g3vmc_testE02
+g3vmc_testE03
+g3vmc_testE06
+g3vmc_exampleE01
+g3vmc_exampleE02
+g3vmc_exampleE03
+g3vmc_exampleE06
+
+#g4root_OpNovice
+#g4vmc_exampleE01
+#g4vmc_exampleE02
+#g4vmc_exampleE03
+#g4vmc_exampleE06
+#g4vmc_testE01
+#g4vmc_testE02
+#g4vmc_testE03
+#g4vmc_testE06
