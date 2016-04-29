da66c344d0fe516aceb7e04efe70446c6c54f7db
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-29 16:34:54 +0200

    Require perl-ext-utils installation

--- libperl.sh
+++ libperl.sh
@@ -1,8 +1,8 @@
 package: libperl
 version: 1.0
-system_requirement_missing: "Please install libperl development package on your system"
+system_requirement_missing: "Please install libperl and perl-ExtUtils-Embed development package on your system"
 system_requirement: ".*"
 system_requirement_check: |
-  printf "#include <EXTERN.h>\n#include <perl.h>\n" | gcc -xc++ -I`perl -MConfig -e 'print $Config{archlib}'`/CORE - -c -o /dev/null
+  printf "#include <EXTERN.h>\n#include <perl.h>\n" | gcc -xc++ -I`perl -MConfig -e 'print $Config{archlib}'`/CORE - -c -o /dev/null && perl -MExtUtils::Embed -e 1
 ---
 
