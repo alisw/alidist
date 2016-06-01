9ea247c2397ad5bf390d1a14b026300aea0ff13e
Dario Berzano <dario.berzano@gmail.com>
2016-06-01 16:24:59 +0200

    Add recipe for RooUnfold (#415)

---
+++ roounfold.sh
@@ -0,0 +1,33 @@
+package: RooUnfold
+version: "%(tag_basename)s%(defaults_upper)s"
+tag: alice/V02-00-01
+source: https://github.com/alisw/RooUnfold
+requires:
+ - ROOT
+ - boost
+---
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
+make ${JOBS:+-j$JOBS} install
+make test
+
+# Modulefile
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION ${BOOST_ROOT:+boost/$BOOST_VERSION-$BOOST_REVISION}
+# Our environment
+setenv ROOUNFOLD_RELEASE \$version
+setenv ROOUNFOLD_VERSION $PKGVERSION
+setenv ROOUNFOLD_ROOT \$::env(BASEDIR)/$PKGNAME/\$::env(ROOUNFOLD_RELEASE)
+prepend-path PATH \$::env(ROOUNFOLD_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(ROOUNFOLD_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(ROOUNFOLD_ROOT)/lib")
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
