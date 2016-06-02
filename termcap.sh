f6fbedbd9fd069ae1903fad2971100cd5798365f
Dario Berzano <dario.berzano@cern.ch>
2016-06-02 12:19:44 +0200

    Fix autotools emacs and termcap problem
    Checks if termcap is available on the system and compiles autotools with emacs
    disabled. Prevents autotools recipe from hanging when checking emacs or failing
    when trying to compile .elc files.

---
+++ termcap.sh
@@ -0,0 +1,8 @@
+package: termcap
+version: 1.0
+system_requirement_missing: "Please install the ncurses development package on your system (usually ncurses-devel or libncurses-dev)"
+system_requirement: ".*"
+system_requirement_check: |
+  printf "#include <termcap.h>\n" | gcc -xc++ - -c -o /dev/null
+---
+
