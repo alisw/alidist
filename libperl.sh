3b1eb8946879497315d1cc2f341c1ee3fea29405
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-08-09 14:32:51 +0200

    Do not depend on c++ to check for perl

--- libperl.sh
+++ libperl.sh
@@ -3,6 +3,6 @@ version: 1.0
 system_requirement_missing: "Please install libperl and perl-ExtUtils-Embed development package on your system"
 system_requirement: ".*"
 system_requirement_check: |
-  printf "#include <EXTERN.h>\nint main(){}\n" | g++ -xc++ -lperl -L`perl -MConfig -e 'print $Config{archlib}'`/CORE -I`perl -MConfig -e 'print $Config{archlib}'`/CORE - -o /dev/null && perl -MExtUtils::Embed -e 1
+  printf "#include <EXTERN.h>\nint main(){}\n" | cc -xc -lperl -L`perl -MConfig -e 'print $Config{archlib}'`/CORE -I`perl -MConfig -e 'print $Config{archlib}'`/CORE - -o /dev/null && perl -MExtUtils::Embed -e 1
 ---
 
