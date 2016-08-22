08361e50c211447a882522631a50f9d9687dd5c7
Dario Berzano <dario.berzano@gmail.com>
2016-08-22 12:15:14 +0200

    Add AliEn-WorkQueue and update CCTools (#504)

---
+++ alien-workqueue.sh
@@ -0,0 +1,35 @@
+package: AliEn-WorkQueue
+version: v1.0
+source: https://github.com/alisw/alien-workqueue
+requires:
+ - "GCC-Toolchain:(?!osx)"
+build_requires:
+ - CMake
+ - cctools
+---
+#!/bin/bash -e
+cmake $SOURCEDIR                          \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
+      -DCCTOOLS=$CCTOOLS_ROOT
+make ${JOBS+-j$JOBS} install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} cctools/$CCTOOLS_VERSION-$CCTOOLS_REVISION
+# Our environment
+setenv ALIEN_WORKQUEUE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(ALIEN_WORKQUEUE_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(ALIEN_WORKQUEUE_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(ALIEN_WORKQUEUE_ROOT)/lib")
+EoF
