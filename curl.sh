003c117ec2c1061e561187c24f3238d717b63869
Dario Berzano <dario.berzano@gmail.com>
2016-08-22 18:30:08 +0200

    Fix curl check on all platforms (#505)
    Notably, `curl --help|--version` returns 2 on SLC5. Here we check whether the
    shell returns 127 (command not found) explicitly.

--- curl.sh
+++ curl.sh
@@ -2,6 +2,6 @@ package: curl
 version: "1.0"
 system_requirement_missing: "Please install Curl."
 system_requirement: ".*"
-system_requirement_check: "curl --help > /dev/null"
+system_requirement_check: "curl --version > /dev/null || [ $? -ne 127 ]"
 ---
 
