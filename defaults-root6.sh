45f11147cb87c875b6a28c4ceef374e47ff5b54c
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-10-26 14:39:09 +0200

    Allow GSL >= 2.0 when compiling with ROOT6

--- defaults-root6.sh
+++ defaults-root6.sh
@@ -8,6 +8,9 @@ overrides:
   ROOT:
     version: "%(tag_basename)s"
     tag: "v6-06-04"
+  GSL:
+    prefer_system_check: |
+      printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
