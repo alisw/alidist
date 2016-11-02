f33a6683498447f651f5f0ecee1dd3a62d9ff921
Dario Berzano <dario.berzano@gmail.com>
2016-11-02 15:23:40 +0100

    Improve ROOT 6 and Python support (#550)
    * Use ROOT v6.09.01 (unstable; plus two commits that fix build)
    * Add Jupyter-related Python packages
    * Improve Python relocation
    * Update CMake to v3.6.2 (required by ROOT 6)

--- defaults-root6.sh
+++ defaults-root6.sh
@@ -5,7 +5,7 @@ env:
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
 overrides:
-  ROOT@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
+  ROOT:
     version: "%(tag_basename)s"
     tag: "v6-08-02"
     source: https://github.com/root-mirror/root
@@ -23,9 +23,9 @@ overrides:
     tag: "v3.5.2"
     prefer_system_check: |
       which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-4].*|3.5.[0-1]) exit 1 ;; esac
-  libpng@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
-  Python-modules@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
-  Python@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
+  libpng:
+  Python-modules:
+  Python:
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
