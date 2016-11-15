02e0df53f7ccd349070c7d01352c5f7157eeb93d
Dario Berzano <dario.berzano@cern.ch>
2016-11-15 11:01:39 +0100

    Align ROOT 6 and CMake overrides in O2 and ROOT 6

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -8,16 +8,29 @@ disable:
   - AliEn-Runtime
   - AliRoot
 overrides:
-  ROOT:
-    version: "%(tag_basename)s"
-    tag: "v6-06-04"
+  ROOT@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
+    tag: "v6-08-02"
+    source: https://github.com/root-mirror/root
+    requires:
+      - AliEn-Runtime:(?!.*ppc64)
+      - GSL
+      - opengl:(?!osx)
+      - Xdevel:(?!osx)
+      - FreeType:(?!osx)
+      - Python-modules
+  GSL:
+    prefer_system_check: |
+      printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
   protobuf:
     version: "%(tag_basename)s"
     tag: "v3.0.2"
   CMake:
     tag: "v3.5.2"
     prefer_system_check: |
-      which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-3].*|3.4.[0-2]) exit 1 ;; esac
+      which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-4].*|3.5.[0-1]) exit 1 ;; esac
+  libpng@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
+  Python-modules@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
+  Python@969984f5f25c5c5326d6b4d4f20e72b0ffad164b:
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
