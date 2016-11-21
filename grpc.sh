750ce7efeb23ad2e330850f0e6511946a7c00c0e
bvonhall <barthelemy.von.haller@cern.ch>
2016-11-21 11:28:08 +0100

    Add the recipes for the Configuration module

---
+++ grpc.sh
@@ -0,0 +1,39 @@
+package: grpc
+version: master
+requires:
+  - protobuf
+build_requires:
+  - "GCC-Toolchain:(?!osx)"
+source: https://github.com/grpc/grpc
+tag:  master
+incremental_recipe: |
+  make ${JOBS:+-j$JOBS} install
+  mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+---
+#!/bin/sh
+
+rsync -a $SOURCEDIR/ ./
+
+git submodule update --init # aie aie aie
+
+make ${JOBS:+-j$JOBS} prefix=$INSTALLROOT
+make prefix=$INSTALLROOT install
+
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+prepend-path PATH \$::env(BASEDIR)/$PKGNAME/\$version/bin
+prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib")
+EoF
