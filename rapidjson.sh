b1840c7c453ac82de0ed713dcbed6f43d2e84d42
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2016-11-28 20:55:31 +0100

    Add the recipes for the Configuration module (#577)
    * Add the recipes for the Configuration module

---
+++ rapidjson.sh
@@ -0,0 +1,32 @@
+package: RapidJSON
+version: 1.1.0
+source: https://github.com/miloyip/rapidjson
+build_requires:
+  - CMake
+tag: v1.1.0
+---
+#!/bin/sh
+
+cmake $SOURCEDIR                                              \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                     
+
+make ${JOBS:+-j$JOBS} install
+
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+prepend-path PATH \$::env(BASEDIR)/$PKGNAME/\$version/bin
+prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib")
+EoF
