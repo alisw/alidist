c803707586209831ca70a289813778f7c57d7c3a
Dario Berzano <dario.berzano@cern.ch>
2016-12-09 23:46:10 +0100

    Bump CMake to v3.5.2 for all
    This is required by Vc

--- defaults-root6.sh
+++ defaults-root6.sh
@@ -19,10 +19,6 @@ overrides:
   GSL:
     prefer_system_check: |
       printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
-  CMake:
-    tag: "v3.5.2"
-    prefer_system_check: |
-      which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-4].*|3.5.[0-1]) exit 1 ;; esac
   libpng:
   Python-modules:
   Python:
