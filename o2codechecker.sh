e34852d2da48c3e43510e1870aa2aaacc65586f1
swenzel <sandro.wenzel@cern.ch>
2016-12-13 22:25:59 +0100

    Add recipes for O2 code checking tools
     - provide recipe to build the O2 code checker tool (o2codechecker)
     - provide recipe to perform the code checks within alibuild (o2checkcode)
     - let O2 install it's compilation database (needed by checks)

---
+++ o2codechecker.sh
@@ -0,0 +1,36 @@
+package: o2codechecker
+version: "%(tag_basename)s%(defaults_upper)s"
+requires:
+  - Clang
+build_requires:
+  - CMake
+source: https://github.com/AliceO2Group/O2CodeChecker.git
+tag: master
+---
+#!/bin/bash -e
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT    \
+                 -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE   \
+                 -DClang_DIR=$CLANG_ROOT/lib/cmake/clang \
+                 -DLLVM_DIR=$CLANG_ROOT/lib/cmake/llvm   
+make ${JOBS+-j$JOBS} install
+ctest
+
+# Modulefile
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ${CLANG_VERSION:+Clang/$CLANG_VERSION-$CLANG_REVISION}
+# Our environment
+setenv O2CODECHECKER_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(O2CODECHECKER_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(O2CODECHECKER_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(O2CODECHECKER_ROOT)/lib")
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
