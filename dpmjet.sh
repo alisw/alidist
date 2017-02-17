0003f3df9aa75f56e1968ed43cfe160f0d350fc9
Jochen Klein <jochen.klein@cern.ch>
2017-02-17 16:30:58 +0100

    Make DPMJET an external package

---
+++ dpmjet.sh
@@ -0,0 +1,36 @@
+package: DPMJET
+version: "%(tag_basename)s%(defaults_upper)s"
+tag: "v3.0.5-alice3"
+source: https://gitlab.cern.ch/ALICEPrivateExternals/DPMJET.git
+requires:
+ - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - CMake
+---
+#!/bin/bash -e
+
+cmake  $SOURCEDIR                           \
+       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
+
+make ${JOBS+-j $JOBS} install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+# Our environment
+setenv DPMJET_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(DPMJET_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(DPMJET_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(DPMJET_ROOT)/lib")
+EoF
