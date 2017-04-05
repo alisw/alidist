5b4ba99f0daff9eae89ba3e773b5ac55b104597a
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-04-05 10:31:59 +0200

    Adapt to new FairRoot version (#670)
    - Bump CMake version for O2 related packages
    - Bump googletest and add it as requirement
    - Cleanup googletest recipe to use make install

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -27,6 +27,11 @@ overrides:
   libpng:
   Python-modules:
   Python:
+  CMake:
+    version: "%(tag_basename)s"
+    tag: "v3.7.2"
+    prefer_system_check: |
+      which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-5].*) exit 1 ;; esac
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
