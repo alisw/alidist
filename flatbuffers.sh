0e0b3baf85b78e12733500cc6f7c4ea584af6a08
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-04-27 09:48:21 +0200

    Add Apache Arrow and Flatbuffers (#693)

---
+++ flatbuffers.sh
@@ -0,0 +1,39 @@
+package: flatbuffers
+version: v1.6.0
+source: https://github.com/google/flatbuffers
+requires:
+  - zlib
+build_requires:
+ - CMake
+ - "GCC-Toolchain:(?!osx)"
+prefer_system: "(?!slc5)"
+prefer_system_check: |
+  printf "#include \"flatbuffers/flatbuffers.h\"\nint main(){}" | c++ -I$(brew --prefix flatbuffers)/include -xc++ -std=c++11 - -o /dev/null
+---
+mkdir -p $INSTALLROOT
+cmake $SOURCEDIR                              \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
+
+make ${JOBS:+-j $JOBS}
+make install
+
+#ModuleFile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+setenv FLATBUFFERS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path LD_LIBRARY_PATH \$::env(FLATBUFFERS_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(FLATBUFFERS_ROOT)/lib")
+prepend-path PATH \$::env(FLATBUFFERS_ROOT)/bin
+EoF
