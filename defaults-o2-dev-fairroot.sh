de4ba23ae2f9ca09a8cf09b660f4b5f21a095ec1
Dario Berzano <dario.berzano@cern.ch>
2017-04-28 11:49:18 +0200

    Cleanup defaults and sync O2 ones

--- defaults-o2-dev-fairroot.sh
+++ defaults-o2-dev-fairroot.sh
@@ -16,6 +16,18 @@ disable:
   - pythia6
   - hijing
 overrides:
+  autotools:
+    tag: v1.5.0
+  boost:
+    requires:
+      - "GCC-Toolchain:(?!osx)"
+      - Python
+  GCC-Toolchain:
+    tag: v6.2.0-alice1
+    prefer_system_check: |
+      set -e
+      which gfortran || { echo "gfortran missing"; exit 1; }
+      which cc && test -f $(dirname $(which cc))/c++ && printf "#define GCCVER ((__GNUC__ << 16)+(__GNUC_MINOR__ << 8)+(__GNUC_PATCHLEVEL__))\n#if (GCCVER < 0x060200)\n#error \"System's GCC cannot be used: we need at least GCC 6.X. We are going to compile our own version.\"\n#endif\n" | cc -xc++ - -c -o /dev/null
   ROOT:
     tag: "v6-08-02"
     source: https://github.com/root-mirror/root
@@ -24,6 +36,7 @@ overrides:
       - opengl:(?!osx)
       - Xdevel:(?!osx)
       - FreeType:(?!osx)
+      - Python-modules
   GSL:
     prefer_system_check: |
       printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
