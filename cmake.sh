bb87d0f31af213ccdff80ef37dda45d159b5b748
Dario Berzano <dario.berzano@cern.ch>
2017-04-28 10:58:47 +0200

    Add modulefile to CMake
    Fixes alisw/alibuild#425

--- cmake.sh
+++ cmake.sh
@@ -30,3 +30,21 @@ $SOURCEDIR/bootstrap --prefix=$INSTALLROOT \
                      ${JOBS:+--parallel=$JOBS}
 make ${JOBS+-j $JOBS}
 make install/strip
+
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 \\
+       ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+# Our environment
+setenv CMAKE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(CMAKE_ROOT)/bin
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
