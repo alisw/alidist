db9bcac4b080986257fa760c4c9b2a872d671ce6
laphecet <laurent.aphecetche@cern.ch>
2017-04-29 10:55:24 +0200

    Fix alo module + some changes in defaults to mirror those in defaults-o2

--- alo.sh
+++ alo.sh
@@ -13,18 +13,18 @@ incremental_recipe: |
   # install the compilation database so that we can post-check the code
   cp ${BUILDDIR}/compile_commands.json ${INSTALLROOT}
 
-  DEVEL_SOURCES="`readlink $SOURCEDIR || echo $SOURCEDIR`"
+  DEVEL_SOURCES="$(readlink $SOURCEDIR || echo $SOURCEDIR)"
   # This really means we are in development mode. We need to make sure we
   # use the real path for sources in this case. We also copy the
   # compile_commands.json file so that IDEs can make use of it directly, this
   # is a departure from our "no changes in sourcecode" policy, but for a good reason
   # and in any case the file is in gitignore.
-  if [ "$DEVEL_SOURCES" != "$SOURCEDIR" ]; then
+  if [[ "$DEVEL_SOURCES" != "$SOURCEDIR" ]]; then
     perl -p -i -e "s|$SOURCEDIR|$DEVEL_SOURCES|" compile_commands.json
     ln -sf $BUILDDIR/compile_commands.json $DEVEL_SOURCES/compile_commands.json
   fi
 ---
-#!/bin/sh
+#!/bin/bash
 
 echo "Build MRRTF alo"
 
@@ -50,6 +50,7 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 module load BASE/1.0 FairRoot/$FAIRROOT_VERSION-$FAIRROOT_REVISION ${DDS_ROOT:+DDS/$DDS_VERSION-$DDS_REVISION} ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${VC_VERSION:+Vc/$VC_VERSION-$VC_REVISION} ${ALIROOT_VERSION:+AliRoot/$ALIROOT_VERSION-$ALIROOT_REVISION}
 
 # Our environment
+setenv ALO_ROOT $::env(BASEDIR)/alo/$version
 prepend-path PATH \$::env(ALO_ROOT)/bin
 prepend-path LD_LIBRARY_PATH \$::env(ALO_ROOT)/lib
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(ALO_ROOT)/lib")
