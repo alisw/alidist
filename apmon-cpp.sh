371f876a89a2cc2efecc469edb5c7b2bfcade5b8
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2017-05-11 10:16:39 +0200

    Add module support to ap-mon (#733)

--- apmon-cpp.sh
+++ apmon-cpp.sh
@@ -13,3 +13,22 @@ autoreconf -ivf
 ./configure --prefix=$INSTALLROOT
 make ${JOBS:+-j$JOBS}
 make install
+
+# Modules
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 \\
+       ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+# Our environment
+setenv APMON_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(APMON_ROOT)/bin
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
