39720108b700d5b0a00efb66b253e28fd8adfc17
Matthias Richter <github@scieq.net>
2017-05-16 14:45:51 +0200

    Adding ZeroMQ as dependency for AliRoot build in defaults-o2 (#743)
    This handles https://github.com/AliceO2Group/AliceO2/issues/369
    
    Note: AliRoot is by default disabled in the defaults, but might be
    included in the build by removing this. Then the correct dependencies
    must be applied.
    
    In the override section, all dependencies have to be specified, simply
    adding to the already defined dependencies is not yet supported.

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -30,6 +30,14 @@ overrides:
       - Xdevel:(?!osx)
       - FreeType:(?!osx)
       - Python-modules
+  AliRoot:
+    requires:
+      - ROOT
+      - fastjet:(?!.*ppc64)
+      - GEANT3
+      - GEANT4_VMC
+      - Vc
+      - ZeroMQ
   GSL:
     prefer_system_check: |
       printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
