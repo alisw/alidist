0698f2c8fcfba4cd6096d7a68dcd829cfd9916fc
Dario Berzano <dario.berzano@gmail.com>
2017-05-19 18:31:16 +0200

    Update defaults for native/DAQ SLC6 builds (#751)
    AliRoot must also compile using the native SLC6 compiler. Those defaults are
    to be used for the native AliRoot build tests, and for generating the DA RPMs

--- daq.sh
+++ daq.sh
@@ -1,12 +1,6 @@
 package: DAQ
 version: v1
-prefer_system: slc6.*
-prefer_system_check: |
-  ! rpm -q date amore daqDA-lib ACT
 ---
 #!/bin/bash -e
-# This is a dummy recipe used to track DAQ dependencies installed via RPMs.
-# AliRoot depends on it and will be rebuilt if this package's version changes.
-# For DAQ use: prefer_system_check returns 1 in order to force the creation of
-# this dummy package. For all the other use cases: prefer_system_check returns 0
-# and this dependency will be silently ignored.
+# We do nothing but checking that those packages are installed.
+rpm -q date amore daqDA-lib ACT
