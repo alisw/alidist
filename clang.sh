df861cd36dcfeb4b3b3342e5b2a0f4f42eb7feea
Dario Berzano <dario.berzano@cern.ch>
2017-06-01 15:08:04 +0200

    Fix clang recipe platforms compatibility

--- clang.sh
+++ clang.sh
@@ -28,8 +28,14 @@ case $ARCHITECTURE in
     ln -sf /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++ $INSTALLROOT/include
   ;;
 esac
-find $SOURCEDIR/tools/clang/tools/scan-build -type f -perm +111 -exec cp {} $INSTALLROOT/bin \;
-find $SOURCEDIR/tools/clang/tools/scan-view -type f -perm +111 -exec cp {} $INSTALLROOT/bin \;
+
+FIND_EXEC='-executable'
+mkdir findtest && touch x && chmod 0777 x
+find findtest -type f $FIND_EXEC &> /dev/null || FIND_EXEC='-perm +011'
+rm -rf findtest
+
+find $SOURCEDIR/tools/clang/tools/scan-build -type f $FIND_EXEC -exec cp {} $INSTALLROOT/bin \;
+find $SOURCEDIR/tools/clang/tools/scan-view -type f $FIND_EXEC -exec cp {} $INSTALLROOT/bin \;
 
 # Modulefile
 mkdir -p etc/modulefiles
