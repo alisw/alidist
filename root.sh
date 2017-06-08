854c4112740fb6396d81ad37c553bbe45cd43ca4
Dario Berzano <dario.berzano@cern.ch>
2017-06-08 15:14:47 +0200

    Add GCC-Toolchain as a direct ROOT dep

--- root.sh
+++ root.sh
@@ -9,6 +9,7 @@ requires:
   - Xdevel:(?!osx)
   - FreeType:(?!osx)
   - "MySQL:slc7.*"
+  - GCC-Toolchain:(?!osx)
 build_requires:
   - CMake
   - "Xcode:(osx.*)"
@@ -149,6 +150,7 @@ set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
 module load BASE/1.0 ${ALIEN_RUNTIME_ROOT:+AliEn-Runtime/$ALIEN_RUNTIME_VERSION-$ALIEN_RUNTIME_REVISION}        \\
+                     ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}     \\
                      ${GSL_VERSION:+GSL/$GSL_VERSION-$GSL_REVISION}                                             \\
                      ${FREETYPE_VERSION:+FreeType/$FREETYPE_VERSION-$FREETYPE_REVISION}                         \\
                      ${PYTHON_VERSION:+Python/$PYTHON_VERSION-$PYTHON_REVISION}                                 \\
