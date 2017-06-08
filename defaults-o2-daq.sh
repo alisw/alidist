c738d712d99a9e19dd0bee6b86cab7a6485c683f
sawenzel <sandro.wenzel@cern.ch>
2017-06-08 09:14:53 +0200

    Create defaults-o2-daq.sh

--- defaults-o2-daq.sh
+++ defaults-o2-daq.sh
@@ -37,6 +37,7 @@ overrides:
       - Xdevel:(?!osx)
       - FreeType:(?!osx)
       - Python-modules
+      - GCC-Toolchain:(?!osx)
   GSL:
     prefer_system_check: |
       printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
