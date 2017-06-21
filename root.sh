695fa3040c935c0e5e66c7d832b51e66780797dd
Hans Beck <hans.beck@cern.ch>
2017-06-21 19:38:38 +0200

    Root: Added -Dbuiltin_vdt=ON for root v6-10-00.

--- root.sh
+++ root.sh
@@ -107,6 +107,7 @@ else
         -Dsoversion=ON                                            \
         -Dshadowpw=OFF                                            \
         -Dvdt=ON                                                  \
+        -Dbuiltin_vdt=ON                                          \
         -DCMAKE_PREFIX_PATH="$FREETYPE_ROOT;$SYS_OPENSSL_ROOT;$GSL_ROOT;$ALIEN_RUNTIME_ROOT;$PYTHON_ROOT;$PYTHON_MODULES_ROOT"
   FEATURES="builtin_pcre mathmore xml ssl opengl minuit2 http
             pythia6 roofit soversion vdt ${CXX11:+cxx11} ${CXX14:+cxx14} ${XROOTD_ROOT:+xrootd}
