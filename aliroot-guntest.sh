6ff8358352c9bacc3dd0b0cf2fcfd91e4051580b
Dario Berzano <dario.berzano@cern.ch>
2017-06-29 15:22:27 +0200

    Minor style fixes plus support for WITHESDCHECK

--- aliroot-guntest.sh
+++ aliroot-guntest.sh
@@ -1,24 +1,20 @@
 package: AliRoot-guntest
-version: "%(year)s%(month)s%(day)s%(defaults_upper)s"
+version: v1
 force_rebuild: 1
 requires:
   - AliRoot
   - AliRoot-OCDB
 ---
-#!/bin/sh
+#!/bin/bash -e
 
 # A simple regression test launching a Geant3 + Geant4 gun simulation + reconstruction.
 # Tests if the processing runs through and yields a reasonable ESD.
 # Note that the test is limited to the default OCDB.
-
-rsync -a $ALIROOT_ROOT/test/vmctest/gun test
-
+rsync -a "$ALIROOT_ROOT"/test/vmctest/gun test
 cd test/gun
 
 # launch the simulation
 ./runtest.sh
 
 # test outcome and return the error code
-# TODO: enable the ESD checks with `WITHESDCHECK=yes ./finalcheck`
-./finalcheck.sh
-exit $?
+WITHESDCHECK=${ALIPHYSICS_VERSION:+yes} ./finalcheck.sh
