14174a02c096ddeeebd1d5775aedebe4c0b90588
Dario Berzano <dario.berzano@cern.ch>
2017-07-10 13:36:22 +0200

    Bump ROOT 6 to v6-10-02 (O2 and ROOT6 defaults)

--- defaults-alo.sh
+++ defaults-alo.sh
@@ -31,7 +31,7 @@ overrides:
       which gfortran || { echo "gfortran missing"; exit 1; }
       which cc && test -f $(dirname $(which cc))/c++ && printf "#define GCCVER ((__GNUC__ << 16)+(__GNUC_MINOR__ << 8)+(__GNUC_PATCHLEVEL__))\n#if (GCCVER < 0x060200)\n#error \"System's GCC cannot be used: we need at least GCC 6.X. We are going to compile our own version.\"\n#endif\n" | cc -xc++ - -c -o /dev/null
   ROOT:
-    tag: "v6-08-02"
+    tag: "v6-10-02"
     source: https://github.com/root-mirror/root
     requires:
       - GSL
