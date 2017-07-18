3b4f61b4f3133f4c70df3c6e975086f2f34949a1
Dario Berzano <dario.berzano@gmail.com>
2017-07-18 10:15:19 +0200

    Fix aliDoctor curl test (#797)

--- curl.sh
+++ curl.sh
@@ -6,5 +6,5 @@ system_requirement_missing: |
    * Ubuntu-compatible systems: you will probably need "curl" and "libcurl4-openssl-dev" (or "libcurl4-gnutls-dev").
 system_requirement: ".*"
 system_requirement_check: |
-  curl --version > /dev/null && [ $? -ne 127 ] && printf "#include <curl/curl.h>\nint main() {}\n" | cc -xc -lcurl - -o /dev/null
+  curl --version > /dev/null; if test $? = 127; then exit 1; else printf "#include <curl/curl.h>\nint main() {}\n" | cc -xc -lcurl - -o /dev/null || exit 2; fi; exit 0
 ---
