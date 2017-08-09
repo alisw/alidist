0050876c056c7dbca63c60a47e8f5669219fb60e
Hans Beck <hans.beck@cern.ch>
2017-08-09 16:57:19 +0200

    UUID preserve links when copying to installroot
    Addresses #608. Use -a flag of cp instead of -p to preserve links

--- uuid.sh
+++ uuid.sh
@@ -25,9 +25,9 @@ autoreconf -ivf
             --enable-libuuid
 make ${JOBS:+-j$JOBS} libuuid.la
 mkdir -p $INSTALLROOT/lib
-cp -p .libs/libuuid.a* $INSTALLROOT/lib
+cp -a .libs/libuuid.a* $INSTALLROOT/lib
 if [[ ${ARCHITECTURE:0:3} != osx ]]; then
-  cp -p .libs/libuuid.so* $INSTALLROOT/lib
+  cp -a .libs/libuuid.so* $INSTALLROOT/lib
 fi
 mkdir -p $INSTALLROOT/include
 make install-uuidincHEADERS
