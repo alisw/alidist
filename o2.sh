alisw/alidist

commit 6a8b896027d7099efed2e7c6fb0b13c3c1c331dd

Remove AliRoot dep from O2 and honor build type (#817)

o2.sh
@@ -3,7 +3,6 @@ version: dev
 tag: dev
 requires:
   - FairRoot
-  - AliRoot
   - DDS
   - Vc
   - hijing
@@ -69,12 +68,13 @@ cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                            
       ${DDS_ROOT:+-DDDS_PATH=$DDS_ROOT}                           \
       -DZMQ_DIR=$ZEROMQ_ROOT                                      \
       -DZMQ_INCLUDE_DIR=$ZEROMQ_ROOT/include                      \
-      -DALIROOT=$ALIROOT_ROOT                                     \
+      ${$ALIROOT_VERSION:+-DALIROOT=$ALIROOT_ROOT}                \
       -DPROTOBUF_INCLUDE_DIR=$PROTOBUF_ROOT/include               \
       -DPROTOBUF_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc      \
       -DPROTOBUF_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.$SONAME   \
       ${GSL_ROOT:+-DGSL_DIR=$GSL_ROOT}                            \
       ${PYTHIA_ROOT:+-DPYTHIA8_INCLUDE_DIR=$PYTHIA_ROOT/include}  \
+      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}   \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 if [[ $GIT_TAG == master ]]; then

commit ccb8d5deeb75f2b3a167e6aedd6fa8401c7704bf

Fix o2.sh by removing spurious $
Bug introduced in #817 : extraneous $ in ALIROOT_VERSION cmake line

o2.sh
@@ -68,7 +68,7 @@ cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                            
       ${DDS_ROOT:+-DDDS_PATH=$DDS_ROOT}                           \
       -DZMQ_DIR=$ZEROMQ_ROOT                                      \
       -DZMQ_INCLUDE_DIR=$ZEROMQ_ROOT/include                      \
-      ${$ALIROOT_VERSION:+-DALIROOT=$ALIROOT_ROOT}                \
+      ${ALIROOT_VERSION:+-DALIROOT=$ALIROOT_ROOT}                \
       -DPROTOBUF_INCLUDE_DIR=$PROTOBUF_ROOT/include               \
       -DPROTOBUF_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc      \
       -DPROTOBUF_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.$SONAME   \