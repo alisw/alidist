227b7e4813b2f28db6d3a5670082b4408ea2e5f6
Pascal Boeschoten <passiepascal@gmail.com>
2017-09-06 16:40:23 +0200

    Set PYTHONPATH in readoutcard.sh

--- readoutcard.sh
+++ readoutcard.sh
@@ -56,6 +56,7 @@ module load BASE/1.0                                                          \\
 setenv READOUTCARD_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$::env(READOUTCARD_ROOT)/bin
 prepend-path LD_LIBRARY_PATH \$::env(READOUTCARD_ROOT)/lib
+prepend-path PYTHONPATH \$::env(READOUTCARD_ROOT)/lib
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(READOUTCARD_ROOT)/lib")
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
