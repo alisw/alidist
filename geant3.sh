37a1ee019b595a41f4d2483a575cfc0ffb717899
Florian Uhlig <f.uhlig@gsi.de>
2017-09-11 16:22:12 +0200

    Add possibility to compile geant3 with a non system compiler.
    Use new tag which solves a linking problem when using clang on linux.

--- geant3.sh
+++ geant3.sh
@@ -6,15 +6,21 @@ build_requires:
   - CMake
   - "Xcode:(osx.*)"
 source: https://github.com/vmc-project/geant3
-tag: v2-4
+tag: v2-5
 prepend_path:
   "LD_LIBRARY_PATH": "$GEANT3_ROOT/lib64"
   "DYLD_LIBRARY_PATH": "$GEANT3_ROOT/lib64"
 ---
 #!/bin/bash -e
-cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT   \
-                 -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE  \
-                 -DCMAKE_SKIP_RPATH=TRUE
+cmake                                                                 \
+      ${C_COMPILER:+-DCMAKE_C_COMPILER=$C_COMPILER}                   \
+      ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER}             \
+      ${Fortran_COMPILER:+-DCMAKE_Fortran_COMPILER=$Fortran_COMPILER} \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                             \
+      -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE                            \
+      -DCMAKE_SKIP_RPATH=TRUE                                         \
+      $SOURCEDIR
+
 make ${JOBS:+-j $JOBS} install
 
 [[ ! -d $INSTALLROOT/lib64 ]] && ln -sf lib $INSTALLROOT/lib64
