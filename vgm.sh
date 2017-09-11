f848e13d93e1db6a059441d6686538369a16c8ef
Florian Uhlig <f.uhlig@gsi.de>
2017-09-11 16:20:41 +0200

    Add possibility to compile vgm with a non system compiler.

--- vgm.sh
+++ vgm.sh
@@ -9,10 +9,13 @@ build_requires:
   - CMake
 ---
 #!/bin/bash -e
-cmake "$SOURCEDIR" \
-  -DCMAKE_CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-  -DCMAKE_INSTALL_LIBDIR="lib"                 \
-  -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"
+cmake                                                 \
+  ${C_COMPILER:+-DCMAKE_C_COMPILER=$C_COMPILER}       \
+  ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER} \
+  -DCMAKE_CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}        \
+  -DCMAKE_INSTALL_LIBDIR="lib"                        \
+  -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"               \
+  "$SOURCEDIR"
 
 make ${JOBS+-j $JOBS} install
 
