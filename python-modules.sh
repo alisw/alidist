2a945f41e109863a2d1916413900d3f05468fce3
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-10-03 11:46:33 +0200

    Avoid pip upgrading dependencies if not needed

--- python-modules.sh
+++ python-modules.sh
@@ -44,17 +44,19 @@ export PYVER=$(python -c 'import distutils.sysconfig; print(distutils.sysconfig.
 # Install as much as possible with pip. Packages are installed one by one as we
 # are not sure that pip exits with nonzero in case one of the packages failed.
 export PYTHONUSERBASE=$INSTALLROOT
-for X in "mock==1.0.0"         \
-         "numpy==1.9.2"        \
-         "certifi==2015.9.6.2" \
-         "ipython==5.1.0"      \
-         "ipywidgets==5.2.2"   \
-         "ipykernel==4.5.0"    \
-         "notebook==4.2.3"     \
-         "metakernel==0.14.0"  \
-         "pyyaml"
-do
-  pip install --user $X
+cat >requirements.txt << \EOF
+mock==1.0.0
+numpy==1.9.2
+certifi==2015.9.6.2
+ipython==5.1.0
+ipywidgets==5.2.2
+ipykernel==4.5.0
+notebook==4.2.3
+metakernel==0.14.0
+pyyaml
+EOF
+for X in `cat requirements.txt | sed -e "s/=.*//"`; do
+  pip install --user --disable-pip-version-check $X -r ./requirements.txt
 done
 unset PYTHONUSERBASE
 
