0e5cb0c45857920f6a7258176abeca8c46637c89
PascalBoeschoten <passiepascal@gmail.com>
2017-10-19 19:00:25 +0200

    Add recipe for Configuration-Benchmark

---
+++ configuration-benchmark.sh
@@ -0,0 +1,54 @@
+package: Configuration-Benchmark
+version: "%(tag_basename)s"
+tag: v0.1.1
+requires:
+  - boost
+  - "GCC-Toolchain:(?!osx)"
+  - Monitoring
+  - Configuration
+build_requires:
+  - CMake
+source: https://github.com/AliceO2Group/ConfigurationBenchmark
+incremental_recipe: |
+  make ${JOBS:+-j$JOBS} install
+  mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+---
+#!/bin/bash -ex
+
+case $ARCHITECTURE in
+  osx*) BOOST_ROOT=$(brew --prefix boost) ;;
+esac
+
+cmake $SOURCEDIR                                              \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                     \
+      ${BOOST_VERSION:+-DBOOST_ROOT=$BOOST_ROOT}              \
+      ${MONITORING_VERSION:+-DMonitoring_ROOT=$MONITORING_ROOT} \
+      ${CONFIGURATION_VERSION:+-DConfiguration_ROOT=$CONFIGURATION_ROOT} \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+make ${JOBS+-j $JOBS} install
+
+#ModuleFile
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0                                                          \\
+            ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION}            \\
+            ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
+            Monitoring/$MONITORING_VERSION-$MONITORING_REVISION               \\
+            Configuration/$CONFIGURATION_VERSION-$CONFIGURATION_REVISION
+
+# Our environment
+setenv CONFIGURATION_BENCHMARK_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(CONFIGURATION_BENCHMARK_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(CONFIGURATION_BENCHMARK_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(CONFIGURATION_BENCHMARK_ROOT)/lib")
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
