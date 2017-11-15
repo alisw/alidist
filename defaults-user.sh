5dccbc82e93213508880cb991fa2156b9a7ab21d
Dario Berzano <dario.berzano@cern.ch>
2017-11-15 11:42:54 +0100

    Add new defaults for prod and user installation

---
+++ defaults-user.sh
@@ -0,0 +1,29 @@
+package: defaults-user
+version: v1
+env:
+  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
+  CFLAGS: "-fPIC -g -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+overrides:
+  AliRoot:
+    version: v5-09-19
+    tag: v5-09-19
+    requires:
+      - ROOT
+      - fastjet:(?!.*ppc64)
+      - Vc
+  AliPhysics:
+    version: v5-09-19-01
+    tag: v5-09-19-01
+---
+# To be used with aliBuild option `--defaults user`.
+#
+# This defaults set is meant to be used on user computers.
+#
+#   * Builds AliRoot and AliPhysics without GEANT3, 4 and DPMJET, which fits
+#     most of the use cases and speeds up compilation time dramatically
+#   * Pinpoints AliRoot and AliPhysics to their latest tags, but they will be
+#     overridden by using packages in development mode
+#
+# This defaults set will become the default and will replace "release" at some
+# point.
