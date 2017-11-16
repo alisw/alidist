c2025f5a023c930b5a6ebdfadd9a2b7ea1f0885e
Dario Berzano <dario.berzano@gmail.com>
2017-11-16 23:36:43 +0100

    Add new defaults for prod and user installation (#924)

---
+++ defaults-prod-latest.sh
@@ -0,0 +1,23 @@
+package: defaults-prod-latest
+version: v1
+env:
+  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
+  CFLAGS: "-fPIC -g -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+overrides:
+  AliRoot:
+    version: v5-09-19
+    tag: v5-09-19
+  AliPhysics:
+    version: v5-09-19-01
+    tag: v5-09-19-01
+---
+# To be used with aliBuild option `--defaults prod-latest`.
+#
+# This is the default set used to produce centralized builds of AliRoot and
+# AliPhysics, with pinpointed versions.
+#
+#   * It is recommended on user laptops to use `--defaults user` instead, which
+#     pinpoints AliRoot and AliPhysics versions.
+#   * Continuous integration tests use this defaults to test AliPhysics changes
+#     against the latest tagged version of AliRoot.
