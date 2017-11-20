6ba1079216bf6e0b67cb9e1a29ea75cf9323341f
Roberto Preghenella <preghenella@bo.infn.it>
2017-11-20 12:51:43 +0100

    tag should always be right after version, if the latter is present

--- hepmc3.sh
+++ hepmc3.sh
@@ -1,7 +1,7 @@
 package: HepMC3
 version: "%(tag_basename)s"
-source: https://gitlab.cern.ch/hepmc/HepMC3.git
 tag: 3.0.0
+source: https://gitlab.cern.ch/hepmc/HepMC3.git
 requires:
   - ROOT
 build_requires:
