2f93f248d697876359ef7adac83aeb7cc6b9d902
laphecet <laurent.aphecetche@cern.ch>
2017-11-30 18:05:31 +0100

    Adding recipe for Google Benchmark framework

---
+++ googlebenchmark.sh
@@ -0,0 +1,14 @@
+package: googlebenchmark
+version: "1.3.0"
+source: https://github.com/google/benchmark
+tag: v1.3.0
+build_requires:
+ - "GCC-Toolchain:(?!osx)"
+ - CMake
+---
+#!/bin/sh
+cmake $SOURCEDIR                           \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
+      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}
+make ${JOBS+-j $JOBS}
+make install
