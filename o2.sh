alisw/alidist

commit 5505463d5e3324da0f43d4694a75008fc38fc7ef

Enable HepMC3 in O2 and avoid conflicts with packages requiring HepMC2 (#873)

o2.sh
@@ -6,6 +6,7 @@ requires:
   - DDS
   - Vc
   - hijing
+  - HepMC3
   - O2HLTCATracking
 build_requires:
   - ms_gsl
@@ -83,6 +84,7 @@ cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                            
       ${PROTOBUF_ROOT:+-DProtoBuf_DIR=$PROTOBUF_ROOT}             \
       ${GSL_ROOT:+-DGSL_DIR=$GSL_ROOT}                            \
       ${PYTHIA_ROOT:+-DPYTHIA8_INCLUDE_DIR=$PYTHIA_ROOT/include}  \
+      ${HEPMC3_ROOT:+-DHEPMC3_DIR=$HEPMC3_ROOT}                   \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}   \
       -DMS_GSL_INCLUDE_DIR=$MS_GSL_ROOT/include                   \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON                          \
@@ -119,7 +121,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 FairRoot/$FAIRROOT_VERSION-$FAIRROOT_REVISION ${DDS_VERSION:+DDS/$DDS_VERSION-$DDS_REVISION} ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${VC_VERSION:+Vc/$VC_VERSION-$VC_REVISION} ${O2HLTCATRACKING_VERSION:+O2HLTCATracking/$O2HLTCATRACKING_VERSION-$O2HLTCATRACKING_REVISION}
+module load BASE/1.0 FairRoot/$FAIRROOT_VERSION-$FAIRROOT_REVISION ${DDS_VERSION:+DDS/$DDS_VERSION-$DDS_REVISION} ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${VC_VERSION:+Vc/$VC_VERSION-$VC_REVISION} ${HEPMC3_VERSION:+HepMC3/$HEPMC3_VERSION-$HEPMC3_REVISION} ${O2HLTCATRACKING_VERSION:+O2HLTCATracking/$O2HLTCATRACKING_VERSION-$O2HLTCATRACKING_REVISION}
 # Our environment
 setenv O2_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv VMCWORKDIR \$::env(O2_ROOT)/share

defaults-o2.sh
@@ -56,6 +56,10 @@ overrides:
     tag: "v3.9.4"
     prefer_system_check: |
       which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-8].*|3.9.[0-3]) exit 1 ;; esac
+  pythia:
+    requires:
+      - lhapdf
+      - boost
   AliRoot:
     version: "%(commit_hash)s_O2"
   AliPhysics:

hepmc3.sh
@@ -1,7 +1,7 @@
 package: HepMC3
 version: "%(tag_basename)s"
+tag: 3.0.0
 source: https://gitlab.cern.ch/hepmc/HepMC3.git
-tag: beta2.0
 requires:
   - ROOT
 build_requires:

pythia.sh
@@ -21,7 +21,7 @@ esac
 
 ./configure --prefix=$INSTALLROOT \
             --enable-shared \
-            --with-hepmc2=${HEPMC_ROOT} \
+            ${HEPMC_ROOT:+--with-hepmc2="$HEPMC_ROOT"} \
             ${LHAPDF_ROOT:+--with-lhapdf6="$LHAPDF_ROOT"} \
             ${BOOST_ROOT:+--with-boost="$BOOST_ROOT"}
 
@@ -48,7 +48,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${LHAPDF_VERSION:+lhapdf/$LHAPDF_VERSION-$LHAPDF_REVISION} ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION} HepMC/$HEPMC_VERSION-$HEPMC_REVISION
+module load BASE/1.0 ${LHAPDF_VERSION:+lhapdf/$LHAPDF_VERSION-$LHAPDF_REVISION} ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION} ${HEPMC_VERSION:+HepMC/$HEPMC_VERSION-$HEPMC_REVISION}
 # Our environment
 setenv PYTHIA_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv PYTHIA8DATA \$::env(PYTHIA_ROOT)/share/Pythia8/xmldoc

simulation.sh
@@ -4,7 +4,6 @@ requires:
   - GEANT4_VMC
   - GEANT4
   - GEANT3
-  - HepMC
 ---
 #!/bin/bash -e
 

commit 98089ec1d34ebed901304c45bf3844af1486531f

Fix O2 modulefile vars expansion typo
Introduced in #873

o2.sh
@@ -70,30 +70,30 @@ if [[ $ALIBUILD_O2_TESTS ]]; then
 fi
 cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                              \
       -DCMAKE_MODULE_PATH="$SOURCEDIR/cmake/modules;$FAIRROOT_ROOT/share/fairbase/cmake/modules;$FAIRROOT_ROOT/share/fairbase/cmake/modules_old"  \
-      -DFairRoot_DIR=$FAIRROOT_ROOT                               \
-      -DALICEO2_MODULAR_BUILD=ON                                  \
-      -DROOTSYS=$ROOTSYS                                          \
-      ${PYTHIA6_ROOT:+-DPythia6_LIBRARY_DIR=$PYTHIA6_ROOT/lib}    \
-      ${GEANT3_ROOT:+-DGeant3_DIR=$GEANT3_ROOT}                   \
-      ${GEANT4_ROOT:+-DGeant4_DIR=$GEANT4_ROOT}                   \
-      ${VGM_ROOT:+-DVGM_DIR=$VGM_ROOT}                            \
-      ${GEANT4_VMC_ROOT:+-DGEANT4_VMC_DIR=$GEANT4_VMC_ROOT}       \
-      -DFAIRROOTPATH=$FAIRROOT_ROOT                               \
-      ${BOOST_ROOT:+-DBOOST_ROOT=$BOOST_ROOT}                     \
-      ${DDS_ROOT:+-DDDS_PATH=$DDS_ROOT}                           \
-      -DZMQ_DIR=$ZEROMQ_ROOT                                      \
-      -DZMQ_INCLUDE_DIR=$ZEROMQ_ROOT/include                      \
-      ${ALIROOT_VERSION:+-DALIROOT=$ALIROOT_ROOT}                 \
-      ${PROTOBUF_ROOT:+-DProtoBuf_DIR=$PROTOBUF_ROOT}             \
-      ${GSL_ROOT:+-DGSL_DIR=$GSL_ROOT}                            \
-      ${PYTHIA_ROOT:+-DPYTHIA8_INCLUDE_DIR=$PYTHIA_ROOT/include}  \
-      ${HEPMC3_ROOT:+-DHEPMC3_DIR=$HEPMC3_ROOT}                   \
-      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}   \
-      -DMS_GSL_INCLUDE_DIR=$MS_GSL_ROOT/include                   \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON                          \
-      ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}                     \
+      -DFairRoot_DIR=$FAIRROOT_ROOT                                                 \
+      -DALICEO2_MODULAR_BUILD=ON                                                    \
+      -DROOTSYS=$ROOTSYS                                                            \
+      ${PYTHIA6_ROOT:+-DPythia6_LIBRARY_DIR=$PYTHIA6_ROOT/lib}                      \
+      ${GEANT3_ROOT:+-DGeant3_DIR=$GEANT3_ROOT}                                     \
+      ${GEANT4_ROOT:+-DGeant4_DIR=$GEANT4_ROOT}                                     \
+      ${VGM_ROOT:+-DVGM_DIR=$VGM_ROOT}                                              \
+      ${GEANT4_VMC_ROOT:+-DGEANT4_VMC_DIR=$GEANT4_VMC_ROOT}                         \
+      -DFAIRROOTPATH=$FAIRROOT_ROOT                                                 \
+      ${BOOST_ROOT:+-DBOOST_ROOT=$BOOST_ROOT}                                       \
+      ${DDS_ROOT:+-DDDS_PATH=$DDS_ROOT}                                             \
+      -DZMQ_DIR=$ZEROMQ_ROOT                                                        \
+      -DZMQ_INCLUDE_DIR=$ZEROMQ_ROOT/include                                        \
+      ${ALIROOT_VERSION:+-DALIROOT=$ALIROOT_ROOT}                                   \
+      ${PROTOBUF_ROOT:+-DProtoBuf_DIR=$PROTOBUF_ROOT}                               \
+      ${GSL_ROOT:+-DGSL_DIR=$GSL_ROOT}                                              \
+      ${PYTHIA_ROOT:+-DPYTHIA8_INCLUDE_DIR=$PYTHIA_ROOT/include}                    \
+      ${HEPMC3_ROOT:+-DHEPMC3_DIR=$HEPMC3_ROOT}                                     \
+      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}                     \
+      -DMS_GSL_INCLUDE_DIR=$MS_GSL_ROOT/include                                     \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON                                            \
+      ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}                                       \
       ${O2HLTCATRACKING_VERSION:+-DO2_TPCCA_TRACKING_LIB_DIR=$O2HLTCATRACKING_ROOT} \
-      ${MONITORING_VERSION:+-DMonitoring_ROOT=$MONITORING_ROOT}   \
+      ${MONITORING_VERSION:+-DMonitoring_ROOT=$MONITORING_ROOT}                     \
       ${CONFIGURATION_VERSION:+-DConfiguration_ROOT=$CONFIGURATION_ROOT}
 
 if [[ $GIT_TAG == master ]]; then
@@ -126,7 +126,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 FairRoot/$FAIRROOT_VERSION-$FAIRROOT_REVISION ${DDS_VERSION:+DDS/$DDS_VERSION-$DDS_REVISION} ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${VC_VERSION:+Vc/$VC_VERSION-$VC_REVISION} $${HEPMC3_VERSION:+HepMC3/$HEPMC3_VERSION-$HEPMC3_REVISION} {O2HLTCATRACKING_VERSION:+O2HLTCATracking/$O2HLTCATRACKING_VERSION-$O2HLTCATRACKING_REVISION} ${MONITORING_VERSION:+Monitoring/$MONITORING_VERSION-$MONITORING_REVISION} ${CONFIGURATION_VERSION:+Configuration/$CONFIGURATION_VERSION-$CONFIGURATION_REVISION}
+module load BASE/1.0 FairRoot/$FAIRROOT_VERSION-$FAIRROOT_REVISION ${DDS_VERSION:+DDS/$DDS_VERSION-$DDS_REVISION} ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${VC_VERSION:+Vc/$VC_VERSION-$VC_REVISION} ${HEPMC3_VERSION:+HepMC3/$HEPMC3_VERSION-$HEPMC3_REVISION} ${O2HLTCATRACKING_VERSION:+O2HLTCATracking/$O2HLTCATRACKING_VERSION-$O2HLTCATRACKING_REVISION} ${MONITORING_VERSION:+Monitoring/$MONITORING_VERSION-$MONITORING_REVISION} ${CONFIGURATION_VERSION:+Configuration/$CONFIGURATION_VERSION-$CONFIGURATION_REVISION}
 # Our environment
 setenv O2_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv VMCWORKDIR \$::env(O2_ROOT)/share