0b4d1bdef4c18c949ce0c600594f1064ea7defd9
Dario Berzano <dario.berzano@cern.ch>
2017-12-08 18:02:33 +0100

    Move latest AliPhysics to v5-09-20a

--- defaults-user.sh
+++ defaults-user.sh
@@ -7,14 +7,14 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-20
+    tag: v5-09-20a
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-20-01
+    tag: v5-09-20a-01
 ---
 # To be used with aliBuild option `--defaults user`.
 #
