30c55b900b571fca1953b9dea6ebe4f7f0768c0f
Luca Barioglio <luca.barioglio@cern.ch>
2017-12-15 12:42:26 +0100

    Fix Protobuf lookup for Configuration on macOS

--- configuration.sh
+++ configuration.sh
@@ -18,29 +18,33 @@ incremental_recipe: |
   make ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
-#!/bin/sh
+#!/bin/bash -e
 
+LIBEXT=so
 case $ARCHITECTURE in
-    osx*) [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost);;
+    osx*)
+      [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost)
+      [[ ! $PROTOBUF_ROOT ]] && PROTOBUF_ROOT=$(brew --prefix protobuf)
+      LIBEXT=dylib
+    ;;
 esac
 
-cmake $SOURCEDIR                                              \
-      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                     \
-      ${BOOST_ROOT:+-DBOOST_ROOT=$BOOST_ROOT}                 \
-      ${BOOST_ROOT:+-DBoost_DIR=$BOOST_ROOT}                  \
-      ${BOOST_ROOT:+-DBoost_INCLUDE_DIR=$BOOST_ROOT/include}  \
-      ${COMMON_O2_VERSION:+-DCommon_ROOT=$COMMON_O2_ROOT}     \
-      -DPROTOBUF_INCLUDE_DIR=${PROTOBUF_ROOT}/include         \
-      -DPROTOBUF_LIBRARY=${PROTOBUF_ROOT}/lib/libprotobuf.so  \
-      ${GRPC_ROOT:+-DGRPC_ROOT=${GRPC_ROOT}}                  \
-      -DRAPIDJSON_INCLUDEDIR=${RAPIDJSON_ROOT}/include        \
-      -DPPCONSUL_INCLUDE_DIRS=${PPCONSUL_ROOT}/include        \
-      -DPPCONSUL_LIBRARY_DIRS=${PPCONSUL_ROOT}/lib            \
+cmake $SOURCEDIR                                                    \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                           \
+      ${BOOST_ROOT:+-DBOOST_ROOT=$BOOST_ROOT}                       \
+      ${BOOST_ROOT:+-DBoost_DIR=$BOOST_ROOT}                        \
+      ${BOOST_ROOT:+-DBoost_INCLUDE_DIR=$BOOST_ROOT/include}        \
+      ${COMMON_O2_VERSION:+-DCommon_ROOT=$COMMON_O2_ROOT}           \
+      -DPROTOBUF_INCLUDE_DIR=${PROTOBUF_ROOT}/include               \
+      -DPROTOBUF_LIBRARY=${PROTOBUF_ROOT}/lib/libprotobuf.${LIBEXT} \
+      ${GRPC_ROOT:+-DGRPC_ROOT=${GRPC_ROOT}}                        \
+      -DRAPIDJSON_INCLUDEDIR=${RAPIDJSON_ROOT}/include              \
+      -DPPCONSUL_INCLUDE_DIRS=${PPCONSUL_ROOT}/include              \
+      -DPPCONSUL_LIBRARY_DIRS=${PPCONSUL_ROOT}/lib                  \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 make ${JOBS+-j $JOBS} install
 
-#ModuleFile
 mkdir -p etc/modulefiles
 cat > etc/modulefiles/$PKGNAME <<EoF
 #%Module1.0
@@ -51,12 +55,12 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0                                                          \\
-            ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION}            \\
+module load BASE/1.0 \\
+            ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION} \\
             ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
             ${PROTOBUF_VERSION:+protobuf/$PROTOBUF_VERSION-$PROTOBUF_REVISION} \\
-            ${GRPC_VERSION:+grpc/$GRPC_VERSION-$GRPC_REVISION}                \\
-            Ppconsul/$PPCONSUL_VERSION-$PPCONSUL_REVISION                     \\
+            ${GRPC_VERSION:+grpc/$GRPC_VERSION-$GRPC_REVISION} \\
+            Ppconsul/$PPCONSUL_VERSION-$PPCONSUL_REVISION \\
             Common-O2/$COMMON_O2_VERSION-$COMMON_O2_REVISION
 
 # Our environment
