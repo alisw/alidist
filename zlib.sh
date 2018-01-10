03a3bc82a67a6bb913e57ba41f777b8393248814
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-01-10 08:57:43 +0100

    Do not require gcc to pickup system version

--- zlib.sh
+++ zlib.sh
@@ -6,7 +6,7 @@ build_requires:
  - "GCC-Toolchain:(?!osx)"
 prefer_system: "(?!slc5)"
 prefer_system_check: |
-  printf "#include <zlib.h>\n" | gcc -xc++ - -c -M 2>&1
+  printf "#include <zlib.h>\n" | cc -xc++ - -c -M 2>&1
 ---
 #!/bin/sh
 
