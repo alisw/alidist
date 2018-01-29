ad0482ebe66d4d1adf17a48d7d4e1d2c55f338f6
Dario Berzano <dario.berzano@cern.ch>
2018-01-29 13:45:01 +0100

    Bump AliRoot to v5-09-22

--- defaults-prod-latest.sh
+++ defaults-prod-latest.sh
@@ -7,10 +7,10 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-21
+    tag: v5-09-22
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-21-01
+    tag: v5-09-22-01
 ---
 # To be used with aliBuild option `--defaults prod-latest`.
 #
--- defaults-user-root6.sh
+++ defaults-user-root6.sh
@@ -7,14 +7,14 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-21
+    tag: v5-09-22
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-21-01
+    tag: v5-09-22-01
   ROOT:
     version: "%(tag_basename)s"
     version: "%(tag_basename)s"
