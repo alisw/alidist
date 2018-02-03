8ffdbd5b57c1aede55573a2dc06b6f4988ed5718
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2018-02-04 01:04:40 +0200

    root.sh :: fftw explicit disable

--- root.sh
+++ root.sh
@@ -126,6 +126,7 @@ else
         -Drfio=OFF                                                                       \
         -Dodbc=OFF                                                                       \
         -Dsqlite=OFF                                                                     \
+        -Dfftw3=OFF                                                                      \
         -DCMAKE_PREFIX_PATH="$FREETYPE_ROOT;$SYS_OPENSSL_ROOT;$GSL_ROOT;$ALIEN_RUNTIME_ROOT;$PYTHON_ROOT;$PYTHON_MODULES_ROOT;$LIBPNG_ROOT;$LZMA_ROOT"
   FEATURES="builtin_pcre mathmore xml ssl opengl minuit2 http
             pythia6 roofit soversion vdt ${CXX11:+cxx11} ${CXX14:+cxx14} ${XROOTD_ROOT:+xrootd}
