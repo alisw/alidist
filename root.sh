1383343d804a3e5f99a3ecc5d1d8e89f7d58e22a
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2018-02-04 01:42:41 +0200

    root.sh :: dcache explicit disable

--- root.sh
+++ root.sh
@@ -128,6 +128,7 @@ else
         -Dodbc=OFF                                                                       \
         -Dsqlite=OFF                                                                     \
         -Dfftw3=OFF                                                                      \
+        -Ddcache=OFF                                                                     \
         ${ALIBUILD_ROOT_CMAKE_ADDOPTS:+${ALIBUILD_ROOT_CMAKE_ADDOPTS}}                   \
         -DCMAKE_PREFIX_PATH="$FREETYPE_ROOT;$SYS_OPENSSL_ROOT;$GSL_ROOT;$ALIEN_RUNTIME_ROOT;$PYTHON_ROOT;$PYTHON_MODULES_ROOT;$LIBPNG_ROOT;$LZMA_ROOT"
   FEATURES="builtin_pcre mathmore xml ssl opengl minuit2 http
