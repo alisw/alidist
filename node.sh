f2ec62c63fcf3fd11e25b64efee34ae0ac848644
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-04-26 11:11:38 +0200

    init node

---
+++ node.sh
@@ -0,0 +1,48 @@
+package: node
+version: "v8.11.1"
+build_requires:
+  - curl
+---
+#!/bin/bash
+
+case $ARCHITECTURE in
+	osx*)
+		NODEOS=darwin
+	;;
+	*x86-64)
+		NODEOS=linux
+	;;
+	*)
+		NODEOS=linux
+	;;
+esac
+
+mkdir ali-node-tmp
+pushd ali-node-tmp
+	TAR_NAME="$PKGNAME-$PKGVERSION-$NODEOS.tar.gz"
+	URL="https://nodejs.org/dist/$PKGVERSION/$TAR_NAME"	
+	curl -O -L $URL
+	tar zxf $TAR_NAME
+popd
+
+/bin/cp -a ./ali-node-tmp/go/* $INSTALLROOT/.
+/bin/rm -rf ali-node-tmp
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+setenv NODE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(NODE_ROOT)/bin
+EoF
