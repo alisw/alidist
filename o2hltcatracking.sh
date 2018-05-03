c790fa099a04c67f3143fd9781ba8b4412333bc2
sawenzel <sandro.wenzel@cern.ch>
2018-05-03 09:46:15 +0200

    Set O2HLTCATRACKING_ROOT environment variable
    This variable is references in other build recipes. We should set it here for consistency.

--- o2hltcatracking.sh
+++ o2hltcatracking.sh
@@ -30,6 +30,7 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies
 module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
 # Our environment
+setenv O2HLTCATRACKING_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
 $([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib")
 EoF
