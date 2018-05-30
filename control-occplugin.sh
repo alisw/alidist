252ef7e7fa07d2ab8eb4e812ecc6f14728f7dbdc
Teo Mrnjavac <teo.mrnjavac@cern.ch>
2018-05-30 16:48:08 +0200

    Add recipe for O² Control and Configuration Plugin
    Source directory occplugin in repository AliceO2Group/Control.

---
+++ control-occplugin.sh
@@ -0,0 +1,61 @@
+package: Control-OCCPlugin
+version: "%(commit_hash)s"
+tag:  master
+requires:
+  - FairRoot
+  - boost
+  - grpc
+  - protobuf
+  - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - CMake
+source: https://github.com/AliceO2Group/Control
+incremental_recipe: |
+  make ${JOBS+-j $JOBS} prefix=$INSTALLROOT
+  make prefix=$INSTALLROOT install
+  mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+---
+#!/bin/bash -e
+
+LIBEXT=so
+case $ARCHITECTURE in
+    osx*)
+      [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost)
+      LIBEXT=dylib
+    ;;
+esac
+
+cmake $SOURCEDIR/occplugin                                                               \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                                \
+      ${BOOST_ROOT:+-DBOOSTPATH=$BOOST_ROOT}                                             \
+      -DGRPCPATH=${GRPC_ROOT}                                                            \
+      -DPROTOBUFPATH=${PROTOBUF_ROOT}                                                    \
+      -DFAIRROOTPATH=${FAIRROOT_ROOT}
+
+make ${JOBS+-j $JOBS} prefix=$INSTALLROOT
+make prefix=$INSTALLROOT install
+
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 \\
+            ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION} \\
+            ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
+            ${PROTOBUF_VERSION:+protobuf/$PROTOBUF_VERSION-$PROTOBUF_REVISION} \\
+            ${FAIRROOT_VERSION:+FairRoot/$FAIRROOT_VERSION-$FAIRROOT_REVISION} \\
+            ${GRPC_VERSION:+grpc/$GRPC_VERSION-$GRPC_REVISION}
+
+# Our environment
+setenv Control_OCCPlugin_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(Control_OCCPlugin_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(Control_OCCPlugin_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib")
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
