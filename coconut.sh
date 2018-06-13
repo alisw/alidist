41d0cb52af2b6ef393b742ea4236835e8ac2d997
Teo Mrnjavac <teo.mrnjavac@cern.ch>
2018-06-13 09:06:30 +0200

    Add recipes for golang, Control and coconut (#1180)
    * Add recipes for golang, Control and coconut.
    
    * Create symlink after make install.
    
    * Add metapackage Control, old Control is now Control-Core.
    
    * Multiple fixes as requested by review.
    
    * Export GOPATH in recipes.
    
    * Minor fixes
    
    * Update Control-OCCPlugin for FairRoot -> FairMQ split

---
+++ coconut.sh
@@ -0,0 +1,40 @@
+package: coconut
+version: "%(commit_hash)s"
+tag: master
+build_requires:
+  - golang
+  - protobuf
+source: https://github.com/AliceO2Group/Control
+---
+#!/bin/bash -e
+
+export GOPATH=$PWD/go
+export PATH=$GOPATH/bin:$PATH
+BUILD=$GOPATH/src/github.com/AliceO2Group/Control
+mkdir -p $BUILD
+rsync -a --delete $SOURCEDIR/ $BUILD/
+pushd $BUILD
+  make WHAT="coconut" all
+  mkdir -p $INSTALLROOT/bin
+  rsync -a --delete bin/ $INSTALLROOT/bin
+popd
+
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+
+# Our environment
+set COCONUT_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$COCONUT_ROOT/bin
+prepend-path LD_LIBRARY_PATH \$COCONUT_ROOT/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$COCONUT_ROOT/lib")
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
