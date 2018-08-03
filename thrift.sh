f76067bb62e9813f66b05d5a2b15bc48839ed753
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-08-03 13:22:11 +0200

    Cleanup thrift recipe
    * Remove dependency of thrift on which
    * Keep linter happy

--- thrift.sh
+++ thrift.sh
@@ -1,10 +1,10 @@
 package: thrift
 version: "%(tag_basename)s"
-source: https://git-wip-us.apache.org/repos/asf/thrift.git
 tag: 0.9.3
+source: https://git-wip-us.apache.org/repos/asf/thrift.git
 build_requires:
  - "GCC-Toolchain:(?!osx)"
 system_requirement: ".*"
 system_requirement_check: |
-  which thrift
+  thrift --version
 ---
