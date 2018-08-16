84416e15e197375bc25567e3151e42269bc38d9d
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-08-16 11:26:28 +0200

    AliEN-CAs: Use e3aad5d commit of the CAs

--- alien-cas.sh
+++ alien-cas.sh
@@ -1,7 +1,7 @@
 package: AliEn-CAs
 version: v1
-tag: master
-source: https://github.com/alisw/alien-cas.git 
+tag: e3aad5d2bc34016abbec3f377ca5fb22fcccb044
+source: https://github.com/alisw/alien-cas.git
 ---
 #!/bin/bash -e
 DEST="$INSTALLROOT/globus/share/certificates"
@@ -10,3 +10,6 @@ for D in $SOURCEDIR/*; do
   [[ ! -d "$D" ]] && continue
   rsync -a "$D/" "$DEST/"
 done
+
+# No Modulefile is needed: the following line acknowledges it (and tests pass)
+#%Module
