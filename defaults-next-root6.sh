bb0d053a5597dfd3d4a109d3bdb11323234ae0ba
Dario Berzano <dario.berzano@cern.ch>
2018-08-29 21:43:23 +0200

    Fix ROOT 6 bump typo in next defaults

--- defaults-next-root6.sh
+++ defaults-next-root6.sh
@@ -17,7 +17,7 @@ overrides:
       which gfortran || { echo "gfortran missing"; exit 1; }
       which cc && test -f $(dirname $(which cc))/c++ && printf "#define GCCVER ((__GNUC__ << 16)+(__GNUC_MINOR__ << 8)+(__GNUC_PATCHLEVEL__))\n#if (GCCVER < 0x070300)\n#error \"System's GCC cannot be used: we need at least GCC 7.X. We are going to compile our own version.\"\n#endif\n" | cc -xc++ - -c -o /dev/null
   ROOT:
-    tag: "v6-04-14"
+    tag: "v6-14-04"
     source: https://github.com/root-mirror/root
     requires:
       - AliEn-Runtime:(?!.*ppc64)
