5f074f74dda6cc3b1c4772f506839668378c107d
Dario Berzano <dario.berzano@gmail.com>
2018-09-25 22:49:24 +0200

    Add ROOT_INCLUDE_PATH to GEANT3 (#1301)
    Same rationale as #1300

--- geant3.sh
+++ geant3.sh
@@ -8,8 +8,9 @@ build_requires:
   - "Xcode:(osx.*)"
 source: https://github.com/vmc-project/geant3
 prepend_path:
-  "LD_LIBRARY_PATH": "$GEANT3_ROOT/lib64"
-  "DYLD_LIBRARY_PATH": "$GEANT3_ROOT/lib64"
+  LD_LIBRARY_PATH: "$GEANT3_ROOT/lib64"
+  DYLD_LIBRARY_PATH: "$GEANT3_ROOT/lib64"
+  ROOT_INCLUDE_PATH: "$GEANT3_ROOT/include/TGeant3"
 ---
 #!/bin/bash -e
 cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT   \
