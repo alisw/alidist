d532906d5a5c471743fa9ffbd4c63233e1fa35ce
Dario Berzano <dario.berzano@cern.ch>
2018-10-02 12:35:05 +0200

    Bump AliRoot to v5-09-38

--- defaults-prod-latest.sh
+++ defaults-prod-latest.sh
@@ -7,10 +7,10 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-37
+    tag: v5-09-38
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-37-01
+    tag: v5-09-38-01
 ---
 # To be used with aliBuild option `--defaults prod-latest`.
 #
