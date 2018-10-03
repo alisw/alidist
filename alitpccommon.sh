05f7e7b72734ba50a1cbde0e90e7f96d5b6fc50c
David Rohr <drohr@jwdt.org>
2018-10-03 13:25:44 +0200

    Bump AliTPCCommon to alitpccommon-v1.4.5

--- alitpccommon.sh
+++ alitpccommon.sh
@@ -1,6 +1,6 @@
 package: AliTPCCommon
 version: "%(tag_basename)s"
-tag: alitpccommon-v1.4.2
+tag: alitpccommon-v1.4.5
 source: https://github.com/AliceO2Group/AliTPCCommon
 build_requires:
   - CMake
