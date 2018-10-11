6d1b323a33cc5b973e20c9cfd33cb67e5a53e33d
Teo Mrnjavac <teo.mrnjavac@cern.ch>
2018-10-11 11:30:56 +0200

    Improve gRPC: faster clone, fix symlink (#1328)
    * Fix symlink creation in gRPC recipe through a regexp
    * Do not copy the whole source; use `git clone --reference` instead

--- grpc.sh
+++ grpc.sh
@@ -12,23 +12,44 @@ incremental_recipe: |
   make ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
-#!/bin/sh
+#!/bin/bash -e
 
-rsync -a $SOURCEDIR/ ./
+if [[ $PKGVERSION =~ ^v((.+)(-alice[0-9]+)|(.+))$ ]]; then
+  # Define a variable with the version number minus the `-alice` suffix.
+  # Works also with non-ALICE versions
+  REALVERSION=${BASH_REMATCH[2]}
+  [[ $REALVERSION ]] || REALVERSION=${BASH_REMATCH[4]}
+else
+  echo "Invalid version number: $PKGVERSION"
+  exit 1
+fi
 
-git submodule update --init # aie aie aie
+# We need the sources here; do not copy everything by using `--reference` wisely
+git clone "$SOURCE0" --reference "$SOURCEDIR" .
+git checkout "$GIT_TAG"
+git submodule update --init
 
-# gRPC has a custom Makefile which readily breaks with some environment vars, better to run it in a clean environment
-env -i HOME="$HOME" LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" PATH="$PATH" USER="$USER" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" make ${JOBS:+-j$JOBS} prefix=$INSTALLROOT
+# gRPC has a custom Makefile which readily breaks with some environment vars,
+# better to run it in a clean environment
+env -i HOME="$HOME"                             \
+       LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" \
+       PATH="$PATH"                             \
+       USER="$USER"                             \
+       LD_LIBRARY_PATH="$LD_LIBRARY_PATH"       \
+       make ${JOBS:+-j$JOBS} prefix=$INSTALLROOT
 
-# ldconfig won't work if we're not running make install as root, and that's ok, we don't need it
+# ldconfig won't work if we're not running make install as root, and that's ok,
+# we don't need it
 sed -i 's/ldconfig/true/' ./Makefile
 make prefix=$INSTALLROOT install
 
-# Add missing symlink. Must be relative, because the directory is moved around after the build.
-# This should normally not be necessary, but it is made necessary by the issues linked in the following upstream pull request: https://github.com/grpc/grpc/pull/13500
-# Once the issue gets fixed, it should be safe to remove this workaround.
-ln -s libgrpc++.so.${PKGVERSION#v} $INSTALLROOT/lib/libgrpc++.so.1
+# Add missing symlink. Must be relative, because the directory is moved around
+# after the build.  This should normally not be necessary, but it is made
+# necessary by the issues linked in the following upstream pull request:
+# https://github.com/grpc/grpc/pull/13500 Once the issue gets fixed, it should
+# be safe to remove this workaround.
+[[ -e $INSTALLROOT/lib/libgrpc++.so.${REALVERSION} ]] || { echo "Library libgrpc++.so.${REALVERSION} not found"; exit 1; }
+ln -nfs libgrpc++.so.${REALVERSION} $INSTALLROOT/lib/libgrpc++.so.1
 
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
