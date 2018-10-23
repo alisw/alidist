854f66b5735bb707cb04ca5533a0b18ada9be28d
Dario Berzano <dario.berzano@gmail.com>
2018-10-23 10:50:26 +0200

    Bump AliRoot to v5-09-40 (#1354)

--- defaults-user.sh
+++ defaults-user.sh
@@ -7,14 +7,14 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-39
+    tag: v5-09-40
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-39-01
+    tag: v5-09-40-01
 ---
 # To be used with aliBuild option `--defaults user`.
 #
