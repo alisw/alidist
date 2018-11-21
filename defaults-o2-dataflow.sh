37e7ac67a8c011206463ea88ba0577199557a9d4
Dario Berzano <dario.berzano@gmail.com>
2018-11-21 16:40:17 +0100

    Add O2Suite and tag O2 (#1403)
    * The O2Suite metapackage builds the whole O2 software components. It will be
      also used for testing the Pull Requests more accurately
    * O2 has been bumped to 1.0.0. All software built with the `o2` default will
      use O2 1.0.0 as a reference. `o2-dataflow`, `o2-ninja`, `o2-dev-fairroot`
      will instead use `dev`

--- defaults-o2-dataflow.sh
+++ defaults-o2-dataflow.sh
@@ -64,6 +64,7 @@ overrides:
       which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-9].*|3.10.*) exit 1 ;; esac
   O2:
     version: "%(short_hash)s%(defaults_upper)s"
+    tag: dev
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
