5d73318e5cd013d984014b4226c272fa29526112
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2018-12-04 09:52:18 +0100

    Update readoutcard.sh

--- readoutcard.sh
+++ readoutcard.sh
@@ -18,6 +18,7 @@ incremental_recipe: |
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
 #!/bin/bash -ex
+
 case $ARCHITECTURE in
     osx*) [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost);;
 esac
