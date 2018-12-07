da60977a38aa8abeae0467a7c685177d97dd1ef1
Laurent Aphecetche <laurent.aphecetche@cern.ch>
2018-12-07 09:07:51 +0100

    Add dependency on yaml-cpp (#1429)

--- alo-aliroot.sh
+++ alo-aliroot.sh
@@ -6,6 +6,7 @@ requires:
   - AliRoot
   - RapidJSON
   - boost
+  - yaml-cpp
 build_requires:
   - CMake
   - flatbuffers
@@ -39,6 +40,7 @@ cmake $SOURCEDIR \
     -DRAPIDJSON_INCLUDEDIR="$RAPIDJSON_ROOT/include" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DMS_GSL_INCLUDE_DIR=$MS_GSL_ROOT/include \
+    -DYAMLCPP=$YAMLCPP_ROOT \
     ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}
 
 cmake --build . -- ${JOBS+-j $JOBS} install
@@ -68,7 +70,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 AliRoot/$ALIROOT_VERSION-$ALIROOT_REVISION ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION}
+module load BASE/1.0 AliRoot/$ALIROOT_VERSION-$ALIROOT_REVISION ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION} yaml-cpp
 # Our environment
 setenv ALO_ALIROOT_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 
