190488640c171a478e57f726c1dc983b17b086b3
Dario Berzano <dario.berzano@gmail.com>
2019-01-15 23:35:27 +0100

    Bump AliRoot to v5-09-45 (#1465)

--- defaults-user-root6.sh
+++ defaults-user-root6.sh
@@ -10,14 +10,14 @@ disable:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-44a
+    tag: v5-09-45
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-44a-01
+    tag: v5-09-45-01
   ROOT:
     tag: "v6-10-08"
     requires:
