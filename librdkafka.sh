4362ae17f124ffbfa9d95d14fc8ec8a3d795efa9
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2019-02-07 13:58:18 +0100

    Add librdkafka recipe

---
+++ librdkafka.sh
@@ -0,0 +1,34 @@
+package: librdkafka
+version: "%(tag_basename)s"
+tag: v1.0.0-RC6
+requires:
+  - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - CMake
+source: https://github.com/edenhill/librdkafka
+---
+#!/bin/bash -ex
+cmake -H$SOURCEDIR 				\
+      -B$SOURCEDIR/_cmake_build			\
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT 	\
+      -DCMAKE_INSTALL_LIBDIR=lib
+
+cmake --build $SOURCEDIR/_cmake_build --target install
+
+#ModuleFile
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+
+# Our environment
+prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
