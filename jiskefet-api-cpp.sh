9a50b77ad600cb1211ba40ab0f3b04b942b58291
Pascal Boeschoten <passiepascal@gmail.com>
2019-03-08 16:25:35 +0100

    Fix openssl for mac in jiskefet-api-cpp

--- jiskefet-api-cpp.sh
+++ jiskefet-api-cpp.sh
@@ -1,11 +1,11 @@
 package: Jiskefet-Api-Cpp
 version: "%(tag_basename)s"
-tag: v0.2.2
+tag: v0.2.3
 requires:
   - boost
   - "GCC-Toolchain:(?!osx)"
   - cpprestsdk
-  - OpenSSL
+  - OpenSSL:(?!osx)
 build_requires:
   - CMake
 source: https://github.com/PascalBoeschoten/jiskefet-api-cpp
@@ -16,12 +16,15 @@ incremental_recipe: |
 #!/bin/bash -ex
 
 case $ARCHITECTURE in
-    osx*) [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost);;
+    osx*) [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost)
+          [[ $OPENSSL_ROOT ]] || OPENSSL_ROOT=$(brew --prefix openssl)
+          ;;
 esac
 
 cmake $SOURCEDIR                                    \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT           \
       ${BOOST_VERSION:+-DBOOST_ROOT=$BOOST_ROOT}    \
+      ${OPENSSL_ROOT:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
       -DCPPREST_ROOT=${CPPRESTSDK_ROOT}          \
       -DCPPREST_LIB=${CPPRESTSDK_ROOT}/lib64/libcpprest.so \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
@@ -42,6 +45,7 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION
 module load BASE/1.0                                                          \\
             ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION}            \\
+            ${OPENSSL_VERSION:+OpenSSL/$OPENSSL_VERSION-$OPENSSL_REVISION}            \\
             ${CPPRESTSDK_VERSION:+cpprestsdk/$CPPRESTSDK_VERSION-$CPPRESTSDK_REVISION} \\
             ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
 
