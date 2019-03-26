ae8109a347025e8a7a4f6b07a0cd7423d59f0927
Dario Berzano <dario.berzano@cern.ch>
2019-03-26 09:55:17 +0100

    Improve AliEn-ROOT-Legacy
    * Use generic `cmake` command supporting generators different than GNU Make
    * Do not rely on version number (not available in development mode) for checking
      the ROOT version but use the presence of `rootcling`

--- alien-root-legacy.sh
+++ alien-root-legacy.sh
@@ -12,19 +12,24 @@ append_path:
   ROOT_PLUGIN_PATH: "$ALIEN_ROOT_LEGACY_ROOT/etc/plugins"
 ---
 #!/bin/bash -e
-case $ARCHITECTURE in
-  osx*) [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT=$(brew --prefix openssl) ;;
-esac
+
+if [[ $ARCHITECTURE == osx* && ! $OPENSSL_ROOT ]]; then
+  OPENSSL_ROOT=$(brew --prefix openssl)
+fi
+
+# Determine whether we are building for ROOT 5 or ROOT 6+
+[[ -x "$ROOTSYS/bin/rootcling" ]] && ROOT_MAJOR="v6-00-00" || ROOT_MAJOR="v5-00-00"
+
 cmake $SOURCEDIR                                         \
+      ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"}          \
       -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"              \
       -DROOTSYS="$ROOTSYS"                               \
        ${OPENSSL_ROOT:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
       -DALIEN_DIR="$XALIENFS_ROOT"                       \
-      -DROOT_VERSION="$ROOT_VERSION"
+      -DROOT_VERSION="$ROOT_MAJOR"
 
+cmake --build . --target install ${JOBS:+-- -j$JOBS}
 
-
-make ${JOBS:+-j $JOBS} install
 for RPKG in $BUILD_REQUIRES; do
   RPKG_UP=$(echo $RPKG|tr '[:lower:]' '[:upper:]'|tr '-' '_')
   RPKG_ROOT=$(eval echo "\$${RPKG_UP}_ROOT")
@@ -34,7 +39,6 @@ for RPKG in $BUILD_REQUIRES; do
   popd
   rm -f $INSTALLROOT/etc/modulefiles/{$RPKG,$RPKG.unrelocated} || true
 done
-
 
 # Modulefile
 mkdir -p etc/modulefiles
