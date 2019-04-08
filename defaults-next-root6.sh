403b787e61c9a1953be65436957a78150d45a1af
Dario Berzano <dario.berzano@cern.ch>
2019-04-08 16:28:36 +0200

    Bump AliRoot to v5-09-47

--- defaults-next-root6.sh
+++ defaults-next-root6.sh
@@ -9,8 +9,8 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s_ROOT6"
-    tag: v5-09-46
+    tag: v5-09-47
   AliPhysics:
     version: "%(tag_basename)s_ROOT6"
-    tag: v5-09-46-01
+    tag: v5-09-47-01
 ---
