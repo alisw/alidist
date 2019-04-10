083fcb480e864a3393aae5ff9682c794e1c4e1f0
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2019-04-10 14:23:18 +0200

    Polish ofi recipe

--- ofi.sh
+++ ofi.sh
@@ -3,11 +3,8 @@ version: "1.6.0"
 system_requirement_missing: |
   libfabric and its development package are missing from your system.
    * RHEL-compatible systems: you will probably need "libfabric" and "libfabric-devel" packages.
-   * Ubuntu-compatible systems: you will probably need "libfabric-bin" and "libfabric-devel".
+   * Ubuntu-compatible systems: you will probably need "libfabric-bin" and "libfabric-dev".
 system_requirement: ".*"
 system_requirement_check: |
-  pkg-config --atleast-version=1.6.0 libfabric 2>&1
-  if [ $? -ne 0 ]; then exit 1; fi
-  printf "#include \"rdma/fabric.h\"\nint main(){}" | gcc -xc - -o /dev/null
-  if [ $? -ne 0 ]; then exit 1; fi
+  pkg-config --atleast-version=1.6.0 libfabric 2>&1 && printf "#include \"rdma/fabric.h\"\nint main(){}" | gcc -xc - -o /dev/null
 ---
