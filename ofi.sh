60a62777f35441a73a768ae16b37a17ac738befe
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-04-29 21:23:19 +0200

    Build own version of OFI if not found on the system (#1603)

--- ofi.sh
+++ ofi.sh
@@ -1,10 +1,34 @@
 package: ofi
-version: "1.6.0"
-system_requirement_missing: |
-  libfabric and its development package are missing from your system.
-   * RHEL-compatible systems: you will probably need "libfabric" and "libfabric-devel" packages.
-   * Ubuntu-compatible systems: you will probably need "libfabric-bin" and "libfabric-dev".
-system_requirement: ".*"
-system_requirement_check: |
-  pkg-config --atleast-version=1.6.0 libfabric 2>&1 && printf "#include \"rdma/fabric.h\"\nint main(){}" | gcc -xc - -o /dev/null
+version: "%(tag_basename)s"
+tag: "v1.7.1"
+source: https://github.com/ofiwg/libfabric
+build_requires:
+  - autotools
+prefer_system: ".*"
+prefer_system_check: |
+  pkg-config --atleast-version=1.6.0 libfabric 2>&1 && printf "#include \"rdma/fabric.h\"\nint main(){}" | c++ -xc - -o /dev/null
 ---
+rsync -a --exclude='**/.git' --delete --delete-excluded "$SOURCEDIR/" ./
+autoreconf -ivf
+./configure --prefix="$INSTALLROOT" --enable-mlx=no 
+make ${JOBS:+-j $JOBS} install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+set OFI_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$OFI_ROOT/bin
+prepend-path LD_LIBRARY_PATH \$OFI_ROOT/lib
+EoF
