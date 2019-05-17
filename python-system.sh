caabf2919327f06f5537ab14a97b7632a90f0ac9
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-05-17 11:33:16 +0200

    Update python-system.sh

--- python-system.sh
+++ python-system.sh
@@ -3,8 +3,8 @@ version: "3.6"
 system_requirement_missing: |
   Python 3.6 is missing from your system. We need the python3 and pip3 executables in the PATH.
   It can be installed with:
-   * apt-get install python3 python3-pip python3-tk
-   * brew install python3
+   * On Ubuntu: sudo apt-get install python3 python3-pip python3-tk
+   * On macOS: brew install python3
 system_requirement: ".*"
 system_requirement_check: |
   python3 -c 'import sys; import sqlite3; sys.exit(1 if sys.version_info < (3,6) else 0)' && pip3 help
