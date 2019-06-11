ccd551e97c7a64b5d1c1c63588d7a732474aeee0
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-06-11 14:04:47 +0200

    Add system requirement on make

---
+++ make.sh
@@ -0,0 +1,11 @@
+package: make
+version: 4
+system_requirement_missing: |
+  Please install make package on your system:
+    * On RHEL-compatible systems: make
+    * On Ubuntu-compatible systems: make
+system_requirement: ".*"
+system_requirement_check: |
+  type make
+---
+
