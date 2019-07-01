57590b27bc136ad108bef2f4aa82f8b2954ef27d
Teo Mrnjavac <teo.m@cern.ch>
2019-07-01 22:14:33 +0200

    Improve OpenSSL support (especially on macOS)
    Updated Homebrew OpenSSL installation broke compilation of gRPC and Control related package.
    This is done by doing the following.
    
    * gRPC: Allow building with homebrew provided packages
    * Find OpenSSL in brew when building OCC on Mac
    * Bump AliECS to v0.8.4

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v0.8.3"
+tag: "v0.8.4"
 build_requires:
   - golang
   - protobuf
--- grpc.sh
+++ grpc.sh
@@ -22,16 +22,24 @@ git checkout "$GIT_TAG"
 git submodule update --init
 popd
 
-cmake $SOURCEDIR                        \
-  -DCMAKE_INSTALL_PREFIX=$INSTALLROOT   \
-  -DgRPC_PROTOBUF_PACKAGE_TYPE="CONFIG" \
-  -DgRPC_BUILD_TESTS=OFF                \
-  -DBUILD_SHARED_LIBS=ON                \
-  -DgRPC_SSL_PROVIDER=package           \
-  -DgRPC_ZLIB_PROVIDER=package          \
-  -DgRPC_GFLAGS_PROVIDER=packet         \
-  -DgRPC_PROTOBUF_PROVIDER=package      \
-  -DgRPC_BENCHMARK_PROVIDER=packet      \
+case $ARCHITECTURE in
+  osx*) 
+    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+    [[ ! $PROTOBUF_ROOT ]] && PROTOBUF_ROOT=$(brew --prefix protobuf)
+  ;;
+esac
+
+cmake $SOURCEDIR                                    \
+  -DCMAKE_INSTALL_PREFIX=$INSTALLROOT               \
+  -DgRPC_PROTOBUF_PACKAGE_TYPE="CONFIG"             \
+  -DgRPC_BUILD_TESTS=OFF                            \
+  -DBUILD_SHARED_LIBS=ON                            \
+  -DgRPC_SSL_PROVIDER=package                       \
+  -DgRPC_ZLIB_PROVIDER=package                      \
+  -DgRPC_GFLAGS_PROVIDER=packet                     \
+  -DgRPC_PROTOBUF_PROVIDER=package                  \
+  -DgRPC_BENCHMARK_PROVIDER=packet                  \
+  ${OPENSSL_ROOT_DIR:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR} \
   -DgRPC_CARES_PROVIDER=package
 
 make ${JOBS:+-j$JOBS} install
