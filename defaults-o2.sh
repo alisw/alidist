47a8e4afc44a9dda06c970cc80ee8b658ee54b36
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-07-17 10:28:32 +0200

    Do not bump for now.
    Given there is issues with the 6-18-00, we do not bump but simply add all the preliminary work.

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -28,7 +28,7 @@ overrides:
   AliPhysics:
     version: "%(commit_hash)s_O2"
   ROOT:
-    tag: "v6-18-00"
+    tag: "v6-16-00"
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
