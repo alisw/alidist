4fea381acdd3e5220329caf320970affc6522c7b
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-07-22 17:03:32 +0200

    Misc fixes

--- glfw.sh
+++ glfw.sh
@@ -5,13 +5,11 @@ build_requires:
   - CMake
   - "GCC-Toolchain:(?!osx)"
 ---
-#!/bin/sh
-
 cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
-  ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"} \
-  -DBUILD_SHARED_LIBS=ON \
-  -DGLFW_BUILD_EXAMPLES=OFF \
-  -DGLFW_BUILD_TESTS=OFF \
+  ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"}          \
+  -DBUILD_SHARED_LIBS=ON                             \
+  -DGLFW_BUILD_EXAMPLES=OFF                          \
+  -DGLFW_BUILD_TESTS=OFF                             \
   -DGLFW_BUILD_DOCS=OFF
 
 cmake --build . -- ${JOBS+-j $JOBS} install
@@ -30,7 +28,5 @@ set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
 module load BASE/1.0
-setenv GLFW_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib")
 EoF
