6f9be0b81f5814a07047e4128c9770ab648faf28
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-07-24 14:37:27 +0200

    Add recipe for GLFW (#1684)

---
+++ glfw.sh
@@ -0,0 +1,32 @@
+package: GLFW
+version: "3.3"
+source: https://github.com/glfw/glfw.git
+build_requires:
+  - CMake
+  - "GCC-Toolchain:(?!osx)"
+---
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
+  ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"}          \
+  -DBUILD_SHARED_LIBS=ON                             \
+  -DGLFW_BUILD_EXAMPLES=OFF                          \
+  -DGLFW_BUILD_TESTS=OFF                             \
+  -DGLFW_BUILD_DOCS=OFF
+
+cmake --build . -- ${JOBS+-j $JOBS} install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat >"$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
+EoF
