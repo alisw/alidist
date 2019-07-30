2d5c49a30a00512a962f3433b5afab2a2686be6c
Nikola Hardi <nikola.hardi@cern.ch>
2019-07-30 13:52:55 +0200

    Add the Module file for AGU

--- alice-grid-utils.sh
+++ alice-grid-utils.sh
@@ -8,3 +8,16 @@ source: https://gitlab.cern.ch/nhardi/alice-grid-utils.git
 DST="$INSTALLROOT/include"
 mkdir -p "$DST"
 cp -v $SOURCEDIR/*.h "$DST/"
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
