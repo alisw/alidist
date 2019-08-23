15f086cd62cf5def3c6c48d9acdbfb6276d288fc
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2019-08-23 10:50:16 +0200

    Enforce no warning code in Readout (#1754)

--- readout.sh
+++ readout.sh
@@ -30,6 +30,11 @@ case $ARCHITECTURE in
     ;;
 esac
 
+# Enforce no warning code in the PR checker
+if [[ $ALIBUILD_O2_TESTS ]]; then
+  CXXFLAGS="${CXXFLAGS} -Werror -Wno-error=deprecated-declarations"
+fi
+
 cmake $SOURCEDIR                                                         \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                \
       ${BOOST_VERSION:+-DBOOST_ROOT=$BOOST_ROOT}                         \
