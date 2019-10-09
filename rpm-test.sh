9b58ffcda01857d00d88fb0cf2bc532c5c3a937c
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-10-09 15:49:29 +0200

    RPM package for testing

---
+++ rpm-test.sh
@@ -0,0 +1,22 @@
+package: rpm-test
+version: "1.0"
+---
+# A really empty package to test RPM creation
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
+EoF
