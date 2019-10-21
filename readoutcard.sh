9401f90a7760a2b4d459d6584fd74eac4259501f
Kostas Alexopoulos <kostorr.alex@gmail.com>
2019-10-21 15:05:51 +0200

    Remove DYLD line

--- readoutcard.sh
+++ readoutcard.sh
@@ -67,7 +67,6 @@ setenv READOUTCARD_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$::env(READOUTCARD_ROOT)/bin
 prepend-path LD_LIBRARY_PATH \$::env(READOUTCARD_ROOT)/lib
 prepend-path PYTHONPATH \$::env(READOUTCARD_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(READOUTCARD_ROOT)/lib")
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 
