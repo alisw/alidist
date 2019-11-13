b59791b1eff71c60c373b6a87d58c6d674ab50ca
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-11-13 11:28:18 +0100

    RapidJSON: do not expose bogus runtime paths (#1878)
    RapidJSON is actually headers only. Proper fix would actually be to use RapidJSON as `build_requires` only.

--- rapidjson.sh
+++ rapidjson.sh
@@ -26,7 +26,4 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies
 module load BASE/1.0
 # Our environment
-prepend-path PATH \$::env(BASEDIR)/$PKGNAME/\$version/bin
-prepend-path LD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(BASEDIR)/$PKGNAME/\$version/lib")
 EoF
