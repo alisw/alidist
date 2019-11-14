a838142ca0844527242e10c855b2c6e1025a7070
Sylvain Chapeland <o2.syc@cern.ch>
2019-11-14 14:08:45 +0100

    osx

--- infologger.sh
+++ infologger.sh
@@ -52,6 +52,5 @@ module load BASE/1.0                                                          \\
 setenv INFOLOGGER_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$::env(INFOLOGGER_ROOT)/bin
 prepend-path LD_LIBRARY_PATH \$::env(INFOLOGGER_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(INFOLOGGER_ROOT)/lib")
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
