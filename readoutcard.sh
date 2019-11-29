d34341611fb87548f67cb5376c48f919c7188489
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2019-11-29 16:20:46 +0100

    fix for the fix

--- readoutcard.sh
+++ readoutcard.sh
@@ -64,10 +64,6 @@ module load BASE/1.0                                                          \\
 
 # Our environment
 set READOUTCARD_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path PATH \$READOUT_ROOT/bin
-prepend-path LD_LIBRARY_PATH \$READOUT_ROOT/lib
-
-setenv READOUTCARD_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$READOUTCARD_ROOT/bin
 prepend-path LD_LIBRARY_PATH \$READOUTCARD_ROOT/lib
 prepend-path PYTHONPATH \$READOUTCARD_ROOT/lib
