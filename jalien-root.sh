586f00e5b37f5b2c6c35397c078e6fe9e7177b3a
Nikola Hardi <nikola.hardi@cern.ch>
2019-12-05 08:22:54 +0100

    Fix the module loading

--- jalien-root.sh
+++ jalien-root.sh
@@ -43,9 +43,9 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
+module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
                      ROOT/${ROOT_VERSION}-${ROOT_REVISION}                                                  \\
-                     ${XJALIENFS_VERSION:+xjalienfs/$XJALIENFS_VERSION-$XJALIENFS_REVISION}
+                     ${XJALIENFS_REVISION:+xjalienfs/$XJALIENFS_VERSION-$XJALIENFS_REVISION}
 
 # Our environment
 set JALIEN_ROOT_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
