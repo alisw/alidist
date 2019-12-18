26e3d9c67e1c61c40ae00591b424f4a43901197d
Nikola Hardi <nhardi@cern.ch>
2019-12-18 13:31:52 +0100

    Fix typo and add quotes in the ApMon-CPP dependencies

--- apmon-cpp.sh
+++ apmon-cpp.sh
@@ -3,7 +3,7 @@ version: "%(tag_basename)s"
 tag: v2.2.8-alice2
 source: https://github.com/alisw/apmon-cpp.git
 build_requires:
- - libtirpc:(!?osx)
+ - "libtirpc:(?!osx)"
  - autotools
  - "GCC-Toolchain:(?!osx)"
 ---
