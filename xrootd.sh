a661b63594e204b6d17b089bde7cc6c567b1f977
Nikola Hardi <nikola.hardi@cern.ch>
2020-01-01 10:21:08 +0100

    XRootD sysroot discovery on the Mac (#1945)
    * Disable manual sysroot discovery on the Mac
    * A more robust sysroot discovery for XRootD py bindings on MacOS
    * Use export CFLAGS aproach
    * Switch back to the upstream XRootD repository
    * Lookup XRootD python path only when building Python bindings

--- xrootd.sh
+++ xrootd.sh
@@ -17,13 +17,14 @@ build_requires:
 [[ -e $SOURCEDIR/bindings ]] && XROOTD_V4=True && XROOTD_PYTHON=True || XROOTD_PYTHON=False
 PYTHON_EXECUTABLE=$( $(realpath $(which python3)) -c 'import sys; print(sys.executable)')
 
-SONAME="so"
 case $ARCHITECTURE in
   osx*)
     [[ $OPENSSL_ROOT ]] || OPENSSL_ROOT=$(brew --prefix openssl)
-    MACOS_SYSROOT="$(find `xcode-select -p` -type d -path *usr/include/c++)"
-    PYTHON_EXECUTABLE="CFLAGS=\"${MACOS_SYSROOT}\" ${PYTHON_EXECUTABLE}"
-    SONAME="dylib"
+
+    # NOTE: Python from Homebrew will have a hardcoded sysroot pointing to Xcode.app directory wchich might not exist.
+    # This seems to be a robust way to discover a working SDK path and present it to Python setuptools.
+    # This fix is needed only on MacOS when building XRootD Python bindings.
+    export CFLAGS="${CFLAGS} -isysroot $(xcrun --show-sdk-path)"
     unset UUID_ROOT
   ;;
 esac
@@ -36,7 +37,7 @@ cmake "$SOURCEDIR"                                                    \
       -DENABLE_PERL=OFF                                               \
       ${XROOTD_PYTHON:+-DENABLE_PYTHON=ON}                            \
       ${XROOTD_PYTHON:+-DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE}        \
-      ${UUID_ROOT:+-DUUID_LIBRARIES=$UUID_ROOT/lib/libuuid.$SONAME}   \
+      ${UUID_ROOT:+-DUUID_LIBRARIES=$UUID_ROOT/lib/libuuid.so}        \
       ${UUID_ROOT:+-DUUID_INCLUDE_DIRS=$UUID_ROOT/include}            \
       -DENABLE_KRB5=OFF                                               \
       -DENABLE_READLINE=OFF                                           \
@@ -46,6 +47,13 @@ cmake "$SOURCEDIR"                                                    \
       -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-Wno-error"
 
 cmake --build . -- ${JOBS:+-j$JOBS} install
+
+if [[ x"$XROOTD_PYTHON" == x"True" ]];
+then
+  pushd $INSTALLROOT
+  XRD_PYTHON_PATH="$(find . -path '*site-packages' -type d)"
+popd
+fi
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -71,7 +79,7 @@ set XROOTD_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$XROOTD_ROOT/bin
 prepend-path LD_LIBRARY_PATH \$XROOTD_ROOT/lib
 if { $XROOTD_PYTHON } {
-  prepend-path PYTHONPATH \$XROOTD_ROOT/lib/python3.6/site-packages
+  prepend-path PYTHONPATH \${XROOTD_ROOT}/${XRD_PYTHON_PATH}
   module load ${PYTHON_REVISION:+Python/$PYTHON_VERSION-$PYTHON_REVISION}                                 \\
               ${PYTHON_MODULES_REVISION:+Python-modules/$PYTHON_MODULES_VERSION-$PYTHON_MODULES_REVISION}
 }
