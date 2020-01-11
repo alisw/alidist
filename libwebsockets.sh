7063b3a28deac0ef04c2061218fb7e4ad81cbbd6
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-01-11 17:45:36 +0100

    Allow libwebsocket from homebrew (#1988)

--- libwebsockets.sh
+++ libwebsockets.sh
@@ -7,6 +7,9 @@ build_requires:
   - "GCC-Toolchain:(?!osx)"
   - "OpenSSL:(?!osx)"
   - zlib
+prefer_system: "osx"
+prefer_system_check: |
+  printf '#if !__has_include(<lws_config.h>)\n#error \"Cannot find libwebsocket\"#endif\nint main(){}' | c++ -I$(brew --prefix libwebsockets)/include -xc++ -std=c++17 - -o /dev/null
 ---
 #!/bin/bash -e
 case $ARCHITECTURE in
@@ -38,11 +41,11 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${OPENSSL_VERSION:+OpenSSL/$OPENSSL_VERSION-$OPENSSL_REVISION}
+module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
+                     ${OPENSSL_REVISION:+OpenSSL/$OPENSSL_VERSION-$OPENSSL_REVISION}
 # Our environment
-setenv LIBWEBSOCKETS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path PATH \$::env(LIBWEBSOCKETS_ROOT)/bin
-prepend-path LD_LIBRARY_PATH \$::env(LIBWEBSOCKETS_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(LIBWEBSOCKETS_ROOT)/lib")
+set LIBWEBSOCKETS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$LIBWEBSOCKETS_ROOT/bin
+prepend-path LD_LIBRARY_PATH \$LIBWEBSOCKETS_ROOT/lib
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
