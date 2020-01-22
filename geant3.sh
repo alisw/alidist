aac341da4b3f80183a8d09003fc09fc15040fc55
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-01-22 14:58:01 +0100

    Explicitely set CXXSTD for Geant3 (#2015)
    Just a consistency thing as otherwise
    the cmake might not pick it up correctly.

--- geant3.sh
+++ geant3.sh
@@ -12,8 +12,9 @@ prepend_path:
   ROOT_INCLUDE_PATH: "$GEANT3_ROOT/include/TGeant3"
 ---
 #!/bin/bash -e
-cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT   \
-                 -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE  \
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT      \
+                 -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE     \
+                 ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}  \
                  -DCMAKE_SKIP_RPATH=TRUE
 make ${JOBS:+-j $JOBS} install
 
