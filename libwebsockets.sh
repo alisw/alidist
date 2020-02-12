26497c8bec71ca057a7e40cd9b218dc80b0f6ade
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-02-12 16:28:26 +0100

    Keep linter happy

--- libwebsockets.sh
+++ libwebsockets.sh
@@ -44,9 +44,8 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies
 module load BASE/1.0 ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${OPENSSL_VERSION:+OpenSSL/$OPENSSL_VERSION-$OPENSSL_REVISION}
 # Our environment
-setenv LIBWEBSOCKETS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path PATH \$::env(LIBWEBSOCKETS_ROOT)/bin
-prepend-path LD_LIBRARY_PATH \$::env(LIBWEBSOCKETS_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(LIBWEBSOCKETS_ROOT)/lib")
+set LIBWEBSOCKETS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$LIBWEBSOCKETS_ROOT/bin
+prepend-path LD_LIBRARY_PATH \$LIBWEBSOCKETS_ROOT/lib
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
