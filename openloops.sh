4ded39481c0e1e05e5c6815864831859a718e4ee
mfasel <markus.fasel@cern.ch>
2020-02-12 15:27:45 +0100

    Update SHERPA to v2.2.8, import OpenLoops
    - Upgrade SHERPA to v2.2.8
    - Add new build recipe for OpenLoops
    - Specify Openloops version alice/v2.1.0 (customization
      for out-of-source builds as usually done by ALICE)
    - Add dependency on OpenLoops in SHERPA

---
+++ openloops.sh
@@ -0,0 +1,48 @@
+package: Openloops
+version: "%(tag_basename)s"
+tag: "alice/v2.1.0"
+source: https://github.com/mfasDa/Openloops
+requires:
+  - "GCC-Toolchain:(?!osx)"
+---
+#!/bin/bash -e
+rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ .
+
+./scons 
+
+JOBS=$((${JOBS:-1}*1/5))
+[[ $JOBS -gt 0 ]] || JOBS=1
+
+PROCESSES=(ppjj ppjj_ew ppjjj ppjjj_ew ppjjj_nf5 ppjjjj)
+for proc in ${PROCESSES[@]}; do
+    ./scons --jobs=$JOBS auto="$proc"  
+done
+
+INSTALL=(examples include lib openloops proclib pyol)
+for inst in ${INSTALL[@]}; do
+    cp -r $inst $INSTALLROOT/
+done
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 
+# Our environment
+setenv OPENLOOPS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv OpenLoopsPath \$::env(OPENLOOPS_ROOT)
+prepend-path PATH \$::env(OPENLOOPS_ROOT)
+prepend-path LD_LIBRARY_PATH \$::env(OPENLOOPS_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(OPENLOOPS_ROOT)/lib")
+prepend-path LD_LIBRARY_PATH \$::env(OPENLOOPS_ROOT)/proclib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(OPENLOOPS_ROOT)/proclib")
+EoF
--- sherpa.sh
+++ sherpa.sh
@@ -1,14 +1,16 @@
 package: SHERPA
 version: "%(tag_basename)s"
-tag: "v2.2.4-alice1"
-source: https://github.com/alisw/SHERPA
+tag: "v2.2.8-alice1"
+source: https://github.com/mfasDa/SHERPA
 requires:
   - "GCC-Toolchain:(?!osx)"
+  - Openloops
+  - HepMC
+  - lhapdf-pdfsets
+  - fastjet
 build_requires:
   - curl
   - autotools
-  - HepMC
-  - lhapdf5
 ---
 #!/bin/bash -e
 
@@ -23,7 +25,9 @@ printf '#!/bin/bash\nexec curl -fO $1' > fakewget/wget && chmod +x fakewget/wget
 PATH=$PATH:fakewget ./configure --prefix=$INSTALLROOT        \
                                 --with-sqlite3=install       \
                                 --enable-hepmc2=$HEPMC_ROOT  \
-                                --enable-lhapdf=$LHAPDF5_ROOT
+                                --enable-lhapdf=$LHAPDF_ROOT
+				                        --enable-openloops=$OPENLOOPS_ROOT \
+			                          --enable-fastjet=$FASTJET_ROOT
 
 make ${JOBS+-j $JOBS}
 make install
@@ -41,7 +45,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+module load BASE/1.0 ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${LHAPDF_VERSION:+lhapdf/$LHAPDF_VERSION-$LHAPDF_REVISION} ${FASTJET_VERSION:+fastjet/$FASTJET_VERSION-$FASTJET_REVISION} ${HEPMC2_VERSION:+hepmc3/$HEPMC2_VERSION-$HEPMC2_REVISION} ${OPENLOOPS_VERSION:+openloops/$OPENLOOPS_VERSION-$OPENLOOPS_REVISION}
 # Our environment
 set SHERPA_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv SHERPA_ROOT \$SHERPA_ROOT
