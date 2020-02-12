d81bc62e361da5e45b0c8c5ecc00f8a1dd18fafc
Markus Fasel <markus.fasel@cern.ch>
2020-02-12 21:45:34 +0100

    Adapt URL for pdfsets to new URL (#2073)

--- lhapdf-pdfsets.sh
+++ lhapdf-pdfsets.sh
@@ -8,7 +8,8 @@ build_requires:
 PDFSETS="cteq6l1 MMHT2014lo68cl MMHT2014nlo68cl cteq66 CT10nlo"
 if [ ! -d $INSTALLROOT/share/LHAPDF ]; then mkdir -p $INSTALLROOT/share/LHAPDF; fi
 pushd $INSTALLROOT/share/LHAPDF
-  REPO=https://lhapdf.hepforge.org/downloads?f=pdfsets/6.2.1
+  # REPO=https://lhapdf.hepforge.org/downloads?f=pdfsets/6.2.1
+  REPO=http://lhapdfsets.web.cern.ch/lhapdfsets/current/ 
   for P in $PDFSETS; do
     PDFPACK=$(printf "%s.tar.gz" $P)
     PDFEXT=$(printf "%s/%s" $REPO $PDFPACK)
