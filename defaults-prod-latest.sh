5209a92b429206a2250d009d8184e31fe38776bf
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-02-25 21:21:16 +0100

    Bump to 20j

--- defaults-prod-latest.sh
+++ defaults-prod-latest.sh
@@ -7,10 +7,10 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-20i
+    tag: v5-09-20j
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-20i-01
+    tag: v5-09-20j-01
 ---
 # To be used with aliBuild option `--defaults prod-latest`.
 #
--- defaults-user.sh
+++ defaults-user.sh
@@ -7,14 +7,14 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-20i
+    tag: v5-09-20j
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-20i-01
+    tag: v5-09-20j-01
 ---
 # To be used with aliBuild option `--defaults user`.
 #
