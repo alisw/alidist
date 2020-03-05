9a4eb74a51cbd082dd19136451780698a924b263
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-03-05 16:53:03 +0100

    Update SHERPA to v2.2.8, import OpenLoops (#2072)
    - Upgrade SHERPA to v2.2.8
    - Add new build recipe for OpenLoops
    - Specify Openloops version alice/v2.1.0 (customization
      for out-of-source builds as usually done by ALICE)
    - GMP needed as build dependency introduced by
      fastjet
    - Several dependencies in the modulefile were
      misspelled, consequently the module was not
      loaded.

---
+++ openloops.sh
@@ -0,0 +1,46 @@
+package: Openloops
+version: "%(tag_basename)s"
+tag: "v2.1.0-alice1"
+source: https://github.com/alisw/Openloops
+requires:
+  - "GCC-Toolchain:(?!osx)"
+---
+#!/bin/bash -e
+rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ .
+
+./scons 
+
+JOBS=$((${JOBS:-1}*1/5))
+[[ $JOBS -gt 0 ]] || JOBS=1
+
+PROCESSES=(ppjj ppjj_ew ppjjj ppjjj_ew ppjjj_nf5 ppjjjj)
+for proc in ${PROCESSES[@]}; do
+    ./scons --jobs=$JOBS auto="$proc"  
+done
+
+INSTALL=(examples include lib openloops proclib pyol)
+for inst in ${INSTALL[@]}; do
+    cp -r $inst $INSTALLROOT/
+done
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 
+# Our environment
+set OPENLOOPS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv OpenLoopsPath \$OPENLOOPS_ROOT
+prepend-path PATH \$OPENLOOPS_ROOT
+prepend-path LD_LIBRARY_PATH \$OPENLOOPS_ROOT/lib
+prepend-path LD_LIBRARY_PATH \$OPENLOOPS_ROOT/proclib
+EoF
--- sherpa.sh
+++ sherpa.sh
@@ -1,14 +1,18 @@
 package: SHERPA
 version: "%(tag_basename)s"
-tag: "v2.2.4-alice1"
-source: https://github.com/alisw/SHERPA
+tag: "v2.2.8-alice1"
+source: https://github.com/mfasDa/SHERPA
 requires:
   - "GCC-Toolchain:(?!osx)"
+  - Openloops
+  - HepMC
+  - lhapdf-pdfsets
+  - fastjet
 build_requires:
   - curl
   - autotools
-  - HepMC
-  - lhapdf5
+  - cgal
+  - GMP
 ---
 #!/bin/bash -e
 
@@ -20,10 +24,14 @@ autoreconf -ivf
 mkdir -p fakewget && [[ -d fakewget ]]
 printf '#!/bin/bash\nexec curl -fO $1' > fakewget/wget && chmod +x fakewget/wget
 
-PATH=$PATH:fakewget ./configure --prefix=$INSTALLROOT        \
-                                --with-sqlite3=install       \
-                                --enable-hepmc2=$HEPMC_ROOT  \
-                                --enable-lhapdf=$LHAPDF5_ROOT
+PATH=$PATH:fakewget 
+export LDFLAGS="$LDFLAGS -L$CGAL_ROOT/lib  -L$GMP_ROOT/lib"
+./configure --prefix=$INSTALLROOT        \
+              --with-sqlite3=install       \
+              --enable-hepmc2=$HEPMC_ROOT  \
+              --enable-lhapdf=$LHAPDF_ROOT \
+              --enable-openloops=$OPENLOOPS_ROOT \
+        	    --enable-fastjet=$FASTJET_ROOT
 
 make ${JOBS+-j $JOBS}
 make install
@@ -41,11 +49,16 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}               \\
+                     ${LHAPDF_PDFSETS_REVISION:+lhapdf-pdfsets/$LHAPDF_PDFSETS_VERSION-$LHAPDF_PDFSETS_REVISION}           \\
+                     ${FASTJET_REVISION:+fastjet/$FASTJET_VERSION-$FASTJET_REVISION}                                       \\
+                     ${HEPMC_REVISION:+HepMC/$HEPMC_VERSION-$HEPMC_REVISION}                                               \\
+                     ${OPENLOOPS_REVISION:+Openloops/$OPENLOOPS_VERSION-$OPENLOOPS_REVISION}
 # Our environment
 set SHERPA_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv SHERPA_ROOT \$SHERPA_ROOT
 setenv SHERPA_INSTALL_PATH \$::env(SHERPA_ROOT)/lib/SHERPA
+setenv SHERPA_SHARE_PATH \$::env(SHERPA_ROOT)/share/SHERPA-MC
 prepend-path PATH \$SHERPA_ROOT/bin
 prepend-path LD_LIBRARY_PATH \$SHERPA_ROOT/lib
 prepend-path LD_LIBRARY_PATH \$SHERPA_ROOT/lib/SHERPA-MC
