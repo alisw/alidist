65e055ab3de71fc15cdae790662b6fe69606bb4b
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-03-16 16:58:57 +0100

    Bump to v5-09-20k

--- defaults-user-root6.sh
+++ defaults-user-root6.sh
@@ -7,14 +7,14 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-20j
+    tag: v5-09-20k
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-20j-01
+    tag: v5-09-20k-01
   ROOT:
     version: "%(tag_basename)s"
     version: "%(tag_basename)s"
