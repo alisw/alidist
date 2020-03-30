99724515c98189124143b0523d3ece9e5ab0bc23
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-03-30 15:04:46 +0200

    Bump to v5-09-02f

--- defaults-user.sh
+++ defaults-user.sh
@@ -7,14 +7,14 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-25
+    tag: v5-09-02f
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
       - Vc
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-25-01
+    tag: v5-09-02f-01
 ---
 # To be used with aliBuild option `--defaults user`.
 #
