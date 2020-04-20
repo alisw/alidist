3cbc6c1f92ef65f0b4e8f0fb984bf4f72c60c54d
Nikola Hardi <nhardi@cern.ch>
2020-04-20 12:15:49 +0200

    Sync defaults root6 and jalien

--- defaults-jalien.sh
+++ defaults-jalien.sh
@@ -7,44 +7,12 @@ env:
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
 overrides:
-  XRootD:
-    tag: "v4.11.1"
-    source: https://github.com/xrootd/xrootd
-  JDK:
-    version: "12.0.1_JALIEN"
-  libxml2:
-    version: "%(tag_basename)s_JALIEN"
-    overrides:
-    requires:
-      - zlib
-    build_requires:
-      - autotools
-      - "GCC-Toolchain:(?!osx)"
-  OpenSSL:
-    version: "v1.0.2o_JALIEN"
-    overrides:
-    requires:
-      - zlib
-    build_requires:
-      - "GCC-Toolchain:(?!osx)"
-  ApMon-CPP:
-    version: "%(tag_basename)s"
-    requires:
-      - "GCC-Toolchain:(?!osx)"
-  AliPhysics:
-    version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-53-01
   AliRoot:
     version: "%(tag_basename)s_JALIEN"
     tag: v5-09-53
-    requires:
-      - ROOT
-      - DPMJET
-      - fastjet:(?!.*ppc64)
-      - GEANT3
-      - GEANT4_VMC
-      - Vc
-      - JAliEn-ROOT
+  AliPhysics:
+    version: "%(tag_basename)s_JALIEN"
+    tag: v5-09-53-01
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
