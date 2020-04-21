d2b012e8164f4988ac9cfeb89ca69d31a532e42d
Nikola Hardi <nhardi@cern.ch>
2020-04-21 09:07:07 +0200

    Fix AliPhysics JAliEn dailies (#2195)
    * Sync defaults root6 and jalien
    * Revert autotools versions in defaults-jalien
    * Build help2man only if src dir is available
    * autoconf depends on m4, move m4 before
    * Run bootstrap only if present
    * Don't run autoreconf when building with defaults-jalien (slc6)

--- defaults-jalien.sh
+++ defaults-jalien.sh
@@ -6,45 +6,16 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  USE_AUTORECONF: "false"
 overrides:
-  XRootD:
-    tag: "v4.11.1"
-    source: https://github.com/xrootd/xrootd
-  JDK:
-    version: "12.0.1_JALIEN"
-  libxml2:
-    version: "%(tag_basename)s_JALIEN"
-    overrides:
-    requires:
-      - zlib
-    build_requires:
-      - autotools
-      - "GCC-Toolchain:(?!osx)"
-  OpenSSL:
-    version: "v1.0.2o_JALIEN"
-    overrides:
-    requires:
-      - zlib
-    build_requires:
-      - "GCC-Toolchain:(?!osx)"
-  ApMon-CPP:
-    version: "%(tag_basename)s"
-    requires:
-      - "GCC-Toolchain:(?!osx)"
-  AliPhysics:
-    version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-53-01
   AliRoot:
     version: "%(tag_basename)s_JALIEN"
     tag: v5-09-53
-    requires:
-      - ROOT
-      - DPMJET
-      - fastjet:(?!.*ppc64)
-      - GEANT3
-      - GEANT4_VMC
-      - Vc
-      - JAliEn-ROOT
+  AliPhysics:
+    version: "%(tag_basename)s_JALIEN"
+    tag: v5-09-53-01
+  autotools:
+    tag: v1.5.0
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
