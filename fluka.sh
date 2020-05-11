b2ceb588629ef6a52b06bb640d703da4d1596331
hristov <peter.Hristov@cern.ch>
2020-05-11 11:09:11 +0200

    Using Fluka 2011-3.0-vmc1

--- fluka.sh
+++ fluka.sh
@@ -1,6 +1,6 @@
 package: FLUKA
 version: "%(tag_basename)s"
-tag: "2011.2x"
+tag: "2011-3.0-vmc1"
 source: https://gitlab.cern.ch/ALICEPrivateExternals/FLUKA.git
 requires:
   - "GCC-Toolchain:(?!osx)"
