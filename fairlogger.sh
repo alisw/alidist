5c75f97989e66b0e0753eccc03686467497833d8
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-05-12 11:21:23 +0200

    fairlogger: bump to 1.6.2
    Now depends on fmt

--- fairlogger.sh
+++ fairlogger.sh
@@ -1,21 +1,30 @@
 package: FairLogger
 version: "%(tag_basename)s"
-tag: v1.5.0
+tag: v1.6.2
 source: https://github.com/FairRootGroup/FairLogger
 build_requires:
  - CMake
  - "GCC-Toolchain:(?!osx)"
+ - "fmt"
 incremental_recipe: |
   cmake --build . --target install ${JOBS:+-- -j$JOBS}
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 prepend_path:
   ROOT_INCLUDE_PATH: "$FAIRLOGGER_ROOT/include"
 ---
+case $ARCHITECTURE in
+  osx*)
+    # If we preferred system tools, we need to make sure we can pick them up.
+    [[ ! $FMT_ROOT ]] && FMT_ROOT=`brew --prefix fmt`
+  ;;
+  *) ;;
+esac
 mkdir -p $INSTALLROOT
 
 cmake $SOURCEDIR                                                 \
       ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER}        \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}  \
+      -DUSE_EXTERNAL_FMT=ON                                      \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                        \
       -DDISABLE_COLOR=ON                                         \
       -DCMAKE_INSTALL_LIBDIR=lib
