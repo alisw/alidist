8a89f1cf610642597657d94e5faa38e8aa8406f5
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-06-10 15:14:23 +0200

    Move master to use 54a (#2291)

--- defaults-user-next-root6.sh
+++ defaults-user-next-root6.sh
@@ -13,8 +13,8 @@ disable:
 overrides:
   AliRoot:
     version: "%(tag_basename)s"
-    tag: v5-09-53
+    tag: v5-09-54a
   AliPhysics:
     version: "%(tag_basename)s"
-    tag: v5-09-53-01
+    tag: v5-09-54a-01
 ---
