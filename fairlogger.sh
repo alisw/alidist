8ed758f3c157f6ae0ac6b4e2fdf4136ae3209fc0
David Rohr <drohr@jwdt.org>
2020-06-18 10:16:10 +0200

    Fixes to build new FairLogger (#2307)
    * Do not use bundled libfmt of fairlogger
    * Fairlogger should build with correct C++ std

--- fairlogger.sh
+++ fairlogger.sh
@@ -2,6 +2,8 @@ package: FairLogger
 version: "%(tag_basename)s"
 tag: v1.5.0
 source: https://github.com/FairRootGroup/FairLogger
+requires:
+ - fmt
 build_requires:
  - CMake
  - "GCC-Toolchain:(?!osx)"
@@ -16,8 +18,10 @@ mkdir -p $INSTALLROOT
 cmake $SOURCEDIR                                                 \
       ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER}        \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}  \
+      ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}                    \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                        \
       -DDISABLE_COLOR=ON                                         \
+      -DUSE_EXTERNAL_FMT=ON                                      \
       -DCMAKE_INSTALL_LIBDIR=lib
 
 cmake --build . ${JOBS:+-- -j$JOBS}
