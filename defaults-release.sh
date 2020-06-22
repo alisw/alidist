23d1cb0e60e37983d339593a27223bd89dfaf869
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-06-22 16:11:54 +0200

    Update defaults-release for AliPhysics-v5-09-20-01-patches

--- defaults-release.sh
+++ defaults-release.sh
@@ -4,7 +4,16 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+override:
+  # Pinpoint AliRoot/AliPhysics
+  AliRoot:
+    version: "%(tag_basename)s"
+    tag: v5-09-20k
+  AliPhysics:
+    version: "%(tag_basename)s"
+    tag: v5-09-20k-01
 ---
+
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
 # "--defaults" option of alibuild.
