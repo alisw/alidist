ae8f10bc3588391d827bfc0a5c18f747d8aad1d6
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-06-24 11:56:51 +0200

    Fix typo

--- defaults-release.sh
+++ defaults-release.sh
@@ -4,7 +4,7 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
-override:
+overrides:
   # Pinpoint AliRoot/AliPhysics
   AliRoot:
     version: "%(tag_basename)s"
