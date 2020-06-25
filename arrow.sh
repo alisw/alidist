4ca51f6d424cd75fe057fbb6319a2d336c3bddf4
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-06-25 15:13:44 +0200

    arrow: add runtime dependency on lz4 and zlib

--- arrow.sh
+++ arrow.sh
@@ -121,6 +121,8 @@ set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
 module load BASE/1.0 ${BOOST_REVISION:+boost/$BOOST_VERSION-$BOOST_REVISION} \\
+                     ${LZ4_REVISION:+lz4/$LZ4_VERSION-$LZ4_REVISION}         \\
+                     ${ZLIB_REVISION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION}     \\
                      ${CLANG_REVISION:+Clang/$CLANG_VERSION-$CLANG_REVISION}
 # Our environment
 set ARROW_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
