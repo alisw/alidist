16a28f21489241574ab2c9418f73712f11e352f5
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-07-03 12:02:36 +0200

    Update defaults-release.sh

--- defaults-release.sh
+++ defaults-release.sh
@@ -4,6 +4,14 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+overrides:
+  # Pinpoint AliRoot/AliPhysics
+  AliRoot:
+    version: "%(tag_basename)s"
+    tag: v5-09-38f
+  AliPhysics:
+    version: "%(tag_basename)s"
+    tag: v5-09-38f-01
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
