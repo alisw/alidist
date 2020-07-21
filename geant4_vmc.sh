63df5bc1869ffb82d73ba91e76f2ff93c5070338
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-07-21 11:35:16 +0200

    Update MC packages to the latest versions: - Versions compatible with Root 5 in the production defaults   (defaults-prod-latest.sh, defaults-release.sh)   GEANT3: v2-7-p2   GEANT4_VMC: v3-6-p6-inclxx-biasing-p2   GEANT4: v10.4.2-alice3   vgm: v4-4 - Versions compatible with Root 6 in the packages defaults   (xyz.sh):   GEANT3: v3-5   GEANT4_VMC: v5-0-p5   GEANT4: v10.5.1   vgm: v4-7

--- geant4_vmc.sh
+++ geant4_vmc.sh
@@ -1,7 +1,7 @@
 package: GEANT4_VMC
 version: "%(tag_basename)s"
-tag: "v3-2-p3-monopole-alice1"
-source: https://github.com/alisw/geant4_vmc
+tag: "v5-0-p5"
+source: https://github.com/vmc-project/geant4_vmc
 requires:
   - ROOT
   - GEANT4
@@ -9,16 +9,19 @@ requires:
 build_requires:
   - CMake
   - "Xcode:(osx.*)"
+prepend_path:
+  ROOT_INCLUDE_PATH: "$GEANT4_VMC_ROOT/include/g4root:$GEANT4_VMC_ROOT/include/geant4vmc:$GEANT4_VMC_ROOT/include/mtroot"
 env:
   G4VMCINSTALL: "$GEANT4_VMC_ROOT"
 ---
 #!/bin/bash -e
-cmake "$SOURCEDIR"                             \
-  -DCMAKE_CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-  -DGeant4VMC_USE_VGM=ON                       \
-  -DCMAKE_INSTALL_LIBDIR=lib                   \
-  -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"        \
-  ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}
+LDFLAGS="$LDFLAGS -L$GEANT4_ROOT/lib"            \
+  cmake "$SOURCEDIR"                             \
+    -DCMAKE_CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+    -DGeant4VMC_USE_VGM=ON                       \
+    -DCMAKE_INSTALL_LIBDIR=lib                   \
+    -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"        \
+    ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}
 
 make ${JOBS+-j $JOBS} install
 G4VMC_SHARE=$(cd "$INSTALLROOT/share"; echo Geant4VMC-* | cut -d' ' -f1)
@@ -37,11 +40,14 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GEANT4_VERSION:+GEANT4/$GEANT4_VERSION-$GEANT4_REVISION} ${ROOT_VERSION:+ROOT/$ROOT_VERSION-$ROOT_REVISION} vgm/$VGM_VERSION-$VGM_REVISION
+module load BASE/1.0 ${GEANT4_REVISION:+GEANT4/$GEANT4_VERSION-$GEANT4_REVISION} ${ROOT_REVISION:+ROOT/$ROOT_VERSION-$ROOT_REVISION} vgm/$VGM_VERSION-$VGM_REVISION
 # Our environment
-setenv GEANT4_VMC_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv G4VMCINSTALL \$::env(GEANT4_VMC_ROOT)
-prepend-path PATH \$::env(GEANT4_VMC_ROOT)/bin
-prepend-path LD_LIBRARY_PATH \$::env(GEANT4_VMC_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(GEANT4_VMC_ROOT)/lib")
+set GEANT4_VMC_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv GEANT4_VMC_ROOT \$GEANT4_VMC_ROOT
+setenv G4VMCINSTALL \$GEANT4_VMC_ROOT
+prepend-path PATH \$GEANT4_VMC_ROOT/bin
+prepend-path ROOT_INCLUDE_PATH \$GEANT4_VMC_ROOT/include/mtroot
+prepend-path ROOT_INCLUDE_PATH \$GEANT4_VMC_ROOT/include/geant4vmc
+prepend-path ROOT_INCLUDE_PATH \$GEANT4_VMC_ROOT/include/g4root
+prepend-path LD_LIBRARY_PATH \$GEANT4_VMC_ROOT/lib
 EoF
