a4f3937d9319c43436b4084d2dfd87583846bc3a
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-07-21 11:52:55 +0200

    Update MC packages to the latest versions: - Versions compatible with Root 5 in the production defaults   (defaults-prod-latest.sh, defaults-release.sh)   GEANT3: v2-7-p2   GEANT4_VMC: v3-6-p6-inclxx-biasing-p2   GEANT4: v10.4.2-alice3   vgm: v4-4 - Versions compatible with Root 6 in the packages defaults   (xyz.sh):   GEANT3: v3-5   GEANT4_VMC: v5-0-p5   GEANT4: v10.5.1   vgm: v4-7

--- defaults-release.sh
+++ defaults-release.sh
@@ -4,6 +4,21 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+overrides:
+ # Use VMC packages compatible with ROOT 5
+  GEANT3:
+    version: "v2-7-p2"
+    tag: "v2-7-p2"
+  GEANT4_VMC:
+    version: "v3-6-p6-inclxx-biasing-p2"
+    tag: "v3-6-p6-inclxx-biasing-p2"
+  GEANT4:
+    source: https://github.com/alisw/geant4.git
+    version: "v10.4.2-alice3"
+    tag: "v10.4.2-alice3"
+  vgm:
+    version: "v4-4"
+    tag: "v4-4"
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
