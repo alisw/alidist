a7b0e03cf1a0ab270a1b5a618b714c062cb3e5c4
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-07-21 11:21:24 +0200

    Update MC packages to the latest versions: - Versions compatible with Root 5 in the production defaults   (defaults-prod-latest.sh, defaults-release.sh)   GEANT3: v2-7-p2   GEANT4_VMC: v3-6-p6-inclxx-biasing-p2   GEANT4: v10.4.2-alice3   vgm: v4-4 - Versions compatible with Root 6 in the packages defaults   (xyz.sh):   GEANT3: v3-5   GEANT4_VMC: v5-0-p5   GEANT4: v10.5.1   vgm: v4-7

--- geant4.sh
+++ geant4.sh
@@ -1,48 +1,56 @@
 package: GEANT4
 version: "%(tag_basename)s"
-tag: v4.10.01.p03-alice1
-source: https://github.com/alisw/geant4
+tag: "v10.5.1"
+source: https://gitlab.cern.ch/geant4/geant4.git
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
   - CMake
   - "Xcode:(osx.*)"
+prepend_path:
+  ROOT_INCLUDE_PATH: "$GEANT4_ROOT/include:$GEANT4_ROOT/include/Geant4"
 incremental_recipe: |
   make ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 env:
   G4INSTALL : $GEANT4_ROOT
   G4DATASEARCHOPT : "-mindepth 2 -maxdepth 4 -type d -wholename"
+  G4ABLADATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4ABLA*'`"  ## v10.4.px only
+  G4ENSDFSTATEDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4ENSDFSTATE*'`"
+  G4INCLDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4INCL*'`"  ## v10.5.px only
   G4LEDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4EMLOW*'`"
   G4LEVELGAMMADATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*PhotonEvaporation*'`"
-  G4RADIOACTIVEDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*RadioactiveDecay*'`"
   G4NEUTRONHPDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4NDL*'`"
-  G4NEUTRONXSDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4NEUTRONXS*'`"
-  G4SAIDXSDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT  '*data*G4SAIDDATA*'`"
+  G4NEUTRONXSDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4NEUTRONXS*'`"   ## v10.4.px only
+  G4PARTICLEXSDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4PARTICLEXS*'`"   ## v10.5.px only
   G4PIIDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4PII*'`"
+  G4RADIOACTIVEDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*RadioactiveDecay*'`"
   G4REALSURFACEDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*RealSurface*'`"
-  G4ENSDFSTATEDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT '*data*G4ENSDFSTATE*'`"
----
-#!/bin/bash -e
+  G4SAIDXSDATA : "`find ${G4INSTALL} $G4DATASEARCHOPT  '*data*G4SAIDDATA*'`"
 
-cmake $SOURCEDIR                                    \
-  -DGEANT4_INSTALL_DATA_TIMEOUT=1500                \
-  -DCMAKE_CXX_FLAGS="-fPIC"                         \
-  -DCMAKE_INSTALL_PREFIX:PATH="$INSTALLROOT"        \
-  -DCMAKE_INSTALL_LIBDIR="lib"                      \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo                 \
-  -DGEANT4_BUILD_TLS_MODEL:STRING="global-dynamic"  \
-  -DGEANT4_ENABLE_TESTING=OFF                       \
-  -DBUILD_SHARED_LIBS=ON                            \
-  -DGEANT4_INSTALL_EXAMPLES=OFF                     \
-  -DCLHEP_ROOT_DIR:PATH="$CLHEP_ROOT"               \
-  -DGEANT4_BUILD_MULTITHREADED=OFF                  \
-  -DCMAKE_STATIC_LIBRARY_CXX_FLAGS="-fPIC"          \
-  -DCMAKE_STATIC_LIBRARY_C_FLAGS="-fPIC"            \
-  -DGEANT4_USE_G3TOG4=ON                            \
-  -DGEANT4_INSTALL_DATA=ON                          \
-  -DGEANT4_USE_SYSTEM_EXPAT=OFF                     \
-  ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}           \
+---
+# if this variable is not defined default it to OFF
+: ${GEANT4_BUILD_MULTITHREADED:=OFF}
+
+cmake $SOURCEDIR                                             \
+  -DGEANT4_INSTALL_DATA_TIMEOUT=2000                         \
+  -DCMAKE_CXX_FLAGS="-fPIC"                                  \
+  -DCMAKE_INSTALL_PREFIX:PATH="$INSTALLROOT"                 \
+  -DCMAKE_INSTALL_LIBDIR="lib"                               \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo                          \
+  -DGEANT4_BUILD_TLS_MODEL:STRING="global-dynamic"           \
+  -DGEANT4_ENABLE_TESTING=OFF                                \
+  -DBUILD_SHARED_LIBS=ON                                     \
+  -DGEANT4_INSTALL_EXAMPLES=OFF                              \
+  -DCLHEP_ROOT_DIR:PATH="$CLHEP_ROOT"                        \
+  -DGEANT4_BUILD_MULTITHREADED="$GEANT4_BUILD_MULTITHREADED" \
+  -DCMAKE_STATIC_LIBRARY_CXX_FLAGS="-fPIC"                   \
+  -DCMAKE_STATIC_LIBRARY_C_FLAGS="-fPIC"                     \
+  -DGEANT4_USE_G3TOG4=ON                                     \
+  -DGEANT4_INSTALL_DATA=ON                                   \
+  -DGEANT4_USE_SYSTEM_EXPAT=OFF                              \
+  ${XERCESC_ROOT:+-DXERCESC_ROOT_DIR=$XERCESC_ROOT}          \
+  ${CXXSTD:+-DGEANT4_BUILD_CXXSTD=$CXXSTD}                   \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 make ${JOBS+-j $JOBS}
@@ -52,15 +60,18 @@ make install
 # in order to avoid putting hard-coded version numbers (which change with every G4 tag)
 # these variables are used to create the modulefile below
 G4DATASEARCHOPT="-mindepth 2 -maxdepth 4 -type d -wholename"
-G4LEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*G4EMLOW*"`
-G4LEVELGAMMADATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*PhotonEvaporation*"`
-G4RADIOACTIVEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*RadioactiveDecay*"`
-G4NEUTRONHPDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*G4NDL*"`
-G4NEUTRONXSDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*G4NEUTRONXS*"`
-G4SAIDXSDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*G4SAIDDATA*"`
-G4PIIDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*G4PII*"`
-G4REALSURFACEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*RealSurface*"`
-G4ENSDFSTATEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT "*data*G4ENSDFSTATE*"`
+G4ABLADATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4ABLA*'`
+G4ENSDFSTATEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4ENSDFSTATE*'`
+G4INCLDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4INCL*'`
+G4LEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4EMLOW*'`
+G4LEVELGAMMADATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*PhotonEvaporation*'`
+G4NEUTRONHPDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4NDL*'`
+G4NEUTRONXSDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4NEUTRONXS*'`
+G4PARTICLEXSDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4PARTICLEXS*'`
+G4PIIDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*G4PII*'`
+G4RADIOACTIVEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*RadioactiveDecay*'`
+G4REALSURFACEDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT '*data*RealSurface*'`
+G4SAIDXSDATA=`find ${INSTALLROOT} $G4DATASEARCHOPT  '*data*G4SAIDDATA*'`
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -75,21 +86,30 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0
+module load BASE/1.0 ${XERCESC_REVISION:+xercesc/$XERCESC_REVISION-$XERCESC_REVISION}
 # Our environment
 set osname [uname sysname]
-setenv GEANT4_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv G4INSTALL \$::env(GEANT4_ROOT)
+set GEANT4_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv GEANT4_ROOT \$GEANT4_ROOT
+setenv G4INSTALL \$GEANT4_ROOT
 setenv G4INSTALL_DATA \$::env(G4INSTALL)/share/
 setenv G4SYSTEM \$osname-g++
-setenv G4LEVELGAMMADATA $G4LEVELGAMMADATA
-setenv G4RADIOACTIVEDATA  $G4RADIOACTIVEDATA
-setenv G4LEDATA $G4LEDATA
-setenv G4NEUTRONHPDATA $G4NEUTRONHPDATA
-setenv G4NEUTRONXSDATA $G4NEUTRONXSDATA
-setenv G4SAIDXSDATA $G4SAIDXSDATA
-setenv G4ENSDFSTATEDATA $G4ENSDFSTATEDATA
-prepend-path PATH \$::env(GEANT4_ROOT)/bin
-prepend-path LD_LIBRARY_PATH \$::env(GEANT4_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(GEANT4_ROOT)/lib")
+
+setenv G4ABLADATA ${G4ABLADATA:-not-defined}
+setenv G4ENSDFSTATEDATA ${G4ENSDFSTATEDATA:-not-defined}
+setenv G4INCLDATA ${G4INCLDATA:-not-defined}
+setenv G4LEDATA ${G4LEDATA:-not-defined}
+setenv G4LEVELGAMMADATA ${G4LEVELGAMMADATA:-not-defined}
+setenv G4NEUTRONHPDATA ${G4NEUTRONHPDATA:-not-defined}
+setenv G4NEUTRONXSDATA ${G4NEUTRONXSDATA:-not-defined}
+setenv G4PARTICLEXSDATA ${G4PARTICLEXSDATA:-not-defined}
+setenv G4PIIDATA ${G4PIIDATA:-not-defined}
+setenv G4RADIOACTIVEDATA  ${G4RADIOACTIVEDATA:-not-defined}
+setenv G4REALSURFACEDATA ${G4REALSURFACEDATA:-not-defined}
+setenv G4SAIDXSDATA ${G4SAIDXSDATA:-not-defined}
+set G4BASE \$::env(GEANT4_ROOT)
+prepend-path PATH \$G4BASE/bin
+prepend-path ROOT_INCLUDE_PATH \$G4BASE/include/Geant4
+prepend-path ROOT_INCLUDE_PATH \$G4BASE/include
+prepend-path LD_LIBRARY_PATH \$G4BASE/lib
 EoF
