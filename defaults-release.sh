6124f7c5571598a765776e7062aa0b0da84ed8b7
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-07-23 16:50:34 +0200

    Updated GEANT4_VMC: v3-6-p6-inclxx-biasing-p3 (#2389)
    This version includes a fix in FindROOT.cmake which should hopefully fix the build problem
    in AliPhysics v5-09-02h.

--- defaults-release.sh
+++ defaults-release.sh
@@ -17,8 +17,8 @@ overrides:
     version: "v2-7-p2"
     tag: "v2-7-p2"
   GEANT4_VMC:
-    version: "v3-6-p6-inclxx-biasing-p2"
-    tag: "v3-6-p6-inclxx-biasing-p2"
+    version: "v3-6-p6-inclxx-biasing-p3"
+    tag: "v3-6-p6-inclxx-biasing-p3"
   GEANT4:
     source: https://github.com/alisw/geant4.git
     version: "v10.4.2-alice3"
