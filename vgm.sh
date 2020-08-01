1b40731b0bab2c66fd586b748ee7cdb9d3189cf1
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-08-01 10:37:59 +0200

    Update MC packages (in AliPhysics-v5-09-20-01-patches) to the latest versions (#2400)
    * Update MC packages to the latest versions:
    - Versions compatible with Root 5 in the production defaults
      (defaults-prod-latest.sh, defaults-release.sh)
      GEANT3: v2-7-p2
      GEANT4_VMC: v3-6-p6-inclxx-biasing-p2
      GEANT4: v10.4.2-alice3
      vgm: v4-4
    - Versions compatible with Root 6 in the packages defaults
      (xyz.sh):
      GEANT3: v3-5
      GEANT4_VMC: v5-0-p5
      GEANT4: v10.5.1
      vgm: v4-7
    
    * Updated GEANT4_VMC: v3-6-p6-inclxx-biasing-p5
    
    This version includes a fix in cmake for compatibility with the minimal CMake version declared.

--- vgm.sh
+++ vgm.sh
@@ -1,7 +1,7 @@
 package: vgm
 version: "%(tag_basename)s"
-tag: "4.3-alice1"
-source: https://github.com/alisw/vgm.git
+tag: "v4-7"
+source: https://github.com/vmc-project/vgm
 requires:
   - ROOT
   - GEANT4
@@ -16,6 +16,12 @@ cmake "$SOURCEDIR" \
   ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}
 
 make ${JOBS+-j $JOBS} install
+
+# Relocation of .cmake files
+for CMAKE in $(find "$INSTALLROOT/lib" -name '*.cmake'); do
+  sed -ideleteme -e "s!$ROOTSYS!\$ENV{ROOTSYS}!g; s!$G4INSTALL!\$ENV{G4INSTALL}!g" "$CMAKE"
+done
+find "$INSTALLROOT/lib" -name '*deleteme' -delete || true
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -32,7 +38,7 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies
 module load BASE/1.0 GEANT4/$GEANT4_VERSION-$GEANT4_REVISION ROOT/$ROOT_VERSION-$ROOT_REVISION
 # Our environment
-setenv VGM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path LD_LIBRARY_PATH \$::env(VGM_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(VGM_ROOT)/lib")
+set VGM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv VGM_ROOT \$VGM_ROOT
+prepend-path LD_LIBRARY_PATH \$VGM_ROOT/lib
 EoF
