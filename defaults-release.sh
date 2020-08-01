40264a650fe2bed058a7af6501a56103b15f2130
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-08-01 15:03:23 +0200

    Update MC packages (in AliPhysics-v5-09-54-01-patches) to the latest versions: (#2405)
    * Update MC packages to the latest versions:
    - Versions compatible with Root 5 in the production defaults
      (defaults-prod-latest.sh, defaults-release.sh)
      GEANT3: v2-7-p2
      GEANT4_VMC: v3-6-p6-inclxx-biasing-p2
      GEANT4: v10.4.2-alice3
      vgm: v4-4
    - Versions compatible with Root 6 in the packages defaults
      (xyz.sh):
      GEANT3: v3-5
      GEANT4_VMC: v5-0-p5
      GEANT4: v10.5.1
      vgm: v4-7
    
    * Updated GEANT4_VMC: v3-6-p6-inclxx-biasing-p5
    
    This version includes a fix in cmake for compatibility with the minimal CMake version declared.

--- defaults-release.sh
+++ defaults-release.sh
@@ -49,14 +49,12 @@ overrides:
     version: "v2-7-p2"
     tag: "v2-7-p2"
   GEANT4_VMC:
-    version: "v3-6-p6-inclxx-biasing-p2"
-    tag: "v3-6-p6-inclxx-biasing-p2"
-    requires:
-      - GEANT4
-      - vgm
+    version: "v3-6-p6-inclxx-biasing-p5"
+    tag: "v3-6-p6-inclxx-biasing-p5"
   GEANT4:
-    version: "v10.4.2"
-    tag: "v10.4.2"
+    source: https://github.com/alisw/geant4.git
+    version: "v10.4.2-alice3"
+    tag: "v10.4.2-alice3"
   vgm:
     version: "v4-4"
     tag: "v4-4"
