c2ba24d7c3dd6a933074966c773bd9b4434e76be
Anton Alkin <anton.alkin@cern.ch>
2020-08-19 08:30:32 +0200

    arrow: bump to 1.0.0 (#2424)

--- utf8proc.sh
+++ utf8proc.sh
@@ -5,6 +5,10 @@ source: https://github.com/JuliaStrings/utf8proc
 build_requires:
  - "GCC-Toolchain:(?!osx)"
  - alibuild-recipe-tools
+prefer_system: "(?!osx)"
+prefer_system_check: \
+  printf "#include <utf8proc.h>\n" | c++ -c -I$(brew --prefix utf8proc)/include -xc++ - -o /dev/null 2>&1;
+  if [ $? -ne 0 ]; then printf "Use brew install utf8proc"; exit 1; fi
 ---
 rsync -a --delete --exclude "**/.git" $SOURCEDIR/ .
 make ${JOBS+-j $JOBS} install prefix=$INSTALLROOT
