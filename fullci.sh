43cae3ef57feef521e481db5ab3a6791d25ce5d2
David Rohr <drohr@cern.ch>
2020-08-26 17:29:49 +0200

    Add FullCI.sh recipe with complete continuous integration setup (#2448)

---
+++ fullci.sh
@@ -0,0 +1,35 @@
+package: FullCI
+version: "1.0.0"
+tag: "FullCI-1.0.0"
+requires:
+  - O2Suite
+  - o2checkcode
+valid_defaults:
+  - o2
+---
+#!/bin/bash -ex
+
+MODULEFILE_DEPS=
+for RPKG in $REQUIRES; do
+  [[ $RPKG != defaults* ]] || continue
+  RPKG_UP=$(echo $RPKG|tr '[:lower:]' '[:upper:]'|tr '-' '_')
+  RPKG_VERSION=$(eval echo "\$${RPKG_UP}_VERSION")
+  RPKG_REVISION=$(eval echo "\$${RPKG_UP}_REVISION")
+  MODULEFILE_DEPS="${MODULEFILE_DEPS} ${RPKG}/${RPKG_VERSION}-${RPKG_REVISION}"
+done
+MODULEFILE_DEPS=$(echo $MODULEFILE_DEPS)
+
+# Modulefile
+mkdir -p $INSTALLROOT/etc/modulefiles
+cat > $INSTALLROOT/etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+
+# Dependencies
+module load BASE/1.0 ${MODULEFILE_DEPS}
+EoF
