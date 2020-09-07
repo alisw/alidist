8007ec9b49fcef6f676962391e0eb7986606ca73
Roberto Preghenella <preghenella@bo.infn.it>
2020-09-07 10:02:49 +0200

    Use alibuild-generate-module

--- delpheso2.sh
+++ delpheso2.sh
@@ -10,40 +10,26 @@ overrides:
     version: "%(short_hash)s%(defaults_upper)s"
     tag: dev
 build_requires:
+  - alibuild-recipe-tools
   - CMake
   - "GCC-Toolchain:(?!osx)"
 source: https://github.com/preghenella/DelphesO2.git
 ---
 #!/bin/bash -ex
 cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT       \
+                 -DCMAKE_INSTALL_LIBDIR=lib                \
                  ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"} \
                  -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE      \
                  -DCMAKE_SKIP_RPATH=TRUE
 cmake --build . -- ${JOBS:+-j$JOBS} install
 
 #ModuleFile
-mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0                                                                                \\
-            ${DELPHES_REVISION:+Delphes/$DELPHES_VERSION-$DELPHES_REVISION}                         \\
-            ${O2_REVISION:+O2/$O2_VERSION-$O2_REVISION}
+mkdir -p $INSTALLROOT/etc/modulefiles
+alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
 
-# Delphes environment:
+cat << EOF >> $INSTALLROOT/etc/modulefiles/$PKGNAME
 set DELPHESO2_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv DELPHESO2_ROOT \$DELPHESO2_ROOT
-
 prepend-path PATH \$DELPHESO2_ROOT/bin
 prepend-path LD_LIBRARY_PATH \$DELPHESO2_ROOT/lib
-prepend-path LD_LIBRARY_PATH \$DELPHESO2_ROOT/lib64
-prepend-path ROOT_INCLUDE_PATH \$DELPHESO2_ROOT/include
-
-EoF
-mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+EOF
