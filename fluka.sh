a2394bbcce983fc3b8b4d1a439bf928d0f0f16f3
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-09-07 16:18:29 +0200

    Update fluka.sh

--- fluka.sh
+++ fluka.sh
@@ -16,7 +16,16 @@ export FLUPRO=$PWD
 export FC=gfortran
 
 rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ "$BUILDDIR"
-make ${JOBS:+-j$JOBS}
+
+FVERSION=`gfortran --version | grep -i fortran | sed -e 's/.* //' | cut -d. -f1`
+if [ $FVERSION -ge 10 ]; then
+    echo "Fortran version $FVERSION"
+# Redefine FC to contain the compiler, -fallow-argument-mismatch and the additional FFLAGS from FLUKA/src/config.mk
+    make FC="gfortran -fallow-argument-mismatch -Wall -Waggregate-return -Wcast-align -Wline-truncation -Wno-conversion -Wno-integer-division -Wno-tabs -Wno-unused-dummy-argument -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable -Wsystem-headers -Wuninitialized -Wunused-label -mtune=generic -msse2 -mfpmath=sse -fPIC -fexpensive-optimizations -funroll-loops -fstrength-reduce -fno-automatic -finit-local-zero -ffixed-form -fbackslash -funderscoring -fd-lines-as-code -frecord-marker=4 -fbacktrace -frange-check -fbounds-check -fdump-core -ftrapping-math -ffpe-trap=invalid,zero,overflow" ${JOBS:+-j$JOBS}
+else
+    make ${JOBS:+-j$JOBS}
+fi
+
 mkdir -p $INSTALLROOT
 cp -rf $BUILDDIR/bin $BUILDDIR/lib $BUILDDIR/include $BUILDDIR/data $INSTALLROOT/
 
