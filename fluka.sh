e532921e2ba541bf985b415db6d05d04dc994c6d
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-09-07 13:12:11 +0200

    fluka: cleanup

--- fluka.sh
+++ fluka.sh
@@ -4,40 +4,28 @@ tag: "2011-3.0-vmc2"
 source: https://gitlab.cern.ch/ALICEPrivateExternals/FLUKA.git
 requires:
   - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - alibuild-recipe-tools
 env:
   FLUPRO: "$FLUKA_ROOT/lib"
   FC: "gfortran"
 prepend_path:
   PATH: "$FLUKA_ROOT/bin"
 ---
-#!/bin/bash -e
-
 export FLUPRO=$PWD
 export FC=gfortran
 
-rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ "$INSTALLROOT"
-cd "$INSTALLROOT"
+rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ "$BUILDDIR"
 make ${JOBS:+-j$JOBS}
+mkdir -p $INSTALLROOT
+cp -rf $BUILDDIR/bin $BUILDDIR/lib $BUILDDIR/include $BUILDDIR/data $INSTALLROOT/
 
-# Modulefile
-MODULEDIR="$INSTALLROOT/etc/modulefiles"
-MODULEFILE="$MODULEDIR/$PKGNAME"
-mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+#ModuleFile
+mkdir -p $INSTALLROOT/etc/modulefiles
+alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
+cat >> $INSTALLROOT/etc/modulefiles/$PKGNAME <<EoF
 # Our environment
 set FLUKA_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv FLUKA_ROOT \$FLUKA_ROOT
 setenv FLUPRO \$::env(BASEDIR)/$PKGNAME/\$version/lib
-setenv FC gfortran
-prepend-path PATH \$FLUKA_ROOT
 prepend-path PATH \$FLUKA_ROOT/bin
 EoF
