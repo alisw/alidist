f726a91700a48a3f4af1c7ac92eff313c04b4de3
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-09-08 11:56:49 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -6,6 +6,9 @@ build_requires:
  - "OpenSSL:(?!osx)"
  - CMake
  - alibuild-recipe-tools
+system_requirement: "slc8.*"
+system_requirement_check:
+   curl --version > /dev/null; if test $? = 127; then exit 1; else printf "#include <curl/curl.h>\nint main() {}\n" | cc -xc -lcurl - -o /dev/null || exit 2; fi; exit 0		
 ---
 #!/bin/bash -e
 
