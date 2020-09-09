c6f2825a403846e627e48d5b013f9a6c00e8f556
Roberto Preghenella <preghenella@bo.infn.it>
2020-09-09 07:13:48 +0200

    Use alibuild-generate-module (#2468)

--- o2dpg.sh
+++ o2dpg.sh
@@ -2,27 +2,20 @@ package: O2DPG
 version: "%(tag_basename)s"
 tag: master
 source: https://github.com/AliceO2Group/O2DPG.git
+build_requires:
+  - alibuild-recipe-tools
 ---
 #!/bin/bash -e
 rsync -a --exclude='**/.git' --delete --delete-excluded \
       $SOURCEDIR/ $INSTALLROOT/
 
 # Modulefile
-mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0
-# Our environment
+mkdir -p $INSTALLROOT/etc/modulefiles
+alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
+
+cat << EOF >> $INSTALLROOT/etc/modulefiles/$PKGNAME
+set O2DPG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv O2DPG_ROOT \$O2DPG_ROOT
 setenv O2DPG_RELEASE \$version
 setenv O2DPG_VERSION $PKGVERSION
-set O2DPG_ROOT \$::env(BASEDIR)/$PKGNAME/\$::env(O2DPG_RELEASE)
-setenv O2DPG_ROOT \$O2DPG_ROOT
-EoF
-mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+EOF
