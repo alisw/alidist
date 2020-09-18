4ac70694895eb339fbbb7195a7f47407499082e8
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-09-18 09:12:04 +0200

    Revert "Adding recipe for ADAPRO (#2471)"
    This reverts commit 34072164c2d9f23a87cae3f97919eebe8e7844e2.

--- systemd.sh
+++
@@ -1,9 +0,0 @@
-package: systemd
-version: "237"
-system_requirement_missing: |
-   Please install systemd develpment package on your system.
-   If they are there, make sure you have them in your default path or check you have `which` installed.
-system_requirement: "(?!osx)"
-system_requirement_check: |
-   printf "#include <systemd/sd-daemon.h>" | cc -xc - -c -o /dev/null
----
