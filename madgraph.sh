1dc3794bde4d63333332a74b498e5a27727d8f1b
mfasel <markus.fasel@cern.ch>
2020-10-06 10:31:43 +0200

    Add NLO process support to Herwig package
    - Add package for MadGraph external loop provider
    - Build Herwig with OpenLoops and MadGraph support

---
+++ madgraph.sh
@@ -0,0 +1,24 @@
+package: madgraph
+version: "%(tag_basename)s"
+tag: "v2.8.1"
+source: https://github.com/alisw/MadGraph
+requires:
+ - "Python:slc.*"
+ - "Python-system:(?!slc.*)"
+build_requires:
+  - alibuild-recipe-tools
+---
+#!/bin/bash -e
+
+rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ $INSTALLROOT/
+
+#ModuleFile
+mkdir -p $INSTALLROOT/etc/modulefiles
+alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
+
+cat << EOF >> $INSTALLROOT/etc/modulefiles/$PKGNAME
+set MADGRAPH_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv MADGRAPH_ROOT \$MADGRAPH_ROOT
+prepend-path PATH \$MADGRAPH_ROOT/bin
+
+EOF
