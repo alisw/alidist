f819f7cbad33a89765663dfe7df545f3140a00d6
Nikola Hardi <nhardi@cern.ch>
2020-11-13 11:31:38 +0100

    Fix curl build when OpenSSL libs are installed on the system (#2597)

--- curl.sh
+++ curl.sh
@@ -17,6 +17,9 @@ fi
 
 rsync -av --delete --exclude="**/.git" $SOURCEDIR/ .
 
+sed -i.deleteme 's/CPPFLAGS="$CPPFLAGS $SSL_CPPFLAGS"/CPPFLAGS="$SSL_CPPFLAGS $CPPFLAGS"/' configure.ac
+sed -i.deleteme 's/LDFLAGS="$LDFLAGS $SSL_LDFLAGS"/LDFLAGS="$SSL_LDFLAGS $LDFLAGS"/' configure.ac
+
 ./buildconf
 ./configure --prefix=$INSTALLROOT --disable-ldap ${OPENSSL_ROOT:+--with-ssl=$OPENSSL_ROOT} --disable-static
 make ${JOBS:+-j$JOBS}
