1e275c0d6a7a0543685a87b7acb22185b566b008
Gvozden Neskovic <neskovic@gmail.com>
2020-11-16 21:11:35 +0100

    datadist: bump to v0.8 (#2606)

--- datadistribution.sh
+++ datadistribution.sh
@@ -1,7 +1,8 @@
 package: DataDistribution
 version: "%(tag_basename)s"
-tag: v0.7.10
+tag: v0.8.0
 requires:
+  - "GCC-Toolchain:(?!osx)"
   - boost
   - FairLogger
   - FairMQ
@@ -13,7 +14,7 @@ requires:
   - fmt
 build_requires:
   - CMake
-  - "GCC-Toolchain:(?!osx)"
+  - alibuild-recipe-tools
 source: https://github.com/AliceO2Group/DataDistribution
 incremental_recipe: |
   # reduce number of compile slots if invoked  by Jenkins
@@ -25,7 +26,6 @@ incremental_recipe: |
 ---
 #!/bin/bash -ex
 
-
 case $ARCHITECTURE in
     osx*)
         [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost)
@@ -36,7 +36,7 @@ esac
 cmake $SOURCEDIR                                              \
       ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"}               \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                     \
-      ${BOOST_ROOT:+-DBOOST_ROOT=$BOOST_ROOT}                 \
+      ${BOOST_ROOT:+-DBoost_ROOT=$BOOST_ROOT}                 \
       ${FAIRLOGGER_ROOT:+-DFairLogger_ROOT=$FAIRLOGGER_ROOT}  \
       ${FAIRMQ_ROOT:+-DFairMQ_ROOT=$FAIRMQ_ROOT}              \
       ${PPCONSUL_ROOT:+-Dppconsul_DIR=${PPCONSUL_ROOT}/cmake} \
@@ -52,36 +52,8 @@ if [ ! "X$JENKINS_HOME" = X ]; then
 fi
 cmake --build . -- ${JOBS+-j $JOBS} install
 
-#ModuleFile
+
+# Modulefile
 mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0                                                                                \\
-            ${BOOST_REVISION:+boost/$BOOST_VERSION-$BOOST_REVISION}                                 \\
-            ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} \\
-            ${FAIRLOGGER_REVISION:+FairLogger/$FAIRLOGGER_VERSION-$FAIRLOGGER_REVISION}             \\
-            ${FAIRMQ_REVISION:+FairMQ/$FAIRMQ_VERSION-$FAIRMQ_REVISION}                             \\
-            ${PPCONSUL_REVISION:+Ppconsul/$PPCONSUL_VERSION-$PPCONSUL_REVISION}                     \\
-            ${GRPC_REVISION:+grpc/$GRPC_VERSION-$GRPC_REVISION}                                     \\
-            ${O2_REVISION:+O2/$O2_VERSION-$O2_REVISION}                                             \\
-            ${MONITORING_REVISION:+Monitoring/$MONITORING_VERSION-$MONITORING_REVISION}
-
-# DataDistribution environment:
-set DATADISTRIBUTION_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-
-prepend-path PATH \$DATADISTRIBUTION_ROOT/bin
-
-# Not used for now:
-# prepend-path LD_LIBRARY_PATH \$DATADISTRIBUTION_ROOT/lib
-# prepend-path LD_LIBRARY_PATH \$DATADISTRIBUTION_ROOT/lib64
-# prepend-path ROOT_INCLUDE_PATH \$DATADISTRIBUTION_ROOT/include
-
-EoF
+alibuild-generate-module --bin --lib > etc/modulefiles/$PKGNAME
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
