88320ea1ab42b583f5ade4c2b1ff5d8e060d3653
Nikola Hardi <nhardi@cern.ch>
2020-11-17 10:54:55 +0100

    Minor fix in the JAliEn module file (#2617)

--- jalien.sh
+++ jalien.sh
@@ -32,7 +32,9 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 JDK/$JDK_VERSION-$JDK_REVISION XRootD/$XROOTD_VERSION-$XROOTD_REVISION
+module load BASE/1.0 JDK/$JDK_VERSION-$JDK_REVISION \\
+            XRootD/$XROOTD_VERSION-$XROOTD_REVISION \\
+            xjalienfs/$XJALIENFS_VERSION-$XJALIENFS_REVISION
 # Our environment
 set JALIEN_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path CLASSPATH \$JALIEN_ROOT/lib/alien-users.jar
