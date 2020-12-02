b901828c404adab9b995d2649a86bb1fec698a97
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-12-02 14:08:08 +0100

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -5,5 +5,5 @@ build_requires:
  - "GCC-Toolchain:(?!osx)"
 ---
 mkdir -p $INSTALLROOT/etc/modulefiles
-cp $GCC_TOOLCHAIN_ROOT/etc/modulesfiles/GCC-Toolchain $INSTALLROOT/etc/modulefiles/GCC-$GCC_VERSION
-ln -sf GCC-$GCC_VERSION $INSTALLROOT/etc/modulefiles/Toolchain
+cp $GCC_TOOLCHAIN_ROOT/etc/modulefiles/GCC-Toolchain $INSTALLROOT/etc/modulefiles/GCC-${GCC_TOOLCHAIN_VERSION//-*}
+ln -sf GCC-${GCC_TOOLCHAIN_VERSION//-*} $INSTALLROOT/etc/modulefiles/Toolchain
