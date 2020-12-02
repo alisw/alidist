2da12bf2594a1b84965ad42f1844682d581e8521
hristov <Peter.Hristov@cern.ch>
2020-12-02 11:36:22 +0100

    Bump FLUKA and FLUKA_VMC

--- fluka.sh
+++ fluka.sh
@@ -1,6 +1,6 @@
 package: FLUKA
 version: "%(tag_basename)s"
-tag: "2011-3.0-vmc2"
+tag: "2011-3.0-vmc3"
 source: https://gitlab.cern.ch/ALICEPrivateExternals/FLUKA.git
 requires:
   - "GCC-Toolchain:(?!osx)"
--- fluka_vmc.sh
+++ fluka_vmc.sh
@@ -1,6 +1,6 @@
 package: FLUKA_VMC
 version: "%(tag_basename)s"
-tag: "2011-3.0-vmc3"
+tag: "2011-3.0-vmc4"
 source: https://gitlab.cern.ch/ALICEPrivateExternals/FLUKA_VMC.git
 requires:
   - "GCC-Toolchain:(?!osx)"
