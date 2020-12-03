df21ea555a5184f622b196d7f016510c20dbaf17
Adrian Sevcenco <adriansev@users.noreply.github.com>
2020-12-03 11:09:06 +0200

    alien-cas:: update IGTF certs to 1.107 (#2660)

--- alien-cas.sh
+++ alien-cas.sh
@@ -1,6 +1,6 @@
 package: AliEn-CAs
 version: v1
-tag: 53fbc54de2fc99129eff587ac0dd6dc814b0439e
+tag: d04b129822b428a6acffaf1e5182b5c928f98174
 source: https://github.com/alisw/alien-cas.git
 ---
 #!/bin/bash -e
