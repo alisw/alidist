77a9a9716cead4ddeedf0917c7717117f830f349
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-12-11 15:26:54 +0100

    Update MC packages (in AliPhysics-v5-09-20-01_GEANT4-patches) to the latest versions: (#2401)

--- vgm.sh
+++ vgm.sh
@@ -1,6 +1,6 @@
 package: vgm
 version: "%(tag_basename)s"
-tag: "v4-4"
+tag: "v4-7"
 source: https://github.com/vmc-project/vgm
 requires:
   - ROOT
@@ -17,6 +17,12 @@ cmake "$SOURCEDIR" \
 
 make ${JOBS+-j $JOBS} install
 
+# Relocation of .cmake files
+for CMAKE in $(find "$INSTALLROOT/lib" -name '*.cmake'); do
+  sed -ideleteme -e "s!$ROOTSYS!\$ENV{ROOTSYS}!g; s!$G4INSTALL!\$ENV{G4INSTALL}!g" "$CMAKE"
+done
+find "$INSTALLROOT/lib" -name '*deleteme' -delete || true
+
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
@@ -32,7 +38,7 @@ module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@
 # Dependencies
 module load BASE/1.0 GEANT4/$GEANT4_VERSION-$GEANT4_REVISION ROOT/$ROOT_VERSION-$ROOT_REVISION
 # Our environment
-setenv VGM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path LD_LIBRARY_PATH \$::env(VGM_ROOT)/lib
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(VGM_ROOT)/lib")
+set VGM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv VGM_ROOT \$VGM_ROOT
+prepend-path LD_LIBRARY_PATH \$VGM_ROOT/lib
 EoF
