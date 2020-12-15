760a5499d249944da6c7a6d0643b778c3bb3d5ed
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-12-15 09:33:14 +0100

    alien-cas: preparations in order to get away from AliEN-Runtime (#2659)

--- alien-cas.sh
+++ alien-cas.sh
@@ -2,14 +2,21 @@ package: AliEn-CAs
 version: v1
 tag: d04b129822b428a6acffaf1e5182b5c928f98174
 source: https://github.com/alisw/alien-cas.git
+build_requires:
+- alibuild-recipe-tools
+env:
+  X509_CERT_DIR: "$ALIEN_CAS_ROOT/globus/share/certificates"
 ---
 #!/bin/bash -e
 DEST="$INSTALLROOT/globus/share/certificates"
 mkdir -p "$DEST"
-for D in $SOURCEDIR/*; do
-  [[ ! -d "$D" ]] && continue
-  rsync -a "$D/" "$DEST/"
-done
+find $SOURCEDIR -type d -maxdepth 1 -mindepth 1 -exec rsync -av {}/ $DEST \;
 
-# No Modulefile is needed: the following line acknowledges it (and tests pass)
-#%Module
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+alibuild-generate-module > $MODULEFILE
+cat >> "$MODULEFILE" <<EOF
+setenv X509_CERT_DIR \$::env(BASEDIR)/AliEn-CAs/\$version/globus/share/certificates
+EOF
