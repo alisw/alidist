36bad6ce1c21bc2573fb5828188a907241f0cc20
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2021-01-05 14:59:37 +0100

    Use AliceO2 weekly tags in dataflow defaults

--- defaults-o2-dataflow.sh
+++ defaults-o2-dataflow.sh
@@ -32,9 +32,6 @@ overrides:
         dryable==1.0.3
         responses==0.10.6
         PyYAML==5.1
-  O2:
-    version: "%(short_hash)s%(defaults_upper)s"
-    tag: dev
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
-version: "v1.3.0"
-tag: "v1.3.0"
+version: "%(tag_basename)s"
+tag: "v20.49"
 requires:
   - arrow
   - FairRoot
