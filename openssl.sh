7ddda2352c8d42e2648c9ad0cb8eb5578a504d8e
David Rohr <drohr@jwdt.org>
2021-01-12 20:43:43 +0100

    Bump OpenSSL

--- openssl.sh
+++ openssl.sh
@@ -1,10 +1,10 @@
 package: OpenSSL
-version: v1.0.2o
-tag: OpenSSL_1_0_2o
+version: v1.1.1i
+tag: OpenSSL_1_1_1i
 source: https://github.com/openssl/openssl
 prefer_system: (?!slc5|slc6)
 prefer_system_check: |
-  if [ `uname` = Darwin ]; then test -d `brew --prefix openssl || echo /dev/nope` || exit 1; fi; echo '#include <openssl/bio.h>' | c++ -x c++ - -I`brew --prefix openssl`/include -c -o /dev/null || exit 1; echo -e "#include <openssl/opensslv.h>\n#if OPENSSL_VERSION_NUMBER >= 0x10100000L\n#error \"System's GCC cannot be used: we need OpenSSL 1.0.x to build XrootD. We are going to compile our own version.\"\n#endif\nint main() { }" | cc -x c++ - -I`brew --prefix openssl`/include -c -o /dev/null || exit 1
+  if [ `uname` = Darwin ]; then test -d `brew --prefix openssl || echo /dev/nope` || exit 1; fi; echo '#include <openssl/bio.h>' | c++ -x c++ - -I`brew --prefix openssl`/include -c -o /dev/null || exit 1; echo -e "#include <openssl/opensslv.h>\n#if OPENSSL_VERSION_NUMBER < 0x1010109fL\n#error \"We require OpenSSL >= 1.1.1i.\"\n#endif\nint main() { }" | cc -x c++ - -I`brew --prefix openssl`/include -c -o /dev/null || exit 1
 build_requires:
  - zlib
  - "GCC-Toolchain:(?!osx)"
@@ -24,7 +24,6 @@ rsync -av --delete --exclude="**/.git" $SOURCEDIR/ .
          no-ecdh                                   \
          no-ecdsa                                  \
          no-asm                                    \
-         no-krb5                                   \
          shared                                    \
          -fno-strict-aliasing                      \
          -L"$INSTALLROOT/lib"                      \
