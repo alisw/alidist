4d19e436651e59edad0bc0b7c178582065e8e4a0
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2021-01-12 14:08:21 +0100

    Use VecGeom tag instead of commit hash

--- vecgeom.sh
+++ vecgeom.sh
@@ -1,6 +1,6 @@
 package: VecGeom
 version: "%(tag_basename)s"
-tag: 89a05d148cc708d4efc2e7b0eb6e2118d2610057
+tag: v1.1.8
 source: https://gitlab.cern.ch/VecGeom/VecGeom.git
 requires:
   - "GCC-Toolchain:(?!osx)"
