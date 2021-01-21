74d74a4072ca49d5d936696f43bc70c07147712c
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-01-21 09:53:38 +0100

    [O2-1976] Properly fix unloading Python module (#2774)

--- python.sh
+++ python.sh
@@ -115,5 +115,10 @@ alibuild-generate-module --bin --lib > "$MODULEFILE"
 cat >> "$MODULEFILE" <<EoF
 setenv PYTHONHOME \$PKG_ROOT
 prepend-path PYTHONPATH \$PKG_ROOT/lib/python/site-packages
-setenv SSL_CERT_FILE  [exec \$PKG_ROOT/bin/python3 -c "import certifi; print(certifi.where())"] 
+if { [module-info mode load] } {
+  setenv SSL_CERT_FILE  [exec \$PKG_ROOT/bin/python3 -c "import certifi; print(certifi.where())"]
+}
+if { [module-info mode remove] } {
+  unsetenv SSL_CERT_FILE
+}
 EoF
