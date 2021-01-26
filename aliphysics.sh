86d27b323fe7e26f3fc7029d8385d0cf46b9123a
ALICE Builder <alibuild@cern.ch>
2021-01-26 11:26:23 +0000

    Auto-update defaults-prod-latest.sh and aliphysics.sh

--- aliphysics.sh
+++ aliphysics.sh
@@ -1,6 +1,6 @@
 package: AliPhysics
 version: "%(commit_hash)s"
-tag: vAN-20210124
+tag: vAN-20210125
 requires:
   - AliRoot
   - RooUnfold
--- defaults-prod-latest.sh
+++ defaults-prod-latest.sh
@@ -7,7 +7,7 @@ env:
   CXXFLAGS: -fPIC -g -O2 -std=c++11
 overrides:
   AliPhysics:
-    tag: vAN-20210124
+    tag: vAN-20210125
     version: '%(tag_basename)s'
   AliRoot:
     requires:
