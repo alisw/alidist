17c503ba8fd8882ac38d06b098dfc71f8fadcd54
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-02-07 11:47:27 +0100

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -14,7 +14,6 @@ build_requires:
 prepend_path:
   ROOT_INCLUDE_PATH: "$BOOST_ROOT/include"
 ---
-#!/bin/bash -e
 BOOST_PYTHON=
 BOOST_CXXFLAGS=
 if [[ $ARCHITECTURE != osx* && $PYTHON_MODULES_VERSION ]]; then
