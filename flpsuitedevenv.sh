082a83dfedf11c6f128dc7d7cd4babec604ced4f
Barthelemy <barthelemy.von.haller@cern.ch>
2021-02-09 16:14:02 +0100

    introduction of a meta package for developing on an FLP

---
+++ flpsuitedevenv.sh
@@ -0,0 +1,32 @@
+package: FLPSuiteDevEnv
+version: "1.0.0"
+tag: "FLPSuiteDevEnv-1.0.0"
+requires:
+  - CMake
+  - O2-customization
+  - alibuild-recipe-tools
+  - ms_gsl
+  - RapidJSON
+  - GMP
+  - MPFR
+  - cub
+  - defaults-release
+  - abseil
+  - freeglut-devel
+  - OpenSSL
+  - abseil
+valid_defaults:
+  - o2
+  - o2-dataflow
+---
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+
+mkdir -p etc/modulefiles
+alibuild-generate-module --bin --lib > etc/modulefiles/$PKGNAME
+cat << EOF >> etc/modulefiles/$PKGNAME
+prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
+EOF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
