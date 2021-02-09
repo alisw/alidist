0cac4843601bc3d1ca97e1059c5391b0fff0c1f4
Teo Mrnjavac <teo.m@cern.ch>
2021-02-09 15:06:05 +0100

    Bump AliECS to v0.20.0

--- control-core.sh
+++ control-core.sh
@@ -1,6 +1,6 @@
 package: Control-Core
 version: "%(tag_basename)s"
-tag: "v0.19.90"
+tag: "v0.20.0"
 build_requires:
   - "GCC-Toolchain:(?!osx)"
   - golang
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v0.19.90"
+version: "v0.20.0"
 requires:
   - Control-Core
   - Control-OCCPlugin
