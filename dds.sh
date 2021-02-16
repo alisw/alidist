f2a72523138566f057d7f82ec975f4f0f8ab7765
Andrey Lebedev <andrey.lebedev@gsi.de>
2021-02-16 10:47:22 +0100

    Remove libboost_* also form lib64

--- dds.sh
+++ dds.sh
@@ -39,7 +39,7 @@ esac
 
 make -j$JOBS install
 
-find "$INSTALLROOT/lib" -name "libboost_*" -delete
+find "$INSTALLROOT/lib" "$INSTALLROOT/lib64" -name "libboost_*" -delete
 rm -f "$INSTALLROOT/LICENSE"
 
 # ModuleFile
