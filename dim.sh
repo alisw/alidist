037ce006e352f1e9b9e5337d4fc40409eee08f64
Kostas Alexopoulos <kostorr.alex@gmail.com>
2021-03-02 15:01:34 +0200

    Use alibuild-generate-module for dim (#2864)

--- dim.sh
+++ dim.sh
@@ -5,6 +5,7 @@ source: https://github.com/alisw/dim
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
+  - alibuild-recipe-tools
   - motif
   - system-curl
   - unzip
@@ -38,27 +39,12 @@ ln -nfs $(which make) buildbin/gmake
   gmake X64=yes )
 
 # Installation
-rsync -av dim/ $INSTALLROOT/dim/                # headers
-rsync -av --exclude "webDi*" WebDID/ $INSTALLROOT/WebDID/ #webdid without executables
-rsync -av --exclude "*.o" linux/ $INSTALLROOT/  # executables and libraries
+rsync -av dim $INSTALLROOT/include                              # headers
+rsync -av --exclude '*.o' --exclude '*.so' --exclude '*.a'  linux/ $INSTALLROOT/bin/  # executables
+rsync -av linux/*.so linux/*.a $INSTALLROOT/lib/                # libraries
+rsync -av --exclude "webDi*" WebDID/ $INSTALLROOT/WebDID/       #webdid without executables
 
 # Modulefile
 mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
-# Our environment
-set DIM_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv DIM_ROOT \$DIM_ROOT
-set BASEDIR \$::env(BASEDIR)
-prepend-path PATH \$BASEDIR/$PKGNAME/\$version
-prepend-path LD_LIBRARY_PATH \$BASEDIR/$PKGNAME/\$version
-EoF
+alibuild-generate-module --bin --lib > etc/modulefiles/$PKGNAME
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
