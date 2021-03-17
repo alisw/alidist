b58c50bd452f0c5871d3f1c2d17d09b05e9ba598
ALICE Builder <alibuild@cern.ch>
2021-03-17 15:40:20 +0000

    Auto-update defaults-next-root6.sh and aliphysics.sh

--- aliphysics.sh
+++ aliphysics.sh
@@ -1,6 +1,6 @@
 package: AliPhysics
 version: "%(commit_hash)s"
-tag: "vAN-20210316"
+tag: "vAN-20210317"
 requires:
   - AliRoot
   - RooUnfold
--- defaults-next-root6.sh
+++ defaults-next-root6.sh
@@ -6,7 +6,7 @@ env:
   CXXFLAGS: -fPIC -g -O2 -std=c++11
 overrides:
   AliPhysics:
-    tag: vAN-20210316
+    tag: vAN-20210317
     version: '%(tag_basename)s_ROOT6'
   AliRoot:
     tag: v5-09-57c
