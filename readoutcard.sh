8c307e55377ec8b689fee74a1b3af9cefb177b52
Kostas Alexopoulos <kostorr.alex@gmail.com>
2021-03-18 18:31:26 +0200

    Require PDA 1.2.0 for ReadoutCard

--- readoutcard.sh
+++ readoutcard.sh
@@ -57,7 +57,7 @@ mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INS
 
 # External RPM dependencies
 cat > $INSTALLROOT/.rpm-extra-deps <<EoF
-pda-kadapter-dkms >= 1.1.0
+pda-kadapter-dkms >= 1.2.0
 libhugetlbfs
 libhugetlbfs-utils
 EoF
