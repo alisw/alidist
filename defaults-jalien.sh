646aed8b16998a7649170b14ff0a3464fd55e915
ALICE Builder <alibuild@cern.ch>
2021-03-20 17:21:35 +0000

    Auto-update defaults-jalien.sh and aliphysics.sh

--- defaults-jalien.sh
+++ defaults-jalien.sh
@@ -1,20 +1,21 @@
-package: defaults-jalien
-version: v1
 disable:
-  - arrow
+- arrow
 env:
-  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
-  CFLAGS: "-fPIC -g -O2"
-  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  CFLAGS: -fPIC -g -O2
+  CMAKE_BUILD_TYPE: RELWITHDEBINFO
+  CXXFLAGS: -fPIC -g -O2 -std=c++11
 overrides:
-  AliRoot:
-    version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-56
   AliPhysics:
-    version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-56-01
+    tag: vAN-20210320
+    version: '%(tag_basename)s_JALIEN'
+  AliRoot:
+    tag: v5-09-56
+    version: '%(tag_basename)s_JALIEN'
   fastjet:
     tag: v3.3.4_1.045-alice1
+package: defaults-jalien
+version: v1
+
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
