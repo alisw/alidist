f2e35fb72d0e08e025260be945ebeadbbee752b8
Markus Fasel <markus.fasel@cern.ch>
2021-03-20 19:28:56 +0100

    Allow AliRoot and AliPhysics to be built using ninja when in development mode
    Mehtod to detect and select ninja for development
    mode ported 1-1 from the O2 build recipe.

--- aliroot.sh
+++ aliroot.sh
@@ -55,6 +55,14 @@ if [ $FVERSION -ge 10 ]; then
    echo "Fortran version $FVERSION"
    SPECIALFFLAGS=1
 fi
+# Use ninja if in devel mode, ninja is found and DISABLE_NINJA is not 1
+if [[ ! $CMAKE_GENERATOR && $DISABLE_NINJA != 1 && $DEVEL_SOURCES != $SOURCEDIR ]]; then
+  NINJA_BIN=ninja-build
+  type "$NINJA_BIN" &> /dev/null || NINJA_BIN=ninja
+  type "$NINJA_BIN" &> /dev/null || NINJA_BIN=
+  [[ $NINJA_BIN ]] && CMAKE_GENERATOR=Ninja || true
+  unset NINJA_BIN
+fi
 
 cmake $SOURCEDIR                                                     \
       -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"                          \
