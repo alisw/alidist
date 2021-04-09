503c6b464209e43ee6c7d179b06ab40d1b87607e
David Rohr <drohr@cern.ch>
2021-04-09 11:05:27 +0200

    Dump simulation log during full system test CI (#2952)

--- o2-full-system-test.sh
+++ o2-full-system-test.sh
@@ -18,7 +18,7 @@ export JOBUTILS_PRINT_ON_ERROR=1
 export JOBUTILS_JOB_TIMEOUT=900
 export NHBPERTF=128
 export SHMSIZE=8000000000
-NEvents=5 NEventsQED=100 O2SIMSEED=12345 $O2_ROOT/prodtests/full_system_test.sh
+ALICE_O2SIM_DUMPLOG=1 NEvents=5 NEventsQED=100 O2SIMSEED=12345 $O2_ROOT/prodtests/full_system_test.sh
 $O2_ROOT/prodtests/full_system_test_ci_extra_tests.sh
 popd
 rm -Rf $BUILDDIR/full-system-test-sim
