f51e531b2841fd2b9fbece48277aee77be9a7d50
saganatt <8majak8@gmail.com>
2021-04-25 23:40:07 +0200

    Updated onnx to custom git

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -1,25 +1,47 @@
 package: onnxruntime
-version: "%(tag_basename)s"
-tag: 1.7.2
-source: https://github.com/microsoft/onnxruntime.git
+version: "1.7.2"
+tag: c073c88ef
+source: https://github.com/saganatt/onnxruntime.git
+requires:
+  - protobuf
+  - re2
 build_requires:
- - "GCC-Toolchain:(?!osx)"
- - CMake
- - Python-modules
- - re2
- - protobuf
+  - CMake
 ---
-# TODO: should be: git clone --recurse-submodules, how to pass git option?
+#!/bin/bash -e
+
+pushd $SOURCEDIR
+  git submodule update --init -- cmake/external/date
+  git submodule update --init -- cmake/external/mp11
+  git submodule update --init -- cmake/external/onnx
+  git submodule update --init -- cmake/external/optional-lite
+  git submodule update --init -- cmake/external/eigen
+  git submodule update --init -- cmake/external/nsync
+  git submodule update --init -- cmake/external/flatbuffers
+  git submodule update --init -- cmake/external/SafeInt
+  git submodule update --init -- cmake/external/json
+popd
 
 mkdir -p $INSTALLROOT
-cmake -Donnxruntime_DEV_MODE=OFF \
-      -Donnxruntime_PREFER_SYSTEM_LIB=ON \
-      -Donnxruntime_ENABLE_PYTHON=ON \
+
+# NOTE: It builds its own flatbuffers, eigen, nsync, pybind11
+cmake "$SOURCEDIR/cmake" \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
       -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
-      -Deigen_SOURCE_PATH=/usr/include/eigen3 \
-      -Donnxruntime_USE_PREINSTALLED_EIGEN=ON \
+      -Donnxruntime_DEV_MODE=OFF \
+      -Donnxruntime_BUILD_UNIT_TESTS=OFF \
+      -Donnxruntime_ENABLE_PYTHON=ON \
+      -DPYTHON_EXECUTABLE=$(python3 -c "import sys; print(sys.executable)") \
+      -Donnxruntime_PREFER_SYSTEM_LIB=ON \
       -Donnxruntime_BUILD_SHARED_LIB=ON \
-      -DCMAKE_INSTALL_PREFIX="$INSTALLROOT" "$SOURCEDIR/cmake"
+      -DProtobuf_USE_STATIC_LIBS=ON \
+      ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a} \
+      ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a} \
+      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a} \
+      ${PROTOBUF_ROOT:+-DProtobuf_INCLUDE_DIR=$PROTOBUF_ROOT/include} \
+      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc} \
+      -Donnxruntime_USE_PREINSTALLED_NSYNC=OFF \
+      ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}
 
 make ${JOBS:+-j $JOBS}
 make install
