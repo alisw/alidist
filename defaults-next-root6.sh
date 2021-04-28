55f7fb5345448182a7174e85c8ca3578ccb5d27f
Peter Hristov <pzhristov@users.noreply.github.com>
2021-04-28 08:59:12 +0200

    Additional flags (align with O2 defaults) (#2987)
    Co-authored-by: hristov <Peter.Hristov@cern.ch>

--- defaults-next-root6.sh
+++ defaults-next-root6.sh
@@ -6,6 +6,8 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  CXXSTD: "11"
+  ENABLE_VMC: "ON"
 overrides:
   AliRoot:
     version: "%(tag_basename)s_ROOT6"
