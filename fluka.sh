21adff2194ca90ef1ff2434c9e4db5052b185396
Peter Hristov <pzhristov@users.noreply.github.com>
2021-04-28 11:19:07 +0200

    Bump FLUKA to 4-1.1-vmc2 (#2991)
    Co-authored-by: hristov <Peter.Hristov@cern.ch>

--- fluka.sh
+++ fluka.sh
@@ -1,6 +1,6 @@
 package: FLUKA
 version: "%(tag_basename)s"
-tag: "4-1.1-vmc1"
+tag: "4-1.1-vmc2"
 source: https://gitlab.cern.ch/ALICEPrivateExternals/FLUKA.git
 requires:
   - "GCC-Toolchain:(?!osx)"
