6942e23a8f639cea6f670f32517981cff4a82805
ALICE Builder <alibuild@cern.ch>
2021-06-09 05:54:33 +0000

    Auto-update defaults-o2.sh and o2.sh

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -23,7 +23,7 @@ overrides:
     tag: v10.2.0-alice2
     version: v10.2.0-alice2
   O2:
-    tag: nightly-20210608
+    tag: nightly-20210609
     version: '%(tag_basename)s'
   XRootD:
     source: https://github.com/xrootd/xrootd
