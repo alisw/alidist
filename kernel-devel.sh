6c1c5c1fde5baa19a44cddec24e18041c67d0490
Michael Lettrich <MichaelLettrich@users.noreply.github.com>
2021-06-30 08:28:50 +0200

    Make kernel-devel receipe compatible with Ubuntu (#3137)

--- kernel-devel.sh
+++ kernel-devel.sh
@@ -1,8 +1,11 @@
 package: kernel-devel
 version: "1.0"
 
-system_requirement_missing: "Please install kernel-devel pciutils-devel kmod-devel"
+system_requirement_missing: |
+  Kernel development packages are missing on your system:
+  * RHEL-compatible systems: Please install kernel-devel, pciutils-devel and kmod-devel
+  * Ubuntu-compatible systems: Please install linux-headers-`uname -r` , libpci-dev, and libkmod-dev
 system_requirement: ".*"
-system_requirement_check: "find /usr/src -name kernel.h | grep include/linux/kernel.h  > /dev/null 2>&1 && ls /usr/include/libkmod.h  > /dev/null 2>&1 && ls /usr/include/pci/pci.h > /dev/null 2>&1"
+system_requirement_check: "find /usr/src -name kernel.h | grep include/linux/kernel.h  > /dev/null 2>&1 && ls /usr/include/libkmod.h  > /dev/null 2>&1 && find /usr/include -name pci.h | grep pci/pci.h  > /dev/null 2>&1"
 
 ---
