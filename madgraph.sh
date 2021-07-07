a7fcf58cfab7afbf617651149b65bd11fd9b86ee
mfasel <markus.fasel@cern.ch>
2021-07-07 11:53:43 +0200

    Installing HEPtools dependencies
    via installer script
    - ninja
    - collier
    - oneloop
    shipped, but need to be installed
    manually as script return to prompt:
    - CutTools
    - IREGI

--- madgraph.sh
+++ madgraph.sh
@@ -15,6 +15,12 @@ build_requires:
 
 rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
 
+# Create empty template directory for MadLoop5 resources which is needed 
+# during processing but cannot be part of the MadGraph repository itself
+# as git does not track empty directories
+mkdir -p Template/loop_material/StandAlone/SubProcesses/MadLoop5_resources
+
+# Do some basic initialization of MadGraph
 echo "generate e+ e- > u u~" >> test.mg
 echo "output" >> test.mg
 echo "quit()" >> test.mg
@@ -22,6 +28,32 @@ echo "quit()" >> test.mg
 bin/mg5_aMC -f test.mg
 rm -rf PROC_sm_0
 rm test.mg
+
+# install dependency on other packages
+# Needed at least: 
+# - oneloop
+# - collier
+# - ninja
+heptools=(collier oneloop ninja)
+for tool in ${heptools[@]}; do
+  echo "install $tool" >> install.mg
+done
+
+bin/mg5_aMC -f install.mg
+rm install.mg
+rm vendor/*.tar.gz
+heptools=(collier oneloop ninja)
+for tool in ${heptools[@]}; do
+  rm HEPTools/$tool/$tool\_install.log
+done
+
+# install cuttools and IREGI
+MGBASE=$PWD
+cd $MGBASE/vendor/CutTools
+make
+cd $MGBASE/vendor/IREGI/src
+make
+cd $MGBASE
 
 rsync -a --exclude='**/.git' --delete --delete-excluded ./ $INSTALLROOT/
 
