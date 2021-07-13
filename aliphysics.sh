1b03707604b7f27a798d8c29f689b7d6c2edff77
ALICE Builder <alibuild@cern.ch>
2021-07-13 15:59:58 +0000

    Auto-update defaults-next-root6.sh and aliphysics.sh

--- aliphysics.sh
+++ aliphysics.sh
@@ -1,6 +1,6 @@
 package: AliPhysics
 version: "%(commit_hash)s"
-tag: "vAN-20210712"
+tag: "vAN-20210713"
 requires:
   - AliRoot
   - RooUnfold
--- defaults-next-root6.sh
+++ defaults-next-root6.sh
@@ -8,7 +8,7 @@ env:
   ENABLE_VMC: 'ON'
 overrides:
   AliPhysics:
-    tag: vAN-20210712
+    tag: vAN-20210713
     version: '%(tag_basename)s_ROOT6'
   AliRoot:
     tag: v5-09-57g
