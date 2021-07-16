5afc587ac3895e6daa55ed297e4bb1eecb950866
Dennis Klein <d.klein@gsi.de>
2021-07-16 18:34:28 +0200

    Add new FairCMakeModules v0.2.0

---
+++ faircmakemodules.sh
@@ -0,0 +1,32 @@
+package: FairCMakeModules
+version: "%(tag_basename)s"
+tag: v0.2.0
+source: https://github.com/FairRootGroup/FairCMakeModules
+build_requires:
+  - CMake
+---
+#!/bin/sh
+
+cmake -S $SOURCEDIR -B .                                        \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                       \
+      ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"}                 \
+      ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}
+
+cmake --build . --target install
+
+# Modulefile
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+set FAIRCMAKEMODULES_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
