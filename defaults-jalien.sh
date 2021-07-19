9c7bea7d679bfd41125cafe10407b75d2feda228
Bernhard Hohlweger <hohlweger@mytum.de>
2021-07-19 08:26:11 +0200

    test to bump fastjet to 3.4.0 and fjcontrib to 1.045 (#3155)
    Co-authored-by: bernie <bhohlweg@cern.ch>

--- defaults-jalien.sh
+++ defaults-jalien.sh
@@ -14,7 +14,7 @@ overrides:
     version: "%(tag_basename)s_JALIEN"
     tag: v5-09-56-01
   fastjet:
-    tag: v3.3.4_1.045-alice1
+    tag: v3.4.0_1.045-alice1
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
