31d4255f052c4b2a2cc70acdb8d9467b4198eb8b
Peter Hristov <pzhristov@users.noreply.github.com>
2021-08-03 07:07:24 +0200

    Use v5-09-57h (Root v6-24-02) (#3204)
    Co-authored-by: hristov <Peter.Hristov@cern.ch>

--- defaults-jalien.sh
+++ defaults-jalien.sh
@@ -9,10 +9,10 @@ env:
 overrides:
   AliRoot:
     version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-56
+    tag: v5-09-57h
   AliPhysics:
     version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-56-01
+    tag: v5-09-57h-01
   fastjet:
     tag: v3.4.0_1.045-alice1
 ---
