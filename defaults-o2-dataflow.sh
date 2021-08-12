076892b3e1a6fc600ac667ee864439b00aba99a5
Barthelemy <barthelemy.von.haller@cern.ch>
2021-08-12 08:48:23 +0200

    Add missing psutil python module.

--- defaults-o2-dataflow.sh
+++ defaults-o2-dataflow.sh
@@ -43,12 +43,16 @@ overrides:
         responses==0.10.6
         PyYAML==5.1
         python-consul==1.1.0
+        psutil==5.8.0
       PIP36_REQUIREMENTS: |
         python-consul==1.1.0
+        psutil==5.8.0
       PIP38_REQUIREMENTS: |
         python-consul==1.1.0
+        psutil==5.8.0
       PIP39_REQUIREMENTS: |
         python-consul==1.1.0
+        psutil==5.8.0
   O2-customization:
     env:
       ENABLE_UPGRADES: OFF # Disable detector upgrades in O2
