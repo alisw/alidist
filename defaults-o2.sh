8dde9d7fe8820a7a7eb9d48fa24f68a63e61657c
ALICE Builder <alibuild@cern.ch>
2021-08-14 05:17:24 +0000

    Auto-update defaults-o2.sh and o2physics.sh

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -26,7 +26,7 @@ overrides:
     tag: nightly-20210728
     version: '%(tag_basename)s'
   O2Physics:
-    tag: nightly-20210727
+    tag: nightly-20210814
     version: '%(tag_basename)s'
   XRootD:
     source: https://github.com/xrootd/xrootd
