2827bbd524cb04f25af539a0b94b9b9063ebc785
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-08-19 11:34:24 +0200

    Update defaults-generators.sh

--- defaults-generators.sh
+++ defaults-generators.sh
@@ -5,8 +5,6 @@ env:
   CMAKE_BUILD_TYPE: RELWITHDEBINFO
   CXXFLAGS: -fPIC -g -O2 -std=c++14
 overrides:
-  AliGenerators:
-    tag: vAN-20210812
   boost:
     requires:
     - GCC-Toolchain:(?!osx)
