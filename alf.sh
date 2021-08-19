a43a9e3c956e7b76e358c9ad8c23ed2675ab5d6a
Kostas Alexopoulos <kostorr.alex@gmail.com>
2021-08-19 10:14:41 +0200

    Bump ReadoutCard to v0.32.3 and ALF to v0.14.1 (#3236)

--- alf.sh
+++ alf.sh
@@ -1,6 +1,6 @@
 package: ALF
 version: "%(tag_basename)s"
-tag: v0.14.0
+tag: v0.14.1
 requires:
   - boost
   - Common-O2
--- readoutcard.sh
+++ readoutcard.sh
@@ -1,6 +1,6 @@
 package: ReadoutCard
 version: "%(tag_basename)s"
-tag: v0.32.1
+tag: v0.32.3
 requires:
   - boost
   - "GCC-Toolchain:(?!osx)"
