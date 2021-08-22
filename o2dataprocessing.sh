220fd53faf6f32f6eb0af1a052f6b6fd991c9ba2
David Rohr <drohr@cern.ch>
2021-08-22 08:47:54 +0200

    Add O2DataProcessing recipe (#3246)

---
+++ o2dataprocessing.sh
@@ -0,0 +1,19 @@
+package: O2DataProcessing
+version: "%(tag_basename)s"
+tag: main
+source: https://github.com/AliceO2Group/O2DataProcessing.git
+build_requires:
+  - alibuild-recipe-tools
+---
+#!/bin/bash -e
+rsync -a --exclude='**/.git' --delete --delete-excluded \
+      $SOURCEDIR/ $INSTALLROOT/
+
+# Modulefile
+mkdir -p $INSTALLROOT/etc/modulefiles
+alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
+
+cat << EOF >> $INSTALLROOT/etc/modulefiles/$PKGNAME
+set O2DATAPROCESSING_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv O2DATAPROCESSING_ROOT \$O2DATAPROCESSING_ROOT
+EOF
