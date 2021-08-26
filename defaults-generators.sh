d351e04033e686f4f9e94cb60ac9d6f835b3933c
ALICE Builder <alibuild@cern.ch>
2021-08-26 16:18:53 +0000

    Auto-update defaults-generators.sh and aligenerators.sh

--- defaults-generators.sh
+++ defaults-generators.sh
@@ -5,6 +5,8 @@ env:
   CMAKE_BUILD_TYPE: RELWITHDEBINFO
   CXXFLAGS: -fPIC -g -O2 -std=c++14
 overrides:
+  AliGenerators:
+    tag: vAN-20210826
   boost:
     requires:
     - GCC-Toolchain:(?!osx)
