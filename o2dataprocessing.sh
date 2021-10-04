eddc5e462bc7f03ad0c6042049954cd74178e107
David Rohr <drohr@jwdt.org>
2021-10-04 09:56:57 +0200

    Bump O2DataProcessing to v0.12

--- o2dataprocessing.sh
+++ o2dataprocessing.sh
@@ -1,6 +1,6 @@
 package: O2DataProcessing
 version: "%(tag_basename)s"
-tag: v0.11
+tag: v0.12
 source: https://github.com/AliceO2Group/O2DataProcessing.git
 build_requires:
   - alibuild-recipe-tools
