9d6262ad96f6675b984f70de6ddc6cc13debcc11
Christian Holm Christensen <Christian.Holm.Christensen@cern.ch>
2021-10-04 09:45:19 +0200

    TEST: First go at making package for AliRivetTask (#2990)
    * Test : First go at making package for AliRivetTask
    
    * Update rivettask.sh
    
    Change name of package from AliRivetTask to RivetTask to match `rivettask.sh`

---
+++ rivettask.sh
@@ -0,0 +1,46 @@
+package: RivetTask
+version: "%{year}s%{month}s%{day}s"
+source: https://gitlab.cern.ch/cholm/alice-rivet-task
+requires:
+  - Rivet
+  - AliRoot 
+build_requires:
+  - GCC-Toolchain:(?!osx)
+  - alibuild-recipe-tools
+---
+#!/bin/bash -e
+
+# Echo all commands
+set -x
+
+# Picking up ROOT from the system when ours is disabled
+[[ -z "$ROOT_ROOT" ]] && ROOT_ROOT="$(root-config --prefix)"
+
+# Print environment to see what is going on - debugging
+printenv
+
+# Copy code here 
+cp ${SOURCEDIR}/AliRivetTask.C .
+cp ${SOURCEDIR}/Build.C .
+
+# Use AcLic to build 
+root -l -b -q Build.C
+
+# Install binary, scripts, and code 
+mkdir -p ${INSTALLROOT}/lib
+mkdir -p ${INSTALLROOT}/include
+mkdir -p ${INSTALLROOT}/share
+cp ${SOURCEDIR}/AliRivetTask.C p ${INSTALLROOT}/include
+cp ${SOURCEDIR}/AliRivetTask_C*  ${INSTALLROOT}/lib/
+cp ${SOURCEDIR}/AddTaskRivet.C   ${INSTALLROOT}/share/
+cp ${SOURCEDIR}/RivetConfig.C    ${INSTALLROOT}/share/
+
+#ModuleFile
+mkdir -p modulefiles
+alibuild-generate-module > etc/modulefiles/$PKGNAME
+cat >> modulefiles/${PKGNAME} <<EOF
+
+EOF
+mkdir -p $INSTALLROOT/etc/modulefiles && \
+    rsync -a --delete modulefiles/ $INSTALLROOT/etc/modulefiles
+
