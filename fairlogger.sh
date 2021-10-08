4e8308af525803fcfdd3b29d86027a929b2500df
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-10-08 19:32:01 +0200

    fairlogger: do not build tests (#3400)

--- fairlogger.sh
+++ fairlogger.sh
@@ -29,6 +29,7 @@ cmake $SOURCEDIR                                                 \
       ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER}        \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}  \
       ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}                    \
+      -DBUILD_TESTING=OFF                                        \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                        \
       -DDISABLE_COLOR=ON                                         \
       -DUSE_EXTERNAL_FMT=ON                                      \
