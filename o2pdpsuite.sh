018b9b995319b1bcef55c6bfdc10e3e73720c5f2
ALICE Builder <alibuild@users.noreply.github.com>
2021-10-13 09:18:41 +0200

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2.sh o2physics.sh (#3416)

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20211012"
+tag: "nightly-20211013"
 requires:
   - O2
   - DataDistribution
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "nightly-20211012"
+tag: "nightly-20211013"
 requires:
   - O2
   - ONNXRuntime
