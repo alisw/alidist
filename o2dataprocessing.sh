ed1aac886bf165fd829f470c2d46ac1a5d00c61e
ALICE Builder <alibuild@cern.ch>
2021-11-05 02:42:41 +0000

    Auto-update: defaults-o2-dataflow.sh o2pdpsuite.sh o2.sh

--- o2dataprocessing.sh
+++ o2dataprocessing.sh
@@ -1,6 +1,6 @@
 package: O2DataProcessing
 version: "%(tag_basename)s"
-tag: v0.19
+tag: v0.20
 source: https://github.com/AliceO2Group/O2DataProcessing.git
 build_requires:
   - alibuild-recipe-tools
