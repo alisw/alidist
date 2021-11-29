3247b35067eaad1e7ecc6778bd0637f168247a6c
benedikt-voelkel <benedikt.voelkel@gmx.de>
2021-11-29 10:17:17 +0100

    Introduce VMC recipe (#3598)
    Co-authored-by: Benedikt Volkel <benedikt.volkel@cern.ch>

--- mcsteplogger.sh
+++ mcsteplogger.sh
@@ -1,10 +1,11 @@
 package: MCStepLogger
 version: "%(tag_basename)s"
-tag: "v0.2.0"
+tag: "v0.3.0"
 source: https://github.com/AliceO2Group/VMCStepLogger.git
 requires:
   - "GCC-Toolchain:(?!osx)"
   - ROOT
+  - VMC
   - boost
 build_requires:
   - CMake
