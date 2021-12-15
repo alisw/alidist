560735c39d1e518c5007132a3f3f0872934c8ebf
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-12-15 12:47:00 +0100

    Update openssl.sh

--- openssl.sh
+++ openssl.sh
@@ -1,6 +1,6 @@
 package: OpenSSL
-version: v1.0.2o
-tag: OpenSSL_1_0_2o
+version: v1.1.1m
+tag: OpenSSL_1_1_1m
 source: https://github.com/openssl/openssl
 prefer_system: (?!slc5|slc6)
 prefer_system_check: |
@@ -37,24 +37,13 @@ make install_sw # no not install man pages
 rm -rf $INSTALLROOT/lib/pkgconfig \
        $INSTALLROOT/lib/*.a
 
-
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
+
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${ZLIB_REVISION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION} ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
-# Our environment
-set OPENSSL_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv OPENSSL_ROOT \$OPENSSL_ROOT
-prepend-path PATH \$OPENSSL_ROOT/bin
-prepend-path LD_LIBRARY_PATH \$OPENSSL_ROOT/lib
-EoF
+alibuild-generate-module --lib --bin > etc/modulefiles/$PKGNAME
+cat << EOF >> etc/modulefiles/$PKGNAME
+prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
+EOF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
