685ddc007391874afaf7c47abb0092b5da5f7e4c
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-12-16 09:39:12 +0100

    Modernise freetype recipe
    - Get rid  of the alien-runtime non-sense
    - Generate the module automatically
    - Use github mirror, rather than relying on curl

--- freetype.sh
+++ freetype.sh
@@ -1,21 +1,19 @@
 package: FreeType
-version: v2.10.1
+version: v2.11.1
+tag: v2-11-1
+source: https://github.com/freetype/freetype
 requires:
- - AliEn-Runtime:(?!.*ppc64)
+  - zlib
 build_requires:
   - "autotools:(slc6|slc7)"
-  - system-curl
 prefer_system: (?!slc5)
 prefer_system_check: |
   printf "#include <ft2build.h>\n" | c++ -xc++ - `freetype-config --cflags` -c -M 2>&1;
   if [ $? -ne 0 ]; then printf "FreeType is missing on your system.\n * On RHEL-compatible systems you probably need: freetype freetype-devel\n * On Ubuntu-compatible systems you probably need: libfreetype6 libfreetype6-dev\n"; exit 1; fi
 ---
 #!/bin/bash -ex
-URL="http://download.savannah.gnu.org/releases/freetype/freetype-${PKGVERSION:1}.tar.gz"
-curl -L -o freetype.tgz $URL
-tar xzf freetype.tgz
-rm -f freetype.tgz
-cd freetype-${PKGVERSION:1}
+rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
+sh ./autogen.sh
 ./configure --prefix=$INSTALLROOT                \
             --with-png=no                        \
             ${ZLIB_ROOT:+--with-zlib=$ZLIB_ROOT}
@@ -26,19 +24,7 @@ make install
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
-mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 $([[ "$ALIEN_RUNTIME_VERSION" ]] && echo AliEn-Runtime/$ALIEN_RUNTIME_VERSION-$ALIEN_RUNTIME_REVISION || echo ${ZLIB_REVISION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION})
-# Our environment
-set FREETYPE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv FREETYPE_ROOT \$FREETYPE_ROOT
-prepend-path LD_LIBRARY_PATH \$FREETYPE_ROOT/lib
-EoF
+
+mkdir -p etc/modulefiles
+alibuild-generate-module --lib > etc/modulefiles/$PKGNAME
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
