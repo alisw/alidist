0d34e4ddbcc068479b32aadbad23bdab43e5ddc7
ALICE Builder <alibuild@cern.ch>
2021-12-21 02:49:08 +0000

    Auto-update: defaults-o2-dataflow.sh o2pdpsuite.sh o2.sh o2dpg.sh

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "nightly-20211216"
+tag: "dataflow-20211221"
 requires:
   - arrow
   - FairRoot
--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20211216"
+tag: "dataflow-20211221"
 requires:
   - O2
   - DataDistribution
