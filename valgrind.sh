58d5f02cb720f2688e9e786fb50a9aa925b3e3d1
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-01-13 10:26:09 +0100

    Add own valgrind
    Apparently compiling with GCC 10.2 and using system valgrind creates troubles on some systems (e.g. Ubuntu 18.04).

---
+++ valgrind.sh
@@ -0,0 +1,30 @@
+package: valgrind
+source: git://sourceware.org/git/valgrind.git
+version: "3.18.1"
+tag: VALGRIND_3_18_1
+build_requires:
+- autotools
+- GCC-Toolchain
+- alibuild-recipe-tools
+---
+
+rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
+CONF_OPTS="--enable-only64bit"
+case $ARCHITECTURE in
+  osx*)
+    CFLAGS="-D__private_extern__=extern"
+    ;;
+esac
+
+./autogen.sh
+./configure --prefix=$INSTALLROOT --without-mpicc --disable-static ${CONF_OPTS}
+make -j 20
+make install
+
+#ModuleFile
+mkdir -p etc/modulefiles
+alibuild-generate-module --bin --lib > etc/modulefiles/$PKGNAME
+cat >> etc/modulefiles/$PKGNAME <<EoF
+prepend-path VALGRIND_LIB \$PKG_ROOT/libexec/valgrind
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
