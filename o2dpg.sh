665c1119b31d8b4ccf63b1f3be307002e101a788
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-01-14 09:31:09 +0100

    Update o2dpg.sh (#3731)

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: dataflow-20220113
+tag: nightly-20220114
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
