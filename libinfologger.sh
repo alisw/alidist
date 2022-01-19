8f9a4505ba6afdfad46fd910a2a2f06084e14697
David Rohr <drohr@jwdt.org>
2022-01-19 22:01:18 +0100

    Add missing dependency to alibuild-recipe-tools

--- libinfologger.sh
+++ libinfologger.sh
@@ -6,6 +6,7 @@ requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
   - CMake
+  - alibuild-recipe-tools
 source: https://github.com/AliceO2Group/InfoLogger
 incremental_recipe: |
   make ${JOBS:+-j$JOBS} install
