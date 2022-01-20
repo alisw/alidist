532b8c6cf506b44ae4e9c18fa3b2b95fdf4b2849
ALICE Builder <alibuild@cern.ch>
2022-01-20 16:52:27 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh o2dpg.sh

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "daily-20220116-1557"
+tag: "daily-20220120-1559"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
