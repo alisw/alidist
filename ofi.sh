a7ff72746e67d826bc9a6c069e12244b5bd83e68
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-01-25 22:56:45 +0100

    Update ofi to prevent issues with the new version on old platforms (#3772)

--- ofi.sh
+++ ofi.sh
@@ -1,36 +1,22 @@
 package: ofi
 version: "%(tag_basename)s"
-tag: "v1.7.1"
+tag: "v1.14.0"
 source: https://github.com/ofiwg/libfabric
 build_requires:
   - autotools
   - "GCC-Toolchain:(?!osx)"
+  - alibuild-recipe-tools
 prefer_system: ".*"
 prefer_system_check: |
-  pkg-config --atleast-version=1.6.0 libfabric 2>&1 && printf "#include \"rdma/fabric.h\"\nint main(){}" | c++ -xc - -o /dev/null
+  pkg-config --atleast-version=1.6.0 libfabric 2>&1 && printf "#include \"rdma/fabric.h\"\nint main(){}" | cc -xc - -o /dev/null
 ---
 rsync -a --exclude='**/.git' --delete --delete-excluded "$SOURCEDIR/" ./
 autoreconf -ivf
-./configure --prefix="$INSTALLROOT" --enable-mlx=no 
+./configure --prefix="$INSTALLROOT" --enable-mlx=no
 make ${JOBS:+-j $JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0
-# Our environment
-set OFI_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv OFI_ROOT \$OFI_ROOT
-prepend-path PATH \$OFI_ROOT/bin
-prepend-path LD_LIBRARY_PATH \$OFI_ROOT/lib
-EoF
+alibuild-generate-module --bin --lib > $MODULEFILE
