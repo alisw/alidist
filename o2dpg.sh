c2b07989ac5ca8fc77fb3bd2d2bb284cd8a3f1f7
ALICE Builder <alibuild@cern.ch>
2022-01-29 05:50:20 +0000

    Auto-update: defaults-o2-dataflow.sh o2pdpsuite.sh o2.sh o2dpg.sh

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "daily-20220120-1559"
+tag: "dataflow-20220129"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
