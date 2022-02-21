625b1d974a84378ae58618ee8f6b36903c903b63
jan-busa <48282469+jan-busa@users.noreply.github.com>
2022-02-21 18:02:14 +0300

    Allowed to choose precision in FFTW3 (#3856)
    Co-authored-by: Jan Busa Jr <busa@jinr.ru>

--- fftw3.sh
+++ fftw3.sh
@@ -8,21 +8,37 @@ build_requires:
   - CMake
 ---
 #!/bin/bash -e
+export FFTW3_WITH_AVX # use AVX with float or double. Unset automatically for long double and quad
+
+if [[ $FFTW3_DOUBLE == "ON" ]]; then # ensure only one option is set and AVX is disabled if necessary
+  FFTW3_FLOAT="OFF"
+  FFTW3_LONG_DOUBLE="OFF"
+elif [[ $FFTW3_LONG_DOUBLE == "ON" ]]; then
+  unset FFTW3_WITH_AVX
+  FFTW3_FLOAT="OFF"
+  FFTW3_DOUBLE="OFF"
+else # keep FLOAT default option as it was
+  FFTW3_FLOAT="ON"
+  FFTW3_DOUBLE="OFF"
+  FFTW3_LONG_DOUBLE="OFF"
+fi
 
 case $ARCHITECTURE in
     osx_arm64)
-	cmake $SOURCEDIR                                   \
-	      -DCMAKE_INSTALL_PREFIX:PATH="${INSTALLROOT}" \
-	      -DCMAKE_INSTALL_LIBDIR:PATH="lib"            \
-	      -DENABLE_FLOAT=ON
-	;;
+  cmake $SOURCEDIR                                          \
+        -DCMAKE_INSTALL_PREFIX:PATH="${INSTALLROOT}"        \
+        -DCMAKE_INSTALL_LIBDIR:PATH="lib"                   \
+        -DENABLE_LONG_DOUBLE=${FFTW3_LONG_DOUBLE-OFF}       \
+        -DENABLE_FLOAT=${FFTW3_FLOAT-OFF}
+  ;;
     *)
-	cmake $SOURCEDIR                                   \
-	      -DCMAKE_INSTALL_PREFIX:PATH="${INSTALLROOT}" \
-	      -DCMAKE_INSTALL_LIBDIR:PATH="lib"            \
-	      -DENABLE_FLOAT=ON                            \
-	      -DENABLE_AVX=ON
-	;;
+  cmake $SOURCEDIR                                          \
+        -DCMAKE_INSTALL_PREFIX:PATH="${INSTALLROOT}"        \
+        -DCMAKE_INSTALL_LIBDIR:PATH="lib"                   \
+        -DENABLE_LONG_DOUBLE=${FFTW3_LONG_DOUBLE-OFF}       \
+        -DENABLE_FLOAT=${FFTW3_FLOAT-OFF}                   \
+        ${FFTW3_WITH_AVX:+-DENABLE_AVX=ON}
+  ;;
 esac
 
 make ${JOBS+-j $JOBS}
