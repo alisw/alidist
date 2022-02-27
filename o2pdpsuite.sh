0f3600c08d2e7f9202a8afe2844ab7ed69d8b263
ALICE Builder <alibuild@users.noreply.github.com>
2022-02-27 09:39:24 +0100

    daily tags: auto-update dist definitions (#3883)

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20220226"
+tag: "nightly-20220227"
 requires:
   - O2
   - DataDistribution
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "nightly-20220226"
+tag: "nightly-20220227"
 requires:
   - O2
   - ONNXRuntime
