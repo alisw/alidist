593bf8806c237c7a527041aba77c53e3523ac298
Gvozden Neskovic <neskovic@compeng.uni-frankfurt.de>
2022-02-28 14:18:52 +0100

    datadist: bump to v1.3.0, add ucx dep (#3880)
    This adds a new dependency: Unified Communication X Library (UCX). It's required to utilize the IB fabric to the full extent, supporting tag matching and adaptive routing offloads. It should only be required for EPN and FLP builds (slc8).
    For compiling ucx features, CentOS builders must have recent rdma-core and rdma-core-devel packages installed, v35 or newer. UCX tag v1.12.1-rc2 is required, as it fixes a bug with excessive CPU utilization.
    
    DataDistribution v1.3.0 again will need synchronized upgrade on FLP and EPNs.

---
+++ ucx.sh
@@ -0,0 +1,47 @@
+package: ucx
+version: "%(tag_basename)s"
+tag: v1.12.1-rc2
+requires:
+  - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - "autotools:(slc6|slc7)"
+  - alibuild-recipe-tools
+source: https://github.com/openucx/ucx
+---
+#!/bin/bash -e
+
+# Unified Communication X Library (linux only)
+## NOTE: rdma-core and rdma-core-devel (v35+) packages must be installed for O2 FLP/EPN use
+
+printf "#include <rdma/rdma_cma.h>" | cc -xc - -c -o /dev/null ||
+( printf "rdma-core not found.\n * On RHEL-compatible systems you probably need: rdma-core-devel\n"; exit 1; )
+
+rsync -a --delete --exclude "**/.git" ${SOURCEDIR}/ .
+./autogen.sh
+./contrib/configure-release-mt --prefix=${INSTALLROOT}     \
+                               --with-verbs                \
+                               --with-rdmacm               \
+                               --with-ib-hw-tm             \
+                               --with-mlx5-dv              \
+                               --with-rc                   \
+                               --with-ud                   \
+                               --with-dc                   \
+                               --with-dm                   \
+                               --with-avx                  \
+                               --with-sse41                \
+                               --with-sse42                \
+                               --without-java              \
+                               --without-go                \
+                               --without-fuse3             \
+                               --without-cuda              \
+                               --without-rocm
+
+make ${JOBS+-j$JOBS} || make -j1
+make install
+
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+alibuild-generate-module --bin --lib > $MODULEFILE
