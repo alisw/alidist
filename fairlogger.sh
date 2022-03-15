6b070fde3250d3e64db071e43d6571d8ff1f2817
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-03-15 10:52:51 +0100

    Make sure FairLogger has a proper version even when doing development with it.

--- fairlogger.sh
+++ fairlogger.sh
@@ -29,6 +29,7 @@ cmake $SOURCEDIR                                                 \
       ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER}        \
       ${CMAKE_BUILD_TYPE:+-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE}  \
       ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}                    \
+      -DPROJECT_GIT_VERSION=$(echo $PKGVERSION | sed -e 's/v//') \
       -DBUILD_TESTING=OFF                                        \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                        \
       -DDISABLE_COLOR=ON                                         \
