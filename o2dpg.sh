31b1ca3db2ff7fdaf666b81e88de87b266dba083
ALICE Builder <alibuild@users.noreply.github.com>
2022-03-20 09:51:16 +0100

    daily tags: auto-update dist definitions (#3974)

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "nightly-20220319"
+tag: "nightly-20220320"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20220319"
+tag: "nightly-20220320"
 requires:
   - O2
   - DataDistribution
