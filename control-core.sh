0222cc71d7e318caccb8caa0089301a17862e617
Vasco Barroso <vmcb@cern.ch>
2022-04-08 16:24:12 +0200

    Revert "Bump AliECS to v0.38.1 (#4032)"
    This reverts commit a8de55856c3f80a2f1e448572d60447e98ab0b00.

--- control-core.sh
+++ control-core.sh
@@ -1,6 +1,6 @@
 package: Control-Core
 version: "%(tag_basename)s"
-tag: "v0.38.1"
+tag: "v0.38.0"
 build_requires:
   - "GCC-Toolchain:(?!osx)"
   - golang
--- control-occplugin.sh
+++ control-occplugin.sh
@@ -1,6 +1,6 @@
 package: Control-OCCPlugin
 version: "%(tag_basename)s"
-tag: "v0.38.1"
+tag: "v0.38.0"
 requires:
   - FairMQ
   - FairLogger
