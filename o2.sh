2a85de89706b03d64a7b84907fcd0a907677d8ab
ALICE Builder <alibuild@users.noreply.github.com>
2022-04-13 08:10:02 +0200

    daily tags: auto-update dist definitions (#4063)

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "nightly-20220412"
+tag: "nightly-20220413"
 requires:
   - arrow
   - FairRoot
--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20220412"
+tag: "nightly-20220413"
 requires:
   - O2
   - DataDistribution
