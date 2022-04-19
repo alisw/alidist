e896dfc724cf741a0d905bb837730e79de2c7a05
ALICE Builder <alibuild@cern.ch>
2022-04-19 02:10:46 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh qualitycontrol.sh o2dpg.sh

--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "nightly-20220414"
+tag: "nightly-20220418"
 requires:
   - O2
   - ONNXRuntime
--- qualitycontrol.sh
+++ qualitycontrol.sh
@@ -1,6 +1,6 @@
 package: QualityControl
 version: "%(tag_basename)s"
-tag: v1.54.1
+tag: "nightly-20220418"
 requires:
   - boost
   - "GCC-Toolchain:(?!osx)"
