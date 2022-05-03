56d551bc70b5036c96e18c5a1525cb60371b043b
David Rohr <drohr@jwdt.org>
2022-05-03 15:48:09 +0200

    Fix incremental build of O2DPG

--- o2dpg.sh
+++ o2dpg.sh
@@ -14,12 +14,13 @@ rsync -a --exclude='**/.git' --delete --delete-excluded \
       $SOURCEDIR/ $INSTALLROOT/
 
 # Modulefile
-mkdir -p $INSTALLROOT/etc/modulefiles
-alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
-
-cat << EOF >> $INSTALLROOT/etc/modulefiles/$PKGNAME
+mkdir -p etc/modulefiles
+alibuild-generate-module --bin > etc/modulefiles/$PKGNAME
+cat << EOF >> etc/modulefiles/$PKGNAME
 set O2DPG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv O2DPG_ROOT \$O2DPG_ROOT
 setenv O2DPG_RELEASE \$version
 setenv O2DPG_VERSION $PKGVERSION
 EOF
+
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
