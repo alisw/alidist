83fb41b982428f58cf2f4699c247defaaeeccab0
Vasco Barroso <vmcb@cern.ch>
2022-05-19 15:05:24 +0200

    bump O2 to nightly-20220519.1

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20220512"
+tag: "nightly-20220519.1"
 requires:
   - O2
   - DataDistribution
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "nightly-20220512"
+tag: "nightly-20220519.1"
 requires:
   - O2
   - ONNXRuntime
