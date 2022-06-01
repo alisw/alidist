2158f7afd79f275b26b4252a7b777b2005392621
Sandro Wenzel <sandro.wenzel@cern.ch>
2022-06-01 09:43:49 +0200

    Revert changes to O2 modulefile (ROOT env) (#4254)
    * Revert "Fix ROOT hack on macOS (#4245)"
    
    This reverts commit 2c5a61e09ffb9015f6f06654b0e8d9402a89e8a5.
    
    * Revert "Dynamically set up env variables to speed up startup of ROOT (#4226)"
    
    This reverts commit eb50a49f891a997e79194d62b2ccfaa77f400b9a.

--- o2.sh
+++ o2.sh
@@ -263,21 +263,6 @@ $([[ ${ARCHITECTURE:0:3} == osx && ! $BOOST_VERSION ]] && echo "prepend-path ROO
 prepend-path ROOT_INCLUDE_PATH \$O2_ROOT/include/GPU
 prepend-path ROOT_INCLUDE_PATH \$O2_ROOT/include
 EoF
-
-case $ARCHITECTURE in
-  osx*);;
-  *)
-cat >> etc/modulefiles/$PKGNAME <<EoF
-# ALICE specific adjustments to speedup ROOT launches
-set searchpath [exec env LD_DEBUG=libs LD_PRELOAD=DOESNOTEXIST /bin/true |& sed -rn {/system search path/{s/.*search path=(.*)[[:space:]]+\(system search path\)$/\1/p; q}}]
-set includepath [exec env LC_ALL=C c++ -xc++ -E -v /dev/null |& sed -rn {/^.include/,\$s/^ (\/.*\+\+.*)/\1/p}]
-set includepath [string map {"\n" :} \$includepath]
-setenv ROOT_LDSYSPATH \$searchpath
-setenv ROOT_CPPSYSINCL \$includepath
-EoF
-    ;;
-esac
-
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 
 if [[ $ALIBUILD_O2_TESTS ]]; then
