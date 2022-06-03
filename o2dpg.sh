a3005822e4f515a13829b69069faf5872fbdd063
Vasco Barroso <vmcb@cern.ch>
2022-06-03 09:24:45 +0200

    bump O2 to nightly-20220603

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "nightly-20220602"
+tag: "nightly-20220603"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "nightly-20220602"
+tag: "nightly-20220603"
 requires:
   - O2
   - ONNXRuntime
