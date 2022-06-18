e818f166470921080434387cfb0a46f9783c19ff
Sandro Wenzel <sandro.wenzel@cern.ch>
2022-06-18 15:45:37 +0200

    Reverting VMC and Geant4 to previous version (#4306)
    Latest G4(_VMC) seems to have a potential issue
    with physics processes/cuts leading to an observable reduction
    in TPC tracks in our MC processing.
    
    Reverting for now to previous version in order to stabilize situation
    and give time to provide and validate fix.

--- geant4.sh
+++ geant4.sh
@@ -1,8 +1,7 @@
 package: GEANT4
 version: "%(tag_basename)s"
-tag: "v11.0.1-alice1"
+tag: "v10.7.2-alice1"
 source: https://github.com/alisw/geant4.git
-#source: https://gitlab.cern.ch/geant4/geant4.git
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
@@ -27,7 +26,6 @@ env:
 cmake $SOURCEDIR                                                \
   -DGEANT4_INSTALL_DATA_TIMEOUT=2000                            \
   -DCMAKE_CXX_FLAGS="-fPIC"                                     \
-  ${CXXSTD:+-DCMAKE_CXX_STANDARD="$CXXSTD"}                     \
   -DCMAKE_INSTALL_PREFIX:PATH="$INSTALLROOT"                    \
   -DCMAKE_INSTALL_LIBDIR="lib"                                  \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo                             \
@@ -44,6 +42,7 @@ cmake $SOURCEDIR                                                \
   ${GEANT4_DATADIR:+-DGEANT4_INSTALL_DATADIR="$GEANT4_DATADIR"} \
   -DGEANT4_USE_SYSTEM_EXPAT=OFF                                 \
   ${XERCESC_ROOT:+-DXERCESC_ROOT_DIR=$XERCESC_ROOT}             \
+  ${CXXSTD:+-DGEANT4_BUILD_CXXSTD=$CXXSTD}                      \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 make ${JOBS+-j $JOBS}
