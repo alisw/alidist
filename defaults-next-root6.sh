16b260d249eda6830f0eaf709e2119af7d524514
ALICE Builder <alibuild@cern.ch>
2022-06-22 15:34:59 +0000

    Auto-update: defaults-next-root6.sh aliphysics.sh

--- defaults-next-root6.sh
+++ defaults-next-root6.sh
@@ -1,16 +1,17 @@
+disable:
+- arrow
+env:
+  CFLAGS: -fPIC -g -O2
+  CMAKE_BUILD_TYPE: RELWITHDEBINFO
+  CXXFLAGS: -fPIC -g -O2 -std=c++11
+overrides:
+  AliPhysics:
+    tag: vAN-20220622
+    version: '%(tag_basename)s_ROOT6'
+  AliRoot:
+    tag: v5-09-59b
+    version: '%(tag_basename)s_ROOT6'
 package: defaults-next-root6
 version: v1
-disable:
-  - arrow
-env:
-  CFLAGS: "-fPIC -g -O2"
-  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
-  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
-overrides:
-  AliRoot:
-    version: "%(tag_basename)s_ROOT6"
-    tag: v5-09-59b
-  AliPhysics:
-    version: "%(tag_basename)s_ROOT6"
-    tag: v5-09-59b-01
+
 ---
