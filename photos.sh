923ab00d278cd87542f85fb2687b4dd6113815f4
Peter Hristov <pzhristov@users.noreply.github.com>
2022-08-17 21:28:45 +0200

    Move to ALICE fork of official PHOTOS GitLab repository. Enable HepMC3 support

--- photos.sh
+++ photos.sh
@@ -1,9 +1,10 @@
 package: PHOTOS
 version: "%(tag_basename)s"
-tag: "v.3.64"
-source: https://github.com/ffionda/PHOTOS.git
+tag: "v3.64"
+source: https://github.com/alisw/photospp.git
 requires:
   - HepMC
+  - HepMC3
 build_requires:
   - "autotools:(slc6|slc7)"
   - alibuild-recipe-tools
@@ -12,7 +13,7 @@ build_requires:
 rsync -a --delete --exclude '**/.git' $SOURCEDIR/ ./
 
 autoreconf -ifv
-./configure --prefix $INSTALLROOT --with-hepmc="$HEPMC_ROOT"
+./configure --prefix $INSTALLROOT --with-hepmc="$HEPMC_ROOT" --with-hepmc3="$HEPMC3_ROOT"
 make -j$JOBS
 make install
 
