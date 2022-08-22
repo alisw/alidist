bfd895195bbf3a00d1b668e4493075525ed19998
Antonin Maire <antonin.maire@cern.ch>
2022-08-22 15:40:48 +0200

    alidist/YODA - Pythonpath adaptation in prepend_path
    In the prepend_path instruction of the header, prepare and set-up what is needed to move from :
       "PYTHONPATH: $YODA_ROOT/lib/python3.9/site-packages"
    to
       "PYTHONPATH: $YODA_ROOT/lib/python/site-packages"
    
    Symlink to be set up between "python3.9" and "python" in  $YODA_ROOT/lib/

--- yoda.sh
+++ yoda.sh
@@ -11,15 +11,23 @@ requires:
 build_requires:
   - "autotools:(slc6|slc7)"
 prepend_path:
-  PYTHONPATH: $YODA_ROOT/lib/python3.9/site-packages # _Question 1_ : 3.9 hardcoded here ?
-    # should/could we move to sthg more adaptative with sthg like $PYTHON_VERSION ?
-    # or use $YODA_ROOT/bin/yoda-config --pythonpath to get the pythonpath ? (~like done below here for the module file)
+  PYTHONPATH: $YODA_ROOT/lib/python/site-packages # See below at built time the management towards generic path .../lib/python/site-packages
 ---
+#!/bin/bash
+
 rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ ./
 
 [[ -e .missing_timestamps ]] && ./missing-timestamps.sh --apply || autoreconf -ivf
 
 (
+
+PYTHON_EXECUTABLE=$(/usr/bin/env python3 -c 'import sys; print(sys.executable)') 
+    # python3 here should come from our own Python package ("requires"), see alidist/python.sh
+    # The command will return sthg like "python3.9"
+PYTHON_VER=$( ${PYTHON_EXECUTABLE} -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")' )
+    # The command will return sthg like "3.9"
+
+
 unset PYTHON_VERSION
     # _Question 2_ :
     # YODA configure needs at least 2 python-related environment variables :
@@ -74,7 +82,42 @@ case $ARCHITECTURE in
 esac
 make -j$JOBS
 make install
+
+
+# Manage after compilation+install the path towards python site-package 
+#   i.e. we aim at adapting from hardcoded "$YODA_ROOT/lib/python3.9/site-packages" to a generic symlink "$YODA_ROOT/lib/python/site-packages"
+#   inspired from recipe alidist/xrootd.sh
+#   (What we do here, at built time, is needed for "prepend_path PYTHONPATH" above in the recipe header;
+#   the prepend_path occurs _after_ the built is completed.)
+
+pushd ${INSTALLROOT}/lib
+# Hypothesis : path towards lib is expected as $YODA_ROOT/lib/ (and nothing like $YODA_ROOT/lib64 or $YODA_ROOT/local/lib/ ...)
+# NOTE : there could be cases where python bindings are installed as relative to INSTALLROOT : case not met so far for YODA (slc, ubuntu) ...
+
+if [[ -d python${PYTHON_VER} ]]; then
+    ln -s python${PYTHON_VER} python # symlink from ${INSTALLROOT}/lib/python3.9 to ${INSTALLROOT}/lib/python
+fi
+
+if [[ ! -e python ]] && echo "YODA env pb: NO PYTHON SYMLINK CREATED for python${PYTHON_VER} in: $(pwd -P)"; then 
+    exit 
+fi    
+
+popd  # get back from INSTALLROOT/lib
+
+case $ARCHITECTURE in
+    osx*)
+        find $INSTALLROOT/lib/python/ -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+        find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+    ;;
+esac
+
+
 )
+
+
+
+
+
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
