b1c13c8baabd5d07a30eb1576ceed651efba94a5
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-10-19 14:46:56 +0200

    Drop apmon-cpp completely from Monitoring

--- monitoring.sh
+++ monitoring.sh
@@ -27,7 +27,6 @@ fi
 cmake $SOURCEDIR                                              \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                     \
       ${BOOST_REVISION:+-DBOOST_ROOT=$BOOST_ROOT}                 \
-      ${APMON_CPP_REVISION:+-DAPMON_ROOT=$APMON_CPP_ROOT}         \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
 
 cp ${BUILDDIR}/compile_commands.json ${INSTALLROOT}
