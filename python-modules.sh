be3c95087f675f77fed291fc7b2e730d06263409
Timo Wilken <timo.wilken@cern.ch>
2022-10-20 10:41:34 +0200

    Use Python modules from master branch as well
    These have the correct requirements when running under Python 3.9.

--- python-modules.sh
+++ python-modules.sh
@@ -1,13 +1,14 @@
 package: Python-modules
 version: "1.0"
 requires:
-  - "Python:slc.*"
-  - "Python-system:(?!slc.*)"
+  - "Python:(slc|ubuntu)"
+  - "Python-system:(?!slc.*|ubuntu)"
   - FreeType
   - libpng
 build_requires:
   - system-curl
   - Python-modules-list
+  - alibuild-recipe-tools
 prepend_path:
   PYTHONPATH: $PYTHON_MODULES_ROOT/share/python-modules/lib/python/site-packages
 ---
@@ -23,12 +24,24 @@ if [ ! "X$VIRTUAL_ENV" = X ]; then
   deactivate
 fi
 
+# Special lists for different platforms
+PIP39_REQUIREMENTS=$(eval echo \${PIP39_REQUIREMENTS_${ARCHITECTURE/-/_}:-$PIP39_REQUIREMENTS})
+PIP310_REQUIREMENTS=$(eval echo \${PIP310_REQUIREMENTS_${ARCHITECTURE/-/_}:-$PIP310_REQUIREMENTS})
+
 # PIP_REQUIREMENTS, PIP36_REQUIREMENTS, PIP38_REQUIREMENTS come from python-modules-list.sh
-echo $PIP_REQUIREMENTS | tr \  \\n > requirements.txt
 case $ARCHITECTURE in
-  slc6*);;
+  osx_arm64)
+  touch requirements.txt
+  ;;
+  slc6*)
+  echo $PIP_REQUIREMENTS | tr \  \\n > requirements.txt;;
   *)
-  if python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3008 else 1)'; then
+  echo $PIP_REQUIREMENTS | tr \  \\n > requirements.txt
+  if python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3010 else 1)'; then
+    echo $PIP310_REQUIREMENTS | tr \  \\n >> requirements.txt
+  elif python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3009 else 1)'; then
+    echo $PIP39_REQUIREMENTS | tr \  \\n >> requirements.txt
+  elif python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3008 else 1)'; then
     echo $PIP38_REQUIREMENTS | tr \  \\n >> requirements.txt
   elif python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3006 else 1)'; then
     echo $PIP36_REQUIREMENTS | tr \  \\n >> requirements.txt
@@ -41,24 +54,29 @@ PYTHON_MODULES_INSTALLROOT=$INSTALLROOT/share/python-modules
 mkdir -p $PYTHON_MODULES_INSTALLROOT
 
 # Create the virtualenv
-python3 -m venv --system-site-packages --symlinks $PYTHON_MODULES_INSTALLROOT
+python3 -m venv $PYTHON_MODULES_INSTALLROOT
 . $PYTHON_MODULES_INSTALLROOT/bin/activate
 
+# Upgrade pip
+python3 -m pip install -IU pip
 # Install setuptools upfront, since this seems to create issues now...
-python3 -m pip install --user -IU setuptools==49.6.0
-python3 -m pip install --user -IU wheel
+python3 -m pip install -IU "setuptools<=60.8.2"
+python3 -m pip install -IU wheel
 
 # FIXME: required because of the newly introduced dependency on scikit-garden requires
 # a numpy to be installed separately
 # See also:
 #   https://github.com/scikit-garden/scikit-garden/issues/23
-grep RootInteractive requirements.txt && python3 -m pip install --user -IU numpy
-python3 -m pip install --user -IU -r requirements.txt
+python3 -m pip install -IU numpy
+python3 -m pip install -IU -r requirements.txt
 
 # Major.minor version of Python
-export PYVER=$(python3 -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_version())')
+export PYVER="$(python3 -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_version())')"
 # Find the proper Python lib library and export it
 pushd "$PYTHON_MODULES_INSTALLROOT"
+  # let's remove any pre-existent symlinks to have a clean slate
+  [ -h lib64 ] && unlink lib64
+  [ -h lib ]   && unlink lib
   if [[ -d lib64 ]]; then
     ln -nfs lib64 lib  # creates lib pointing to lib64
   elif [[ -d lib ]]; then
@@ -69,15 +87,9 @@ pushd "$PYTHON_MODULES_INSTALLROOT"
   popd
   pushd bin
     # Fix shebangs: remove hardcoded Python path
-    find . -type f -exec sed -i.deleteme -e "1 s|^#!${PYTHON_MODULES_INSTALLROOT}/bin/\(.*\)$|#!/usr/bin/env \1|" \;
+    find . -type f -exec sed -i.deleteme -e "s|${PYTHON_MODULES_INSTALLROOT}|/usr|;s|python3|env python3|" '{}' \;
     find . -name "*.deleteme" -delete
   popd
-popd
-
-# Patch long shebangs (by default max is 128 chars on Linux)
-pushd "$PYTHON_MODULES_INSTALLROOT/bin"
-  find . -type f -exec sed -i.deleteme -e "1 s|^#!${PYTHON_MODULES_INSTALLROOT}/bin/\(.*\)$|#!/usr/bin/env \1|" \;
-  find . -name "*.deleteme" -delete
 popd
 
 # Remove useless stuff
@@ -88,18 +100,9 @@ find "$PYTHON_MODULES_INSTALLROOT"/lib/python* \
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
-MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${PYTHON_REVISION:+Python/$PYTHON_VERSION-$PYTHON_REVISION} ${ALIEN_RUNTIME_REVISION:+AliEn-Runtime/$ALIEN_RUNTIME_VERSION-$ALIEN_RUNTIME_REVISION}
+alibuild-generate-module > "$MODULEDIR/$PKGNAME"
+cat >> "$MODULEDIR/$PKGNAME" <<EoF
 # Our environment
 set PYTHON_MODULES_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$PYTHON_MODULES_ROOT/share/python-modules/bin
