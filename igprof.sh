034876a03b35f228b72e713d76a61c3abadac7c9
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-11-17 16:24:15 +0100

    Fix igprof recipe to work on hyperloop

--- igprof.sh
+++ igprof.sh
@@ -4,14 +4,22 @@ source: https://github.com/igprof/igprof
 tag: v5.9.16
 requires:
   - libunwind
+build_requires:
   - CMake
+  - ninja
+  - alibuild-recipe-tools
 ---
 #!/bin/sh
 
 cmake $SOURCEDIR \
+      -G Ninja \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
       -DUNWIND_INCLUDE_DIR=$LIBUNWIND_ROOT/include \
       -DUNWIND_LIBRARY=$LIBUNWIND_ROOT/lib/libunwind.so \
       -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g -O3 -U_FORTIFY_SOURCE -Wno-attributes"
-make ${JOBS+-j $JOBS}
-make install
+cmake --build . -- ${JOBS:+-j$JOBS} install
+
+# Modulefile
+mkdir -p etc/modulefiles
+alibuild-generate-module --bin --lib > etc/modulefiles/$PKGNAME
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
