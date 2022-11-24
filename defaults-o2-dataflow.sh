b4243157f3d83ce81d81ae5c04d402c9863ea67c
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-11-24 11:15:51 +0100

    Make sure the o2-dataflow uses a setuptools which does not break xrootd (#4681)

--- defaults-o2-dataflow.sh
+++ defaults-o2-dataflow.sh
@@ -47,15 +47,7 @@ overrides:
         PyYAML==5.1
         python-consul==1.1.0
         psutil==5.8.0
-      PIP36_REQUIREMENTS: |
-        python-consul==1.1.0
-        psutil==5.8.0
-      PIP38_REQUIREMENTS: |
-        python-consul==1.1.0
-        psutil==5.8.0
-      PIP39_REQUIREMENTS: |
-        python-consul==1.1.0
-        psutil==5.8.0
+        setuptools==65.5.1
   O2-customization:
     env:
       ENABLE_UPGRADES: OFF # Disable detector upgrades in O2
