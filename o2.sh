539ffad54c616892e5e7459da7762c6d97eb820d
ALICE Builder <alibuild@cern.ch>
2022-12-19 05:01:34 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh o2dpg.sh

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "nightly-20221218"
+tag: "nightly-20221219"
 requires:
   - arrow
   - FairRoot
--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20221218"
+tag: "nightly-20221219"
 requires:
   - O2
   - DataDistribution
