097f132f49b6760a4391a0e5fae503024915ea63
Teo Mrnjavac <teo.m@cern.ch>
2023-01-09 15:01:36 +0100

    Bump AliECS to v0.58.2

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v0.58.1"
+tag: "v0.58.2"
 build_requires:
   - golang
   - protobuf
--- control-occplugin.sh
+++ control-occplugin.sh
@@ -1,6 +1,6 @@
 package: Control-OCCPlugin
 version: "%(tag_basename)s"
-tag: "v0.58.1"
+tag: "v0.58.2"
 requires:
   - FairMQ
   - FairLogger
