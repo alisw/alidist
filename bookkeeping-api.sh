8864693f27660ebc6710c097859d8ae860b978f8
Barthelemy <barthelemy.von.haller@cern.ch>
2023-01-20 08:51:32 +0100

    update according to comments

--- bookkeeping-api.sh
+++ bookkeeping-api.sh
@@ -18,12 +18,9 @@ incremental_recipe: |
 case $ARCHITECTURE in
   osx*)
     # If we preferred system tools, we need to make sure we can pick them up.
+    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT=$(brew --prefix openssl@1.1)
     [[ ! $GRPC_ROOT ]] && GRPC_ROOT=`brew --prefix grpc`
-    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT_DIR=$(brew --prefix openssl@1.1)
-
-    SONAME=dylib
   ;;
-  *) SONAME=so ;;
 esac
 
 cmake $SOURCEDIR/cxx-client                  \
@@ -32,7 +29,7 @@ cmake $SOURCEDIR/cxx-client                  \
       ${CONFIGURATION_REVISION:+-DConfiguration_ROOT=$CONFIGURATION_ROOT} \
       ${INFOLOGGER_REVISION:+-DInfoLogger_ROOT=$INFOLOGGER_ROOT} \
       -DCMAKE_INSTALL_LIBDIR=lib          \
-      -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR \
+      ${OPENSSL_ROOT:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
 
 VERBOSE=1 cmake --build . -- ${JOBS+-j $JOBS} install
