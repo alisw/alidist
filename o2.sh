0a213302eac98433406bfed266b8e46e174c8fd8
ALICE Builder <alibuild@users.noreply.github.com>
2023-01-29 11:37:53 +0100

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh o2dpg.sh (#4787)

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "nightly-20230128"
+tag: "nightly-20230129"
 requires:
   - arrow
   - FairRoot
--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "nightly-20230128"
+tag: "nightly-20230129"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
