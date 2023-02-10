68168de8476c97a0beee4f62f1f24cf1d852eb06
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-02-10 17:11:38 +0100

    jq: do not load GCC runtime
    There is no need for it and it can only break things if jq is loaded together with something which used a different GCC version.

--- jq.sh
+++ jq.sh
@@ -36,8 +36,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0                                                                        \\
-       ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
+module load BASE/1.0                                                                       
 # Our environment
 set JQ_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$JQ_ROOT/bin
