1e64d9ef39569a263a76a2cd2d6883eab5a0a329
Barthelemy <barthelemy.von.haller@cern.ch>
2023-02-16 14:35:07 +0100

    attempt at fixing the issue in the grpc recipe

--- bookkeeping-api.sh
+++ bookkeeping-api.sh
@@ -16,7 +16,7 @@ incremental_recipe: |
 case $ARCHITECTURE in
   osx*)
     # If we preferred system tools, we need to make sure we can pick them up.
-    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT=$(brew --prefix openssl@1.1)
+    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT_DIR=$(brew --prefix openssl@1.1)
     [[ ! $GRPC_ROOT ]] && GRPC_ROOT=`brew --prefix grpc`
   ;;
 esac
@@ -27,7 +27,7 @@ cmake $SOURCEDIR/cxx-client                  \
       ${CONFIGURATION_REVISION:+-DConfiguration_ROOT=$CONFIGURATION_ROOT} \
       ${INFOLOGGER_REVISION:+-DInfoLogger_ROOT=$INFOLOGGER_ROOT} \
       -DCMAKE_INSTALL_LIBDIR=lib          \
-      ${OPENSSL_ROOT:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
+      ${OPENSSL_ROOT_DIR:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR} \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
 
 VERBOSE=1 cmake --build . -- ${JOBS+-j $JOBS} install
