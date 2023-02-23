7662b0b4cde65d5eb27470ea189aa577523aca22
Teo Mrnjavac <126364+teo@users.noreply.github.com>
2023-02-23 16:29:02 +0100

    Bump AliECS to v0.63.0 (#4847)

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v0.62.0"
+tag: "v0.63.0"
 build_requires:
   - golang
   - protobuf
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v0.62.0"
+version: "v0.63.0"
 requires:
   - Control-Core
   - Control-OCCPlugin
