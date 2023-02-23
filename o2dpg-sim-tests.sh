31d428e7bff8f4b34a28ba0dff23482b98fd65bf
benedikt-voelkel <benedikt.voelkel@gmx.de>
2023-02-23 16:37:29 +0100

    Add workflow creation (#4849)
    Co-authored-by: Benedikt Volkel <benedikt.volkel@cern.ch>

--- o2dpg-sim-tests.sh
+++ o2dpg-sim-tests.sh
@@ -10,18 +10,24 @@ if [[ "$G4INSTALL" != "" ]]; then
   `$G4INSTALL/bin/geant4-config --datasets | sed 's/[^ ]* //' | sed 's/G4/export G4/' | sed 's/DATA /DATA=/'`
 fi
 
-rm -Rf $BUILDDIR/o2dpg-sim_tests
-mkdir $BUILDDIR/o2dpg-sim_tests
-pushd $BUILDDIR/o2dpg-sim_tests
+TEST_DIR=$BUILDDIR/o2dpg-sim_tests
+LOGFILE="${TEST_DIR}/o2dpg-sim-tests.log"
 
-LOGFILE="o2dpg-sim-tests.log"
+rm -Rf ${TEST_DIR}
+mkdir ${TEST_DIR}
+pushd ${TEST_DIR}
 
-O2DPG_TEST_EXITCODE=0
-{ O2DPG_TEST_REPO_DIR=${SOURCEDIR}/../../../O2DPG/${O2DPG_VERSION}/0 "${O2DPG_ROOT}/test/run_generator_tests.sh" &> ${LOGFILE} ; O2DPG_TEST_EXITCODE=$?; } || true  # don't quit immediately on error
+O2DPG_TEST_GENERATOR_EXITCODE=0
+{ O2DPG_TEST_REPO_DIR=${SOURCEDIR}/../../../O2DPG/${O2DPG_VERSION}/0 "${O2DPG_ROOT}/test/run_generator_tests.sh" &> ${LOGFILE} ; O2DPG_TEST_GENERATOR_EXITCODE=$?; } || true  # don't quit immediately on error
 # keep only logs, remove everything else for now
 # if we needed/possible in the future, we might keep some of the other files
 find . -type f ! -name '*.log' -and ! -name "*serverlog*" -and ! -name "*mergerlog*" -and ! -name "*workerlog*" -delete
-if [ ${O2DPG_TEST_EXITCODE} != "0" ] ; then
+
+O2DPG_TEST_WORKFLOW_EXITCODE=0
+{ O2DPG_TEST_REPO_DIR=${SOURCEDIR}/../../../O2DPG/${O2DPG_VERSION}/0 "${O2DPG_ROOT}/test/run_workflow_tests.sh" >> ${LOGFILE} 2>&1 ; O2DPG_TEST_WORKFLOW_EXITCODE=$?; } || true  # don't quit immediately on error
+# only log/test files created, nothing to delete here
+
+if [ "${O2DPG_TEST_GENERATOR_EXITCODE}" != "0" -o "${O2DPG_TEST_WORKFLOW_EXITCODE}" != "0" ] ; then
   # something is wrong
   echo "error detected in ${PKGNAME}"
   cat ${LOGFILE}
@@ -31,9 +37,7 @@ else
   echo "${PKGNAME} passed"
 fi
 
-
 popd
-
 
 # Dummy modulefile
 mkdir -p $INSTALLROOT/etc/modulefiles
