99dcdf309cf337ccdc018a5bd611cb2caed7a405
Sandro Wenzel <sandro.wenzel@cern.ch>
2023-02-23 03:12:25 +0100

    modulefile for libunwind (#4844)
    This is needed so that
    
    `alienv enter IgProf`
    
    correctly works (loading also the necessary libunwind dependency).

--- libunwind.sh
+++ libunwind.sh
@@ -3,6 +3,7 @@ version: v1.6.2
 source: http://github.com/libunwind/libunwind
 build_requires:
   - libatomic_ops
+  - alibuild-recipe-tools
 ---
 #!/bin/sh
 (cd $SOURCEDIR && autoreconf -i)
@@ -14,3 +15,8 @@ $SOURCEDIR/configure \
 
 make ${JOBS+-j $JOBS}
 make install
+
+# Modulefile
+mkdir -p etc/modulefiles
+alibuild-generate-module --lib > etc/modulefiles/$PKGNAME
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
