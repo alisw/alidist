2ca2efc4a9d8b63b03db0996f6567392a5535399
sy-c <sy-c@users.noreply.github.com>
2023-03-01 23:24:08 +0100

    bump readoutCard + readout (#4863)
    * bump readoutCard
    
    * bump readout
    
    * readout: add bookkeeping-api

--- readout.sh
+++ readout.sh
@@ -1,6 +1,6 @@
 package: Readout
 version: "%(tag_basename)s"
-tag: v2.16.1
+tag: v2.17.0
 requires:
   - boost
   - "GCC-Toolchain:(?!osx)"
@@ -16,6 +16,7 @@ requires:
   - ZeroMQ
   - fmt
   - "MySQL:(slc.*)"
+  - bookkeeping-api
 build_requires:
   - CMake
   - alibuild-recipe-tools
@@ -50,6 +51,7 @@ cmake $SOURCEDIR                                                         \
       ${LZ4_ROOT:+-DLZ4_DIR=$LZ4_ROOT}                                   \
       ${CONTROL_OCCPLUGIN_REVISION:+-DOcc_ROOT=$CONTROL_OCCPLUGIN_ROOT}   \
       ${ZEROMQ_ROOT:+-DZMQ_ROOT=$ZEROMQ_ROOT}                             \
+      ${BOOKKEEPINGAPI_REVISION:+-DBookeepingApi_ROOT=$BOOKKEEPINGAPI_ROOT} \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON                                  \
       -DBUILD_SHARED_LIBS=ON
 
