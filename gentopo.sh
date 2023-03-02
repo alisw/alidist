e2e7366b89648e446d0895f1d80562ce1c82569f
David Rohr <drohr@jwdt.org>
2023-03-02 13:24:58 +0100

    Install also apptainer wrappers

--- gentopo.sh
+++ gentopo.sh
@@ -7,7 +7,7 @@ build_requires:
 ---
 #!/bin/bash -e
 mkdir $INSTALLROOT/bin
-cp $SOURCEDIR/DATA/tools/epn/gen_topo*.sh $INSTALLROOT/bin
+cp $SOURCEDIR/DATA/tools/epn/gen_topo*.sh $SOURCEDIR/DATA/tools/epn/apptainer*.sh $INSTALLROOT/bin
 
 # Modulefile
 mkdir -p etc/modulefiles
