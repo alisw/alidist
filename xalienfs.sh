ebd38f4e8c2185e5502f12e276c25d7781fab9f4
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-03-10 17:53:20 +0200

    fix some alidistlint messages in xalienfs.sh

--- xalienfs.sh
+++ xalienfs.sh
@@ -3,61 +3,63 @@ version: "%(tag_basename)s"
 tag: v1.0.14r1-alice3
 source: https://github.com/alisw/xalienfs.git
 requires:
- - "GCC-Toolchain:(?!osx)"
- - "Xcode:(osx.*)"
- - zlib
- - libxml2
- - "OpenSSL:(?!osx)"
- - "osx-system-openssl:(osx.*)"
- - AliEn-CAs
- - ApMon-CPP
- - UUID
- - XRootD
+  - "GCC-Toolchain:(?!osx)"
+  - "Xcode:(osx.*)"
+  - zlib
+  - libxml2
+  - "OpenSSL:(?!osx)"
+  - "osx-system-openssl:(osx.*)"
+  - AliEn-CAs
+  - ApMon-CPP
+  - UUID
+  - XRootD
 build_requires:
- - "autotools:(slc6|slc7)"
- - SWIG
- - libperl
- - alibuild-recipe-tools
+  - "autotools:(slc6|slc7)"
+  - SWIG
+  - libperl
+  - alibuild-recipe-tools
 prepend_path:
- PERLLIB: "$ALIEN_RUNTIME_ROOT/lib/perl"
+  PERLLIB: "$ALIEN_RUNTIME_ROOT/lib/perl"
 env:
   GSHELL_ROOT: "$XALIENFS_ROOT"
   GSHELL_NO_GCC: "1"
 ---
 #!/bin/bash -e
-[[ ! $SWIG_ROOT ]] && SWIG_LIB=`swig -swiglib`
+
+[[ -z "${SWIG_ROOT}" ]] && SWIG_LIB="$(swig -swiglib)"
 
 rsync -a --delete --exclude='**/.git' --delete-excluded \
-      $SOURCEDIR/ ./
+      "${SOURCEDIR}/" ./
 ./bootstrap.sh
 autoreconf -ivf
 case $ARCHITECTURE in
   osx_x86-64)
-    [[ "$OPENSSL_ROOT" != "" ]] || OPENSSL_ROOT=`brew --prefix openssl@1.1`
+    [[ -z "${OPENSSL_ROOT}" ]] && OPENSSL_ROOT=$(brew --prefix openssl@1.1)
     EXTRA_PERL_CXXFLAGS="-I$(perl -MConfig -e 'print $Config{archlib}')/CORE"
   ;;
   osx_arm64)
-    [[ "$OPENSSL_ROOT" != "" ]] || OPENSSL_ROOT=`brew --prefix openssl@1.1`
+    [[ -z "${OPENSSL_ROOT}" ]] && OPENSSL_ROOT="$(brew --prefix openssl@1.1)"
     EXTRA_PERL_CXXFLAGS="-I$(perl -MConfig -e 'print $Config{archlib}')/CORE"
-    export LDFLAGS="-L$(brew --prefix readline)/lib"
-    export CPPFLAGS="-I$(brew --prefix readline)/include"
-    export PKG_CONFIG_PATH="$(brew --prefix readline)/lib/pkgconfig"
+    LDFLAGS="-L$(brew --prefix readline)/lib"
+    CPPFLAGS="-I$(brew --prefix readline)/include"
+    PKG_CONFIG_PATH="$(brew --prefix readline)/lib/pkgconfig"
+    export LDFLAGS CPPFLAGS PKG_CONFIG_PATH
   ;;
 esac
 export CXXFLAGS="$CXXFLAGS -I$XROOTD_ROOT/include -I$XROOTD_ROOT/include/xrootd/private \
                  ${UUID_ROOT:+-I$UUID_ROOT/include -L$UUID_ROOT/lib}                    \
                  ${OPENSSL_ROOT:+-I$OPENSSL_ROOT/include -L$OPENSSL_ROOT/lib}           \
                  $EXTRA_PERL_CXXFLAGS"
-./configure --prefix=$INSTALLROOT                \
-            --with-xrootd-location=$XROOTD_ROOT  \
+./configure --prefix="${INSTALLROOT}"                \
+            --with-xrootd-location="${XROOTD_ROOT}"  \
             --enable-perl-module                 \
             --with-perl=perl                     \
-            --with-swig-inc="$SWIG_LIB"          \
+            --with-swig-inc="${SWIG_LIB}"        \
             --enable-build-server
 # May not work in multicore
 make
-make install INSTALLSITEARCH=$INSTALLROOT/lib/perl \
-             INSTALLARCHLIB=$INSTALLROOT/lib/perl
+make install INSTALLSITEARCH="${INSTALLROOT}/lib/perl" \
+             INSTALLARCHLIB="${INSTALLROOT}/lib/perl"
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
