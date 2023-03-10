8ab18f8a14c11535e98f65eecc03e45f3abb99d4
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-03-10 17:13:49 +0200

    fix some alidistlint messages in python.sh

--- python.sh
+++ python.sh
@@ -3,22 +3,22 @@ version: "%(tag_basename)s"
 tag: v3.9.12
 source: https://github.com/python/cpython
 requires:
- - "GCC-Toolchain:(?!osx)"
- - "Xcode:(osx.*)"
- - zlib
- - libxml2
- - "OpenSSL:(?!osx)"
- - "osx-system-openssl:(osx.*)"
- - AliEn-CAs
- - ApMon-CPP
- - UUID
- - FreeType
- - libpng
- - sqlite
- - libffi
- - curl
+  - "GCC-Toolchain:(?!osx)"
+  - "Xcode:(osx.*)"
+  - zlib
+  - libxml2
+  - "OpenSSL:(?!osx)"
+  - "osx-system-openssl:(osx.*)"
+  - AliEn-CAs
+  - ApMon-CPP
+  - UUID
+  - FreeType
+  - libpng
+  - sqlite
+  - libffi
+  - curl
 build_requires:
- - alibuild-recipe-tools
+  - alibuild-recipe-tools
 env:
   SSL_CERT_FILE: "$(export PATH=$PYTHON_ROOT/bin:$PATH; export LD_LIBRARY_PATH=$PYTHON_ROOT/lib:$LD_LIBRARY_PATH; python -c \"import certifi; print(certifi.where())\")"
   PYTHONHOME: "$PYTHON_ROOT"
@@ -27,7 +27,9 @@ prefer_system: "(?!slc5|ubuntu)"
 prefer_system_check:
   python3 -c 'import sys; import sqlite3; sys.exit(1 if sys.version_info < (3, 5) else 0)' && python3 -m pip --help > /dev/null && printf '#include "pyconfig.h"' | cc -c $(python3-config --includes) -xc -o /dev/null -; if [ $? -ne 0 ]; then printf "Python, the Python development packages, and pip must be installed on your system.\nUsually those packages are called python, python-devel (or python-dev) and python-pip.\n"; exit 1; fi
 ---
-rsync -av --exclude '**/.git' $SOURCEDIR/ $BUILDDIR/
+#!/bin/bash -e
+
+rsync -av --exclude '**/.git' "${SOURCEDIR}/" "${BUILDDIR}/"
 
 # According to cmsdist, this is required to pick up our own version
 export LIBFFI_ROOT
@@ -35,27 +37,20 @@ export LIBFFI_ROOT
 # The only way to pass externals to Python
 LDFLAGS=
 CPPFLAGS=
-for ext in $ALIEN_RUNTIME_ROOT $ZLIB_ROOT $FREETYPE_ROOT $LIBPNG_ROOT $SQLITE_ROOT $LIBFFI_ROOT; do
-  LDFLAGS="$(find $ext -type d \( -name lib -o -name lib64 \) -exec echo -L\{\} \;) $LDFLAGS"
-  CPPFLAGS="$(find $ext -type d -name include -exec echo -I\{\} \;) $CPPFLAGS"
+for ext in "${ZLIB_ROOT}" "${FREETYPE_ROOT}" "${LIBPNG_ROOT}" "${SQLITE_ROOT}" "${LIBFFI_ROOT}"; do
+  LDFLAGS="$(find $ext -type d \( -name lib -o -name lib64 \) -exec echo -L\{\} \; | tr '\n' ' ' ) ${LDFLAGS}"
+  CPPFLAGS="$(find $ext -type d -name include -exec echo -I\{\} \;  | tr '\n' ' ' ) ${CPPFLAGS}"
 done
-export LDFLAGS=$(echo $LDFLAGS)
-export CPPFLAGS=$(echo $CPPFLAGS)
 
-# Check if OpenSSL and zlib are from AliEn
-if [[ $ALIEN_RUNTIME_VERSION ]]; then
-  OPENSSL_ROOT=${OPENSSL_ROOT:+$ALIEN_RUNTIME_ROOT}
-  ZLIB_ROOT=${ZLIB_ROOT:+$ALIEN_RUNTIME_ROOT}
-fi
-case $ARCHITECTURE in
-  osx*) [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT=$(brew --prefix openssl@1.1) ;;
+case ${ARCHITECTURE} in
+  osx*) [[ -z "${OPENSSL_ROOT}" ]] && OPENSSL_ROOT="$(brew --prefix openssl@1.1)" ;;
 esac
 
 # Set own OpenSSL if appropriate
-if [[ $OPENSSL_ROOT ]]; then
-  export CPATH="$OPENSSL_ROOT/include:$OPENSSL_ROOT/include/openssl:$CPATH"
-  export CPPFLAGS="-I$OPENSSL_ROOT/include -I$OPENSSL_ROOT/include/openssl $CPPFLAGS"
-  export CFLAGS="-I$OPENSSL_ROOT/include -I$OPENSSL_ROOT/include/openssl"
+if [[ -n "${OPENSSL_ROOT}" ]]; then
+  export CPATH="${OPENSSL_ROOT}/include:${OPENSSL_ROOT}/include/openssl:${CPATH}"
+  export CPPFLAGS="-I${OPENSSL_ROOT}/include -I${OPENSSL_ROOT}/include/openssl ${CPPFLAGS}"
+  export CFLAGS="-I${OPENSSL_ROOT}/include -I${OPENSSL_ROOT}/include/openssl"
   cat >> Modules/Setup.dist <<EOF
 
 SSL=$OPENSSL_ROOT
@@ -76,8 +71,8 @@ make altinstall
 
 # Patch long shebangs (by default max is 128 chars on Linux) and add pip(3)/python(3) symlinks
 pushd "$INSTALLROOT/bin"
-  sed -i.deleteme -e "1 s|^#!${INSTALLROOT}/bin/\(.*\)$|#!/usr/bin/env \1|" * || true
-  rm -f *.deleteme
+  sed -i.deleteme -e "1 s|^#!${INSTALLROOT}/bin/\(.*\)$|#!/usr/bin/env \1|" ./* || true
+  rm -f -- *.deleteme
   PYTHON_BIN=$(for X in python*; do echo "$X"; done | grep -E '^python[0-9]+\.[0-9]+$' | head -n1)
   PIP_BIN=$(for X in pip*; do echo "$X"; done | grep -E '^pip[0-9]+\.[0-9]+$' | head -n1)
   PYTHON_CONFIG_BIN=$(for X in python*-config; do echo "$X"; done | grep -E '^python[0-9]+\.[0-9]+m?-config$' | head -n1)
