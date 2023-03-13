0ab070f6e1f44d62e70c9c028148521844839f3e
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2023-03-13 14:52:41 +0100

    Update grpc.sh

--- grpc.sh
+++ grpc.sh
@@ -24,7 +24,7 @@ popd
 
 case $ARCHITECTURE in
   osx*)
-    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT_DIR=$(brew --prefix openssl@1.1)
+    [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT=$(brew --prefix openssl@1.1)
     [[ ! $PROTOBUF_ROOT ]] && PROTOBUF_ROOT=$(brew --prefix protobuf)
     # to avoid issues with rpath on mac
     extra_cmake_variables="-DCMAKE_INSTALL_RPATH=$INSTALLROOT/lib \
@@ -33,7 +33,6 @@ case $ARCHITECTURE in
   ;;
 esac
 
-echo "OPENSSL_ROOT_DIR : $OPENSSL_ROOT_DIR"
 echo "OPENSSL_ROOT : $OPENSSL_ROOT"
 echo "OPENSSL_REVISION: $OPENSSL_REVISION"
 
@@ -52,9 +51,8 @@ cmake $SOURCEDIR                                    \
   -DgRPC_BUILD_GRPC_CPP_PLUGIN=ON                   \
   -DgRPC_BUILD_CSHARP_EXT=OFF                       \
   -DgRPC_RE2_PROVIDER=package                       \
-  ${OPENSSL_REVISION:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR} \
-  ${OPENSSL_REVISION:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
-  ${OPENSSL_REVISION:+-DOpenSSL_ROOT="$OPENSSL_ROOT"}   \
+  ${OPENSSL_ROOT:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
+  ${OPENSSL_ROOT:+-DOpenSSL_ROOT="$OPENSSL_ROOT"}   \
   -DgRPC_CARES_PROVIDER=package \
   $extra_cmake_variables
 
