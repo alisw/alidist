c5855b54c0adf4dd87915476680f653393039560
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-03-14 17:15:45 +0200

    Revert "zlib:: use PKG_ROOT"
    This reverts commit a2538357974932efaaa4729e117db9d132f3924e.

--- zlib.sh
+++ zlib.sh
@@ -34,7 +34,7 @@ mkdir -p etc/modulefiles
 alibuild-generate-module --lib > "etc/modulefiles/${PKGNAME}"
 
 cat >> "etc/modulefiles/${PKGNAME}" <<EoF
-setenv ZLIB_ROOT \$PKG_ROOT
+setenv ZLIB_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 EoF
 
 mkdir -p "${INSTALLROOT}/etc/modulefiles"
