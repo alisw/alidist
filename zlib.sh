95910a936bf373b01938405c30512ab91b6a5631
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-03-14 10:38:45 +0200

    zlib:: solve alidistlint warnings

--- zlib.sh
+++ zlib.sh
@@ -3,27 +3,27 @@ version: "%(tag_basename)s"
 tag: v1.2.8
 source: https://github.com/star-externals/zlib
 build_requires:
- - "GCC-Toolchain:(?!osx)"
+  - "GCC-Toolchain:(?!osx)"
 prefer_system: "(?!slc5)"
 prefer_system_check: |
   printf "#include <zlib.h>\n" | cc -xc++ - -c -M 2>&1
 ---
-#!/bin/sh
+#!/bin/bash -e
 
 echo "Building ALICE zlib. To avoid this install zlib development package."
-rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
+rsync -a --delete --exclude '**/.git' --delete-excluded "${SOURCEDIR}/" ./
 
-case $ARCHITECTURE in
+case "${ARCHITECTURE}" in
    *_amd64_gcc4[56789]*)
      CFLAGS="-fPIC -O3 -DUSE_MMAP -DUNALIGNED_OK -D_LARGEFILE64_SOURCE=1 -msse3" \
-     ./configure --prefix=$INSTALLROOT
+     ./configure --prefix="${INSTALLROOT}"
      ;;
    *_armv7hl_gcc4[56789]* )
      CFLAGS="-fPIC -O3 -DUSE_MMAP -DUNALIGNED_OK -D_LARGEFILE64_SOURCE=1" \
-     ./configure --prefix=$INSTALLROOT
+     ./configure --prefix="${INSTALLROOT}"
      ;;
    * )
-     ./configure --prefix=$INSTALLROOT
+     ./configure --prefix="${INSTALLROOT}"
    ;;
 esac
 make ${JOBS+-j $JOBS}
