ba1d5f4d139aa357e7ae833d7f7a2a350834b5b3
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-03-14 12:29:22 +0200

    libxml2 :: move to alibuild-generate-module and add lzma dependency

--- libxml2.sh
+++ libxml2.sh
@@ -1,43 +1,38 @@
 package: libxml2
 version: "%(tag_basename)s"
 tag: v2.9.3
+requires:
+  - zlib
+  - lzma
 build_requires:
   - "autotools:(slc6|slc7)"
-  - zlib
   - "GCC-Toolchain:(?!osx)"
 source: https://github.com/alisw/libxml2.git
 prefer_system: "(?!slc5)"
 prefer_system_check: |
-  xml2-config --version;
-  if [ $? -ne 0 ]; then printf "libxml2 not found.\n * On RHEL-compatible systems you probably need: libxml2 libxml2-devel\n * On Ubuntu-compatible systems you probably need: libxml2 libxml2-dev"; exit 1; fi
+  xml2-config --version || { printf "libxml2 not found.
+  * On RHEL-compatible systems you probably need: libxml2 libxml2-devel
+  * On Ubuntu-compatible systems you probably need: libxml2 libxml2-dev"; exit 1; }
 ---
-#!/bin/sh
+#!/bin/bash -e
+
 echo "Building ALICE libxml. To avoid this install libxml development package."
-rsync -a $SOURCEDIR/ ./
+rsync -a "${SOURCEDIR}/" ./
 autoreconf -i
 ./configure --disable-static \
-            --prefix=$INSTALLROOT \
-            --with-zlib="${ZLIB_ROOT}" --without-python
+            --prefix="${INSTALLROOT}" \
+            --with-zlib="${ZLIB_ROOT}" --with-lzma="${LZMA_ROOT}" --without-python
 
 make ${JOBS+-j $JOBS}
 make install
+
 # Modulefile
 mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${ZLIB_REVISION:+zlib/$ZLIB_VERSION-$ZLIB_REVISION}
-# Our environment
-setenv LIBXML2_VERSION \$version
-set LIBXML2_ROOT \$::env(BASEDIR)/$PKGNAME/\$::env(LIBXML2_VERSION)
-setenv LIBXML2_ROOT \$LIBXML2_ROOT
-prepend-path PATH \$LIBXML2_ROOT/bin
-prepend-path LD_LIBRARY_PATH \$LIBXML2_ROOT/lib
+alibuild-generate-module --lib --bin > "etc/modulefiles/${PKGNAME}"
+
+cat >> "etc/modulefiles/${PKGNAME}" <<EoF
+setenv LIBXML2_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 EoF
-mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+
+mkdir -p "${INSTALLROOT}/etc/modulefiles"
+rsync -a --delete etc/modulefiles/ "${INSTALLROOT}/etc/modulefiles"
