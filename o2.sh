aa5811b438fb7d58580b7d9a5aad1a3b7141765a
vascobarroso <>
2023-03-24 09:36:24 +0100

    Bump O2 to 20230324

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "nightly-20230323"
+tag: "nightly-20230324"
 requires:
   - arrow
   - FairRoot
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "nightly-20230323"
+tag: "nightly-20230324"
 requires:
   - O2
   - ONNXRuntime
