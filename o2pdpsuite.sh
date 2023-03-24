03d57e04400f6876aac99f1c20abfffa54392986
ALICE Builder <alibuild@cern.ch>
2023-03-24 15:39:03 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20230219"
+tag: "async-async-20230216.1-alma-alidist-O2PDPSuite-nightly-20230219"
 requires:
   - O2
   - DataDistribution
