576bbe6ae4556ab7e14ea33bd5e6b1f9bda40152
ALICE Builder <alibuild@users.noreply.github.com>
2023-04-12 16:57:26 +0200

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh o2dpg.sh (#4948)

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "nightly-20230405"
+tag: "nightly-20230412"
 requires:
   # List important packages separately, even though they're also
   # pulled in by O2sim, so they show up in the dependency list on Monalisa.
