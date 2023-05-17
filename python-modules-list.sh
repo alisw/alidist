b3c8cc8e176c7aad1f19023324e0dd150ac4802d
Timo Wilken <timo.wilken@cern.ch>
2023-05-17 15:53:55 +0200

    Use Python modules from 56-patches branch

--- python-modules-list.sh
+++ python-modules-list.sh
@@ -2,20 +2,20 @@ package: Python-modules-list
 version: "1.0"
 env:
   PIP_REQUIREMENTS: |
-    requests==2.21.0
+    requests==2.27.1
     ipykernel==5.1.0
     ipython==7.4.0
     ipywidgets==7.4.2
     metakernel==0.20.14
     mock==2.0.0
     notebook==5.7.8
-    numpy==1.16.2
-    pandas==0.24.2
     PyYAML==5.1
-    scikit-learn==0.20.3
-    scipy==1.2.1
-    uproot==3.4.18
+    uproot==4.1.0
+    psutil==5.8.0
+    scons==4.1.0
   PIP36_REQUIREMENTS: |
+    numpy==1.16.2
+    scipy==1.2.1
     Cython==0.29.16
     seaborn==0.9.0
     sklearn-evaluation==0.4
@@ -24,7 +24,46 @@ env:
     xgboost==0.82
     dryable==1.0.3
     responses==0.10.6
-    RootInteractive==0.0.10
+    pandas==0.24.2
+    scikit-learn==0.20.3
+  PIP38_REQUIREMENTS: |
+    scipy==1.6.1
+    Cython==0.29.21
+    seaborn==0.9.0
+    sklearn-evaluation==0.4
+    Keras==2.4.3
+    tensorflow==2.4.1
+    xgboost==1.3.3
+    numpy==1.19.5
+    dryable==1.0.3
+    responses==0.10.6
+    pandas==1.2.3
+    scikit-learn==0.24.1
+  PIP39_REQUIREMENTS: |
+    numpy==1.21.4
+    scipy==1.7.3
+    Cython==0.29.21
+    seaborn==0.11.0
+    scikit-learn==0.24.1
+    sklearn-evaluation==0.5.2
+    Keras==2.4.3
+    xgboost==1.2.0
+    dryable==1.0.5
+    responses==0.10.6
+    pandas==1.1.5
+  "PIP39_REQUIREMENTS_ubuntu2110_x86_64": |
+    setuptools<=60.8.2
+    numpy==1.21.4
+    scipy==1.7.3
+    Cython==0.29.21
+    seaborn==0.11.0
+    scikit-learn==0.24.1
+    sklearn-evaluation==0.5.2
+    Keras==2.4.3
+    xgboost==1.2.0
+    dryable==1.0.5
+    responses==0.10.6
+    pandas==1.1.5
 ---
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
--- python-modules.sh
+++ python-modules.sh
@@ -1,45 +1,82 @@
 package: Python-modules
 version: "1.0"
 requires:
-  - "Python:slc.*"
-  - "Python-system:(?!slc.*)"
+  - "Python:(slc|ubuntu)"
+  - "Python-system:(?!slc.*|ubuntu)"
   - FreeType
   - libpng
 build_requires:
-  - curl
+  - system-curl
   - Python-modules-list
+  - alibuild-recipe-tools
 prepend_path:
   PYTHONPATH: $PYTHON_MODULES_ROOT/share/python-modules/lib/python/site-packages
 ---
-#!/bin/bash -ex
 
-# Major.minor version of Python
-export PYVER=$(python3 -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_version())')
-
-# Ignore what is already in PYTHONPATH. We will set PYTHONPATH or PYTHONUSERBASE per command
+# A spurios PYTHONPATH can affect later commands
 unset PYTHONPATH
-
-# PIP_REQUIREMENTS & PIP36_REQUIREMENTS come from Python-modules-list
-echo $PIP_REQUIREMENTS | tr \  \\n > requirements.txt
-if python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3006 else 1)' && [[ $ARCHITECTURE != slc6* ]]; then
-  echo $PIP36_REQUIREMENTS | tr \  \\n >> requirements.txt
+# If we are in a virtualenv, assume that what you want to do is to copy
+# the same installation in your alibuild one.
+if [ ! "X$VIRTUAL_ENV" = X ]; then
+  # Once more to get the deactivate
+  . $VIRTUAL_ENV/bin/activate
+  pip freeze > system-requirements.txt
+  deactivate
 fi
 
+# Special lists for different platforms
+PIP39_REQUIREMENTS=$(eval echo \${PIP39_REQUIREMENTS_${ARCHITECTURE/-/_}:-$PIP39_REQUIREMENTS})
+PIP310_REQUIREMENTS=$(eval echo \${PIP310_REQUIREMENTS_${ARCHITECTURE/-/_}:-$PIP310_REQUIREMENTS})
+
+# PIP_REQUIREMENTS, PIP36_REQUIREMENTS, PIP38_REQUIREMENTS come from python-modules-list.sh
+case $ARCHITECTURE in
+  osx_arm64)
+  touch requirements.txt
+  ;;
+  slc6*)
+  echo $PIP_REQUIREMENTS | tr \  \\n > requirements.txt;;
+  *)
+  echo $PIP_REQUIREMENTS | tr \  \\n > requirements.txt
+  if python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3010 else 1)'; then
+    echo $PIP310_REQUIREMENTS | tr \  \\n >> requirements.txt
+  elif python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3009 else 1)'; then
+    echo $PIP39_REQUIREMENTS | tr \  \\n >> requirements.txt
+  elif python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3008 else 1)'; then
+    echo $PIP38_REQUIREMENTS | tr \  \\n >> requirements.txt
+  elif python3 -c 'import sys; exit(0 if 1000*sys.version_info.major + sys.version_info.minor >= 3006 else 1)'; then
+    echo $PIP36_REQUIREMENTS | tr \  \\n >> requirements.txt
+  fi
+  ;;
+esac
 # We use a different INSTALLROOT, so that we can build updatable RPMS which
 # do not conflict with the underlying Python installation.
 PYTHON_MODULES_INSTALLROOT=$INSTALLROOT/share/python-modules
 mkdir -p $PYTHON_MODULES_INSTALLROOT
+
+# Create the virtualenv
+python3 -m venv $PYTHON_MODULES_INSTALLROOT
+. $PYTHON_MODULES_INSTALLROOT/bin/activate
+
+# Upgrade pip
+python3 -m pip install -IU pip
+# Install setuptools upfront, since this seems to create issues now...
+python3 -m pip install -IU "setuptools<=60.8.2"
+python3 -m pip install -IU wheel
+
 # FIXME: required because of the newly introduced dependency on scikit-garden requires
 # a numpy to be installed separately
 # See also:
 #   https://github.com/scikit-garden/scikit-garden/issues/23
-grep RootInteractive requirements.txt && env PYTHONUSERBASE="$PYTHON_MODULES_INSTALLROOT" pip3 install --user -IU numpy
-# Do not move cython from 0.29.06 for now since 3.0.0rc1 breaks on GPU
-grep RootInteractive requirements.txt && env PYTHONUSERBASE="$PYTHON_MODULES_INSTALLROOT" pip3 install --user -IU cython==0.29.16
-env PYTHONUSERBASE="$PYTHON_MODULES_INSTALLROOT" pip3 install --user -IU -r requirements.txt
+python3 -m pip install -IU numpy
+python3 -m pip install -IU -r requirements.txt
 
+# Major.minor version of Python
+export PYVER="$(python3 -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_version())')"
 # Find the proper Python lib library and export it
 pushd "$PYTHON_MODULES_INSTALLROOT"
+  # let's remove any pre-existent symlinks to have a clean slate
+  [ -h lib64 ] && unlink lib64
+  [ -h lib ]   && unlink lib
   if [[ -d lib64 ]]; then
     ln -nfs lib64 lib  # creates lib pointing to lib64
   elif [[ -d lib ]]; then
@@ -50,55 +87,9 @@ pushd "$PYTHON_MODULES_INSTALLROOT"
   popd
   pushd bin
     # Fix shebangs: remove hardcoded Python path
-    sed -i.deleteme -e "1 s|^#!${PYTHON_MODULES_INSTALLROOT}/bin/\(.*\)$|#!/usr/bin/env \1|" * || true
-    rm -f *.deleteme || true
+    find . -type f -exec sed -i.deleteme -e "s|${PYTHON_MODULES_INSTALLROOT}|/usr|;s|python3|env python3|" '{}' \;
+    find . -name "*.deleteme" -delete
   popd
-popd
-
-# Install matplotlib (quite tricky)
-MATPLOTLIB_TAG="3.0.3"
-if [[ $ARCHITECTURE != slc* ]]; then
-  # Simply get it via pip in most cases
-  env PYTHONUSERBASE=$PYTHON_MODULES_INSTALLROOT pip3 install --user "matplotlib==$MATPLOTLIB_TAG"
-else
-
-  # We are on a RHEL-compatible OS. We compile it ourselves, and link it to our dependencies
-
-  # Check if we can enable the Tk interface
-  python3 -c 'import _tkinter' && MATPLOTLIB_TKAGG=True || MATPLOTLIB_TKAGG=False
-  MATPLOTLIB_URL="https://github.com/matplotlib/matplotlib/archive/v${MATPLOTLIB_TAG}.tar.gz"  # note the "v"
-  curl -SsL "$MATPLOTLIB_URL" | tar xzf -
-  cd matplotlib-*
-  cat > setup.cfg <<EOF
-[directories]
-basedirlist  = ${FREETYPE_ROOT:+$PWD/fake_freetype_root,$FREETYPE_ROOT,}${LIBPNG_ROOT:+$LIBPNG_ROOT,}${ZLIB_ROOT:+$ZLIB_ROOT,}/usr/X11R6,$(freetype-config --prefix),$(libpng-config --prefix)
-[gui_support]
-gtk = False
-gtkagg = False
-tkagg = $MATPLOTLIB_TKAGG
-wxagg = False
-macosx = False
-EOF
-
-  # matplotlib wants include files in <PackageRoot>/include, but this is not the case for FreeType
-  if [[ $FREETYPE_ROOT ]]; then
-    mkdir fake_freetype_root
-    ln -nfs $FREETYPE_ROOT/include/freetype2 fake_freetype_root/include
-  fi
-
-  export PYTHONPATH="$PYTHON_MODULES_INSTALLROOT/lib/python/site-packages"
-    python3 setup.py build
-    python3 setup.py install --prefix "$PYTHON_MODULES_INSTALLROOT"
-  unset PYTHONPATH
-fi
-
-# Test if matplotlib can be loaded
-env PYTHONPATH="$PYTHON_MODULES_INSTALLROOT/lib/python/site-packages" python3 -c 'import matplotlib'
-
-# Patch long shebangs (by default max is 128 chars on Linux)
-pushd "$PYTHON_MODULES_INSTALLROOT/bin"
-  sed -i.deleteme -e '1 s|^#!.*$|#!/usr/bin/env python3|' * || true
-  rm -f *.deleteme
 popd
 
 # Remove useless stuff
@@ -109,18 +100,9 @@ find "$PYTHON_MODULES_INSTALLROOT"/lib/python* \
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
-MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${PYTHON_REVISION:+Python/$PYTHON_VERSION-$PYTHON_REVISION} ${ALIEN_RUNTIME_REVISION:+AliEn-Runtime/$ALIEN_RUNTIME_VERSION-$ALIEN_RUNTIME_REVISION}
+alibuild-generate-module > "$MODULEDIR/$PKGNAME"
+cat >> "$MODULEDIR/$PKGNAME" <<EoF
 # Our environment
 set PYTHON_MODULES_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$PYTHON_MODULES_ROOT/share/python-modules/bin
