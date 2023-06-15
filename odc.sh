c018c2793ff60cfcee365f2af0497afd31a53076
Luboš Krčál <lubos.krcal@gmail.com>
2023-06-15 07:59:31 +0200

    Bump ODC to 0.78.0

--- odc.sh
+++ odc.sh
@@ -1,7 +1,7 @@
 # Online Device Control
 package: ODC
 version: "%(tag_basename)s"
-tag: "0.78.0-beta"
+tag: "0.78.0"
 source: https://github.com/FairRootGroup/ODC.git
 requires:
   - boost
@@ -71,7 +71,7 @@ module load BASE/1.0                                                            
             ${DDS_REVISION:+DDS/$DDS_VERSION-$DDS_REVISION}                                         \\
             ${GRPC_REVISION:+grpc/$GRPC_VERSION-$GRPC_REVISION}                                     \\
             ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
-            
+
 # Our environment
 set ODC_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$ODC_ROOT/bin
