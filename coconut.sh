32e9671699e8ce068c793f333023c60e9b45d0a6
Teo Mrnjavac <teo.m@cern.ch>
2023-06-27 12:31:54 +0200

    Bump AliECS to v0.70.2

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v0.70.0"
+tag: "v0.70.2"
 build_requires:
   - golang
   - protobuf
--- control-core.sh
+++ control-core.sh
@@ -1,6 +1,6 @@
 package: Control-Core
 version: "%(tag_basename)s"
-tag: "v0.70.0"
+tag: "v0.70.2"
 build_requires:
   - "GCC-Toolchain:(?!osx)"
   - golang
