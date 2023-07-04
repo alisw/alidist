caa8e6d3f839e6d685304c2a42920e76c5bdb481
Timo Wilken <timo.wilken@cern.ch>
2023-07-04 17:37:16 +0200

    Try mirroring expected vars when using find_package(CONFIG)

--- geant4.sh
+++ geant4.sh
@@ -1,6 +1,6 @@
 package: GEANT4
 version: "%(tag_basename)s"
-tag: "v11.0.4-alice1"
+tag: "v11.0.4-alice2"
 source: https://github.com/alisw/geant4.git
 # source: https://gitlab.cern.ch/geant4/geant4.git
 requires:
