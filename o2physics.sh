9c0f47cd5cc490f1842c5f222dbd042722d0e0b8
ALICE Builder <alibuild@cern.ch>
2023-07-10 02:18:21 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh qualitycontrol.sh o2dpg.sh

--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "daily-20230709-0200"
+tag: "gpu-nightly-20230710"
 requires:
   - O2
   - ONNXRuntime
--- qualitycontrol.sh
+++ qualitycontrol.sh
@@ -1,6 +1,6 @@
 package: QualityControl
 version: "%(tag_basename)s"
-tag: v1.108.0
+tag: "gpu-nightly-20230710"
 requires:
   - boost
   - "GCC-Toolchain:(?!osx)"
