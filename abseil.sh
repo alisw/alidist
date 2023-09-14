4e5cacc6cc8a89a37fa8b80bbee90cd516e0c071
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-09-14 08:48:20 +0200

    Update abseil.sh
    Avoid picking stuff from the system explicitly, since it apparently does not work.

--- abseil.sh
+++ abseil.sh
@@ -8,9 +8,6 @@ build_requires:
   - ninja
   - alibuild-recipe-tools
 source: https://github.com/abseil/abseil-cpp
-prefer_system: "osx"
-prefer_system_check: |
-  printf '#include <absl/container/flat_hash_map.h>' | c++ -std=c++17 -I`brew --prefix abseil`/include -c -xc++ - >/dev/null
 incremental_recipe: |
   cmake --build . -- ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
