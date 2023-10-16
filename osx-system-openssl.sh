6678e0a451a56eb28de4474136f2f195883ed02a
Timo Wilken <timo.wilken@cern.ch>
2023-10-16 12:25:32 +0200

    Minor update of shell syntax

--- osx-system-openssl.sh
+++ osx-system-openssl.sh
@@ -4,6 +4,6 @@ system_requirement_missing: |
   Please make sure you install openssl using Homebrew (brew install openssl@3)
 system_requirement: "osx.*"
 system_requirement_check: |
-  echo '#include <openssl/bio.h>' | c++ -x c++ - -I`brew --prefix openssl@3`/include -c -o /dev/null
+  echo '#include <openssl/bio.h>' | c++ -x c++ - -I"$(brew --prefix openssl@3)/include" -c -o /dev/null
 ---
 
