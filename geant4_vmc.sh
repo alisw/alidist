4efe8ad39e02b353eded0e5fc0580b5c9bc44516
Timo Wilken <timo.wilken@cern.ch>
2023-10-27 14:09:32 +0200

    Update G4_VMC in line with G4

--- geant4_vmc.sh
+++ geant4_vmc.sh
@@ -1,31 +1,33 @@
 package: GEANT4_VMC
 version: "%(tag_basename)s"
-tag: "v5-0-p6"
+tag: "v6-1-p8"
 source: https://github.com/vmc-project/geant4_vmc
 requires:
   - ROOT
+  - VMC
   - GEANT4
   - vgm
 build_requires:
   - CMake
+  - ninja
   - "Xcode:(osx.*)"
 prepend_path:
-  ROOT_INCLUDE_PATH: "$GEANT4_VMC_ROOT/include/g4root:$GEANT4_VMC_ROOT/include/geant4vmc:$GEANT4_VMC_ROOT/include/mtroot"
+  ROOT_INCLUDE_PATH: "$GEANT4_VMC_ROOT/include/g4root:$GEANT4_VMC_ROOT/include/geant4vmc"
 env:
   G4VMCINSTALL: "$GEANT4_VMC_ROOT"
 ---
 #!/bin/bash -e
 LDFLAGS="$LDFLAGS -L$GEANT4_ROOT/lib"            \
   cmake "$SOURCEDIR"                             \
+    -GNinja                                      \
     -DCMAKE_CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
     -DGeant4VMC_USE_VGM=ON                       \
     -DCMAKE_INSTALL_LIBDIR=lib                   \
+    -DGeant4VMC_BUILD_EXAMPLES=OFF               \
     -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"        \
     ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}
 
-make ${JOBS+-j $JOBS} install
-G4VMC_SHARE=$(cd "$INSTALLROOT/share"; echo Geant4VMC-* | cut -d' ' -f1)
-ln -nfs "$G4VMC_SHARE/examples" "$INSTALLROOT/share/examples"
+cmake --build . -- ${JOBS+-j $JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -40,13 +42,13 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${GEANT4_REVISION:+GEANT4/$GEANT4_VERSION-$GEANT4_REVISION} ${ROOT_REVISION:+ROOT/$ROOT_VERSION-$ROOT_REVISION} vgm/$VGM_VERSION-$VGM_REVISION
+module load BASE/1.0 ${GEANT4_REVISION:+GEANT4/$GEANT4_VERSION-$GEANT4_REVISION} ${ROOT_REVISION:+ROOT/$ROOT_VERSION-$ROOT_REVISION} ${VMC_REVISION:+VMC/$VMC_VERSION-$VMC_REVISION} vgm/$VGM_VERSION-$VGM_REVISION
 # Our environment
 set GEANT4_VMC_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv GEANT4_VMC_ROOT \$GEANT4_VMC_ROOT
 setenv G4VMCINSTALL \$GEANT4_VMC_ROOT
+setenv USE_VGM 1
 prepend-path PATH \$GEANT4_VMC_ROOT/bin
-prepend-path ROOT_INCLUDE_PATH \$GEANT4_VMC_ROOT/include/mtroot
 prepend-path ROOT_INCLUDE_PATH \$GEANT4_VMC_ROOT/include/geant4vmc
 prepend-path ROOT_INCLUDE_PATH \$GEANT4_VMC_ROOT/include/g4root
 prepend-path LD_LIBRARY_PATH \$GEANT4_VMC_ROOT/lib
