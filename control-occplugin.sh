bed3da8b5b232bff73074704af4bf7d9fb4f443d
Teo Mrnjavac <teo.m@cern.ch>
2023-11-10 13:53:04 +0100

    Bump AliECS to v0.83.0

--- control-occplugin.sh
+++ control-occplugin.sh
@@ -1,6 +1,6 @@
 package: Control-OCCPlugin
 version: "%(tag_basename)s"
-tag: "v0.82.2"
+tag: "v0.83.0"
 requires:
   - FairMQ
   - FairLogger
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v0.82.2"
+version: "v0.83.0"
 requires:
   - Control-Core
   - Control-OCCPlugin
