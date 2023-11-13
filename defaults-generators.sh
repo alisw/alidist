19d2001f1412729b3268eb34a64265faa1b1f8e7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-11-13 08:54:42 +0100

    Update defaults-generators.sh

--- defaults-generators.sh
+++ defaults-generators.sh
@@ -5,7 +5,7 @@ disable:
 env:
   CFLAGS: -fPIC -g -O2
   CMAKE_BUILD_TYPE: RELWITHDEBINFO
-  CXXFLAGS: -fPIC -g -O2 -std=c++14
+  CXXFLAGS: -fPIC -g -O2 -std=c++17
 overrides:
   GCC-Toolchain:
     version: v12.2.0-alice1
