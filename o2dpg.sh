ba03e26bac1e95fbe0f59257ca1552526d36da10
ALICE Builder <alibuild@cern.ch>
2023-11-14 02:31:52 +0000

    Auto-update: defaults-o2-epn.sh o2pdpsuite.sh o2.sh o2dpg.sh

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "epn-20231110.1"
+tag: "epn-20231114"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
