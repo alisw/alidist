aa93b26c964f4985dc4e968718763f6e3ab0cfea
Timo Wilken <timo.wilken@cern.ch>
2023-11-20 15:42:19 +0100

    Release AliRoot/AliPhysics v5-09-59r

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -14,8 +14,10 @@ disable:
 overrides:
   AliPhysics:
     version: '%(commit_hash)s_O2'
+    tag: v5-09-59r-01
   AliRoot:
     version: '%(commit_hash)s_O2'
+    tag: v5-09-59r
     requires:
       - ROOT
       - DPMJET
