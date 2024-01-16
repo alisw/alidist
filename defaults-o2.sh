6d960c5ecee2e0348597be0e7debc258affe30d8
ALICE Builder <alibuild@cern.ch>
2024-01-16 15:24:31 +0000

    Auto-update: defaults-o2.sh aliphysics.sh

--- defaults-o2.sh
+++ defaults-o2.sh
@@ -1,5 +1,6 @@
-package: defaults-o2
-version: v1
+disable:
+- mesos
+- MySQL
 env:
   CFLAGS: -fPIC -O2
   CMAKE_BUILD_TYPE: RELWITHDEBINFO
@@ -8,37 +9,37 @@ env:
   ENABLE_VMC: 'ON'
   GEANT4_BUILD_MULTITHREADED: 'OFF'
   MACOSX_DEPLOYMENT_TARGET: '10.15'
-disable:
-  - mesos
-  - MySQL
 overrides:
   AliPhysics:
-    version: '%(commit_hash)s_O2'
-    tag: v5-09-59q-01
+    tag: vAN-20240116
+    version: '%(tag_basename)s_O2'
   AliRoot:
-    version: '%(commit_hash)s_O2'
-    tag: v5-09-59q
     requires:
-      - ROOT
-      - DPMJET
-      - fastjet:(?!.*ppc64)
-      - GEANT3
-      - GEANT4_VMC
-      - Vc
-      - ZeroMQ
-      - JAliEn-ROOT
+    - ROOT
+    - DPMJET
+    - fastjet:(?!.*ppc64)
+    - GEANT3
+    - GEANT4_VMC
+    - Vc
+    - ZeroMQ
+    - JAliEn-ROOT
+    tag: v5-09-59q
+    version: '%(commit_hash)s_O2'
   GCC-Toolchain:
-    version: v12.2.0-alice1
     tag: v12.2.0-alice1
+    version: v12.2.0-alice1
   cgal:
     version: 4.12.2
   fastjet:
     tag: v3.4.1_1.052-alice1
   pythia:
-    tag: v8304
     requires:
-      - lhapdf
-      - boost
+    - lhapdf
+    - boost
+    tag: v8304
+package: defaults-o2
+version: v1
+
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
