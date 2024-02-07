0fa3a66569c84d4b51d814c3d19c9d615669e8d2
Timo Wilken <timo.wilken@cern.ch>
2024-02-07 15:43:16 +0100

    Reinstate virtualenv

--- python-modules.sh
+++ python-modules.sh
@@ -9,42 +9,48 @@ build_requires:
   - Python-modules-list
   - alibuild-recipe-tools
 prepend_path:
+  # If we need tensorflow-metal to work on Mac during subsequent builds, we
+  # must use lib/python$pyver, not lib/python here.
   PYTHONPATH: "$PYTHON_MODULES_ROOT/lib/python/site-packages"
 ---
 #!/bin/bash -e
 unset VIRTUAL_ENV
 
-# Install Python packages in a semi-standard location.
-PYTHON_MODULES_INSTALLROOT=$INSTALLROOT/lib/python/site-packages
-# Make sure we pick up the pip we install through base-requirements.txt directly.
-export PYTHONPATH="$PYTHON_MODULES_INSTALLROOT:${PYTHONPATH:+:$PYTHONPATH}"
+# Users might want to install more packages in the same environment. A venv
+# provides a pip binary that will install packages into the same path.
+# This copies the system python binary (or the one from PYTHON_ROOT) into the
+# venv. We must not use symlinks, since those break if the package is uploaded
+# to a remote store.
+python3 -m venv --copies "$INSTALLROOT"
+. "$INSTALLROOT/bin/activate"
+
+# Major.minor version of Python, needed for PYTHONPATH.
+pyver=$(python3 -c 'import distutils.sysconfig; print(distutils.sysconfig.get_python_version())')
 
 # Install pinned basic requirements for python infrastructure
 echo "$PIP_BASE_REQUIREMENTS" > base-requirements.txt
-python3 -m pip install -IU -t "$PYTHON_MODULES_INSTALLROOT" -r base-requirements.txt
+python3 -m pip install -IU -r base-requirements.txt
 # The above updates pip and setuptools, so install the rest of the packages separately.
 echo "$PIP_REQUIREMENTS" > requirements.txt
-python3 -m pip install -IU -t "$PYTHON_MODULES_INSTALLROOT" -r requirements.txt
+python3 -m pip install -IU -r requirements.txt
 
 # Remove useless stuff
-rm -rvf "$PYTHON_MODULES_INSTALLROOT/share"
-find "$PYTHON_MODULES_INSTALLROOT" -mindepth 2 -maxdepth 2 \
+rm -rvf "$INSTALLROOT/share"
+find "$INSTALLROOT" -mindepth 2 -maxdepth 2 \
      -type d -and \( -name test -or -name tests \) -exec rm -rvf '{}' \;
 
-# By default, "pip install --target" installs binaries inside the given target
-# dir as well, but we want them directly under $INSTALLROOT/bin instead.
-rm -rf "${INSTALLROOT:?}/bin"
-mv "$PYTHON_MODULES_INSTALLROOT/bin" "$INSTALLROOT/bin"
-
-# Fix shebangs: remove hardcoded Python path. Most scripts will have a shebang
-# like "#!<PYTHON_ROOT>/bin/python3" by default, which we must change.
-sed -r -i.deleteme -e "1s,^#!(${PYTHON_ROOT:+$PYTHON_ROOT|}$PYTHON_MODULES_INSTALLROOT)/bin/(.+),#!/usr/bin/env \2," \
-    "$INSTALLROOT"/bin/*
+# Fix shebangs: remove hardcoded Python path. Scripts' shebangs will point at
+# the venv's python using an absolute path by default, which we must change.
+sed -r -i.deleteme -e "1s,^#!$INSTALLROOT/bin/,#!/usr/bin/env ," "$INSTALLROOT"/bin/*
 rm -f "$INSTALLROOT"/bin/*.deleteme
+
+# Link python -> python$pyver, so we can refer to it in PYTHONPATH without knowing pyver.
+ln -nsf "python$pyver" "$INSTALLROOT/lib/python"
 
 # Modulefile
 mkdir -p "$INSTALLROOT/etc/modulefiles"
 alibuild-generate-module --bin > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
 cat >> "$INSTALLROOT/etc/modulefiles/$PKGNAME" <<EOF
-prepend-path PYTHONPATH \$PKG_ROOT/lib/python/site-packages
+# We need to use lib/python$pyver, not lib/python here so that tensorflow-metal works on Mac.
+prepend-path PYTHONPATH \$PKG_ROOT/lib/python$pyver/site-packages
 EOF
