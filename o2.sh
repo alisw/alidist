b841ec258afdb82954497e0b856337835db12922
Matteo Concas <matteo.concas@cern.ch>
2024-02-13 14:54:28 +0100

    Remove obsolete exception in o2.sh
    CTests are not run anymore in fullCI, this is now superfluous.

--- o2.sh
+++ o2.sh
@@ -96,7 +96,7 @@ incremental_recipe: |
     find $PWD -name "*.root" -delete
     rm -rf test_logs
     TESTERR=
-    ctest -C ${CMAKE_BUILD_TYPE} -E "(test_Framework)|(test_GPUsort(CUDA|HIP))" --output-on-failure ${JOBS+-j $JOBS} || TESTERR=$?
+    ctest -C ${CMAKE_BUILD_TYPE} -E "test_Framework" --output-on-failure ${JOBS+-j $JOBS} || TESTERR=$?
     ctest -C ${CMAKE_BUILD_TYPE} -R test_Framework --output-on-failure || TESTERR=$?
     # Display additional logs for tests that timed out in a non-fatal way
     set +x
