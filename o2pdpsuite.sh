6f44a8fb704097b246d0ab85da41b1a8c6be071c
ALICE Builder <alibuild@cern.ch>
2024-02-21 01:17:41 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh qualitycontrol.sh o2dpg.sh

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "daily-20240220-0100"
+tag: "gpu-nightly-20240221"
 requires:
   # List important packages separately, even though they're also
   # pulled in by O2sim, so they show up in the dependency list on Monalisa.
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "daily-20240220-0100"
+tag: "gpu-nightly-20240221"
 requires:
   - O2
   - ONNXRuntime
