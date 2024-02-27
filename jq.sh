481fb7f38521836d07d18f531251f2b968639802
Timo Wilken <timo.wilken@cern.ch>
2024-02-27 16:01:35 +0100

    Fix oniguruma's dependency on GCC
    This made aliBuild think that jq runtime-depends on GCC. That's bad because
    alibuild-generate-module would then generate a modulefile that loads GCC,
    which we don't want if we're loading jq's modulefile as part of the toolchain.

--- jq.sh
+++ jq.sh
@@ -6,38 +6,26 @@ build_requires:
   - "GCC-Toolchain:(?!osx)"
   - oniguruma
   - "autotools:(slc6|slc7)"
+  - alibuild-recipe-tools
 prefer_system: (?!slc5.*)
 prefer_system_check: |
   type jq
 ---
+#!/bin/bash -e
 # Hack to avoid having to do autogen inside $SOURCEDIR
-rsync -a --exclude '**/.git' --delete $SOURCEDIR/ $BUILDDIR
-cd $BUILDDIR
+rsync -a --exclude '**/.git' --delete "$SOURCEDIR/" "$BUILDDIR"
+cd "$BUILDDIR"
 autoreconf -iv
-./configure --prefix=$INSTALLROOT          \
+./configure --prefix="$INSTALLROOT"        \
             --enable-static                \
             --disable-shared               \
             --disable-maintainer-mode      \
-            --with-oniguruma=$ONIGURUMA_ROOT \
+            --with-oniguruma="$ONIGURUMA_ROOT" \
             --disable-dependency-tracking
 
 make ${JOBS+-j $JOBS}
 make install
+
 # Modulefile
-MODULEDIR="$INSTALLROOT/etc/modulefiles"
-MODULEFILE="$MODULEDIR/$PKGNAME"
-mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0                                                                       
-# Our environment
-set JQ_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path PATH \$JQ_ROOT/bin
-EoF
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module --bin > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
