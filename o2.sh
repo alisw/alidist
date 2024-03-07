3462e0f9c5882efb0e71b387d09402ff033c5610
ALICE Builder <alibuild@cern.ch>
2024-03-07 08:02:16 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh o2dpg.sh

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "daily-20240306-0100"
+tag: "daily-20240307-0101"
 requires:
   - arrow
   - FairRoot
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "daily-20240306-0100"
+tag: "daily-20240307-0101"
 requires:
   - O2
   - ONNXRuntime
