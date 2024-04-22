625862c3c1621f12059a3c1bc1939fda0e916f70
Martin Vala <vala.martin@gmail.com>
2024-04-22 16:02:15 +0200

    ndmspc v0.20240422.0 release (#5432)

--- ndmspc.sh
+++ ndmspc.sh
@@ -1,11 +1,11 @@
 package: ndmspc
 version: "%(tag_basename)s"
-tag: "v0.0.1"
+tag: "v0.20240422.0"
 requires:
   - ROOT
 build_requires:
-  - "Clang:(?!osx)"
   - CMake
+  - ninja
   - alibuild-recipe-tools
 source: https://gitlab.com/ndmspc/ndmspc.git
 incremental_recipe: |
@@ -19,12 +19,16 @@ if [[ $ALIBUILD_NDMSPC_TESTS ]]; then
   # Impose extra errors.
   CXXFLAGS="${CXXFLAGS} -Werror -Wno-error=deprecated-declarations"
 fi
-cmake --version
 
 # When O2 is built against Gandiva (from Arrow), then we need to use
 # -DLLVM_ROOT=$CLANG_ROOT, since O2's CMake calls into Gandiva's
 # -CMake, which requires it.
-cmake "$SOURCEDIR" "-DCMAKE_INSTALL_PREFIX=$INSTALLROOT"
+cmake "$SOURCEDIR" "-DCMAKE_INSTALL_PREFIX=$INSTALLROOT"          \
+      -G Ninja                                                    \
+      ${CMAKE_BUILD_TYPE:+"-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"} \
+      ${CXXSTD:+"-DCMAKE_CXX_STANDARD=$CXXSTD"}                   \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
 cmake --build . -- ${JOBS+-j $JOBS} install
 
 # export compile_commands.json in (taken from o2.sh)
@@ -40,7 +44,7 @@ MODULEFILE="etc/modulefiles/$PKGNAME"
 alibuild-generate-module --bin --lib > "$MODULEFILE"
 cat >> "$MODULEFILE" <<EoF
 # Our environment
-set NDMSPC_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv NDMSPC_ROOT \$NDMSPC_ROOT
+setenv NDMSPC_MACRO_DIR \$PKG_ROOT/macros
+prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
