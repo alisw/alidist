b3febc4bc4fd70e9d38f2d703d68abac8eab69db
Anton Alkin <anton.alkin@cern.ch>
2024-04-25 15:22:23 +0200

    Bump arrow

--- arrow.sh
+++ arrow.sh
@@ -1,6 +1,6 @@
 package: arrow
-version: "v14.0.1-alice1"
-tag: apache-arrow-14.0.1-alice1
+version: "v16.0.0-alice1"
+tag: apache-arrow-16.0.0-alice1
 source: https://github.com/alisw/arrow.git
 requires:
   - boost
