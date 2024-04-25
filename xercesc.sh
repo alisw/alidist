211cd1f8175d7b8f5a8e71be6fcf6c97d4897ff8
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-04-25 09:06:38 +0200

    Bump xercesc to 3.2.5 (#5424)
    * Bump xercesc to 3.2.5
    * Fix prefer_system_check

--- xercesc.sh
+++ xercesc.sh
@@ -1,19 +1,21 @@
 package: xercesc
-version: Xerces-C_3_2_4
-tag: v3.2.4
+version: Xerces-C_3_2_5
+tag: v3.2.5
 source: https://github.com/apache/xerces-c
 build_requires:
   - CMake
   - alibuild-recipe-tools
+  - ninja
 prefer_system: ".*"
 prefer_system_check: |
-  pkg-config --atleast-version=3.2.0 xerces-c 2>&1 && printf "#include \"<xercesc/util/PlatformUtils.hpp>\"\nint main(){}" | c++ -xc - -o /dev/null
+  pkg-config --atleast-version=3.2.0 xerces-c 2>&1 && printf "#include <xercesc/util/PlatformUtils.hpp>" | c++ -xc++ -I$(brew --prefix)/include -c -
 prepend_path:
   ROOT_INCLUDE_PATH: "$XERCESC_ROOT/include"
   CMAKE_PREFIX_PATH: "$XERCESC_ROOT"
 ---
 
 cmake $SOURCEDIR                                         \
+      -G Ninja                                           \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                \
       -DBUILD_SHARED_LIBS=ON                             \
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}             \
@@ -21,8 +23,7 @@ cmake $SOURCEDIR                                         \
       -Dnetwork:BOOL=OFF                                 \
       -DCMAKE_INSTALL_LIBDIR=lib
 
-make ${JOBS:+-j $JOBS}
-make install
+cmake --build . -- ${JOBS:+-j $JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
