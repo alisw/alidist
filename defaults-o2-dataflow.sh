fc03858698e3c4bbfcb66ea47bc33e427a7753fb
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-04-29 08:52:27 +0200

    Improve GCC builds
    - Move to GCC 13.2.0
    - Move to GDB 14.2
    - Do not use GCC 7.3.0 as default, since now AliRoot it's built
      with its own default.
    - Add support for CCACHE. While disabled by default it can be enabled
      by passing CCACHE_DISABLE=false as environment variable.

--- defaults-o2-dataflow.sh
+++ defaults-o2-dataflow.sh
@@ -35,9 +35,6 @@ disable:
   - OpenSSL
   - curl:(?!osx.*)
 overrides:
-  GCC-Toolchain:
-    version: "v12.2.0-alice1"
-    tag: "v12.2.0-alice1"
   Python-modules-list:
     env:
       PIP_BASE_REQUIREMENTS: |
