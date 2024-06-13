9cabd072c66232ceb5a43c6adf4cc7e8f77c58f8
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-06-13 11:46:08 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
 package: ROOT
 version: "%(tag_basename)s"
-tag: "v6-32-00"
-source: https://github.com/root-project/root.git
+tag: "v6-32-00-alice1"
+source: https://github.com/alisw/root.git
 requires:
   - arrow
   - AliEn-Runtime:(?!.*ppc64)
