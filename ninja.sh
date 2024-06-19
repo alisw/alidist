a2d4b39af420818ab29cafb6949b3a44a3703736
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-06-19 14:08:18 +0200

    Apply suggestions from code review

--- ninja.sh
+++ ninja.sh
@@ -7,11 +7,12 @@ build_requires:
   - "CMake"
   - alibuild-recipe-tools
 ---
+#!/bin/bash
 cmake -Bbuild-cmake $SOURCEDIR
 cmake --build build-cmake
 
 mkdir -p $INSTALLROOT/bin
-cp build-cmake/ninja $INSTALLROOT/bin
+cp build-cmake/ninja "$INSTALLROOT/bin"
 
-mkdir -p $INSTALLROOT/etc/modulefiles
+mkdir -p "$INSTALLROOT/etc/modulefiles"
 alibuild-generate-module --bin >"$INSTALLROOT/etc/modulefiles/$PKGNAME"
