cb3d74dc9892d05297259617568e126644d1d54a
Michal Tichák <michal.tichak@cern.ch>
2024-07-02 16:55:19 +0200

    Add moderncppkafka for QC

--- qualitycontrol.sh
+++ qualitycontrol.sh
@@ -14,6 +14,7 @@ requires:
   - Python-modules
   - libjalienO2
   - bookkeeping-api
+  - moderncppkafka
 build_requires:
   - CMake
   - "Clang:(?!osx)"   # for Gandiva
@@ -79,6 +80,7 @@ cmake $SOURCEDIR                                                                
       ${LIBINFOLOGGER_REVISION:+-DInfoLogger_ROOT=$LIBINFOLOGGER_ROOT}                                          \
       -DO2_ROOT=$O2_ROOT                                                                                        \
       -DMS_GSL_INCLUDE_DIR=$MS_GSL_ROOT/include                                                                 \
+      -DMODERNCPPKAFKA_ROOT=${MODERNCPPKAFKA_ROOT}/include                                                      \
       ${ARROW_ROOT:+-DGandiva_DIR=$ARROW_ROOT/lib/cmake/Gandiva}                                                \
       ${ARROW_ROOT:+-DArrow_DIR=$ARROW_ROOT/lib/cmake/Arrow}                                                    \
       ${ARROW_ROOT:+${CLANG_ROOT:+-DLLVM_ROOT=$CLANG_ROOT}}                                                     \
