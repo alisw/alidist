39b0cfee25069ee7718d93cc26b5852dfa3a3ef8
Michal Tichák <michal.tichak@cern.ch>
2024-07-02 16:55:19 +0200

    Add moderncppkafka for QC
    linter fixes

---
+++ moderncppkafka.sh
@@ -0,0 +1,16 @@
+package: moderncppkafka
+version: "%(tag_basename)s"
+tag: v2023.03.07
+requires:
+  - librdkafka
+source: https://github.com/morganstanley/modern-cpp-kafka.git
+---
+#!/bin/bash -e
+
+# this is header only library, so we can just copy it
+mkdir -p "$INSTALLROOT"
+cp -r "$SOURCEDIR/include" "$INSTALLROOT/include"
+
+# Modulefile
+mkdir -p "etc/modulefiles"
+alibuild-generate-module --lib > "etc/modulefiles/$PKGNAME"
