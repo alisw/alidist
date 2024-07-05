40f1555c492aee0c3ba16bac42a015d7a8649a08
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2024-07-05 09:26:19 +0200

    Fix for RPMs for moderncppkafka.sh (#5538)

--- moderncppkafka.sh
+++ moderncppkafka.sh
@@ -5,12 +5,15 @@ requires:
   - librdkafka
 source: https://github.com/morganstanley/modern-cpp-kafka.git
 ---
-#!/bin/bash -e
+#!/bin/bash -e                                                                                                                                                                                           
 
-# this is header only library, so we can just copy it
+# this is header only library, so we can just copy it                                                                                                                                                    
 mkdir -p "$INSTALLROOT"
 cp -r "$SOURCEDIR/include" "$INSTALLROOT/include"
+rm "$INSTALLROOT/include/CMakeLists.txt" # for some reason it is there                                                                                                                                   
 
-# Modulefile
+# Modulefile                                                                                                                                                                                             
 mkdir -p "etc/modulefiles"
 alibuild-generate-module --lib > "etc/modulefiles/$PKGNAME"
+mkdir -p "$INSTALLROOT/etc/modulefiles" && rsync -a --delete etc/modulefiles/ "$INSTALLROOT/etc/modulefiles"
+
