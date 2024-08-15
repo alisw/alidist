94d6d7141e7f9a9330d92be1df93a44ded4fb90d
Anton Alkin <anton.alkin@cern.ch>
2024-08-15 14:34:06 +0200

    fixup! arrow: use clang from llvm installation to avoid stack probing issue with llvm@18

--- arrow.sh
+++ arrow.sh
@@ -1,6 +1,6 @@
 package: arrow
-version: "v17.0.0-alice4"
-tag: apache-arrow-17.0.0-alice4
+version: "v17.0.0-alice5"
+tag: apache-arrow-17.0.0-alice5
 source: https://github.com/alisw/arrow.git
 requires:
   - boost
