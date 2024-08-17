431bf1743df652d9d32c76a8cc5af0e00e653c0b
ALICE Builder <alibuild@cern.ch>
2024-08-17 01:28:56 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh qualitycontrol.sh o2dpg.sh

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "daily-20240816-0200"
+tag: "gpu-nightly-20240817"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "daily-20240816-0200"
+tag: "gpu-nightly-20240817"
 requires:
   - O2
   - ONNXRuntime
