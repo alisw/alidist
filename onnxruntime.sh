23c3e5f5916c167995d098fea53b50cbc5c26576
Christian Sonnabend <sonnabendch@gmail.com>
2024-09-04 18:37:50 +0200

    Modifying recipe for build on Nvidia GPU's (still needs testing)

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -19,7 +19,60 @@ prepend_path:
 
 mkdir -p $INSTALLROOT
 
+ORT_BUILD_FLAGS=""
+case $ARCHITECTURE in
+  osx_*)
+    if [[ $ARCHITECTURE == *_x86-64 ]]; then
+      echo "Installing ONNXRuntime for MacOS (CPU version)"
+    else
+      echo "Installing ONNXRuntime for MacOS (Metal backend)"
+    fi
+  ;;
+  *)
+    if command -v rocminfo >/dev/null 2>&1; then
+      echo "Installing ONNXRuntime for ROCm"
+      ORT_BUILD_FLAGS="\
+                      -Donnxruntime_USE_ROCM=ON                                                       \
+                      -Donnxruntime_ROCM_HOME=/opt/rocm                                               \
+                      -DCMAKE_HIP_COMPILER=/opt/rocm/llvm/bin/clang++                                 \
+                      -D__HIP_PLATFORM_AMD__=1                                                        \
+                      -DCMAKE_HIP_ARCHITECTURES=gfx906,gfx908                                         \
+                      -Donnxruntime_USE_COMPOSABLE_KERNEL=OFF                                         \
+                      -Donnxruntime_USE_ROCBLAS_EXTENSION_API=ON                                      \
+                      -Donnxruntime_USE_COMPOSABLE_KERNEL_CK_TILE=ON                                  \
+                      -Donnxruntime_ENABLE_TRAINING=OFF                                               \
+                      -Donnxruntime_DISABLE_RTTI=OFF                                                  \
+                      -DMSVC=OFF                                                                      \
+                      "
+    elif command -v nvcc >/dev/null 2>&1; then
+      CUDA_VERSION=$(nvcc --version | grep "release" | awk '{print $NF}' | cut -d. -f1)
+      if [[ "$CUDA_VERSION" == "V11" ]]; then
+        echo "Installing ONNXRuntime for CUDA 11.x"
+        ORT_BUILD_FLAGS="\
+                          -Donnxruntime_USE_CUDA=ON                                                     \
+                          -Donnxruntime_USE_CUDA_NHWC_OPS=ON                                            \
+                          -Donnxruntime_CUDA_USE_TENSORRT=ON                                            \
+                          "
+      elif [[ "$CUDA_VERSION" == "V12" ]]; then
+        echo "Installing ONNXRuntime for CUDA 12.x"
+        ORT_BUILD_FLAGS="\
+                          -Donnxruntime_USE_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_ROOT                  \
+                          -Donnxruntime_USE_CUDA_NHWC_OPS=ON                                            \
+                          -Donnxruntime_CUDA_USE_TENSORRT=ON                                            \
+                          "
+      else
+        echo "CUDA version is not 11.x or 12.x, installing ONNXRuntime basic CPU version"
+        ORT_BUILD_FLAGS=""
+      fi
+    else
+      echo "Installing ONNXRuntime basic CPU version"
+      ORT_BUILD_FLAGS=""
+    fi
+  ;;
+esac
+
 cmake "$SOURCEDIR/cmake"                                                              \
+      "$ORT_BUILD_FLAGS"                                                              \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                             \
       -DCMAKE_BUILD_TYPE=Release                                                      \
       -DCMAKE_INSTALL_LIBDIR=lib                                                      \
@@ -27,18 +80,7 @@ cmake "$SOURCEDIR/cmake"                                                        
       -Donnxruntime_BUILD_UNIT_TESTS=OFF                                              \
       -Donnxruntime_PREFER_SYSTEM_LIB=ON                                              \
       -Donnxruntime_BUILD_SHARED_LIB=ON                                               \
-      -Donnxruntime_USE_ROCM=ON                                                       \
-      -Donnxruntime_ROCM_HOME=/opt/rocm                                               \
-      -DCMAKE_HIP_COMPILER=/opt/rocm/llvm/bin/clang++                                 \
-      -D__HIP_PLATFORM_AMD__=1                                                        \
-      -DCMAKE_HIP_ARCHITECTURES=gfx906,gfx908                                         \
-      -Donnxruntime_USE_COMPOSABLE_KERNEL=OFF                                         \
-      -Donnxruntime_USE_ROCBLAS_EXTENSION_API=ON                                      \
-      -Donnxruntime_USE_COMPOSABLE_KERNEL_CK_TILE=ON                                  \
-      -Donnxruntime_ENABLE_TRAINING=OFF                                               \
       -DProtobuf_USE_STATIC_LIBS=ON                                                   \
-      -Donnxruntime_DISABLE_RTTI=OFF                                                  \
-      -DMSVC=OFF                                                                      \
       ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}           \
       ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a} \
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}      \
