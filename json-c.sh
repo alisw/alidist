bc3bd88565702c4161dc0dffb71facda12180005
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-09-05 13:22:57 +0200

    Bump json-c to v17.0
    As suggested by @davidrohr, this should bypass the reversed calloc
    arguments error

--- json-c.sh
+++ json-c.sh
@@ -1,15 +1,14 @@
 package: json-c
-version: "v0.13.1"
-tag: "json-c-0.13.1-20180305"
+version: "v0.17.0"
+tag: "json-c-0.17-20230812"
 source: https://github.com/json-c/json-c
 build_requires:
-  - "autotools:(slc6|slc7)"
+  - CMake
   - "GCC-Toolchain:(?!osx)"
 ---
 #!/bin/bash -e
 rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
-autoreconf -ivf
-./configure --disable-shared --enable-static --prefix="$INSTALLROOT"
+./cmake-configure --disable-shared --enable-static --prefix="$INSTALLROOT"
 make ${JOBS+-j $JOBS} install
 
 # Modulefile
