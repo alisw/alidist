98a426f255384736789a9f8d0c270014227c26b4
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-09-05 13:53:11 +0200

    Use new modulefile script too

--- json-c.sh
+++ json-c.sh
@@ -5,6 +5,7 @@ source: https://github.com/json-c/json-c
 build_requires:
   - CMake
   - "GCC-Toolchain:(?!osx)"
+  - alibuild-recipe-tools
 ---
 #!/bin/bash -e
 rsync -a --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
@@ -14,22 +15,5 @@ cd build
 ../cmake-configure --disable-shared --enable-static --prefix="$INSTALLROOT"
 make ${JOBS+-j $JOBS} install
 
-# Modulefile
-mkdir -p etc/modulefiles
-cat > etc/modulefiles/$PKGNAME <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}
-# Our environment
-set JSON_C_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv JSON_C_ROOT \$JSON_C_ROOT
-prepend-path PATH \$JSON_C_ROOT/bin
-prepend-path LD_LIBRARY_PATH \$JSON_C_ROOT/lib
-EoF
-mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
+mkdir -p "$INSTALLROOT/etc/modulefiles"
+alibuild-generate-module --bin --lib --cmake > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
