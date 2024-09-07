efec63183c342b788450b2f0d10f0db7867a2ad7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-09-07 08:28:36 +0200

    Properly built catch2 library (#5604)
    This will build the new Catch2 library (3.7.0) and a standalone archive library, as suggested by the catch2 authors themselves.

---
+++ catch2.sh
@@ -0,0 +1,26 @@
+package: Catch2
+version: "%(tag_basename)s"
+tag: v3.7.0
+source: https://github.com/catchorg/Catch2
+requires:
+  - "GCC-Toolchain:(?!osx)"
+build_requires:
+  - CMake
+  - alibuild-recipe-tools
+  - ninja
+prepend_path:
+  PKG_CONFIG_PATH: "$CATCH2_ROOT/share/pkgconfig"
+---
+cmake "$SOURCEDIR" -GNinja -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"
+
+cmake --build . --target install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+
+alibuild-generate-module --lib --cmake > "$MODULEFILE"
+cat << EOF >> "$MODULEFILE"
+prepend-path PKG_CONFIG_PATH \$PKG_ROOT/share/pkgconfig
+EOF
