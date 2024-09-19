fcd9ef21c33e36dce53c44a047f64bc8b289c634
David Rohr <drohr@jwdt.org>
2024-09-19 15:12:44 +0200

    Don't fix an old GCC version since meanwhile we have generally bumped GCC (#5626)

--- defaults-generators.sh
+++ defaults-generators.sh
@@ -7,9 +7,6 @@ env:
   CMAKE_BUILD_TYPE: RELWITHDEBINFO
   CXXFLAGS: -fPIC -g -O2 -std=c++17
 overrides:
-  GCC-Toolchain:
-    version: v12.2.0-alice1
-    tag: v12.2.0-alice1
   boost:
     requires:
       - GCC-Toolchain:(?!osx)
