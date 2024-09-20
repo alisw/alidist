05e25d4e64677ebc4e11207a2c92e243ef8940a4
Piotr Konopka <piotr.jan.konopka@cern.ch>
2024-09-20 16:11:01 +0200

    Bump AliECS to v1.19.1 (#5631)

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v1.19.0"
+tag: "v1.19.1"
 build_requires:
   - golang
   - protobuf
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v1.19.0"
+version: "v1.19.1"
 requires:
   - Control-Core
   - Control-OCCPlugin
