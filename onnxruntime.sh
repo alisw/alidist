5e42e469e858fee076b88972374e9491c56662a2
Christian Sonnabend <sonnabendch@gmail.com>
2024-10-03 13:48:17 +0200

    Adding automatic checks for migraphx, changing build cmake flags and adding env-variables for GPU enabling during code execution. For al9_gpu container and simultaneous CUDA & ROCm build, this requires https://github.com/ChSonnabend/onnxruntime/commit/6ffc40c3357ab13c83b65447404594d1f53e34db

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -30,10 +30,15 @@ case $ARCHITECTURE in
   ;;
   *)
     if command -v rocminfo >/dev/null 2>&1; then
-      echo "Installing ONNXRuntime for ROCm"
-      ORT_BUILD_FLAGS="\
+      echo "Enabling ONNXRuntime build for ROCm"
+      export ORT_ROCM_BUILD=1
+      if command -v find /opt/rocm* -name "libmigraphx*" >/dev/null 2>&1; then
+        echo "  - ROCm build with MIGraphX"
+        export ORT_MIGRAPHX_BUILD=1
+        ORT_BUILD_FLAGS="${ORT_BUILD_FLAGS} -Donnxruntime_USE_MIGRAPHX=ON"
+      fi
+      ORT_BUILD_FLAGS="${ORT_BUILD_FLAGS}                                                             \
                       -Donnxruntime_USE_ROCM=ON                                                       \
-                      -Donnxruntime_USE_MIGRAPHX=ON                                                   \
                       -Donnxruntime_ROCM_HOME=/opt/rocm                                               \
                       -DCMAKE_HIP_COMPILER=/opt/rocm/llvm/bin/clang++                                 \
                       -D__HIP_PLATFORM_AMD__=1                                                        \
@@ -41,33 +46,25 @@ case $ARCHITECTURE in
                       -Donnxruntime_USE_COMPOSABLE_KERNEL=OFF                                         \
                       -Donnxruntime_USE_ROCBLAS_EXTENSION_API=ON                                      \
                       -Donnxruntime_USE_COMPOSABLE_KERNEL_CK_TILE=ON                                  \
-                      -Donnxruntime_ENABLE_TRAINING=OFF                                               \
                       -Donnxruntime_DISABLE_RTTI=OFF                                                  \
+                      -Donnxruntime_ENABLE_TRAINING=OFF                                               \
                       -DMSVC=OFF                                                                      \
                       "
     elif command -v nvcc >/dev/null 2>&1; then
-      CUDA_VERSION=$(nvcc --version | grep "release" | awk '{print $NF}' | cut -d. -f1)
-      if [[ "$CUDA_VERSION" == "V11" ]]; then
-        echo "Installing ONNXRuntime for CUDA 11.x"
-        ORT_BUILD_FLAGS=" -Donnxruntime_USE_CUDA=ON                                                     \
-                          -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_ROOT                                            \
-                          -Donnxruntime_USE_CUDA_NHWC_OPS=ON                                            \
-                          -Donnxruntime_CUDA_USE_TENSORRT=ON                                            \
-                          "
-      elif [[ "$CUDA_VERSION" == "V12" ]]; then
-        echo "Installing ONNXRuntime for CUDA 12.x"
-        ORT_BUILD_FLAGS=" -Donnxruntime_USE_CUDA=ON                                                     \
-                          -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_ROOT                                            \
-                          -Donnxruntime_USE_CUDA_NHWC_OPS=ON                                            \
-                          -Donnxruntime_CUDA_USE_TENSORRT=ON                                            \
-                          "
-      else
-        echo "CUDA version is not 11.x or 12.x, installing ONNXRuntime basic CPU version"
-        ORT_BUILD_FLAGS=""
+      CUDA_VERSION=$(nvcc --version | grep "release" | awk '{print $NF}' | cut -dV -f2)
+      export ORT_CUDA_BUILD=1
+      # if [[ "$CUDA_VERSION" == "V11" ]]; then
+      echo "Enabling ONNXRuntime build for CUDA (version ${CUDA_VERSION})"
+      ORT_BUILD_FLAGS="${ORT_BUILD_FLAGS}                                                            \
+                      -Donnxruntime_USE_CUDA=ON                                                      \
+                      -Donnxruntime_USE_CUDA_NHWC_OPS=ON                                             \
+                      "
+      if command -v 'find /usr -name "libnvinfer*"' >/dev/null 2>&1; then
+        ORT_BUILD_FLAGS="${ORT_BUILD_FLAGS} -Donnxruntime_CUDA_USE_TENSORRT=ON"
       fi
     else
-      echo "Installing ONNXRuntime basic CPU version"
-      ORT_BUILD_FLAGS=""
+      echo "Building ONNXRuntime basic CPU version"
+      export ORT_CPU_BUILD=1
     fi
   ;;
 esac
@@ -82,6 +79,7 @@ cmake "$SOURCEDIR/cmake"                                                        
       -Donnxruntime_PREFER_SYSTEM_LIB=ON                                              \
       -Donnxruntime_BUILD_SHARED_LIB=ON                                               \
       -DProtobuf_USE_STATIC_LIBS=ON                                                   \
+      -Donnxruntime_ENABLE_TRAINING=OFF                                               \
       ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}           \
       ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a} \
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}      \
