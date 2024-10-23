0dad49e838c01165aa8458556fb408daf5448d90
Teo Mrnjavac <teo.m@cern.ch>
2024-10-23 11:39:15 +0200

    Bump AliECS to v1.22.1

--- control-occplugin.sh
+++ control-occplugin.sh
@@ -1,6 +1,6 @@
 package: Control-OCCPlugin
 version: "%(tag_basename)s"
-tag: "v1.22.0"
+tag: "v1.22.1"
 requires:
   - FairMQ
   - FairLogger
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v1.22.0"
+version: "v1.22.1"
 requires:
   - Control-Core
   - Control-OCCPlugin
