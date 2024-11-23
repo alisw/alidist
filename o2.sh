e16241f281b0a33a9004628b1e9238b7bdf3db36
ALICE Builder <alibuild@cern.ch>
2024-11-23 01:34:38 +0000

    Auto-update: defaults-o2.sh o2pdpsuite.sh o2physics.sh o2.sh qualitycontrol.sh o2dpg.sh

--- o2.sh
+++ o2.sh
@@ -1,6 +1,6 @@
 package: O2
 version: "%(tag_basename)s"
-tag: "daily-20241122-0000"
+tag: "gpu-nightly-20241123"
 requires:
   - abseil
   - arrow
--- o2physics.sh
+++ o2physics.sh
@@ -1,6 +1,6 @@
 package: O2Physics
 version: "%(tag_basename)s"
-tag: "daily-20241122-0000"
+tag: "gpu-nightly-20241123"
 requires:
   - O2
   - ONNXRuntime
