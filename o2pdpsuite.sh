25ea3ad8afa387bad357dd008134ff8dc22aaf3e
ALICE Builder <alibuild@cern.ch>
2024-11-25 15:10:09 +0000

    Auto-update: defaults-o2-epn.sh o2pdpsuite.sh

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "daily-20241125-0000"
+tag: "epn-20241125-DDv1.6.5-QCv1.164.0-flp-suite-v1.47.0"
 requires:
   # List important packages separately, even though they're also
   # pulled in by O2sim, so they show up in the dependency list on Monalisa.
