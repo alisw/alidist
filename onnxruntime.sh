62ece4e12cdbd619b91cda7dd057a024152acea8
Christian Sonnabend <sonnabendch@gmail.com>
2024-11-28 14:28:29 +0100

    Adding check for Alma9 (if system is AlmaLinux). exports's still need to passed to o2.sh

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -20,6 +20,9 @@ prepend_path:
 mkdir -p $INSTALLROOT
 
 # Check ROCm build conditions
+if [[ -f /etc/redhat-release ]]; then
+  export ALMA_LINUX_MAJOR_VERSION=$(awk '{print $3}' /etc/redhat-release | cut -d. -f1)
+fi
 
 if [[ "$ALIBUILD_O2_FORCE_GPU" -eq 1 ]] || [[ "$ALIBUILD_ENABLE_HIP" -eq 1 ]] || \
   ( ( [[ -z "$DISABLE_GPU" ]] || [[ "$DISABLE_GPU" -eq 0 ]] ) && \
@@ -32,9 +35,11 @@ if [[ "$ALIBUILD_O2_FORCE_GPU" -eq 1 ]] || [[ "$ALIBUILD_ENABLE_HIP" -eq 1 ]] ||
   [[ -d /opt/rocm/include/rocrand ]] && \
   [[ -d /opt/rocm/include/miopen ]] && \
   [[ -d /opt/rocm/include/rccl ]] && \
-  [[ -z "$ORT_ROCM_BUILD" ]] ); then
+  [[ -z "$ORT_ROCM_BUILD" ]] && \
+  ([[ -z "$ALMA_LINUX_MAJOR_VERSION" ]] || [[ "$ALMA_LINUX_MAJOR_VERSION" -eq 9 ]]) ); then
   export ORT_ROCM_BUILD=1
   : ${ALIBUILD_O2_OVERRIDE_HIP_ARCHS:="gfx906,gfx908"}
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm/lib
 else
   export ORT_ROCM_BUILD=0
 fi
@@ -110,7 +115,6 @@ mkdir -p "$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$INSTALLROOT/etc/modulefiles/$PKGNAME"
 alibuild-generate-module --lib > "$MODULEFILE"
 cat >> "$MODULEFILE" <<EoF
-
 # Our environment
 set ${PKGNAME}_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path ROOT_INCLUDE_PATH \$${PKGNAME}_ROOT/include/onnxruntime
