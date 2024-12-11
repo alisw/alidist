a8357f0a6ad24698a0ccebcdcba0e53823ed066f
Sergio Garcia <47090312+singiamtel@users.noreply.github.com>
2024-12-11 15:01:05 +0100

    Update tensorflow and tensorflow-mac (#5711)
    * Bump tensorflow to 2.15.1
    
    * Disable tensorflow-metal
    
    * Bump keras
    
    * Bump numpy
    
    * Pin tensorflow to highest Python version

--- python-modules-list.sh
+++ python-modules-list.sh
@@ -43,9 +43,7 @@ env:
 
     numpy == 1.16.2; python_version < '3.8'
     numpy == 1.19.5; python_version == '3.8'
-    numpy == 1.23.4; python_version >= '3.9' and python_version <= '3.10'
-    numpy == 1.23.5; python_version == '3.11'
-    numpy == 1.26.4; python_version >= '3.12'
+    numpy; python_version >= '3.9'
 
     scipy == 1.2.1; python_version < '3.8'
     scipy == 1.6.1; python_version == '3.8'
@@ -73,15 +71,16 @@ env:
 
     Keras == 2.2.4; python_version < '3.8'
     Keras == 2.4.3; python_version == '3.8'
-    Keras == 2.13.1; python_version >= '3.9' and python_version <= '3.10'
+    Keras == 2.15.0; python_version >= '3.9' and python_version <= '3.11'
 
     tensorflow == 1.13.1; python_version < '3.8'
     tensorflow == 2.4.1; python_version == '3.8'
-    # tensorflow == 2.7.1; python_version == '3.9'
-    tensorflow == 2.13.1; python_version >= '3.9' and python_version <= '3.11'
+    # Pinned to 2.15.1 for compatibility with https://github.com/onnx/tensorflow-onnx
+    tensorflow == 2.15.1; python_version >= '3.9' and python_version <= '3.11'
 
     # See version compatibility table at https://pypi.org/project/tensorflow-metal/
-    tensorflow-metal == 1.0.0; sys_platform == 'darwin' and python_version == '3.11'
+    # Temporarily disabled as it brings validation loss regressions in some cases [O2-5627]
+    # tensorflow-metal == 1.0.0; sys_platform == 'darwin' and python_version == '3.11'
 
     xgboost == 0.82; python_version < '3.8'
     xgboost == 1.3.3; python_version == '3.8'
