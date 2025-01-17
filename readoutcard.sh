79a3407f31fdd3d0895a9bf712800bacdf3c892c
sy-c <sy-c@users.noreply.github.com>
2025-01-17 12:32:10 +0100

    fix for python (#5736)
    * fix for python
    
    * fix for python
    
    * Update lla.sh
    
    * Update readoutcard.sh
    
    * fix python

--- readoutcard.sh
+++ readoutcard.sh
@@ -34,7 +34,7 @@ cmake $SOURCEDIR                                                      \
       ${MONITORING_REVISION:+-DMonitoring_ROOT=$MONITORING_ROOT} \
       ${LIBINFOLOGGER_REVISION:+-DInfoLogger_ROOT=$LIBINFOLOGGER_ROOT} \
       ${PDA_REVISION:+-DPDA_ROOT=$PDA_ROOT}                            \
-      ${PYTHON_REVISION:+-DPython3_ROOT_DIR="$PYTHON_ROOT"}            \
+      ${PYTHON_ROOT:+-DPython3_EXECUTABLE="$(which python3)"}      \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON                               \
       -DBUILD_SHARED_LIBS=ON
 
