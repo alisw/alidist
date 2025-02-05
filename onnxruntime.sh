405f18fe37eb828e90e5cb697440a83afbe33b21
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-02-05 13:35:07 +0100

    Use our own abseil to avoid conflicts downstream

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -82,39 +82,42 @@ export ORT_MIGRAPHX_BUILD=$ORT_MIGRAPHX_BUILD
 export ORT_TENSORRT_BUILD=$ORT_TENSORRT_BUILD
 EOF
 
-cmake "$SOURCEDIR/cmake"                                                                                    \
-      -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"                                                                   \
-      -DCMAKE_BUILD_TYPE=Release                                                                            \
-      -DCMAKE_INSTALL_LIBDIR=lib                                                                            \
-      -DPYTHON_EXECUTABLE="$(python3 -c 'import sys; print(sys.executable)')"                                 \
-      -Donnxruntime_BUILD_UNIT_TESTS=OFF                                                                    \
-      -Donnxruntime_PREFER_SYSTEM_LIB=ON                                                                    \
-      -Donnxruntime_BUILD_SHARED_LIB=ON                                                                     \
-      -DProtobuf_USE_STATIC_LIBS=ON                                                                         \
-      -Donnxruntime_ENABLE_TRAINING=OFF                                                                     \
-      ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}                                 \
-      ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a}                       \
-      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}                            \
-      ${PROTOBUF_ROOT:+-DProtobuf_INCLUDE_DIR=$PROTOBUF_ROOT/include}                                       \
-      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc}                              \
-      ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}                                                    \
-      ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                                              \
-      -Donnxruntime_USE_MIGRAPHX=${ORT_MIGRAPHX_BUILD}                                                      \
-      -Donnxruntime_USE_ROCM=${ORT_ROCM_BUILD}                                                              \
-      -Donnxruntime_ROCM_HOME=/opt/rocm                                                                     \
-      -Donnxruntime_CUDA_HOME=/usr/local/cuda                                                               \
-      -DCMAKE_HIP_COMPILER=/opt/rocm/llvm/bin/clang++                                                       \
-      -D__HIP_PLATFORM_AMD__=${ORT_ROCM_BUILD}                                                              \
-      ${ALIBUILD_O2_OVERRIDE_HIP_ARCHS:+-DCMAKE_HIP_ARCHITECTURES=${ALIBUILD_O2_OVERRIDE_HIP_ARCHS}}        \
-      ${ALIBUILD_O2_OVERRIDE_CUDA_ARCH:+-CMAKE_CUDA_ARCHITECTURES=${ALIBUILD_O2_OVERRIDE_CUDA_ARCHS}}       \
-      -Donnxruntime_USE_COMPOSABLE_KERNEL=OFF                                                               \
-      -Donnxruntime_USE_ROCBLAS_EXTENSION_API=${ORT_ROCM_BUILD}                                             \
-      -Donnxruntime_USE_COMPOSABLE_KERNEL_CK_TILE=ON                                                        \
-      -Donnxruntime_DISABLE_RTTI=OFF                                                                        \
-      -DMSVC=OFF                                                                                            \
-      -Donnxruntime_USE_CUDA=${ORT_CUDA_BUILD}                                                              \
-      -Donnxruntime_USE_CUDA_NHWC_OPS=${ORT_CUDA_BUILD}                                                     \
-      -Donnxruntime_CUDA_USE_TENSORRT=${ORT_TENSORRT_BUILD}                                                 \
+cmake "$SOURCEDIR/cmake"                                                                               \
+      -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"                                                            \
+      -DCMAKE_BUILD_TYPE=Release                                                                       \
+      -DCMAKE_INSTALL_LIBDIR=lib                                                                       \
+      -DPYTHON_EXECUTABLE="$(python3 -c 'import sys; print(sys.executable)')"                          \
+      -Donnxruntime_BUILD_UNIT_TESTS=OFF                                                               \
+      -Donnxruntime_PREFER_SYSTEM_LIB=ON                                                               \
+      -Donnxruntime_BUILD_SHARED_LIB=ON                                                                \
+      -DProtobuf_USE_STATIC_LIBS=ON                                                                    \
+      -Donnxruntime_ENABLE_TRAINING=OFF                                                                \
+      -DFETCHCONTENT_QUIET=OFF                                                                         \
+      -DFETCHCONTENT_SOURCE_DIR_ABSEIL_CPP=${ABSEIL_ROOT}/src                                          \
+      ${PROTOBUF_REVISION:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}                        \
+      ${PROTOBUF_REVISION:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a}              \
+      ${PROTOBUF_REVISION:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}                   \
+      ${PROTOBUF_REVISION:+-DProtobuf_INCLUDE_DIR=$PROTOBUF_ROOT/include}                              \
+      ${PROTOBUF_REVISION:+-DProtobuf_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc}                     \
+      ${PROTOBUF_REVISION:+-DONNX_CUSTOM_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc}                  \
+      ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}                                               \
+      ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                                         \
+      -Donnxruntime_USE_MIGRAPHX=${ORT_MIGRAPHX_BUILD}                                                 \
+      -Donnxruntime_USE_ROCM=${ORT_ROCM_BUILD}                                                         \
+      -Donnxruntime_ROCM_HOME=/opt/rocm                                                                \
+      -Donnxruntime_CUDA_HOME=/usr/local/cuda                                                          \
+      -DCMAKE_HIP_COMPILER=/opt/rocm/llvm/bin/clang++                                                  \
+      -D__HIP_PLATFORM_AMD__=${ORT_ROCM_BUILD}                                                         \
+      ${ALIBUILD_O2_OVERRIDE_HIP_ARCHS:+-DCMAKE_HIP_ARCHITECTURES=${ALIBUILD_O2_OVERRIDE_HIP_ARCHS}}   \
+      ${ALIBUILD_O2_OVERRIDE_CUDA_ARCH:+-CMAKE_CUDA_ARCHITECTURES=${ALIBUILD_O2_OVERRIDE_CUDA_ARCHS}}  \
+      -Donnxruntime_USE_COMPOSABLE_KERNEL=OFF                                                          \
+      -Donnxruntime_USE_ROCBLAS_EXTENSION_API=${ORT_ROCM_BUILD}                                        \
+      -Donnxruntime_USE_COMPOSABLE_KERNEL_CK_TILE=ON                                                   \
+      -Donnxruntime_DISABLE_RTTI=OFF                                                                   \
+      -DMSVC=OFF                                                                                       \
+      -Donnxruntime_USE_CUDA=${ORT_CUDA_BUILD}                                                         \
+      -Donnxruntime_USE_CUDA_NHWC_OPS=${ORT_CUDA_BUILD}                                                \
+      -Donnxruntime_CUDA_USE_TENSORRT=${ORT_TENSORRT_BUILD}                                            \
       -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-pass-failed -Wno-error=unused-but-set-variable -Wno-pass-failed=transform-warning -Wno-error=deprecated -Wno-error=maybe-uninitialized" \
       -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-pass-failed -Wno-error=unused-but-set-variable -Wno-pass-failed=transform-warning -Wno-error=deprecated -Wno-error=maybe-uninitialized"
 
