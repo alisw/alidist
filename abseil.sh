389361328e98817768c810519078981da0a1398a
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-03-25 13:48:18 +0100

    Bump abseil to latest version

--- abseil.sh
+++ abseil.sh
@@ -1,6 +1,6 @@
 package: abseil
 version: "%(tag_basename)s"
-tag: "20220623.1"
+tag: "20240722.0"
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
@@ -11,7 +11,7 @@ build_requires:
 source: https://github.com/abseil/abseil-cpp
 prefer_system: "osx"
 prefer_system_check: |
-  printf '#include <absl/container/flat_hash_map.h>' | c++ -std=c++17 -I"$(brew --prefix abseil)/include" -c -xc++ - >/dev/null
+  printf '#include <absl/container/flat_hash_map.h>' | c++ -std=c++20 -I"$(brew --prefix abseil)/include" -c -xc++ - >/dev/null
 incremental_recipe: |
   cmake --build . -- ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
