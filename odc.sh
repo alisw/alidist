0dc0bc82379c4c592b0a6fbfef0c25b7780b593c
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-03-28 09:28:24 +0100

    Fix ODC repository for now

--- odc.sh
+++ odc.sh
@@ -1,8 +1,8 @@
 # Online Device Control
 package: ODC
-version: "0.84.2"
+version: "0.84.2-alice1"
 tag: "2d500f0cf3f131caaf42286afd614ad53827e474"
-source: https://github.com/FairRootGroup/ODC.git
+source: https://github.com/alisw/ODC.git
 requires:
   - boost
   - protobuf
