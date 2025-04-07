5dc9f9b4808f49a4e6e491e1aa61d9e4b956eaa5
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-04-07 09:54:24 +0200

    Improve reproducibility

--- cmake.sh
+++ cmake.sh
@@ -5,6 +5,8 @@ source: https://github.com/Kitware/CMake
 requires:
   - "OpenSSL:(?!osx)"
   - "GCC-Toolchain:(?!osx)"
+  - zlib
+  - curl
 build_requires:
   - make
   - alibuild-recipe-tools
@@ -21,6 +23,10 @@ prefer_system_replacement_specs:
       CMAKE_VERSION: ""
 ---
 #!/bin/bash -e
+SONAME=so
+case $ARCHITECTURE in
+  osx*) SONAME=dylib ;;
+esac
 
 cat > build-flags.cmake <<- EOF
 # Disable Java capabilities; we don't need it and on OS X might miss the
@@ -29,13 +35,24 @@ SET(JNI_H FALSE CACHE BOOL "" FORCE)
 SET(Java_JAVA_EXECUTABLE FALSE CACHE BOOL "" FORCE)
 SET(Java_JAVAC_EXECUTABLE FALSE CACHE BOOL "" FORCE)
 
+SET(ZLIB_LIBRARY $ZLIB_ROOT/lib/libz.$SONAME)
+SET(ZLIB_INCLUDE_DIR $ZLIB_ROOT/include)
+
+SET(CURL_LIBRARY $CURL_ROOT/lib/libcurl.$SONAME)
+SET(CURL_INCLUDE_DIR $CURL_ROOT/include)
+SET(BUILD_TESTING OFF)
+
 # SL6 with GCC 4.6.1 and LTO requires -ltinfo with -lcurses for link to succeed,
 # but cmake is not smart enough to find it. We do not really need ccmake anyway,
 # so just disable it.
 SET(BUILD_CursesDialog FALSE CACHE BOOL "" FORCE)
 EOF
 
-$SOURCEDIR/bootstrap --prefix=$INSTALLROOT \
+rsync -a --chmod=ugo=rwX --delete --exclude '**/.git' --delete-excluded $SOURCEDIR/ ./
+
+./bootstrap --prefix=$INSTALLROOT \
+                     ${ZLIB_ROOT:+--no-system-zlib} \
+                     ${CURL_ROOT:+--no-system-curl} \
                      --init=build-flags.cmake \
                      ${JOBS:+--parallel=$JOBS}
 make ${JOBS+-j $JOBS}
