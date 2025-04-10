f50f00cda18f5e03f6793b635c5d2354cbeeed11
Chunzheng Wang <83008337+ChunzhengLab@users.noreply.github.com>
2025-04-10 11:51:14 +0200

    Fix the system check on safe_int and nlohmann_json (#5839)

--- safe_int.sh
+++ safe_int.sh
@@ -4,7 +4,7 @@ tag: 3.0.28a
 source: https://github.com/dcleblanc/SafeInt
 prefer_system: .*
 prefer_system_check: |
-  printf "#include <SafeInt.hpp>" | cc -I$(brew --prefix safeint)/include - -xc++ -c -o/dev/null
+  printf "#include <SafeInt.hpp>\n" | cc -xc++ - -I"$(brew --prefix safeint)/include" -c -o /dev/null
 ---
 #!/bin/bash -e
 mkdir -p $INSTALLROOT/include
