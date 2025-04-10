4d16e47dd650fd46e29d90d7965f12a4466aaa85
Vasco Barroso <vmcb@cern.ch>
2025-04-10 10:57:39 +0200

    bump O2 to daily-20250410-0000

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "daily-20250404-0000"
+tag: "daily-20250410-0000"
 requires:
   # List important packages separately, even though they're also
   # pulled in by O2sim, so they show up in the dependency list on Monalisa.
