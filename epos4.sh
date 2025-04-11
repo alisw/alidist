fefe2047d89a424d771ddf2ad3da30a19ecf5142
Marco Giacalone <mgiacalo@gmail.com>
2025-04-11 11:33:02 +0200

    Improved EPSO4 recipe

--- epos4.sh
+++ epos4.sh
@@ -6,38 +6,26 @@ requires:
   - ROOT
   - fastjet
   - HepMC3
+  - alibuild-recipe-tools
 ---
 #!/bin/bash -ex
 
-export EPO4VSN=${PKGVERSION}
-
-# The following two variables *must* have a trailing slash! EPOS installation
-# will make a mess otherwise.
-export EPO4=$PWD/
-export BUILD_DIR=${EPO4}build
 export CC=gcc
 export CXX=g++
 export FC=gfortran
 export COP=BASIC
 
-rsync -a --exclude='**/.git' --delete ${SOURCEDIR}/ .
-
-# patch few CMakeFiles
-find ./ -name "CM*.txt" -exec sed -i -e 's/-m64//' {} ';' # not platform independent
-find ./ -name "CM*.txt" -exec sed -i -e 's/-fPIC//' {} ';' # not needed and clashes with mcmodel=large on AARCH64
-
 export LIBRARY_PATH="$LD_LIBRARY_PATH"
-cmake -S $EPO4 -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=${EPO4} \
+cmake -S ${SOURCEDIR} -DCMAKE_INSTALL_PREFIX=${INSTALLROOT} \
           -DCOMPILE_OPTION=${COP} -DCMAKE_BUILD_TYPE=Release \
           -DFASTSYS=$FASTJET \
           -DCMAKE_INSTALL_MESSAGE=LAZY
-cmake --build $BUILD_DIR -j8
-cmake --install $BUILD_DIR
-ctest --test-dir $BUILD_DIR --verbose
+cmake --build . -- ${JOBS:+-j $JOBS}
+cmake --install .
+if [[ $ALIBUILD_O2_TESTS ]]; then
+  ctest --test-dir . --verbose
+fi
 
-# "Install"
-INST_SUBROOT=$INSTALLROOT/epos4
-mkdir -p $INST_SUBROOT 
 rsync -a \
       --exclude='**/CMakeModules' \
       --exclude=CMakeLists.txt \
@@ -46,31 +34,18 @@ rsync -a \
       --exclude=*.c \
       --exclude=*.cpp \
       --exclude=*.f \
-      ./ $INST_SUBROOT/
-chmod u+x $INST_SUBROOT/bin/epos
-find $INSTALLROOT -type d -empty -exec rmdir '{}' \; > /dev/null 2>&1 || true
-[ -d "$INST_SUBROOT" ]
+      $SOURCEDIR/ $INSTALLROOT/
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION fastjet/$FASTJET_VERSION-$FASTJET_REVISION HepMC3/$HEPMC3_VERSION-$HEPMC3_REVISION
-# Our environment
-set EPOS4_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv EPOS4_ROOT \$EPOS4_ROOT
+alibuild-generate-module --lib --bin >$MODULEFILE
+cat >> "$MODULEFILE" <<EoF
+setenv EPOS4_ROOT \$PKG_ROOT
 setenv EPO4VSN 4.0.3
 # Final slash is required by EPOS, please leave it be
-setenv EPO4 \$::env(EPOS4_ROOT)/epos4/
+setenv EPO4 \$PKG_ROOT/
 prepend-path PATH \$::env(EPO4)bin
 setenv OPT ./
 setenv HTO ./
