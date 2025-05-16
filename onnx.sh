0bfb912d1e74f29db5968c410406d162c623a293
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-05-16 11:47:58 +0200

    Update onnx.sh

--- onnx.sh
+++ onnx.sh
@@ -15,7 +15,7 @@ build_requires:
 #!/bin/bash -e
 CPPFLAFS="-I$ABSEIL_ROOT/include"  cmake "$SOURCEDIR"                             \
     -G Ninja                                  \
-    -DABSEIL_DIR=$ABSEIL_ROOT                 \
+    -DABSEIL_DIR="$ABSEIL_ROOT"               \
     -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON     \
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}    \
     -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"     \
