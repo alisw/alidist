5bdae1eb3cd348be44dbb3a91cce7dffa8756c2f
Piotr Konopka <piotr.jan.konopka@cern.ch>
2025-05-22 09:52:00 +0200

    Bump AliECS to v1.36.0

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v1.35.0"
+tag: "v1.36.0"
 build_requires:
   - golang
   - protobuf
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v1.35.0"
+version: "v1.36.0"
 requires:
   - Control-Core
   - Control-OCCPlugin
