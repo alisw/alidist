18838c0ad1550965f6e05f2c53e873d4130646df
Piotr Konopka <piotr.jan.konopka@cern.ch>
2025-06-16 17:02:17 +0200

    Bump AliECS to v1.39.1 (#5926)

--- control-occplugin.sh
+++ control-occplugin.sh
@@ -1,6 +1,6 @@
 package: Control-OCCPlugin
 version: "%(tag_basename)s"
-tag: "v1.39.0"
+tag: "v1.39.1"
 requires:
   - FairMQ
   - FairLogger
--- control.sh
+++ control.sh
@@ -1,5 +1,5 @@
 package: Control
-version: "v1.39.0"
+version: "v1.39.1"
 requires:
   - Control-Core
   - Control-OCCPlugin
