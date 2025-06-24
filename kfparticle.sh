alisw/alidist

commit 5ad12275fcd37652a523968202a085be5e8851e1

Modernize kfparticle recipe (#5927)

kfparticle.sh
@@ -1,6 +1,6 @@
 package: KFParticle
 version: "%(tag_basename)s"
-tag: alice/v1.1-5
+tag: alice/v1.1-6
 source: https://github.com/alisw/KFParticle
 requires:
   - ROOT
@@ -8,35 +8,27 @@ requires:
   - Vc
 build_requires:
   - CMake
+  - ninja
+  - alibuild-recipe-tools
 ---
 #!/bin/bash -e
 
 cmake $SOURCEDIR                                        \
-      ${VC_REVISION:+-DVc_INCLUDE_DIR=$VC_ROOT/include}  \
+      -G Ninja                                          \
+      ${VC_REVISION:+-DVc_INCLUDE_DIR=$VC_ROOT/include} \
       ${VC_VERSIOM:+-DVc_LIBRARIES=$VCROOT/lib/libVc.a} \
-      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT               \
+      -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"             \
       -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"            \
       -DFIXTARGET=FALSE
-make ${JOBS+-j $JOBS} install
+cmake --build . -- ${JOBS+-j $JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
 cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ${GCC_TOOLCHAIN_REVISION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${VC_REVISION:+Vc/$VC_VERSION-$VC_REVISION} ${ROOT_REVISION:+ROOT/$ROOT_VERSION-$ROOT_REVISION}
+$(alibuild-generate-module --bin --lib)
 # Our environment
-set KFPARTICLE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv KFPARTICLE_ROOT \$KFPARTICLE_ROOT
-set BASEDIR \$::env(BASEDIR)
-prepend-path ROOT_INCLUDE_PATH \$BASEDIR/$PKGNAME/\$version/include
-prepend-path LD_LIBRARY_PATH \$BASEDIR/$PKGNAME/\$version/lib
+prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
 EoF

o2.sh
@@ -28,7 +28,6 @@ requires:
   - ONNXRuntime
   - nlohmann_json
   - MLModels
-  - KFParticle
   - RapidJSON
   - bookkeeping-api
   - AliEn-CAs

commit 16fd116e7287a0abfe02c4c1e23f7e9160e61f51

Fix KFParticle module
Regression in #5927. Currently breaks daily build

kfparticle.sh
@@ -29,6 +29,6 @@ mkdir -p "$MODULEDIR"
 cat > "$MODULEFILE" <<EoF
 $(alibuild-generate-module --bin --lib)
 # Our environment
-setenv KFPARTICLE_ROOT \$KFPARTICLE_ROOT
+setenv KFPARTICLE_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
 EoF