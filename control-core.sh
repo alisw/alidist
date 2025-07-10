be701a715aaa6351ee0733833030613a77e6aade
Piotr Konopka <piotr.jan.konopka@cern.ch>
2025-07-10 09:17:48 +0200

    Bump AliECS to v1.40.0

--- control-core.sh
+++ control-core.sh
@@ -1,6 +1,6 @@
 package: Control-Core
 version: "%(tag_basename)s"
-tag: "v1.39.1"
+tag: "v1.40.0"
 build_requires:
   - "GCC-Toolchain:(?!osx)"
   - golang
--- control-occplugin.sh
+++ control-occplugin.sh
@@ -1,6 +1,6 @@
 package: Control-OCCPlugin
 version: "%(tag_basename)s"
-tag: "v1.39.1"
+tag: "v1.40.0"
 requires:
   - FairMQ
   - FairLogger
