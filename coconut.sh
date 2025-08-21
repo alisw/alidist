bc97f8e45d09197931d09eafbaa1a7fc427bf15a
Piotr Konopka <piotr.jan.konopka@cern.ch>
2025-08-21 15:38:55 +0200

    Bump AliECS to v1.42.0 (#5984)

--- coconut.sh
+++ coconut.sh
@@ -1,6 +1,6 @@
 package: coconut
 version: "%(tag_basename)s"
-tag: "v1.41.0"
+tag: "v1.42.0"
 build_requires:
   - golang
   - protobuf
--- control-core.sh
+++ control-core.sh
@@ -1,6 +1,6 @@
 package: Control-Core
 version: "%(tag_basename)s"
-tag: "v1.41.0"
+tag: "v1.42.0"
 build_requires:
   - "GCC-Toolchain:(?!osx)"
   - golang
