f8fdaa995fa71c936e8e80709334b25493b9ee79
ALICE Builder <alibuild@cern.ch>
2025-09-02 10:25:15 +0200

    Auto-update: defaults-o2.sh o2pdpsuite.sh

--- o2pdpsuite.sh
+++ o2pdpsuite.sh
@@ -1,6 +1,6 @@
 package: O2PDPSuite
 version: "%(tag_basename)s"
-tag: "daily-20250902-0000"
+tag: "MC-prod-2025-v10"
 requires:
   # List important packages separately, even though they're also
   # pulled in by O2sim, so they show up in the dependency list on Monalisa.
