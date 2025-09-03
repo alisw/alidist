649a0c2eeb02298735c12b382d111af61da5114c
Vasco Barroso <17883719+vascobarroso@users.noreply.github.com>
2025-09-03 08:53:55 +0200

    Lock ROOT and FairMQ versions for online environment (FLP builds) (#5991)
    * Lock ROOT and FairMQ versions for online environment
    
    We don't want to change the versions of FairMQ and ROOT in the online environment until the end of the 2025 run. On the other hand, bumps of those packages are needed in other setups (e.g. async). 
    
    This change ensures that when we build the RPMs for the FLPs we keep the current versions.
    
    * Fix typo

--- defaults-o2-dataflow.sh
+++ defaults-o2-dataflow.sh
@@ -72,6 +72,10 @@ overrides:
       - libInfoLogger
       - librdkafka   # this one added
       - grpc   # this one added
+  FairMQ:
+    tag: "v1.9.2"
+  ROOT:
+    tag: "v6-32-06-alice9"
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
