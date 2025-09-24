b0699eb539dddfd52827890f6d1b518b2340b36b
Martin Vala <vala.martin@gmail.com>
2025-09-24 16:06:54 +0200

    ndmspc v0.20250923.0 update (#6004)

--- opentelemetry-cpp.sh
+++ opentelemetry-cpp.sh
@@ -1,47 +1,35 @@
-package: ndmspc
+package: opentelemetry-cpp
 version: "%(tag_basename)s"
-tag: "v0.20250923.0"
+tag: "v1.22.0"
 requires:
-  - ROOT
-  - JAliEn-ROOT
-  - nlohmann_json
-  - opentelemetry-cpp
-  - libwebsockets
   - curl
-  - libuv
+  - protobuf
 build_requires:
   - CMake
   - ninja
   - alibuild-recipe-tools
-  - "OpenSSL:(?!osx)"
-source: https://gitlab.com/ndmspc/ndmspc.git
+source: https://github.com/open-telemetry/opentelemetry-cpp.git
 incremental_recipe: |
   [[ $ALIBUILD_NDMSPC_TESTS ]] && CXXFLAGS="${CXXFLAGS} -Werror -Wno-error=deprecated-declarations"
   cmake --build . -- ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
 #!/bin/bash -e
-case $ARCHITECTURE in
-  osx*)
-        [[ -n $OPENSSL_ROOT ]] || OPENSSL_ROOT=$(brew --prefix openssl@3)
-        [[ -n $LIBWEBSOCKETS_ROOT ]] || LIBWEBSOCKETS_ROOT=$(brew --prefix libwebsockets)
-  ;;
-esac
 
 if [[ $ALIBUILD_NDMSPC_TESTS ]]; then
   # Impose extra errors.
   CXXFLAGS="${CXXFLAGS} -Werror -Wno-error=deprecated-declarations"
 fi
 
-cmake "$SOURCEDIR" "-DCMAKE_INSTALL_PREFIX=$INSTALLROOT"                \
-      -G Ninja                                                          \
-      ${CMAKE_BUILD_TYPE:+"-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"}       \
-      ${CXXSTD:+"-DCMAKE_CXX_STANDARD=$CXXSTD"}                         \
-      ${PROTOBUF_ROOT:+"-DPROTOBUF_ROOT=$PROTOBUF_ROOT"}                \
-      ${LIBUV_ROOT:+"-DLIBUV_ROOT=$LIBUV_ROOT"}                         \
-      ${LIBWEBSOCKETS_ROOT:+"-DLIBWEBSOCKETS_ROOT=$LIBWEBSOCKETS_ROOT"} \
-      ${NLOHMANN_JSON_ROOT:+"-DNLOHMANN_JSON_ROOT=$NLOHMANN_JSON_ROOT"} \
-      ${CURL_ROOT:+"-DCURL_ROOT=$CURL_ROOT"}                            \
+cmake "$SOURCEDIR" "-DCMAKE_INSTALL_PREFIX=$INSTALLROOT"          \
+      ${CMAKE_BUILD_TYPE:+"-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"} \
+      ${CXXSTD:+"-DCMAKE_CXX_STANDARD=$CXXSTD"}                   \
+      -DWITH_BENCHMARK=OFF                                        \
+      -DBUILD_TESTING=OFF                                         \
+      -DWITH_EXAMPLES=OFF                                         \
+      -DWITH_OTLP_FILE=ON                                         \
+      -DWITH_OTLP_HTTP=ON                                         \
+      -DBUILD_SHARED_LIBS=ON                                      \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 cmake --build . -- ${JOBS+-j $JOBS} install
@@ -59,7 +47,6 @@ MODULEFILE="etc/modulefiles/$PKGNAME"
 alibuild-generate-module --bin --lib > "$MODULEFILE"
 cat >> "$MODULEFILE" <<EoF
 # Our environment
-prepend-path ROOT_DYN_PATH \$PKG_ROOT/lib
-prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include/ndmspc
+prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
 EoF
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
