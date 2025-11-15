30f91fafc4c2e4242a829f021d23385b3e8b01b7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-11-15 06:29:14 +0100

    Cleanup monitoring recipe (#6034)
    * Move to latest alibuild-recipe-tools
    * Cleanup monitoring recipe
    Avoid picking up incompatible protobuf from the system. Needs a new release of
    monitoring to be effective.

--- monitoring.sh
+++ monitoring.sh
@@ -9,44 +9,42 @@ requires:
 build_requires:
   - CMake
   - alibuild-recipe-tools
-  - protobuf
+  - ninja
 source: https://github.com/AliceO2Group/Monitoring
 incremental_recipe: |
-  make ${JOBS:+-j$JOBS} install
+  cmake --build . -- ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
 ---
 #!/bin/bash -ex
-
 case $ARCHITECTURE in
-    osx*) [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost);;
+    osx*) [[ ! $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost) ;;
 esac
+
 
 if [[ $ALIBUILD_O2_TESTS ]]; then
   CXXFLAGS="${CXXFLAGS} -Werror -Wno-error=deprecated-declarations"
 fi
 
 cmake $SOURCEDIR                                              \
+  -G Ninja                                                    \
   ${LIBRDKAFKA_REVISION:+-DRDKAFKA_ROOT="${LIBRDKAFKA_ROOT}"} \
   ${GRPC_REVISION:+-DGRPC_ROOT="${GRPC_ROOT}"}                \
   -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                         \
   ${BOOST_REVISION:+-DBOOST_ROOT=$BOOST_ROOT}                 \
+  ${LIBGRPC_REVISION:--DO2_MONITORING_CONTROL_ENABLE=0}       \
+  ${LIBRDKAFKA_REVISION:--DO2_MONITORING_KAFKA_ENABLE=0}      \
+  ${PROTOBUF_ROOT:+-DPROTOBUF_ROOT=$PROTOBUF_ROOT}            \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 cp ${BUILDDIR}/compile_commands.json ${INSTALLROOT}
 
-make ${JOBS+-j $JOBS} install
+cmake --build . -- ${JOBS+-j $JOBS} install
 
 if [[ $ALIBUILD_O2_TESTS ]]; then
   ctest --output-on-failure
 fi
 
-
 #ModuleFile
 mkdir -p etc/modulefiles
-alibuild-generate-module --bin --lib > etc/modulefiles/$PKGNAME
-cat >> etc/modulefiles/$PKGNAME <<EoF
-# Our environment
-set MONITORING_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-prepend-path ROOT_INCLUDE_PATH \$PKG_ROOT/include
-EoF
+alibuild-generate-module --bin --lib --root > etc/modulefiles/$PKGNAME
 mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
