2f25974722a1d58e17a2902c0136c6de079b2135
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-01-05 11:02:50 +0100

    Modernize pythia6 recipe

--- pythia6.sh
+++ pythia6.sh
@@ -7,34 +7,24 @@ requires:
   - GCC-Toolchain:(?!osx)
 build_requires:
   - CMake
+  - ninja-fortran
+  - alibuild-recipe-tools
 ---
 #!/bin/sh
 
 cmake ${SOURCEDIR}                           \
+      -G Ninja                               \
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
       -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}  \
       -DCMAKE_INSTALL_LIBDIR=lib
-make ${JOBS+-j$JOBS}
-make install
+cmake --build . -- ${JOBS:+-j$JOBS} install
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
+alibuild-generate-module --lib > "$MODULEFILE"
 cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0
-# Our environment
-set PYTHIA6_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv PYTHIA6_ROOT \$PYTHIA6_ROOT
-prepend-path LD_LIBRARY_PATH \$PYTHIA6_ROOT/lib
+setenv PYTHIA6_ROOT \$PKG_ROOT
 prepend-path AGILE_GEN_PATH \$PYTHIA6_ROOT
 EoF
-
