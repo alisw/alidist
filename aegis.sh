7e3204a7fcbffbf3a093352a5991c98d55d912e6
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-01-05 15:36:05 +0100

    Moderninze AEGIS recipe
    - Fixes for CMake 4
    - Use ninja
    - Use alibuild-generate-module

--- aegis.sh
+++ aegis.sh
@@ -1,6 +1,6 @@
 package: AEGIS
 version: "%(tag_basename)s"
-tag: v1.5.8
+tag: v1.5.9
 requires:
   - ROOT
   - VMC
@@ -10,6 +10,8 @@ build_requires:
   - CMake
   - hijing
   - "Xcode:(osx.*)"
+  - ninja-fortran
+  - alibuild-recipe-tools
 source: https://github.com/AliceO2Group/AEGIS.git
 prepend_path:
   LD_LIBRARY_PATH: "$AEGIS_ROOT/lib"
@@ -23,15 +25,14 @@ if [ $FVERSION -ge 10 ]; then
    SPECIALFFLAGS=1
 fi
 cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT       \
-                 ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"} \
+                 -G "Ninja"                                \
                  -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE      \
                  -DCMAKE_SKIP_RPATH=TRUE                   \
                  -DPYTHIA6_DIR=${PYTHIA6_ROOT}             \
-		 ${SPECIALFFLAGS:+-DCMAKE_Fortran_FLAGS="-fallow-argument-mismatch"}
+                 ${SPECIALFFLAGS:+-DCMAKE_Fortran_FLAGS="-fallow-argument-mismatch"}
 cmake --build . -- ${JOBS:+-j$JOBS} install
 
 # Add an extra RPATH for the local libraries on macOS
-
 case ${ARCHITECTURE} in
   osx*)
     install_name_tool -add_rpath $INSTALLROOT/lib $INSTALLROOT/lib/libTEPEMGEN.dylib
@@ -42,19 +43,8 @@ esac
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
 mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 ROOT/$ROOT_VERSION-$ROOT_REVISION 
+alibuild-generate-module --lib --root > "$MODULEFILE"
+cat >> "$MODULEFILE" <<EoF
 # Our environment
-set AEGIS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
-setenv AEGIS_ROOT \$AEGIS_ROOT
-prepend-path LD_LIBRARY_PATH \$AEGIS_ROOT/lib
-prepend-path ROOT_INCLUDE_PATH \$AEGIS_ROOT/include
+setenv AEGIS_ROOT \$PKG_ROOT
 EoF
