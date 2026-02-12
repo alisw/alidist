a316ceda067202e139b5737427070169aa9432d2
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-02-12 19:44:24 +0100

    Bump alibuild-recipe-tools to v0.3.0
    Fixes support for custom python

--- alibuild-recipe-tools.sh
+++ alibuild-recipe-tools.sh
@@ -1,6 +1,6 @@
 package: alibuild-recipe-tools
-version: "0.2.6"
-tag: "v0.2.6"
+version: "v0.3.0"
+tag: "v0.3.0"
 source: https://github.com/alisw/alibuild-recipe-tools
 ---
 mkdir -p $INSTALLROOT/bin
