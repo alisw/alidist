c61b5b18315f1b776e115385e237222d42e3a423
Felix Schlepper <felix.schlepper@cern.ch>
2026-02-18 08:00:59 +0100

    Update gbl.sh for improved cmake usage
    Refactor gbl.sh to use modern cmake syntax and improve error handling.

--- gbl.sh
+++ gbl.sh
@@ -3,18 +3,16 @@ version: "%(tag_basename)s"
 tag: "V03-01-04"
 source: https://gitlab.desy.de/claus.kleinwort/general-broken-lines.git
 requires:
- - Eigen3
- - ROOT
+  - Eigen3
+  - ROOT
 build_requires:
- - CMake
+  - CMake
 ---
-#!/bin/bash -ex
+#!/bin/bash -e
 
-rsync -a "$SOURCEDIR"/cpp/ "$BUILDDIR"/
-cd "$BUILDDIR" || exit
-cmake . -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
-    -DCMAKE_INSTALL_PREFIX="$INSTALLROOT" \
+cmake -S "${SOURCEDIR}/cpp/" -B "$BUILDDIR"            \
+    -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"             \
+    -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"              \
     -DEIGEN3_INCLUDE_DIR="$EIGEN3_ROOT/include/eigen3" \
     -DSUPPORT_ROOT=ON
 cmake --build . -- ${JOBS:+-j$JOBS}
