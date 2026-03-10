d828d8ff54184aac49df7e49c7e1daa4c98322b7
Nicolò Jacazio <njacazio@users.noreply.github.com>
2026-03-10 11:44:52 +0100

    Update ACTS setup script to include license and paths

--- actso2.sh
+++ actso2.sh
@@ -4,6 +4,7 @@ tag: "master"
 requires:
   - ROOT
   - ACTS
+license: GPL-3.0
 build_requires:
   - "GCC-Toolchain:(?!osx)"
   - CMake
@@ -18,9 +19,22 @@ source: https://github.com/AliceO2Group/actsO2.git
 cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
                  -DACTS_ROOT=$ACTS_ROOT \
                  -DCMAKE_PREFIX_PATH=$ACTS_ROOT
-echo "Using ACTS from $ACTS_ROOT"
 cmake --build . -- ${JOBS:+-j$JOBS}
 cmake --install .
+
+case $ARCHITECTURE in
+    osx*)
+        find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/python/acts {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${GEANT4_ROOT}/lib {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${XERCESC_ROOT}/lib {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${ROOT_DYN_PATH} {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${TBB_ROOT}/lib {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${HEPMC3_ROOT}/lib {} \;
+        find $INSTALLROOT/python/actso2 -name "*.so" -exec install_name_tool -add_rpath ${PYTHIA_ROOT}/lib {} \;
+	;;
+esac
 
 [[ -d $INSTALLROOT/lib64 ]] && [[ ! -d $INSTALLROOT/lib ]] && ln -sf ${INSTALLROOT}/lib64 $INSTALLROOT/lib
 
@@ -38,4 +52,4 @@ EOF
 
 # Print a message to remind people to source the ACTS python bindings
 echo -e "\033[1mTo use the ACTS python bindings, source the following script:\033[0m"
-echo ". $ACTSO2_ROOT/python/setup.sh"
+echo ". $ACTS_ROOT/python/setup.sh"
