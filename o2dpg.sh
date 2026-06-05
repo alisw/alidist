2244c40a625213d96f4085b7c9a114de79181db5
Alberto Caliva <alberto.caliva@cern.ch>
2026-06-05 13:18:00 +0200

    Patch O2DPG for O2PDPSuite MC-prod-2026-v8-1-patch1

--- o2dpg.sh
+++ o2dpg.sh
@@ -1,6 +1,6 @@
 package: O2DPG
 version: "%(tag_basename)s"
-tag: "daily-20260522-0000"
+tag: "MC-prod-2026-v8-1-patch1"
 source: https://github.com/AliceO2Group/O2DPG.git
 build_requires:
   - alibuild-recipe-tools
