ecd7d456f3f55f2c5ae6309ebe680083ab13032b
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-06-23 11:12:58 +0200

    Do not copy module.modulemap

--- jalien-root.sh
+++ jalien-root.sh
@@ -36,7 +36,7 @@ ALIBUILD_CMAKE_BUILD_DIR=$SOURCEDIR
 if [ ! -f "$JALIEN_ROOT_ROOT/cmake/modules/FindAliceGridUtils.cmake" ]; then
   ALIBUILD_CMAKE_BUILD_DIR="$BUILDDIR"
   rsync -a --exclude .git --delete --delete-excluded "$SOURCEDIR/" "$BUILDDIR"
-  rsync -a "$ALICE_GRID_UTILS_ROOT/include/" "$BUILDDIR/inc"
+  rsync -a --exclude "module.modulemap" "$ALICE_GRID_UTILS_ROOT/include/" "$BUILDDIR/inc"
 fi
 
 cmake "$ALIBUILD_CMAKE_BUILD_DIR"                        \
