baea28b6c25b4232a2bf270ab3daa99da18aae7e
Nicolò Jacazio <njacazio@users.noreply.github.com>
2026-07-08 04:41:10 +0200

    Install ACTS headers

--- acts.sh
+++ acts.sh
@@ -19,49 +19,58 @@ build_requires:
 source: https://github.com/AliceO2Group/acts.git
 ---
 #!/bin/bash -ex
-cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT       \
-                 -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE      \
-                 -DACTS_BUILD_PYTHON_BINDINGS=ON           \
-                 -DACTS_BUILD_FATRAS=ON                    \
-                 -DACTS_BUILD_EXAMPLES=ON                  \
-                 -DACTS_BUILD_ANALYSIS_APPS=ON             \
-                 -DACTS_BUILD_EXAMPLES_PYTHIA8=ON          \
-                 -DACTS_BUILD_PLUGIN_ROOT=ON               \
-                 -DCMAKE_PREFIX_PATH=${PYTHIA_ROOT}        \
-                 -DACTS_BUILD_PLUGIN_GEANT4=ON             \
-                 -DACTS_BUILD_FATRAS_GEANT4=ON             \
-                 -DACTS_BUILD_EXAMPLES_GEANT4=ON           \
-                 -DACTS_BUILD_EXAMPLES_ROOT=ON             \
-                 -DACTS_BUILD_ALIGNMENT=ON                 \
-                 -DGeant4_DIR=${GEANT4_ROOT}/lib           \
-                 -G Ninja 
+
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
+    -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+    -DACTS_BUILD_PYTHON_BINDINGS=ON \
+    -DACTS_BUILD_FATRAS=ON \
+    -DACTS_BUILD_EXAMPLES=ON \
+    -DACTS_BUILD_ANALYSIS_APPS=ON \
+    -DACTS_BUILD_EXAMPLES_PYTHIA8=ON \
+    -DACTS_BUILD_PLUGIN_ROOT=ON \
+    -DCMAKE_PREFIX_PATH=${PYTHIA_ROOT} \
+    -DACTS_BUILD_PLUGIN_GEANT4=ON \
+    -DACTS_BUILD_FATRAS_GEANT4=ON \
+    -DACTS_BUILD_EXAMPLES_GEANT4=ON \
+    -DACTS_BUILD_EXAMPLES_ROOT=ON \
+    -DACTS_BUILD_ALIGNMENT=ON \
+    -DGeant4_DIR=${GEANT4_ROOT}/lib \
+    -G Ninja
 
 cmake --build . -- ${JOBS:+-j$JOBS}
 cmake --install .
 
+# Install ACTS example Digitization headers
+cmake -E copy_directory \
+    "$SOURCEDIR/Examples/Algorithms/Digitization/include/ActsExamples/Digitization" \
+    "$INSTALLROOT/include/ActsExamples/Digitization"
+
 case $ARCHITECTURE in
-    osx*)
-        find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/python/acts {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${GEANT4_ROOT}/lib {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${XERCESC_ROOT}/lib {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${ROOT_DYN_PATH} {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${TBB_ROOT}/lib {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${HEPMC3_ROOT}/lib {} \;
-        find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${PYTHIA_ROOT}/lib {} \;
-	;;
+osx*)
+    find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${INSTALLROOT}/python/acts {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${GEANT4_ROOT}/lib {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${XERCESC_ROOT}/lib {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${ROOT_DYN_PATH} {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${TBB_ROOT}/lib {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${HEPMC3_ROOT}/lib {} \;
+    find $INSTALLROOT/python/acts -name "*.so" -exec install_name_tool -add_rpath ${PYTHIA_ROOT}/lib {} \;
+    ;;
 esac
 
 [[ -d $INSTALLROOT/lib64 ]] && [[ ! -d $INSTALLROOT/lib ]] && ln -sf ${INSTALLROOT}/lib64 $INSTALLROOT/lib
 
-#ModuleFile
+# ModuleFile
 MODULEDIR="${INSTALLROOT}/etc/modulefiles"
 MODULEFILE="${MODULEDIR}/${PKGNAME}"
+
 mkdir -p ${MODULEDIR}
-alibuild-generate-module --bin --lib > "${MODULEFILE}"
+
+alibuild-generate-module --bin --lib >"${MODULEFILE}"
+
 # extra environment
-cat >> ${MODULEFILE} <<EOF
+cat >>${MODULEFILE} <<EOF
 set ACTS_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv ACTS_ROOT \$ACTS_ROOT
 prepend-path ROOT_INCLUDE_PATH \$ACTS_ROOT/include
