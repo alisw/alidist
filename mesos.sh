45f2d8d3f1e334dfcf7257d9ddfb64d4125316ca
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-08-21 15:54:42 +0200

    Make mesos compatible with Protobuf >22

--- mesos.sh
+++ mesos.sh
@@ -1,7 +1,7 @@
 package: mesos
 version: v1.11.0
-tag: 1.11.0-alice5
-source: https://github.com/AliceO2Group/mesos.git
+tag: a69e7359b
+source: https://github.com/ktf/mesos.git
 requires:
   - zlib
   - glog
