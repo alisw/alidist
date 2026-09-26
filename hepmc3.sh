f8bcd5148cdd1f743cc4f229863f1f6994bae7f7
Nicolò Jacazio <njacazio@users.noreply.github.com>
2026-09-26 08:20:22 +0200

    Remove Hepmc3 abs path for ROOT libs (#6336)

--- hepmc3.sh
+++ hepmc3.sh
@@ -10,18 +10,44 @@ build_requires:
   - CMake
 prepend_path:
   ROOT_INCLUDE_PATH: "$HEPMC3_ROOT/include"
+track_env:
+  CMAKE_CXX_COMPILER_LAUNCHER: echo ${USE_RECC+recc}
+  CMAKE_C_COMPILER_LAUNCHER: echo ${USE_RECC+recc}
 ---
 #!/bin/bash -e
 
+# Do not use RECC for link steps. On macOS, its command parser
+# misinterprets valid @rpath/... install names as response files.
+# Compilation can still use the compiler launcher normally.
 cmake  $SOURCEDIR                          \
        -DROOT_DIR=$ROOT_ROOT               \
        -DCMAKE_INSTALL_PREFIX=$INSTALLROOT \
        -DCMAKE_INSTALL_LIBDIR=lib          \
+       -DCMAKE_C_LINKER_LAUNCHER=          \
+       -DCMAKE_CXX_LINKER_LAUNCHER=        \
        -DHEPMC3_ENABLE_PYTHON=OFF          \
        -DHEPMC3_ENABLE_ROOTIO=ON
 
 make ${JOBS+-j $JOBS}
 make install
+
+# HepMC3::rootIO exports ROOT_INCLUDE_DIRS as an absolute path.
+# This makes the installed HepMC3 package non-relocatable and can leave
+# references to the ROOT installation of the machine where HepMC3 was built.
+#
+# ROOT is already propagated through ROOT::Tree, ROOT::RIO and ROOT::Core,
+# so the explicit absolute ROOT include directory is unnecessary.
+# Locate the CMake import file installed for HepMC3's ROOT I/O target.
+ROOTIO_TARGETS="$INSTALLROOT/share/HepMC3/cmake/HepMC3rootIOTargets.cmake"
+
+# Remove the build machine's absolute ROOT include path from the exported target.
+# Keep a backup temporarily so sed can edit the file in place safely.
+sed -i.bak \
+    "s#$ROOT_ROOT/include##g" \
+    "$ROOTIO_TARGETS"
+
+# Delete the temporary backup after the replacement succeeds.
+rm -f "${ROOTIO_TARGETS}.bak"
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
